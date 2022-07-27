92ab01149fa09f40b553237abd1f49432b583c40
Carlos Quintana <carlos.quintana@proton.ch>
2022-07-27 12:32:26 +0200

    Fixes for the crypto of CreateVault

--- passwordManager/data/src/main/kotlin/me/proton/core/pass/data/crypto/Utils.kt
+++ passwordManager/data/src/main/kotlin/me/proton/core/pass/data/crypto/Utils.kt
@@ -5,21 +5,19 @@ import kotlinx.serialization.json.Json
 import me.proton.core.crypto.common.context.CryptoContext
 import me.proton.core.crypto.common.keystore.EncryptedByteArray
 import me.proton.core.crypto.common.pgp.Armored
-import me.proton.core.key.domain.entity.key.*
-import me.proton.core.key.domain.entity.keyholder.KeyHolderContext
-import me.proton.core.key.domain.publicKey
+import me.proton.core.key.domain.entity.key.ArmoredKey
+import me.proton.core.key.domain.entity.key.PrivateKey
+import me.proton.core.key.domain.entity.key.PublicKey
 
 object Utils {
-    const val PASSPHRASE_LENGTH = 32
+    private const val PASSPHRASE_LENGTH = 32
 
     fun generatePassphrase() = getRandomString(PASSPHRASE_LENGTH)
 
     fun getPrimaryV5Fingerprint(cryptoContext: CryptoContext, key: Armored): String {
         val fingerprints = cryptoContext.pgpCrypto.getJsonSHA256Fingerprints(key)
         val decodedFingerprints = Json.decodeFromString<List<String>>(fingerprints)
-        if (decodedFingerprints.isEmpty()) {
-            throw Exception("Key does not contain any fingerprint")
-        }
+        require(decodedFingerprints.isNotEmpty())
         return decodedFingerprints.first()
     }
 
@@ -38,26 +36,6 @@ object Utils {
         }
         throw Exception("Could not detect the kind of key")
     }
-
-    fun getPublicKey(context: CryptoContext, privateKey: PrivateKey): PublicKey =
-        PublicKey(
-            key = context.pgpCrypto.getPublicKey(privateKey.key),
-            isPrimary = privateKey.isPrimary,
-            isActive = privateKey.isActive,
-            canEncrypt = privateKey.canEncrypt,
-            canVerify = privateKey.canVerify
-        )
-
-    fun <R> usingPrivateKey(context: CryptoContext, key: ArmoredKey, block: (KeyHolderContext) -> R) =
-        when (key) {
-            is ArmoredKey.Public -> throw Exception("Cannot use public key as private key")
-            is ArmoredKey.Private ->
-                KeyHolderContext(
-                    context,
-                    PrivateKeyRing(context, listOf(key.key)),
-                    PublicKeyRing(listOf(key.key.publicKey(context)))
-                ).use { block(it) }
-        }
 
     private fun getRandomString(length: Int): String {
         val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')

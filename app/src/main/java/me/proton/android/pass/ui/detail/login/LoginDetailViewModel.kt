5ecd81a9625753381366ffb4b3d823184a9b23f6
Carlos Quintana <carlos.quintana@proton.ch>
2022-08-02 14:54:39 +0200

    Fix copy to clipboard

--- app/src/main/java/me/proton/android/pass/ui/detail/login/LoginDetailViewModel.kt
+++ app/src/main/java/me/proton/android/pass/ui/detail/login/LoginDetailViewModel.kt
@@ -1,15 +1,19 @@
 package me.proton.android.pass.ui.detail.login
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.launch
 import me.proton.core.crypto.common.context.CryptoContext
+import me.proton.core.crypto.common.keystore.EncryptedString
 import me.proton.core.crypto.common.keystore.decrypt
+import me.proton.core.crypto.common.keystore.encrypt
 import me.proton.core.pass.domain.Item
 import me.proton.core.pass.domain.ItemType
-import javax.inject.Inject
 
 @HiltViewModel
 class LoginDetailViewModel @Inject constructor(
@@ -17,9 +21,15 @@ class LoginDetailViewModel @Inject constructor(
 ) : ViewModel() {
 
     private val itemFlow: MutableStateFlow<Item?> = MutableStateFlow(null)
+    private val passwordState: MutableStateFlow<PasswordState> = MutableStateFlow(PasswordState.Concealed("".encrypt(cryptoContext.keyStoreCrypto)))
 
+    val copyToClipboardFlow: MutableSharedFlow<String?> = MutableSharedFlow()
     val initialViewState = getInitialState()
-    val viewState: Flow<LoginUiModel> = itemFlow.mapLatest { getUiModel(it) }
+    val viewState = combine(
+        itemFlow,
+        passwordState,
+        ::getUiModel
+    )
 
     fun setItem(item: Item) {
         if (itemFlow.value == null) {
@@ -27,14 +37,40 @@ class LoginDetailViewModel @Inject constructor(
         }
     }
 
-    private fun getUiModel(item: Item?): LoginUiModel {
+    fun copyPasswordToClipboard() = viewModelScope.launch {
+        val item = itemFlow.value
+        if (item != null) {
+            val itemType = item.itemType as ItemType.Login
+            val text = when (val password = passwordState.value) {
+                is PasswordState.Revealed -> password.clearText
+                is PasswordState.Concealed -> itemType.password.decrypt(cryptoContext.keyStoreCrypto)
+            }
+            copyToClipboardFlow.emit(text)
+        }
+    }
+
+    fun togglePassword() {
+        val item = itemFlow.value ?: return
+        val itemType = item.itemType as ItemType.Login
+
+        when (passwordState.value) {
+            is PasswordState.Concealed -> {
+                passwordState.value = PasswordState.Revealed(itemType.password, itemType.password.decrypt(cryptoContext.keyStoreCrypto))
+            }
+            is PasswordState.Revealed -> {
+                passwordState.value = PasswordState.Concealed(itemType.password)
+            }
+        }
+    }
+
+    private fun getUiModel(item: Item?, password: PasswordState): LoginUiModel {
         if (item == null) return getInitialState()
 
         val itemContents = item.itemType as ItemType.Login
         return LoginUiModel(
             title = item.title.decrypt(cryptoContext.keyStoreCrypto),
             username = itemContents.username,
-            password = itemContents.password.decrypt(cryptoContext.keyStoreCrypto),
+            password = password,
             websites = emptyList(),
             note = ""
         )
@@ -44,7 +80,7 @@ class LoginDetailViewModel @Inject constructor(
         LoginUiModel(
             title = "",
             username = "",
-            password = "",
+            password = PasswordState.Concealed("".encrypt(cryptoContext.keyStoreCrypto)),
             websites = emptyList(),
             note = ""
         )
@@ -52,8 +88,13 @@ class LoginDetailViewModel @Inject constructor(
     data class LoginUiModel(
         val title: String,
         val username: String,
-        val password: String, // TODO: Keep this encrypted
+        val password: PasswordState,
         val websites: List<String>,
         val note: String
     )
+
+    sealed class PasswordState(open val encrypted: EncryptedString) {
+        data class Concealed(override val encrypted: EncryptedString) : PasswordState(encrypted)
+        data class Revealed(override val encrypted: EncryptedString, val clearText: String) : PasswordState(encrypted)
+    }
 }

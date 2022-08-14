177e76645ec6f879fb5b430dc44cc1bafba6a43e
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2022-08-14 16:10:03 +0200

    Populate CreatePasswordScreen

--- app/src/main/java/me/proton/android/pass/ui/create/password/CreatePasswordScreen.kt
+++ app/src/main/java/me/proton/android/pass/ui/create/password/CreatePasswordScreen.kt
@@ -1,23 +1,45 @@
 package me.proton.android.pass.ui.create.password
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Slider
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import me.proton.android.pass.R
 import me.proton.android.pass.ui.shared.ArrowBackIcon
 import me.proton.android.pass.ui.shared.TopBarTitleView
+import me.proton.core.compose.component.ProtonSolidButton
 import me.proton.core.compose.component.appbar.ProtonTopAppBar
+import me.proton.core.compose.theme.ProtonTheme
+import me.proton.core.pass.presentation.components.common.rememberFlowWithLifecycle
+
+internal typealias OnConfirm = (String) -> Unit
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun CreatePasswordView(
     @StringRes topBarTitle: Int,
     onUpClick: () -> Unit,
-    onConfirm: (String) -> Unit
+    onConfirm: OnConfirm
 ) {
     Scaffold(
         topBar = {
@@ -27,8 +49,122 @@ fun CreatePasswordView(
                 actions = {},
             )
         }) { padding ->
-        Column(modifier = Modifier.padding(padding)) {
-            Text("Create password")
+        val viewModel by remember { mutableStateOf(CreatePasswordViewModel()) }
+        CreatePasswordViewContent(
+            modifier = Modifier.padding(padding),
+            onUpClick = onUpClick,
+            onConfirm = onConfirm,
+            viewModel = viewModel,
+        )
+    }
+}
+
+@Composable
+private fun CreatePasswordViewContent(
+    modifier: Modifier,
+    onUpClick: () -> Unit,
+    onConfirm: OnConfirm,
+    viewModel: CreatePasswordViewModel,
+) {
+    val password by rememberFlowWithLifecycle(viewModel.password)
+        .collectAsState("")
+
+    val length by rememberFlowWithLifecycle(viewModel.length)
+        .collectAsState(CreatePasswordViewModel.defaultLength)
+
+    val hasSpecialCharacters by rememberFlowWithLifecycle(viewModel.hasSpecialCharacters)
+        .collectAsState(true)
+
+    Column(modifier = modifier) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .padding(top = 16.dp, bottom = 48.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            PasswordText(
+                modifier = Modifier.weight(1f),
+                password = password
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            IconButton(
+                onClick = { viewModel.regenerate() }
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_proton_arrows_rotate),
+                    contentDescription = null
+                )
+            }
+        }
+
+        Divider()
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(stringResource(R.string.character_count, length))
+            Spacer(modifier = Modifier.width(8.dp))
+            Slider(
+                value = length.toFloat(),
+                valueRange = CreatePasswordViewModel.lengthRange,
+                onValueChange = { viewModel.onLengthChange(it.toInt()) }
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(stringResource(R.string.special_characters))
+            Switch(
+                checked = hasSpecialCharacters,
+                onCheckedChange = { viewModel.onHasSpecialCharactersChange(it) }
+            )
+        }
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        ProtonSolidButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .height(48.dp),
+            onClick = { onConfirm(password) }
+        ) {
+            Text(stringResource(R.string.generate_password_confirm))
         }
     }
+}
+
+@Composable
+private fun PasswordText(
+    modifier: Modifier = Modifier,
+    password: String
+) {
+    val annotatedString = password
+        .map {
+            val color = when {
+                CharacterSet.Digit.value.contains(it) -> ProtonTheme.colors.notificationError
+                CharacterSet.Special.value.contains(it) -> ProtonTheme.colors.notificationSuccess
+                else -> ProtonTheme.colors.textNorm
+            }
+            AnnotatedString(it.toString(), SpanStyle(color))
+        }
+        .reduceOrNull { acc, next -> acc.plus(next) } ?: AnnotatedString("")
+
+    Text(
+        modifier = modifier,
+        text = annotatedString,
+        fontWeight = FontWeight.Bold,
+        style = MaterialTheme.typography.h6
+    )
 }

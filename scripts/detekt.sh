edd613348f824cd3ef34079501296d382cb3d73a
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-09-15 10:56:27 +0200

    (fix) Pass detekt config on pre-commit

--- scripts/detekt.sh
+++ scripts/detekt.sh
@@ -5,7 +5,7 @@ fileArray=($@)
 detektInput=$(IFS=,;printf  "%s" "${fileArray[*]}")
 echo "Input files: $detektInput"
 
-OUTPUT=$(detekt --input "$detektInput" 2>&1)
+OUTPUT=$(detekt --input "$detektInput" --config "./config/detekt/config.yml" 2>&1)
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
   echo "${OUTPUT}"

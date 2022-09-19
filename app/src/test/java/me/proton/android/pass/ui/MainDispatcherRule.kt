a989e85177d4d3ebd6e250fa5c5b6297ff1c4129
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-09-19 17:40:01 +0200

    (test) Add main dispatcher rule

---
+++ app/src/test/java/me/proton/android/pass/ui/MainDispatcherRule.kt
@@ -0,0 +1,21 @@
+package me.proton.android.pass.ui
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+class MainDispatcherRule(
+    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+) : TestWatcher() {
+    override fun starting(description: Description) {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description) {
+        Dispatchers.resetMain()
+    }
+}

cfe3074c88cdc8aaea0ed046ce83721f57a3ecbf
Carlos Quintana <carlosquintana@protonmail.com>
2022-09-21 05:59:45 +0000

    * (refactor) Remove initialState variable in HomeViewModel
    * (lint) remove unnecessary accessors
    
    * (lint): detekt fixes
    
    * (feature) Add loading to home

---
+++ passwordManager/domain/src/main/kotlin/me/proton/core/pass/domain/usecases/ObserveActiveShare.kt
@@ -0,0 +1,33 @@
+package me.proton.core.pass.domain.usecases
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
+import me.proton.core.accountmanager.domain.AccountManager
+import me.proton.core.pass.domain.ShareId
+import me.proton.core.user.domain.UserManager
+import javax.inject.Inject
+
+class ObserveActiveShare @Inject constructor(
+    private val accountManager: AccountManager,
+    private val userManager: UserManager,
+    private val observeShares: ObserveShares
+) {
+
+    private val getCurrentUserIdFlow = accountManager.getPrimaryUserId()
+        .filterNotNull()
+        .flatMapLatest { userManager.observeUser(it) }
+        .distinctUntilChanged()
+
+    operator fun invoke(): Flow<ShareId?> = getCurrentUserIdFlow
+        .filterNotNull()
+        .flatMapLatest { user ->
+            observeShares(user.userId).map { shares ->
+                shares.firstOrNull()?.id
+            }
+        }
+        .distinctUntilChanged()
+
+}

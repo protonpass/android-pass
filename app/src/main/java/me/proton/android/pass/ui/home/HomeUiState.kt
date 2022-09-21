87b9be4ae6c6a6b8e0444aa0d1a4e3705a048210
Carlos Quintana <carlosquintana@protonmail.com>
2022-09-21 05:59:45 +0000

    * (refactor) Remove initialState variable in HomeViewModel
    * (lint) remove unnecessary accessors
    
    * (lint): detekt fixes
    
    * (feature) Add loading to home

---
+++ app/src/main/java/me/proton/android/pass/ui/home/HomeUiState.kt
@@ -0,0 +1,13 @@
+package me.proton.android.pass.ui.home
+
+import me.proton.core.pass.domain.ShareId
+import me.proton.core.pass.presentation.components.model.ItemUiModel
+
+sealed class HomeUiState {
+    object Loading : HomeUiState()
+    data class Content(
+        val items: List<ItemUiModel>,
+        val selectedShare: ShareId? = null
+    ) : HomeUiState()
+    data class Error(val message: String) : HomeUiState()
+}

3db1f5d74282ecddaf41c3e8007564991979d19f
Carlos Quintana <carlosquintana@protonmail.com>
2022-09-23 06:42:07 +0000

    * (build) Clean up build files
    * (lint) detekt fixes
    
    * (refactor) adapt implementation and tests to newly defined logic
    
    * (refactor) extract observeActiveItems into its own class for testing
    
    * (refactor) Move random item creation to test module
    
    * (refactor) use Kotlin random instead of java random so we avoid java.util imports
    
    * (refactor) return interface rather than implementation
    
    * (codequality) remove helper function
    
    * (refactor) remove underscore from private flow
    
    * (refactor) Proper name to SearchModule
    
    * (refactor) rename TestKeyStore -> TestKeyStoreCrypto
    
    * (build) remove passwordManager/build.gradle.kts
    
    * (feature) add search usecase

---
+++ passwordManager/search/api/src/main/kotlin/me/proton/core/pass/search/SearchItems.kt
@@ -0,0 +1,9 @@
+package me.proton.core.pass.search
+
+import kotlinx.coroutines.flow.Flow
+import me.proton.core.pass.domain.Item
+
+interface SearchItems {
+    fun observeResults(): Flow<List<Item>>
+    fun updateQuery(query: String)
+}

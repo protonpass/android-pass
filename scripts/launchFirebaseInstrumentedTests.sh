50858fd986cbd4d5dbc26f561f235feb65483dcc
Carlos Quintana <carlosquintana@protonmail.com>
2022-09-28 09:20:55 +0000

    * (ci) Fix firebase test launch
    * (fix) Fix launchFirebaseInstrumentedTests.sh permissions
    
    * (ci) build the test APK during the build job too
    
    * (ci) Add firebase instrumented tests

---
+++ scripts/launchFirebaseInstrumentedTests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+REPO_ROOT=$(echo "${SCRIPT_DIR}" | sed 's:scripts::g')
+
+APK_PATH=$(find "${REPO_ROOT}/app/build/outputs/apk/dev/debug" -type f -name '*.apk')
+TEST_APK_PATH=$(find "${REPO_ROOT}/passwordManager/data/build/outputs/apk/androidTest/debug" -type f -name '*.apk')
+
+gcloud firebase test android run \
+  --app "${APK_PATH}" \
+  --test "${TEST_APK_PATH}" \
+  --type=instrumentation \
+  --device model=Pixel2,version=28 \
+  --use-orchestrator \
+  --num-flaky-test-attempts=1 \
+  --timeout 10m \
+  --no-record-video

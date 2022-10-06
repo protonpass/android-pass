cdd0266c21fbb4af027b256e315285a7d177a8ea
Carlos Quintana <carlosquintana@protonmail.com>
2022-10-06 08:00:41 +0000

    * (refactor) Move PreviewProviders into their own package
    * (chore) Separate form components and add previews

---
+++ passwordManager/presentation/src/main/kotlin/me/proton/core/pass/presentation/components/form/ProtonTextTitle.kt
@@ -0,0 +1,35 @@
+package me.proton.core.pass.presentation.components.form
+
+import androidx.annotation.StringRes
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import me.proton.core.compose.theme.ProtonTheme
+import me.proton.core.compose.theme.caption
+
+@Composable
+fun ProtonTextTitle(
+    @StringRes title: Int,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = stringResource(title),
+        color = ProtonTheme.colors.textNorm,
+        style = ProtonTheme.typography.caption,
+        fontWeight = FontWeight.W500,
+        fontSize = 12.sp,
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true, widthDp = 200, heightDp = 40)
+@Composable
+fun Preview_ProtonTextTitle() {
+    ProtonTheme {
+        ProtonTextTitle(title = me.proton.core.pass.presentation.R.string.field_title_title)
+    }
+}

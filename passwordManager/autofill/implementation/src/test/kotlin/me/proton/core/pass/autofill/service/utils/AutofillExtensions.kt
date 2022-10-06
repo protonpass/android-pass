050ffd559ffd0aefbae15096c8ed3a0505eb9236
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-10-06 08:12:00 +0000

    * (fix) Avoid custom parcelables

--- passwordManager/autofill/implementation/src/test/kotlin/me/proton/core/pass/autofill/service/utils/AutofillExtensions.kt
+++ passwordManager/autofill/implementation/src/test/kotlin/me/proton/core/pass/autofill/service/utils/AutofillExtensions.kt
@@ -1,24 +1,9 @@
 package me.proton.core.pass.autofill.service.utils
 
-import android.os.Parcel
-import android.os.Parcelable
 import me.proton.core.pass.autofill.entities.AutofillFieldId
 import kotlin.random.Random
 
 fun newAutofillFieldId() = FakeAutofillFieldId()
 
 /** Used for testing purposes */
-class FakeAutofillFieldId(val id: Int = Random.nextInt()) : AutofillFieldId {
-    constructor(parcel: Parcel) : this(parcel.readInt())
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(id)
-    }
-
-    override fun describeContents(): Int = 0
-
-    companion object CREATOR : Parcelable.Creator<FakeAutofillFieldId> {
-        override fun createFromParcel(parcel: Parcel): FakeAutofillFieldId = FakeAutofillFieldId(parcel)
-        override fun newArray(size: Int): Array<FakeAutofillFieldId?> = arrayOfNulls(size)
-    }
-}
+class FakeAutofillFieldId(val id: Int = Random.nextInt()) : AutofillFieldId

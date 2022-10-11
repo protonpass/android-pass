46cf81a83e30c6b5788ac38e5e62965b46d86082
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-10-11 16:34:37 +0200

    (fix) Tune autofill rules for web

--- passwordManager/autofill/implementation/src/main/kotlin/me/proton/core/pass/autofill/AssistNodeTraversal.kt
+++ passwordManager/autofill/implementation/src/main/kotlin/me/proton/core/pass/autofill/AssistNodeTraversal.kt
@@ -46,39 +46,48 @@ class AssistNodeTraversal {
 
     private fun nodeSupportsAutoFill(node: AutofillNode): Boolean {
         val isImportant = node.isImportantForAutofill
-        val hasAutoFillHints = node.autofillHints.isNotEmpty()
-        val hasHtmlInfo = node.htmlAttributes.isNotEmpty()
-        val hasAutofillInfo = hasAutoFillHints || hasHtmlInfo || nodeHasValidInputType(node)
-
+        val hasAutofillInfo = nodeHasValidHints(node.autofillHints.toSet()) &&
+            nodeHasValidHtmlInfo(node.htmlAttributes) ||
+            nodeHasValidInputType(node)
         return node.id != null && hasAutofillInfo && isImportant
     }
+
+    private fun nodeHasValidHints(autofillHints: Set<String>): Boolean = autofillHints
+        .intersect(AUTOFILL_HINT_VALUES)
+        .isNotEmpty()
+
+    private fun nodeHasValidHtmlInfo(htmlAttributes: List<Pair<String, String>>): Boolean =
+        htmlAttributes.map { it.first }.contains("type")
 
     private fun nodeHasValidInputType(node: AutofillNode): Boolean {
         val hasMultilineFlag = node.inputType.value.hasFlag(InputType.TYPE_TEXT_FLAG_MULTI_LINE) ||
             node.inputType.value.hasFlag(InputType.TYPE_TEXT_FLAG_IME_MULTI_LINE)
         val hasAutoCorrectFlag = node.inputType.value.hasFlag(InputType.TYPE_TEXT_FLAG_AUTO_CORRECT)
         // InputType.TYPE_TEXT_FLAG_CAP_SENTENCES might also be considered in the future
-        return !hasMultilineFlag && !hasAutoCorrectFlag && detectFieldTypeUsingInputType(node.inputType) != null
+        return !hasMultilineFlag &&
+            !hasAutoCorrectFlag &&
+            detectFieldTypeUsingInputType(node.inputType) != FieldType.Unknown
     }
 
-    private fun detectFieldType(node: AutofillNode): FieldType? {
+    private fun detectFieldType(node: AutofillNode): FieldType {
         val autofillHint = node.autofillHints.firstOrNull()
         val htmlAttributes = node.htmlAttributes
         return if (autofillHint != null) detectFieldTypeUsingAutofillHint(autofillHint)
-        else if (htmlAttributes.isNotEmpty()) detectFieldTypeUsingHtmlInfo(htmlAttributes)
+        else if (htmlAttributes.isNotEmpty() && htmlAttributes.firstOrNull { it.first == "type" } != null)
+            detectFieldTypeUsingHtmlInfo(htmlAttributes)
         else detectFieldTypeUsingInputType(node.inputType)
     }
 
     fun detectFieldTypeUsingHtmlInfo(
         htmlAttributes: List<Pair<String, String>>
-    ): FieldType? {
+    ): FieldType {
         val typeAttribute = htmlAttributes.firstOrNull { it.first == "type" }
         return when (typeAttribute?.second) {
             "tel" -> FieldType.Phone
             "email" -> FieldType.Email
             "password" -> FieldType.Password
             "text" -> FieldType.Other
-            else -> null
+            else -> FieldType.Unknown
         }
     }
 
@@ -88,27 +97,38 @@ class AssistNodeTraversal {
         View.AUTOFILL_HINT_USERNAME -> FieldType.Username
         View.AUTOFILL_HINT_PASSWORD, HINT_CURRENT_PASSWORD -> FieldType.Password
         View.AUTOFILL_HINT_PHONE -> FieldType.Phone
-        else -> null
+        else -> FieldType.Unknown
     }
 
-    fun detectFieldTypeUsingInputType(inputType: InputTypeValue): FieldType? = when {
+    fun detectFieldTypeUsingInputType(inputType: InputTypeValue): FieldType = when {
         inputType.value == InputType.TYPE_CLASS_PHONE -> FieldType.Phone
         inputType.hasVariations(
             InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS,
             InputType.TYPE_TEXT_VARIATION_WEB_EMAIL_ADDRESS
         ) -> FieldType.Email
         inputType.hasVariations(
+            InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+        ) -> FieldType.Username
+        inputType.hasVariations(
             InputType.TYPE_TEXT_VARIATION_PASSWORD,
-            InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD,
-            InputType.TYPE_TEXT_VARIATION_WEB_PASSWORD,
-            InputType.TYPE_NUMBER_VARIATION_PASSWORD
+            InputType.TYPE_TEXT_VARIATION_WEB_PASSWORD
         ) -> FieldType.Password
-        inputType.hasVariations(InputType.TYPE_TEXT_VARIATION_PERSON_NAME) -> FieldType.FullName
-        else -> null
+        inputType.hasVariations(
+            InputType.TYPE_TEXT_VARIATION_PERSON_NAME
+        ) -> FieldType.FullName
+        else -> FieldType.Unknown
     }
 
     companion object {
         const val HINT_CURRENT_PASSWORD = "current-password"
+        private val AUTOFILL_HINT_VALUES: Set<String> = setOf(
+            View.AUTOFILL_HINT_EMAIL_ADDRESS,
+            View.AUTOFILL_HINT_NAME,
+            View.AUTOFILL_HINT_USERNAME,
+            View.AUTOFILL_HINT_PASSWORD,
+            HINT_CURRENT_PASSWORD,
+            View.AUTOFILL_HINT_PHONE
+        )
     }
 }
 

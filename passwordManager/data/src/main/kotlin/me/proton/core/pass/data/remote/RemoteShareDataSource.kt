b5c9d742c6a4182f25b3b5da3559625898ba3cba
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-10-17 12:01:38 +0200

    (change) Return result on remote share

--- passwordManager/data/src/main/kotlin/me/proton/core/pass/data/remote/RemoteShareDataSource.kt
+++ passwordManager/data/src/main/kotlin/me/proton/core/pass/data/remote/RemoteShareDataSource.kt
@@ -1,12 +1,13 @@
 package me.proton.core.pass.data.remote
 
 import me.proton.core.domain.entity.UserId
+import me.proton.core.pass.common.api.Result
 import me.proton.core.pass.data.requests.CreateVaultRequest
 import me.proton.core.pass.data.responses.ShareResponse
 import me.proton.core.pass.domain.ShareId
 
 interface RemoteShareDataSource {
-    suspend fun createVault(userId: UserId, body: CreateVaultRequest): ShareResponse
-    suspend fun getShares(userId: UserId): List<ShareResponse>
-    suspend fun getShareById(userId: UserId, shareId: ShareId): ShareResponse?
+    suspend fun createVault(userId: UserId, body: CreateVaultRequest): Result<ShareResponse>
+    suspend fun getShares(userId: UserId): Result<List<ShareResponse>>
+    suspend fun getShareById(userId: UserId, shareId: ShareId): Result<ShareResponse?>
 }

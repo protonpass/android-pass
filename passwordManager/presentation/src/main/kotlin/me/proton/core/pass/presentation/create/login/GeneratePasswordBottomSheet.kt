08f34d617c8eac5e71cdf38440f4b585323879a3
Carlos Quintana <carlos.quintana@proton.ch>
2022-10-20 16:59:53 +0200

    (refactor) extract GeneratePasswordBottomSheetContent component

--- passwordManager/presentation/src/main/kotlin/me/proton/core/pass/presentation/create/login/GeneratePasswordBottomSheet.kt
+++ passwordManager/presentation/src/main/kotlin/me/proton/core/pass/presentation/create/login/GeneratePasswordBottomSheet.kt
@@ -1,17 +1,10 @@
 package me.proton.core.pass.presentation.create.login
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import me.proton.core.compose.theme.ProtonTheme
-import me.proton.core.pass.presentation.R
-import me.proton.core.pass.presentation.components.common.bottomsheet.BottomSheetTitle
-import me.proton.core.pass.presentation.components.common.bottomsheet.BottomSheetTitleButton
-import me.proton.core.pass.presentation.create.password.CreatePasswordViewContent
 import me.proton.core.pass.presentation.create.password.CreatePasswordViewModel
 
 @Composable
@@ -22,28 +15,12 @@ fun GeneratePasswordBottomSheet(
     val viewModel = hiltViewModel<CreatePasswordViewModel>()
     val state by viewModel.state.collectAsStateWithLifecycle()
 
-    Column(modifier = modifier) {
-        BottomSheetTitle(
-            title = R.string.button_generate_password,
-            button = BottomSheetTitleButton(
-                R.string.generate_password_confirm,
-                onClick = { onConfirm(state.password) },
-                enabled = true
-            )
-        )
-        CreatePasswordViewContent(
-            state = state,
-            onLengthChange = { viewModel.onLengthChange(it) },
-            onRegenerateClick = { viewModel.regenerate() },
-            onSpecialCharactersChange = { viewModel.onHasSpecialCharactersChange(it) }
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun Preview_GeneratePasswordBottomSheet() {
-    ProtonTheme {
-        GeneratePasswordBottomSheet(onConfirm = {})
-    }
+    GeneratePasswordBottomSheetContent(
+        modifier = modifier,
+        state = state,
+        onLengthChange = { viewModel.onLengthChange(it) },
+        onRegenerateClick = { viewModel.regenerate() },
+        onHasSpecialCharactersChange = { viewModel.onHasSpecialCharactersChange(it) },
+        onConfirm = onConfirm
+    )
 }

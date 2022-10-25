09401ba396057169c1658f6340aa3f42a3ce1fed
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-10-25 10:48:25 +0200

    (test) Add compose preview paparazzi test

---
+++ passwordManager/screenshot-tests/src/test/java/me/proton/android/pass/screenshottests/ScreenshotTestsShowkaseModule.kt
@@ -0,0 +1,7 @@
+package me.proton.android.pass.screenshottests
+
+import com.airbnb.android.showkase.annotation.ShowkaseRoot
+import com.airbnb.android.showkase.annotation.ShowkaseRootModule
+
+@ShowkaseRoot
+class ScreenshotTestsShowkaseModule : ShowkaseRootModule

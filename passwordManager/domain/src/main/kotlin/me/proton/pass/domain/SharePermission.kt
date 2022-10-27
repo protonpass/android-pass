dea39e8b7e890d024a96ca0e2ba21718a9351edc
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-10-27 16:57:49 +0200

    (chore) Convert single data classes to value

--- passwordManager/domain/src/main/kotlin/me/proton/pass/domain/SharePermission.kt
+++ passwordManager/domain/src/main/kotlin/me/proton/pass/domain/SharePermission.kt
@@ -2,7 +2,8 @@ package me.proton.pass.domain
 
 import me.proton.core.util.kotlin.hasFlag
 
-data class SharePermission(val value: Int)
+@JvmInline
+value class SharePermission(val value: Int)
 
 fun SharePermission.hasFlag(flag: SharePermissionFlag): Boolean = value.hasFlag(flag.value)
 fun SharePermission.flags(): List<SharePermissionFlag> =

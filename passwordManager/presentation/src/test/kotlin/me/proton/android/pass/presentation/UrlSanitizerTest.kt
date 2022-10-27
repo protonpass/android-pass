050c26759807000a5391e9a6da488189ae80ab2e
Carlos Quintana <carlos.quintana@proton.ch>
2022-10-27 11:43:10 +0200

    (code) Create UrlSanitizer

---
+++ passwordManager/presentation/src/test/kotlin/me/proton/android/pass/presentation/UrlSanitizerTest.kt
@@ -0,0 +1,46 @@
+package me.proton.android.pass.presentation
+
+import me.proton.pass.common.api.Result
+import me.proton.pass.presentation.UrlSanitizer
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class UrlSanitizerTest {
+
+    @Test
+    fun `empty url should return success`() {
+        assertTrue(UrlSanitizer.sanitize("") is Result.Success)
+    }
+
+    @Test
+    fun `url without scheme should have it added`() {
+        val domain = "some.domain"
+        val res = UrlSanitizer.sanitize(domain)
+        assertTrue(res is Result.Success)
+        assertEquals(res.data, "https://$domain")
+    }
+
+    @Test
+    fun `url with scheme should return success and not have it edited`() {
+        val domain = "ssh://some.domain"
+        val res = UrlSanitizer.sanitize(domain)
+        assertTrue(res is Result.Success)
+        assertEquals(res.data, domain)
+    }
+
+    @Test
+    fun `url with path has its path removed`() {
+        val domain = "some.domain"
+        val res = UrlSanitizer.sanitize("$domain/login")
+        assertTrue(res is Result.Success)
+        assertEquals(res.data, "https://$domain")
+    }
+
+    @Test
+    fun `url with empty scheme should return error`() {
+        val domain = "://some.domain"
+        val res = UrlSanitizer.sanitize(domain)
+        assertTrue(res is Result.Error)
+    }
+}

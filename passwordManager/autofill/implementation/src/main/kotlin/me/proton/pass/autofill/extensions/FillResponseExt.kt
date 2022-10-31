963964a5657cd9526938f45f0d4ad0cfd5f763b5
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-10-31 16:33:20 +0100

    (feat) Autofill elements on suggestion click

--- passwordManager/autofill/implementation/src/main/kotlin/me/proton/pass/autofill/extensions/FillResponseExt.kt
+++ passwordManager/autofill/implementation/src/main/kotlin/me/proton/pass/autofill/extensions/FillResponseExt.kt
@@ -9,24 +9,62 @@ import android.service.autofill.SaveInfo
 import android.view.autofill.AutofillId
 import android.widget.inline.InlinePresentationSpec
 import androidx.annotation.RequiresApi
+import me.proton.core.crypto.common.context.CryptoContext
+import me.proton.core.crypto.common.keystore.decrypt
 import me.proton.pass.autofill.DatasetUtils
 import me.proton.pass.autofill.InlinePresentationUtils
+import me.proton.pass.autofill.PendingIntentUtils
 import me.proton.pass.autofill.entities.AndroidAutofillFieldId
 import me.proton.pass.autofill.entities.AssistField
 import me.proton.pass.autofill.entities.AssistInfo
 import me.proton.pass.autofill.service.R
 import me.proton.pass.common.api.None
 import me.proton.pass.common.api.Option
+import me.proton.pass.common.api.Some
 import me.proton.pass.common.api.some
 import me.proton.pass.common.api.toOption
+import me.proton.pass.domain.Item
 
 @RequiresApi(Build.VERSION_CODES.R)
 internal fun FillResponse.Builder.addInlineSuggestion(
+    context: Context,
+    cryptoContext: CryptoContext,
+    item: Item,
+    spec: InlinePresentationSpec,
+    assistFields: List<AssistField>
+) {
+    val inlinePresentation: InlinePresentation =
+        InlinePresentationUtils.create(
+            title = item.title.decrypt(cryptoContext.keyStoreCrypto),
+            subtitle = Some("subtitle"),
+            inlinePresentationSpec = spec,
+            pendingIntent = PendingIntentUtils.getEmptyPendingIntent(context)
+        )
+    val dataset = DatasetUtils.buildDataset(
+        context = context,
+        cryptoContext = cryptoContext,
+        item = item.toOption(),
+        authenticateView = None,
+        inlinePresentation = inlinePresentation.some(),
+        pendingIntent = None,
+        assistFields = assistFields
+    )
+    addDataset(dataset)
+}
+
+@RequiresApi(Build.VERSION_CODES.R)
+internal fun FillResponse.Builder.addInlineSuggestion(
+    context: Context,
+    cryptoContext: CryptoContext,
+    item: Option<Item>,
     inlinePresentation: InlinePresentation,
     pendingIntent: Option<PendingIntent>,
     assistFields: List<AssistField>
 ) {
     val dataset = DatasetUtils.buildDataset(
+        context = context,
+        cryptoContext = cryptoContext,
+        item = item,
         authenticateView = None,
         inlinePresentation = inlinePresentation.some(),
         pendingIntent = pendingIntent,
@@ -38,6 +76,7 @@ internal fun FillResponse.Builder.addInlineSuggestion(
 @RequiresApi(Build.VERSION_CODES.R)
 internal fun FillResponse.Builder.addOpenAppInlineSuggestion(
     context: Context,
+    cryptoContext: CryptoContext,
     inlinePresentationSpec: InlinePresentationSpec,
     pendingIntent: PendingIntent,
     assistFields: List<AssistField>
@@ -51,6 +90,9 @@ internal fun FillResponse.Builder.addOpenAppInlineSuggestion(
             pendingIntent = pendingIntent
         )
     addInlineSuggestion(
+        context = context,
+        cryptoContext = cryptoContext,
+        item = None,
         inlinePresentation = inlinePresentation,
         pendingIntent = pendingIntent.toOption(),
         assistFields = assistFields

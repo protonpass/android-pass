c7dbaeb444408f7dfb163c9e69909a811ee3739b
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-10-31 08:46:28 +0100

    Revert "(chore) Convert single data classes to value"
    This reverts commit 8c5ab33e04ce0e54d02e87e38590b2f67d39f72f.

--- passwordManager/domain/src/main/kotlin/me/proton/pass/domain/SharePermission.kt
+++ passwordManager/domain/src/main/kotlin/me/proton/pass/domain/SharePermission.kt
@@ -2,8 +2,7 @@ package me.proton.pass.domain
 
 import me.proton.core.util.kotlin.hasFlag
 
-@JvmInline
-value class SharePermission(val value: Int)
+data class SharePermission(val value: Int)
 
 fun SharePermission.hasFlag(flag: SharePermissionFlag): Boolean = value.hasFlag(flag.value)
 fun SharePermission.flags(): List<SharePermissionFlag> =

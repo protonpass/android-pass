87b58ce714737468b0f0c0758812b632014a6605
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-11-03 08:10:27 +0100

    (style) Show script execution message

--- scripts/ci/setupCIGradleProperties.sh
+++ scripts/ci/setupCIGradleProperties.sh
@@ -2,6 +2,7 @@
 
 set -eu
 
+echo "🪛 Overriding CI Gradle properties"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 REPO_ROOT=$(echo "${SCRIPT_DIR}" | sed 's:scripts/ci::g')
 PROPERTIES_FILE="$REPO_ROOT/gradle.properties"

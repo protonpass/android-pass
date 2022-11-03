7fb6c9b5789d97c6088798229ed68cffe8d27cc6
Carlos Quintana <carlos.quintana@proton.ch>
2022-11-03 08:21:06 +0100

    (refactor) separate preferences DI into two modules

---
+++ passwordManager/preferences/implementation/src/main/java/me/proton/android/pass/preferences/PreferencesBinderModule.kt
@@ -0,0 +1,13 @@
+package me.proton.android.pass.preferences
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class PreferencesBinderModule {
+    @Binds
+    abstract fun bindPreferenceRepository(impl: PreferenceRepositoryImpl): PreferenceRepository
+}

f7ea7f0f45b7a00a3eeed8f49a04cce0a49ae455
Carlos Quintana <carlos.quintana@proton.ch>
2022-11-03 12:53:07 +0100

    (feature) Finish settings screen

--- app/src/main/java/me/proton/android/pass/ui/home/BottomSheet.kt
+++ app/src/main/java/me/proton/android/pass/ui/home/BottomSheet.kt
@@ -1,31 +1,23 @@
 package me.proton.android.pass.ui.home
 
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Divider
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
 import androidx.compose.material.ModalBottomSheetState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
 import me.proton.android.pass.R
-import me.proton.core.compose.theme.ProtonTheme
 import me.proton.pass.common.api.Option
 import me.proton.pass.domain.ShareId
+import me.proton.pass.presentation.components.common.bottomsheet.BottomSheetItem
 
 @ExperimentalMaterialApi
 @Composable
@@ -69,30 +61,6 @@ fun BottomSheetContents(
                     shareId.map { navigation.toCreatePassword(it) }
                 }
             }
-        )
-    }
-}
-
-@Composable
-private fun BottomSheetItem(
-    @DrawableRes icon: Int,
-    @StringRes title: Int,
-    onItemClick: () -> Unit
-) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable(onClick = { onItemClick() })
-            .padding(horizontal = 16.dp, vertical = 12.dp)
-    ) {
-        Icon(painter = painterResource(icon), contentDescription = stringResource(title))
-        Text(
-            text = stringResource(title),
-            fontSize = 16.sp,
-            modifier = Modifier.padding(start = 20.dp),
-            fontWeight = FontWeight.W400,
-            color = ProtonTheme.colors.textNorm
         )
     }
 }
--- passwordManager/preferences/implementation/src/main/java/me/proton/android/pass/preferences/PassPreferences.kt
+++ passwordManager/preferences/implementation/src/main/java/me/proton/android/pass/preferences/PassPreferences.kt
@@ -1,7 +1,9 @@
 package me.proton.android.pass.preferences
 
 import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.intPreferencesKey
 
 object PassPreferences {
     val BIOMETRIC_LOCK = booleanPreferencesKey("biometric_lock")
+    val THEME = intPreferencesKey("theme")
 }

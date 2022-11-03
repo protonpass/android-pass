9cbde4a4f28a2ce15da1c64f7e110f8b99d560f7
Carlos Quintana <carlos.quintana@proton.ch>
2022-11-03 13:04:52 +0100

    (preview) Add preview to AuthenticationSection

--- passwordManager/presentation/src/main/kotlin/me/proton/pass/presentation/settings/AuthenticationSection.kt
+++ passwordManager/presentation/src/main/kotlin/me/proton/pass/presentation/settings/AuthenticationSection.kt
@@ -3,12 +3,19 @@ package me.proton.pass.presentation.settings
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
 import androidx.compose.material.Switch
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
+import me.proton.core.compose.theme.ProtonTheme
+import me.proton.pass.commonui.api.ThemePairPreviewProvider
 import me.proton.pass.presentation.R
+import me.proton.pass.presentation.components.previewproviders.ButtonEnabledPreviewProvider
 import me.proton.pass.presentation.components.settings.SettingPreferenceDescription
 import me.proton.pass.presentation.components.settings.SettingPreferenceTitle
 import me.proton.pass.presentation.components.settings.SettingSectionTitle
@@ -23,7 +30,7 @@ fun AuthenticationSection(
 ) {
     Column(modifier = modifier.padding(vertical = 12.dp)) {
         SettingSectionTitle(text = stringResource(R.string.settings_authentication_section_title))
-        Row {
+        Row(verticalAlignment = Alignment.CenterVertically) {
             SettingPreferenceTitle(
                 modifier = Modifier
                     .padding(vertical = 20.dp)
@@ -39,5 +46,23 @@ fun AuthenticationSection(
         SettingPreferenceDescription(
             text = stringResource(R.string.settings_authentication_preference_description)
         )
+    }
+}
+
+class ThemedBooleanPreviewProvider :
+    ThemePairPreviewProvider<IsButtonEnabled>(ButtonEnabledPreviewProvider())
+
+@Preview
+@Composable
+fun AuthenticationSectionPreview(
+    @PreviewParameter(ThemedBooleanPreviewProvider::class) input: Pair<Boolean, IsButtonEnabled>
+) {
+    ProtonTheme(isDark = input.first) {
+        Surface {
+            AuthenticationSection(
+                isToggleChecked = input.second,
+                onToggleChange = {}
+            )
+        }
     }
 }

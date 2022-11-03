36aaf36c3ea1a53631a0c42ffa2a4f4074e3fd54
Carlos Quintana <carlos.quintana@proton.ch>
2022-11-03 08:21:06 +0100

    (refactor) separate preferences DI into two modules

--- passwordManager/preferences/implementation/src/main/java/me/proton/android/pass/preferences/PreferencesProviderModule.kt
+++ passwordManager/preferences/implementation/src/main/java/me/proton/android/pass/preferences/PreferencesProviderModule.kt
@@ -3,7 +3,6 @@ package me.proton.android.pass.preferences
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,11 +11,8 @@ import me.proton.android.pass.preferences.extensions.dataStore
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class PreferenceModule {
+abstract class PreferencesProviderModule {
 
     @Provides
     fun provideDataStore(context: Context): DataStore<Preferences> = context.dataStore
-
-    @Binds
-    abstract fun bindPreferenceRepository(impl: PreferenceRepositoryImpl): PreferenceRepository
 }

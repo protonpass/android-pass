f1fd2bd76f79e6026763c74a811704ba9e32f8b8
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-11-04 11:15:44 +0100

    (chore) Rename passwordManager to pass

---
+++ pass/autofill/implementation/build.gradle.kts
@@ -0,0 +1,101 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt")
+    id("dagger.hilt.android.plugin")
+    id("kotlin-parcelize")
+}
+
+android {
+    compileSdk = libs.versions.compileSdk.get().toInt()
+    buildFeatures.buildConfig = false
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+    }
+
+    sourceSets {
+        getByName("main").java.srcDirs("src/main/kotlin")
+        getByName("test").java.srcDirs("src/test/kotlin")
+        getByName("androidTest").java.srcDirs("src/androidTest/kotlin")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+        }
+    }
+    flavorDimensions += "default"
+    productFlavors {
+        maybeCreate("dev")
+        maybeCreate("alpha")
+        maybeCreate("prod")
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.androidx.compose.compiler.get()
+    }
+
+    hilt {
+        enableAggregatingTask = true
+    }
+}
+
+dependencies {
+    implementation(libs.accompanist.navigation.animation)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.autofill)
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.foundationLayout)
+    implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.compose.runtime)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.uiTooling)
+    implementation(libs.androidx.hilt.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.hilt.work)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.core.domain)
+    implementation(libs.core.key)
+    implementation(libs.core.presentation)
+    implementation(libs.core.presentation.compose)
+    implementation(libs.core.user)
+    implementation(libs.core.utilKotlin)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(project(":pass:common:api"))
+    implementation(project(":pass:common-ui:api"))
+    implementation(project(":pass:data"))
+    implementation(project(":pass:domain"))
+    implementation(project(":pass:notifications:api"))
+    implementation(project(":pass:preferences:api"))
+    implementation(project(":pass:presentation"))
+    implementation(project(":pass:search:api"))
+    implementation(project(":pass:log"))
+
+    testImplementation(project(":pass:test"))
+    testImplementation(libs.turbine)
+    testImplementation(libs.truth)
+    testImplementation(libs.kotlinTest)
+    testImplementation(libs.coroutines.test)
+    testImplementation(libs.junit)
+    testImplementation(libs.core.test.kotlin)
+
+    implementation(libs.dagger.hilt.android)
+    kapt(libs.dagger.hilt.android.compiler)
+    kapt(libs.androidx.hilt.compiler)
+}
--- passwordManager/notifications/api/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("org.jetbrains.kotlin.jvm")
-}
-
-dependencies {
-    implementation(libs.kotlinx.coroutines.core)
-
-    implementation(project(":passwordManager:common:api"))
-}

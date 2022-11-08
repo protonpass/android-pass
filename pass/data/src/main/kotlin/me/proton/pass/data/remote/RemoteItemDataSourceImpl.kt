6503b0dfc3241f2db287fb66b6e322d577626880
Carlos Quintana <carlos.quintana@proton.ch>
2022-11-08 11:39:57 +0100

    (feature) handle no more aliases can be created

--- pass/data/src/main/kotlin/me/proton/pass/data/remote/RemoteItemDataSourceImpl.kt
+++ pass/data/src/main/kotlin/me/proton/pass/data/remote/RemoteItemDataSourceImpl.kt
@@ -2,6 +2,7 @@ package me.proton.pass.data.remote
 
 import me.proton.core.domain.entity.UserId
 import me.proton.core.network.data.ApiProvider
+import me.proton.core.network.domain.ApiResult
 import me.proton.pass.common.api.Result
 import me.proton.pass.common.api.map
 import me.proton.pass.common.api.toResult
@@ -15,7 +16,10 @@ import me.proton.pass.data.responses.ItemRevision
 import me.proton.pass.data.responses.TrashItemsResponse
 import me.proton.pass.domain.ItemId
 import me.proton.pass.domain.ShareId
+import me.proton.pass.domain.errors.CannotCreateMoreAliasesError
 import javax.inject.Inject
+
+const val CODE_CANNOT_CREATE_MORE_ALIASES = 2011
 
 class RemoteItemDataSourceImpl @Inject constructor(
     private val api: ApiProvider
@@ -35,11 +39,20 @@ class RemoteItemDataSourceImpl @Inject constructor(
         userId: UserId,
         shareId: ShareId,
         body: CreateAliasRequest
-    ): Result<ItemRevision> =
-        api.get<PasswordManagerApi>(userId)
-            .invoke { createAlias(shareId.id, body) }
-            .toResult()
-            .map { it.item }
+    ): Result<ItemRevision> {
+        val res = api.get<PasswordManagerApi>(userId).invoke { createAlias(shareId.id, body) }
+        when (res) {
+            is ApiResult.Success -> return Result.Success(res.value.item)
+            is ApiResult.Error -> {
+                if (res is ApiResult.Error.Http) {
+                    if (res.proton?.code == CODE_CANNOT_CREATE_MORE_ALIASES) {
+                        return Result.Error(CannotCreateMoreAliasesError())
+                    }
+                }
+                return Result.Error(res.cause)
+            }
+        }
+    }
 
     override suspend fun updateItem(
         userId: UserId,

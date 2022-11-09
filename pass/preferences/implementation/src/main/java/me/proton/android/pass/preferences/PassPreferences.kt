104657d674895833465620af5e54fabf84b48160
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-11-09 17:53:37 +0100

    (feat) Add onboarding and authenticated preference flags

--- pass/preferences/implementation/src/main/java/me/proton/android/pass/preferences/PassPreferences.kt
+++ pass/preferences/implementation/src/main/java/me/proton/android/pass/preferences/PassPreferences.kt
@@ -5,5 +5,7 @@ import androidx.datastore.preferences.core.intPreferencesKey
 
 object PassPreferences {
     val BIOMETRIC_LOCK = booleanPreferencesKey("biometric_lock")
+    val HAS_AUTHENTICATED = booleanPreferencesKey("has_authenticated")
+    val HAS_COMPLETED_ON_BOARDING = booleanPreferencesKey("has_completed_on_boarding")
     val THEME = intPreferencesKey("theme")
 }

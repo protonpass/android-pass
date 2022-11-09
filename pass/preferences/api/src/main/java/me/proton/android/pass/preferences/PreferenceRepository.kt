c5f142a9e43385a54e73521b233920887340956d
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-11-09 17:53:37 +0100

    (feat) Add onboarding and authenticated preference flags

--- pass/preferences/api/src/main/java/me/proton/android/pass/preferences/PreferenceRepository.kt
+++ pass/preferences/api/src/main/java/me/proton/android/pass/preferences/PreferenceRepository.kt
@@ -6,6 +6,12 @@ interface PreferenceRepository {
     fun setBiometricLockState(state: BiometricLockState): Flow<Unit>
     fun getBiometricLockState(): Flow<BiometricLockState>
 
+    fun setHasAuthenticated(state: HasAuthenticated): Flow<Unit>
+    fun getHasAuthenticated(): Flow<HasAuthenticated>
+
+    fun setHasCompletedOnBoarding(state: HasCompletedOnBoarding): Flow<Unit>
+    fun getHasCompletedOnBoarding(): Flow<HasCompletedOnBoarding>
+
     fun setThemePreference(theme: ThemePreference): Flow<Unit>
     fun getThemePreference(): Flow<ThemePreference>
 

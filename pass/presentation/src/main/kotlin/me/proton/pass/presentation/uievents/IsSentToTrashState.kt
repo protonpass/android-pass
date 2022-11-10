7216f0c0dcd61021f3786a79064402996b46be0c
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-11-10 11:48:37 +0100

    (chore) Add stable to ui events

--- pass/presentation/src/main/kotlin/me/proton/pass/presentation/uievents/IsSentToTrashState.kt
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/uievents/IsSentToTrashState.kt
@@ -1,5 +1,8 @@
 package me.proton.pass.presentation.uievents
 
+import androidx.compose.runtime.Stable
+
+@Stable
 sealed interface IsSentToTrashState {
     object Sent : IsSentToTrashState
     object NotSent : IsSentToTrashState
--- pass/presentation/src/main/kotlin/me/proton/pass/presentation/uievents/ItemSavedState.kt
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/uievents/ItemSavedState.kt
@@ -1,7 +1,9 @@
 package me.proton.pass.presentation.uievents
 
+import androidx.compose.runtime.Stable
 import me.proton.pass.domain.ItemId
 
+@Stable
 sealed interface ItemSavedState {
     object Unknown : ItemSavedState
     data class Success(val itemId: ItemId) : ItemSavedState

378a30eff8a321050c93f13d22e7c184dbb701b3
Carlos Quintana <carlos.quintana@proton.ch>
2022-11-10 10:13:00 +0100

    (chore) add clear preferences button to internal drawer

---
+++ app/src/dev/kotlin/me/proton/android/pass/ui/InternalDrawerSnackbarMessage.kt
@@ -0,0 +1,14 @@
+package me.proton.android.pass.ui
+
+import androidx.annotation.StringRes
+import me.proton.android.pass.R
+import me.proton.android.pass.notifications.api.SnackbarMessage
+import me.proton.android.pass.notifications.api.SnackbarType
+
+enum class InternalDrawerSnackbarMessage(
+    @StringRes override val id: Int,
+    override val type: SnackbarType
+): SnackbarMessage {
+    PreferencesCleared(R.string.snackbar_preferences_cleared_success, SnackbarType.SUCCESS),
+    PreferencesClearError(R.string.snackbar_preferences_cleared_error, SnackbarType.ERROR)
+}

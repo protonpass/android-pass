077dce829bd78ec31238073526b0cf3e8ec196b0
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-11-10 11:48:37 +0100

    (chore) Add stable to ui events

--- pass/presentation/src/main/kotlin/me/proton/pass/presentation/uievents/IsRefreshingState.kt
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/uievents/IsRefreshingState.kt
@@ -1,5 +1,8 @@
 package me.proton.pass.presentation.uievents
 
+import androidx.compose.runtime.Stable
+
+@Stable
 sealed interface IsRefreshingState {
     object Refreshing : IsRefreshingState
     object NotRefreshing : IsRefreshingState

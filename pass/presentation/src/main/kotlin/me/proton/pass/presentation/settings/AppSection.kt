e524989cb51e6b12f3ac62886da1c6017d66bb72
Neil Marietta <neil.marietta@protonmail.com>
2022-11-10 15:48:13 +0100

    test: Added AppSectionPreview.

--- pass/presentation/src/main/kotlin/me/proton/pass/presentation/settings/AppSection.kt
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/settings/AppSection.kt
@@ -2,9 +2,12 @@ package me.proton.pass.presentation.settings
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import me.proton.core.compose.component.ProtonSettingsHeader
 import me.proton.core.compose.theme.ProtonTheme
@@ -21,5 +24,17 @@ fun AppSection(
         ProtonSettingsHeader(title = stringResource(R.string.settings_app_section_title))
         TelemetrySettingToggleItem()
         CrashReportSettingToggleItem(divider = {})
+    }
+}
+
+@Preview
+@Composable
+fun AppSectionPreview(
+    @PreviewParameter(ThemePreviewProvider::class) isDark: Boolean
+) {
+    ProtonTheme(isDark = isDark) {
+        Surface {
+            AppSection()
+        }
     }
 }

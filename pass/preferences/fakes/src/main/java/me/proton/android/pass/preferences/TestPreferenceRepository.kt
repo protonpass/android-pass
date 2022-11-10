1aaf77e31daa41db56074768bdac27680da9cf69
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-11-10 08:34:37 +0100

    (test) Fix broken tests

--- pass/preferences/fakes/src/main/java/me/proton/android/pass/preferences/TestPreferenceRepository.kt
+++ pass/preferences/fakes/src/main/java/me/proton/android/pass/preferences/TestPreferenceRepository.kt
@@ -15,12 +15,34 @@ class TestPreferenceRepository : PreferenceRepository {
         replay = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST, extraBufferCapacity = 1
     )
 
+    private val hasAuthenticated = MutableSharedFlow<HasAuthenticated>(
+        replay = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST, extraBufferCapacity = 1
+    )
+
+    private val hasCompletedOnBoarding = MutableSharedFlow<HasCompletedOnBoarding>(
+        replay = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST, extraBufferCapacity = 1
+    )
+
     override fun setBiometricLockState(state: BiometricLockState): Flow<Unit> {
         biometricLockState.tryEmit(state)
         return flowOf(Unit)
     }
 
     override fun getBiometricLockState(): Flow<BiometricLockState> = biometricLockState
+
+    override fun setHasAuthenticated(state: HasAuthenticated): Flow<Unit> {
+        hasAuthenticated.tryEmit(state)
+        return flowOf(Unit)
+    }
+
+    override fun getHasAuthenticated(): Flow<HasAuthenticated> = hasAuthenticated
+
+    override fun setHasCompletedOnBoarding(state: HasCompletedOnBoarding): Flow<Unit> {
+        hasCompletedOnBoarding.tryEmit(state)
+        return flowOf(Unit)
+    }
+
+    override fun getHasCompletedOnBoarding(): Flow<HasCompletedOnBoarding> = hasCompletedOnBoarding
 
     override fun setThemePreference(theme: ThemePreference): Flow<Unit> {
         themePreference.tryEmit(theme)

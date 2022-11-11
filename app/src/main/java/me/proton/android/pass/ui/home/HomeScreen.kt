370b939e2351d560eb62ae569630902b393b43ce
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-11-11 12:32:39 +0100

    (chore) Remove autofill from home

--- app/src/main/java/me/proton/android/pass/ui/home/HomeScreen.kt
+++ app/src/main/java/me/proton/android/pass/ui/home/HomeScreen.kt
@@ -1,19 +1,13 @@
 package me.proton.android.pass.ui.home
 
-import android.content.Intent
-import android.net.Uri
-import android.provider.Settings.ACTION_REQUEST_SET_AUTOFILL_SERVICE
-import android.view.autofill.AutofillManager
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.launch
@@ -35,7 +29,6 @@ fun HomeScreen(
     )
     val scope = rememberCoroutineScope()
 
-    RequestAutofillIfSupported()
     ProtonModalBottomSheetLayout(
         sheetState = bottomSheetState,
         sheetContent = {
@@ -58,22 +51,5 @@ fun HomeScreen(
             onAddItemClick = { scope.launch { bottomSheetState.show() } },
             onRefresh = { viewModel.onRefresh() }
         )
-    }
-}
-
-@Composable
-private fun RequestAutofillIfSupported() {
-    val context = LocalContext.current
-    val autofillManager = context.getSystemService(AutofillManager::class.java)
-
-    // We are only requesting Autofill if the user does not have any autofill provider selected
-    // We should investigate a way for inviting the user to select our app even if they have another one,
-    // probably in an onboarding process or similar flow
-    if (!autofillManager.isEnabled) {
-        LaunchedEffect(true) {
-            val intent = Intent(ACTION_REQUEST_SET_AUTOFILL_SERVICE)
-            intent.data = Uri.parse("package:${context.packageName}")
-            context.startActivity(intent)
-        }
     }
 }

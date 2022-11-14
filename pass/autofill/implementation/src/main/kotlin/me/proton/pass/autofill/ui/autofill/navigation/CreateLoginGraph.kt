502d4d9a8f8b8340821850890851509a16734e12
Carlos Quintana <carlos.quintana@proton.ch>
2022-11-14 15:34:21 +0100

    (refactor) model autofillapp navigation with navgraphs

---
+++ pass/autofill/implementation/src/main/kotlin/me/proton/pass/autofill/ui/autofill/navigation/CreateLoginGraph.kt
@@ -0,0 +1,43 @@
+package me.proton.pass.autofill.ui.autofill.navigation
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavGraphBuilder
+import me.proton.android.pass.navigation.api.AppNavigator
+import me.proton.android.pass.navigation.api.composable
+import me.proton.pass.autofill.entities.AutofillAppState
+import me.proton.pass.autofill.ui.autofill.AutofillNavItem
+import me.proton.pass.presentation.components.model.ItemUiModel
+import me.proton.pass.presentation.create.alias.RESULT_CREATED_ALIAS
+import me.proton.pass.presentation.create.login.CreateLogin
+import me.proton.pass.presentation.create.login.InitialCreateLoginUiState
+
+@OptIn(ExperimentalAnimationApi::class, ExperimentalMaterialApi::class)
+fun NavGraphBuilder.createLoginGraph(
+    appNavigator: AppNavigator,
+    state: AutofillAppState,
+    onItemCreated: (ItemUiModel) -> Unit
+) {
+    composable(AutofillNavItem.CreateLogin) {
+        val createdAlias by appNavigator.navState<String>(RESULT_CREATED_ALIAS, null)
+            .collectAsStateWithLifecycle()
+
+        CreateLogin(
+            initialContents = InitialCreateLoginUiState(
+                title = state.title,
+                username = createdAlias,
+                url = state.webDomain.value()
+            ),
+            onClose = { appNavigator.onBackClick() },
+            onSuccess = onItemCreated,
+            onCreateAliasClick = {
+                appNavigator.navigate(
+                    AutofillNavItem.CreateAlias,
+                    AutofillNavItem.CreateAlias.createNavRoute(it)
+                )
+            }
+        )
+    }
+}
---
+++ pass/autofill/implementation/src/main/kotlin/me/proton/pass/autofill/ui/autofill/navigation/SelectItemGraph.kt
@@ -0,0 +1,31 @@
+package me.proton.pass.autofill.ui.autofill.navigation
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.navigation.NavGraphBuilder
+import me.proton.android.pass.navigation.api.AppNavigator
+import me.proton.android.pass.navigation.api.composable
+import me.proton.pass.autofill.entities.AutofillAppState
+import me.proton.pass.autofill.entities.AutofillItem
+import me.proton.pass.autofill.ui.autofill.AutofillNavItem
+import me.proton.pass.autofill.ui.autofill.select.SelectItemInitialState
+import me.proton.pass.autofill.ui.autofill.select.SelectItemScreen
+
+@OptIn(ExperimentalAnimationApi::class)
+fun NavGraphBuilder.selectItemGraph(
+    appNavigator: AppNavigator,
+    state: AutofillAppState,
+    onAutofillItemClicked: (AutofillItem) -> Unit
+) {
+    composable(AutofillNavItem.SelectItem) {
+        SelectItemScreen(
+            initialState = SelectItemInitialState(
+                packageName = state.packageName,
+                webDomain = state.webDomain
+            ),
+            onItemSelected = onAutofillItemClicked,
+            onCreateLoginClicked = {
+                appNavigator.navigate(AutofillNavItem.CreateLogin)
+            }
+        )
+    }
+}

22051215b8d642a734f1777fe9cf3de965284d0a
Carlos Quintana <carlos.quintana@proton.ch>
2022-11-15 12:33:44 +0100

    (feature) add snackbar to autofillapp

--- pass/autofill/implementation/src/main/kotlin/me/proton/pass/autofill/ui/autofill/AutofillAppUiState.kt
+++ pass/autofill/implementation/src/main/kotlin/me/proton/pass/autofill/ui/autofill/AutofillAppUiState.kt
@@ -1,17 +1,22 @@
 package me.proton.pass.autofill.ui.autofill
 
+import me.proton.android.pass.notifications.api.SnackbarMessage
 import me.proton.android.pass.preferences.ThemePreference
+import me.proton.pass.common.api.None
+import me.proton.pass.common.api.Option
 
 data class AutofillAppUiState(
     val theme: ThemePreference,
     val isFingerprintRequired: Boolean,
-    val itemSelected: AutofillItemSelectedState
+    val itemSelected: AutofillItemSelectedState,
+    val snackbarMessage: Option<SnackbarMessage>
 ) {
     companion object {
         val Initial = AutofillAppUiState(
             theme = ThemePreference.System,
             isFingerprintRequired = false,
-            itemSelected = AutofillItemSelectedState.Unknown
+            itemSelected = AutofillItemSelectedState.Unknown,
+            snackbarMessage = None
         )
     }
 }

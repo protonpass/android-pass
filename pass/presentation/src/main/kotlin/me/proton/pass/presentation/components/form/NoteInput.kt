52721448dd2ab31089a6713742a87ba1dd4aff4b
Carlos Quintana <carlos.quintana@proton.ch>
2022-11-15 16:34:16 +0100

    (design) create note design

--- pass/presentation/src/main/kotlin/me/proton/pass/presentation/components/form/NoteInput.kt
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/components/form/NoteInput.kt
@@ -8,15 +8,22 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import me.proton.core.compose.theme.ProtonTheme
-import me.proton.pass.commonui.api.ThemePreviewProvider
+import me.proton.pass.commonui.api.ThemePairPreviewProvider
 import me.proton.pass.presentation.R
+import me.proton.pass.presentation.components.previewproviders.NoteInputPreviewParameter
 import me.proton.pass.presentation.components.previewproviders.NoteInputPreviewProvider
+import me.proton.pass.presentation.uievents.value
 
 @Composable
-fun NoteInput(value: String, onChange: (String) -> Unit) {
+fun NoteInput(
+    value: String,
+    enabled: Boolean = true,
+    onChange: (String) -> Unit
+) {
     ProtonFormInput(
         title = R.string.field_note_title,
         placeholder = R.string.field_note_hint,
+        editable = enabled,
         value = value,
         onChange = onChange,
         modifier = Modifier.padding(top = 28.dp),
@@ -25,22 +32,22 @@ fun NoteInput(value: String, onChange: (String) -> Unit) {
     )
 }
 
-@Preview
-@Composable
-fun NoteInputThemePreview(@PreviewParameter(ThemePreviewProvider::class) isDarkMode: Boolean) {
-    ProtonTheme(isDark = isDarkMode) {
-        Surface {
-            NoteInput("Note input") {}
-        }
-    }
-}
+
+class ThemedNoteInputPreviewProvider :
+    ThemePairPreviewProvider<NoteInputPreviewParameter>(NoteInputPreviewProvider())
 
 @Preview
 @Composable
-fun NoteInputPreview(@PreviewParameter(NoteInputPreviewProvider::class) value: String) {
-    ProtonTheme {
+fun NoteInputPreview(
+    @PreviewParameter(ThemedNoteInputPreviewProvider::class) input: Pair<Boolean, NoteInputPreviewParameter>
+) {
+    ProtonTheme(isDark = input.first) {
         Surface {
-            NoteInput(value) {}
+            NoteInput(
+                value = input.second.value,
+                enabled = input.second.enabled,
+                onChange = {}
+            )
         }
     }
 }

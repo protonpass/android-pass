167b8c33f2e56c81926a9ac00f5f4453cd3e8d58
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-11-17 12:57:23 +0100

    (test) Add screenshots

--- pass/presentation/src/main/kotlin/me/proton/pass/presentation/components/common/item/ActionableItemRow.kt
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/components/common/item/ActionableItemRow.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
@@ -62,7 +63,17 @@ fun ActionableItemRowPreview(
 ) {
     ProtonTheme(isDark = input.first) {
         Surface {
-            ActionableItemRow(item = input.second)
+            ActionableItemRow(
+                item = input.second,
+                itemActions = listOf(
+                    ItemAction(
+                        onSelect = {},
+                        title = 0,
+                        icon = 0,
+                        textColor = Color.Blue
+                    )
+                )
+            )
         }
     }
 }

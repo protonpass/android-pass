67597c71fc78b1df9d279987d28613d2a5518b79
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-11-17 14:57:45 +0100

    (test) Add dark mode preview

--- pass/presentation/src/main/kotlin/me/proton/pass/presentation/components/common/item/PlaceholderExt.kt
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/components/common/item/PlaceholderExt.kt
@@ -5,18 +5,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.placeholder.PlaceholderHighlight
-import com.google.accompanist.placeholder.fade
 import com.google.accompanist.placeholder.placeholder
+import com.google.accompanist.placeholder.shimmer
 import me.proton.core.compose.theme.ProtonTheme
 
 fun Modifier.placeholder(): Modifier =
     composed {
         placeholder(
             visible = true,
-            color = ProtonTheme.colors.backgroundDeep,
+            color = ProtonTheme.colors.interactionWeakNorm,
             shape = RoundedCornerShape(8.dp),
-            highlight = PlaceholderHighlight.fade(
-                highlightColor = ProtonTheme.colors.backgroundSecondary
+            highlight = PlaceholderHighlight.shimmer(
+                highlightColor = ProtonTheme.colors.interactionWeakPressed
             )
         )
     }
--- pass/presentation/src/main/kotlin/me/proton/pass/presentation/components/common/item/PlaceholderItemRow.kt
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/components/common/item/PlaceholderItemRow.kt
@@ -14,8 +14,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import me.proton.core.compose.theme.ProtonTheme
+import me.proton.pass.commonui.api.ThemePreviewProvider
 
 @Composable
 fun PlaceholderItemRow(modifier: Modifier = Modifier) {
@@ -50,8 +52,10 @@ fun PlaceholderItemRow(modifier: Modifier = Modifier) {
 
 @Preview
 @Composable
-fun PlaceholderRowPreview() {
-    ProtonTheme {
+fun PlaceholderRowPreview(
+    @PreviewParameter(ThemePreviewProvider::class) isDark: Boolean
+) {
+    ProtonTheme(isDark = isDark) {
         Surface {
             PlaceholderItemRow()
         }

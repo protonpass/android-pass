f622f35cc7df43d9f15fc2bbc1c18c2154edd901
Carlos Quintana <carlos.quintana@proton.ch>
2022-11-18 16:13:50 +0100

    (refactor) move protos into their own module

--- pass/data/impl/build.gradle.kts
+++ pass/data/impl/build.gradle.kts
@@ -1,29 +1,10 @@
-import com.google.protobuf.gradle.generateProtoTasks
-import com.google.protobuf.gradle.id
-import com.google.protobuf.gradle.plugins
-import com.google.protobuf.gradle.protobuf
-import com.google.protobuf.gradle.protoc
-
-// Necessary imports for the protobuf section to work
-
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.kapt")
     id("com.google.devtools.ksp")
     kotlin("plugin.serialization")
-    id("com.google.protobuf")
     id("dagger.hilt.android.plugin")
-}
-
-// Necessary hack for the Protobuf plugin until they support AGP 7.x
-// https://github.com/google/protobuf-gradle-plugin/issues/540
-fun com.android.build.api.dsl.AndroidSourceSet.proto(action: SourceDirectorySet.() -> Unit) {
-    (this as? ExtensionAware)
-        ?.extensions
-        ?.getByName("proto")
-        ?.let { it as? SourceDirectorySet }
-        ?.apply(action)
 }
 
 android {
@@ -34,22 +15,6 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         testInstrumentationRunner = Config.testInstrumentationRunner
-    }
-
-
-    sourceSets {
-        getByName("main") {
-            java.srcDirs("src/main/kotlin")
-            proto {
-                srcDir("contents-proto-definition/protos")
-            }
-        }
-        getByName("test") {
-            java.srcDirs("src/test/kotlin")
-        }
-        getByName("androidTest") {
-            java.srcDirs("src/androidTest/kotlin")
-        }
     }
 
     compileOptions {
@@ -91,6 +56,7 @@ dependencies {
     implementation(project(":pass:data:api"))
     implementation(project(":pass:domain"))
     implementation(project(":pass:log"))
+    implementation(project(":pass:protos"))
 
     testImplementation(libs.core.test.kotlin)
     testImplementation(libs.junit)
@@ -111,26 +77,4 @@ dependencies {
     androidTestImplementation(libs.androidx.test.espresso.core)
     androidTestImplementation(libs.kotlinTest)
     androidTestImplementation(libs.core.test.android.instrumented)
-}
-
-
-protobuf {
-    val archSuffix = if (org.apache.tools.ant.taskdefs.condition.Os.isFamily(org.apache.tools.ant.taskdefs.condition.Os.FAMILY_MAC)) ":osx-x86_64" else ""
-    val catalog = extensions.getByType<VersionCatalogsExtension>().named("libs")
-    protoc {
-        artifact = "${catalog.findLibrary("google.protobuf.protoc").get().get()}$archSuffix"
-    }
-    plugins {
-        id("javalite") {
-            artifact =
-                "${catalog.findLibrary("google.protobuf.protocGenJavalite").get().get()}$archSuffix"
-        }
-    }
-    generateProtoTasks {
-        all().forEach { task ->
-            task.plugins {
-                id("javalite") { }
-            }
-        }
-    }
 }

7c64d8b145c6564e9c4f9003c6432b94922e1a21
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-11-18 16:14:44 +0100

    (delete) Remove unused flow code

--- app/src/main/java/me/proton/android/pass/extension/SavedStateHandle.kt
+++
@@ -1,6 +0,0 @@
-package me.proton.android.pass.extension
-
-import androidx.lifecycle.SavedStateHandle
-
-fun <T : Any> SavedStateHandle.require(key: String) =
-    get<T>(key) ?: throw IllegalArgumentException("$key is required")

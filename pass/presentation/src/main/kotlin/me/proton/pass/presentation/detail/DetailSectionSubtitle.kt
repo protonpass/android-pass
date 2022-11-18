a34a32cc11cae5065e56e5eb468b62c9c7b493a3
Carlos Quintana <carlos.quintana@proton.ch>
2022-11-18 08:35:24 +0100

    (preview) remove DetailSectionTitle and DetailSectionSubtitle previews

--- pass/presentation/src/main/kotlin/me/proton/pass/presentation/detail/DetailSectionSubtitle.kt
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/detail/DetailSectionSubtitle.kt
@@ -1,15 +1,11 @@
 package me.proton.pass.presentation.detail
 
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.sp
 import me.proton.core.compose.theme.ProtonTheme
-import me.proton.pass.commonui.api.ThemePreviewProvider
 
 @Composable
 fun DetailSectionSubtitle(
@@ -23,18 +19,4 @@ fun DetailSectionSubtitle(
         fontSize = 16.sp,
         color = ProtonTheme.colors.textWeak
     )
-}
-
-@Preview
-@Composable
-fun DetailSectionSubtitlePreview(
-    @PreviewParameter(ThemePreviewProvider::class) isDark: Boolean
-) {
-    ProtonTheme(isDark = isDark) {
-        Surface {
-            DetailSectionSubtitle(
-                text = "This is a section subtitle"
-            )
-        }
-    }
 }

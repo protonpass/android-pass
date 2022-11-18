ce6cd79e100621f29706490be32be627610c98e9
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-11-18 12:15:14 +0100

    (feat) Add empty list

---
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/components/common/item/EmptyList.kt
@@ -0,0 +1,57 @@
+package me.proton.pass.presentation.components.common.item
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import me.proton.core.compose.theme.ProtonTheme
+import me.proton.core.compose.theme.defaultWeak
+import me.proton.core.compose.theme.headline
+import me.proton.pass.presentation.R
+
+@Composable
+fun EmptyList(modifier: Modifier = Modifier, emptyListMessage: String) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.placeholder_bound_box),
+            contentDescription = ""
+        )
+        Text(
+            text = stringResource(id = R.string.empty_list_title),
+            style = ProtonTheme.typography.headline
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = emptyListMessage,
+            style = ProtonTheme.typography.defaultWeak
+        )
+    }
+}
+
+@Preview
+@Composable
+fun EmptListPreview() {
+    ProtonTheme {
+        Surface {
+            EmptyList(emptyListMessage = "Create a new item")
+        }
+    }
+}
--- pass/presentation/src/main/kotlin/me/proton/pass/presentation/components/common/item/ItemsList.kt
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/components/common/item/ItemsList.kt
@@ -1,16 +1,11 @@
 package me.proton.pass.presentation.components.common.item
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.google.accompanist.swiperefresh.SwipeRefreshState
@@ -56,17 +51,7 @@ fun ItemsList(
                 }
             }
         } else {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .verticalScroll(rememberScrollState())
-            ) {
-                Text(
-                    text = stringResource(id = emptyListMessage),
-                    modifier = Modifier.align(Alignment.Center)
-                )
-            }
+            EmptyList(emptyListMessage = stringResource(id = emptyListMessage))
         }
     }
 }
-

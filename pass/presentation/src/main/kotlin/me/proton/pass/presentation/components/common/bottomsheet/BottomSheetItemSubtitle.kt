e5f1aad525174c9f02fa728828db57a9ccc03928
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-11-28 12:07:14 +0100

    (feat) Add bottomsheet itemtype title

---
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/components/common/bottomsheet/BottomSheetItemSubtitle.kt
@@ -0,0 +1,24 @@
+package me.proton.pass.presentation.components.common.bottomsheet
+
+import androidx.annotation.StringRes
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import me.proton.core.compose.theme.ProtonTheme
+import me.proton.core.compose.theme.defaultSmallWeak
+
+@Composable
+fun BottomSheetItemSubtitle(
+    modifier: Modifier = Modifier,
+    @StringRes textId: Int,
+    textcolor: Color = Color.Unspecified
+) {
+    Text(
+        modifier = modifier,
+        text = stringResource(id = textId),
+        style = ProtonTheme.typography.defaultSmallWeak,
+        color = textcolor
+    )
+}
--- pass/presentation/src/main/kotlin/me/proton/pass/presentation/components/common/bottomsheet/BottomSheetItemTitle.kt
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/components/common/bottomsheet/BottomSheetItemTitle.kt
@@ -4,15 +4,21 @@ import androidx.annotation.StringRes
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import me.proton.core.compose.theme.ProtonTheme
 import me.proton.core.compose.theme.default
 
 @Composable
-fun BottomSheetItemText(modifier: Modifier = Modifier, @StringRes textId: Int) {
+fun BottomSheetItemTitle(
+    modifier: Modifier = Modifier,
+    @StringRes textId: Int,
+    textcolor: Color = Color.Unspecified
+) {
     Text(
         modifier = modifier,
         text = stringResource(id = textId),
-        style = ProtonTheme.typography.default
+        style = ProtonTheme.typography.default,
+        color = textcolor
     )
 }

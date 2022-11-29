4ea0c8db8622142ba86d5c6cc75d840c55a6a4b4
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-11-29 13:53:52 +0100

    (feat) Add itemtype to ItemEntity

---
+++ pass/data/impl/src/androidTest/kotlin/me/proton/android/pass/data/impl/db/AppDatabaseMigrationsTest.kt
@@ -0,0 +1,43 @@
+package me.proton.android.pass.data.impl.db
+
+import androidx.room.Room
+import androidx.room.testing.MigrationTestHelper
+import androidx.sqlite.db.framework.FrameworkSQLiteOpenHelperFactory
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import me.proton.android.pass.data.impl.db.AppDatabaseMigrations.MIGRATION_1_2
+import me.proton.android.pass.data.impl.db.AppDatabaseMigrations.MIGRATION_2_3
+import me.proton.android.pass.data.impl.db.AppDatabaseMigrations.MIGRATION_3_4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+
+@RunWith(AndroidJUnit4::class)
+class MigrationTest {
+
+    // Array of all migrations
+    private val migrations = arrayOf(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4)
+
+    @get:Rule
+    val helper: MigrationTestHelper = MigrationTestHelper(
+        InstrumentationRegistry.getInstrumentation(),
+        AppDatabase::class.java.canonicalName,
+        FrameworkSQLiteOpenHelperFactory()
+    )
+
+    @Test
+    @Throws(IOException::class)
+    fun migrateAll() {
+        // Create earliest version of the database.
+        helper.createDatabase(AppDatabase.DB_NAME, 1).apply { close() }
+
+        // Open latest version of the database. Room will validate the schema
+        // once all migrations execute.
+        Room.databaseBuilder(
+            InstrumentationRegistry.getInstrumentation().targetContext,
+            AppDatabase::class.java,
+            AppDatabase.DB_NAME
+        ).addMigrations(*migrations).build().apply { openHelper.writableDatabase.close() }
+    }
+}
--- pass/data/impl/src/main/kotlin/me/proton/android/pass/data/impl/db/AppDatabaseMigrations.kt
+++ pass/data/impl/src/main/kotlin/me/proton/android/pass/data/impl/db/AppDatabaseMigrations.kt
@@ -1,22 +1,13 @@
-package me.proton.android.pass.db
+package me.proton.android.pass.data.impl.db
 
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
-import me.proton.android.pass.data.impl.db.PassDatabase
 import me.proton.core.featureflag.data.db.FeatureFlagDatabase
 import me.proton.core.humanverification.data.db.HumanVerificationDatabase
 import me.proton.core.payment.data.local.db.PaymentDatabase
-import me.proton.core.usersettings.data.db.OrganizationDatabase
 
 @Suppress("MagicNumber")
 object AppDatabaseMigrations {
-
-    val MIGRATION_0_1 = object : Migration(0, 1) {
-        override fun migrate(database: SupportSQLiteDatabase) {
-            PassDatabase.MIGRATION_0.migrate(database)
-            OrganizationDatabase.MIGRATION_0.migrate(database)
-        }
-    }
 
     val MIGRATION_1_2 = object : Migration(1, 2) {
         override fun migrate(database: SupportSQLiteDatabase) {
@@ -31,6 +22,12 @@ object AppDatabaseMigrations {
     val MIGRATION_2_3 = object : Migration(2, 3) {
         override fun migrate(database: SupportSQLiteDatabase) {
             PaymentDatabase.MIGRATION_0.migrate(database)
+        }
+    }
+
+    val MIGRATION_3_4 = object : Migration(3, 4) {
+        override fun migrate(database: SupportSQLiteDatabase) {
+            PassDatabase.MIGRATION_4.migrate(database)
         }
     }
 }

81f661a481780d12aeb91482a4ea8e0858307a02
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-11-29 15:19:54 +0100

    (chore) Cleanup files

--- app/src/main/java/me/proton/android/pass/AuthRequiredState.kt
+++
@@ -1,8 +0,0 @@
-package me.proton.android.pass
-
-import me.proton.android.pass.preferences.BiometricLockState
-
-sealed interface AuthRequiredState {
-    object Loading : AuthRequiredState
-    data class Value(val state: BiometricLockState) : AuthRequiredState
-}

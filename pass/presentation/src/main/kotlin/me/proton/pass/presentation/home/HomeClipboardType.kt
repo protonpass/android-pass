4da8b265affe6bf052109f2074cc7a8d320827cb
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-11-29 09:22:39 +0100

    (feat) Add dialog confirmation and snackbar messages on copied

---
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/home/HomeClipboardType.kt
@@ -0,0 +1,8 @@
+package me.proton.pass.presentation.home
+
+enum class HomeClipboardType {
+    Alias,
+    Note,
+    Password,
+    Username
+}
--- pass/presentation/src/main/kotlin/me/proton/pass/presentation/home/HomeSnackbarMessage.kt
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/home/HomeSnackbarMessage.kt
@@ -11,6 +11,10 @@ enum class HomeSnackbarMessage(
 ) : SnackbarMessage {
     ObserveItemsError(R.string.error_observing_items, SnackbarType.ERROR),
     ObserveShareError(R.string.error_observing_share, SnackbarType.ERROR),
-    RefreshError(R.string.error_refreshing, SnackbarType.ERROR)
+    RefreshError(R.string.error_refreshing, SnackbarType.ERROR),
+    AliasCopied(R.string.home_snackbar_alias_copied, SnackbarType.NORM),
+    NoteCopied(R.string.home_snackbar_note_copied, SnackbarType.NORM),
+    PasswordCopied(R.string.home_snackbar_password_copied, SnackbarType.NORM),
+    UsernameCopied(R.string.home_snackbar_username_copied, SnackbarType.NORM),
 }
 

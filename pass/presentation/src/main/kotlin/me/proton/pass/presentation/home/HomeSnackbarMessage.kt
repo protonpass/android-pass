48770108fe43e76c95dc6cae31b52a38458302ac
Carlos Quintana <carlos.quintana@proton.ch>
2022-11-29 15:50:44 +0100

    (resources) unify copied to clipboard strings

--- pass/presentation/src/main/kotlin/me/proton/pass/presentation/home/HomeSnackbarMessage.kt
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/home/HomeSnackbarMessage.kt
@@ -12,9 +12,9 @@ enum class HomeSnackbarMessage(
     ObserveItemsError(R.string.error_observing_items, SnackbarType.ERROR),
     ObserveShareError(R.string.error_observing_share, SnackbarType.ERROR),
     RefreshError(R.string.error_refreshing, SnackbarType.ERROR),
-    AliasCopied(R.string.home_snackbar_alias_copied, SnackbarType.NORM),
-    NoteCopied(R.string.home_snackbar_note_copied, SnackbarType.NORM),
-    PasswordCopied(R.string.home_snackbar_password_copied, SnackbarType.NORM),
-    UsernameCopied(R.string.home_snackbar_username_copied, SnackbarType.NORM),
+    AliasCopied(R.string.alias_copied_to_clipboard, SnackbarType.NORM),
+    NoteCopied(R.string.note_copied_to_clipboard, SnackbarType.NORM),
+    PasswordCopied(R.string.password_copied_to_clipboard, SnackbarType.NORM),
+    UsernameCopied(R.string.username_copied_to_clipboard, SnackbarType.NORM),
 }
 

ccc3162545c5f9b3b3bddea5aa6f9d756dbe7117
Carlos Quintana <carlos.quintana@proton.ch>
2022-11-29 08:42:18 +0100

    (design) implement Note Detail and remove useless classes

--- pass/presentation/src/main/kotlin/me/proton/pass/presentation/detail/DetailSnackbarMessages.kt
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/detail/DetailSnackbarMessages.kt
@@ -12,5 +12,6 @@ enum class DetailSnackbarMessages(
     InitError(R.string.detail_init_error, SnackbarType.ERROR),
     SendToTrashError(R.string.detail_send_to_trash_error, SnackbarType.ERROR),
     ItemNotLoadedError(R.string.detail_item_not_loaded_error, SnackbarType.ERROR),
-    AliasCopiedToClipboard(R.string.alias_copied_to_clipboard, SnackbarType.NORM)
+    AliasCopiedToClipboard(R.string.alias_copied_to_clipboard, SnackbarType.NORM),
+    NoteCopiedToClipboard(R.string.note_copied_to_clipboard, SnackbarType.NORM)
 }
--- pass/presentation/src/main/kotlin/me/proton/pass/presentation/detail/note/NoteDetail.kt
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/detail/note/NoteDetail.kt
@@ -1,19 +1,12 @@
 package me.proton.pass.presentation.detail.note
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import me.proton.pass.presentation.detail.login.Section
-import me.proton.core.compose.theme.ProtonTheme
 import me.proton.pass.domain.Item
-import me.proton.pass.presentation.R
 
 @Composable
 fun NoteDetail(
@@ -21,39 +14,14 @@ fun NoteDetail(
     item: Item,
     viewModel: NoteDetailViewModel = hiltViewModel()
 ) {
-    viewModel.setItem(item)
+    LaunchedEffect(item) {
+        viewModel.setItem(item)
+    }
 
     val model by viewModel.viewState.collectAsStateWithLifecycle()
-    NoteContentView(
+    NoteContent(
+        modifier = modifier,
         model = model,
-        modifier = modifier
+        onCopyToClipboard = { viewModel.onCopyToClipboard() }
     )
-}
-
-@Composable
-internal fun NoteContentView(
-    model: NoteDetailViewModel.NoteUiModel,
-    modifier: Modifier = Modifier
-) {
-    Column {
-        Column(modifier = modifier.padding(horizontal = 16.dp)) {
-            NoteRow(model)
-        }
-        Divider(
-            modifier = Modifier.fillMaxWidth(),
-            color = ProtonTheme.colors.separatorNorm
-        )
-    }
-}
-
-@Composable
-internal fun NoteRow(
-    model: NoteDetailViewModel.NoteUiModel
-) {
-    if (model.note.isNotEmpty()) {
-        Section(
-            title = R.string.field_note_title,
-            content = model.note
-        )
-    }
 }

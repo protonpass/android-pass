e13b9f8554c57fd165c2b0860e509bd72693dad2
Carlos Quintana <carlos.quintana@proton.ch>
2022-12-01 10:50:07 +0100

    (feature) handle errors in event application

---
+++ app/src/main/java/me/proton/android/pass/ui/AppSnackbarMessage.kt
@@ -0,0 +1,14 @@
+package me.proton.android.pass.ui
+
+import androidx.annotation.StringRes
+import me.proton.android.pass.R
+import me.proton.android.pass.notifications.api.SnackbarMessage
+import me.proton.android.pass.notifications.api.SnackbarType
+
+enum class AppSnackbarMessage(
+    @StringRes override val id: Int,
+    override val type: SnackbarType
+) : SnackbarMessage {
+    CouldNotRefreshItems(R.string.snackbar_could_not_refresh_items, SnackbarType.ERROR)
+}
+

95df1c5df40db5652518d5a2745aa5d6a9c80ce8
Carlos Quintana <carlos.quintana@proton.ch>
2022-12-01 10:20:13 +0100

    (refactor) Move ApplyPendingEvents to suspend instead of Flow

--- pass/data/impl/src/main/kotlin/me/proton/android/pass/data/impl/usecases/ApplyPendingEventsImpl.kt
+++ pass/data/impl/src/main/kotlin/me/proton/android/pass/data/impl/usecases/ApplyPendingEventsImpl.kt
@@ -1,13 +1,12 @@
 package me.proton.android.pass.data.impl.usecases
 
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import me.proton.android.pass.data.api.PendingEventList
 import me.proton.android.pass.data.api.repositories.ItemRepository
 import me.proton.android.pass.data.api.usecases.ApplyPendingEvents
 import me.proton.android.pass.data.impl.extensions.toDomain
 import me.proton.android.pass.data.impl.repositories.EventRepository
 import me.proton.android.pass.data.impl.responses.EventList
+import me.proton.android.pass.log.PassLogger
 import me.proton.core.domain.entity.UserId
 import me.proton.core.user.domain.extension.primary
 import me.proton.core.user.domain.repository.UserAddressRepository
@@ -19,25 +18,33 @@ class ApplyPendingEventsImpl @Inject constructor(
     private val addressRepository: UserAddressRepository,
     private val itemRepository: ItemRepository
 ) : ApplyPendingEvents {
-    override fun invoke(userId: UserId, shareId: ShareId): Flow<Unit> = flow {
+    override suspend fun invoke(userId: UserId, shareId: ShareId) {
         val address = requireNotNull(addressRepository.getAddresses(userId).primary())
         while (true) {
             val events = eventRepository.getEvents(userId, address.addressId, shareId)
             if (events.fullRefresh) {
+                PassLogger.i(TAG, "Performing full refresh")
                 itemRepository.refreshItems(userId, shareId)
+                PassLogger.i(TAG, "Finished full refresh")
             } else {
+                PassLogger.i(TAG, "Applying events")
                 itemRepository.applyEvents(userId, address.addressId, shareId, events.toDomain())
+                PassLogger.i(TAG, "Applied events. Storing latest event ID")
                 eventRepository.storeLatestEventId(userId, address.addressId, shareId, events.latestEventId)
             }
 
             if (!events.eventsPending) break
         }
 
-        emit(Unit)
+        PassLogger.i(TAG, "ApplyPendingEvents finished")
     }
 
     private fun EventList.toDomain(): PendingEventList = PendingEventList(
         updatedItems = updatedItems.map { it.toDomain() },
         deletedItemIds = deletedItemIds
     )
+
+    companion object {
+        private const val TAG = "ApplyPendingEventsImpl"
+    }
 }

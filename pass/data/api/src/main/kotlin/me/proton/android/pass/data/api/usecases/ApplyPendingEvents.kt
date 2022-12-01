9985f2870795bd3bc504e45e9959ff50673109f5
Carlos Quintana <carlos.quintana@proton.ch>
2022-12-01 10:20:13 +0100

    (refactor) Move ApplyPendingEvents to suspend instead of Flow

--- pass/data/api/src/main/kotlin/me/proton/android/pass/data/api/usecases/ApplyPendingEvents.kt
+++ pass/data/api/src/main/kotlin/me/proton/android/pass/data/api/usecases/ApplyPendingEvents.kt
@@ -1,12 +1,11 @@
 package me.proton.android.pass.data.api.usecases
 
-import kotlinx.coroutines.flow.Flow
 import me.proton.core.domain.entity.UserId
 import me.proton.pass.domain.ShareId
 
 interface ApplyPendingEvents {
-    operator fun invoke(
+    suspend operator fun invoke(
         userId: UserId,
         shareId: ShareId
-    ): Flow<Unit>
+    )
 }

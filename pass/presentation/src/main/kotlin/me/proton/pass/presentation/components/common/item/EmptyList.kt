4ff4fcee521cc9275b42e2105ca634fbab89b179
Carlos Quintana <carlos.quintana@proton.ch>
2022-12-02 16:30:54 +0100

    (feature) allow pull to refresh on empty list

--- pass/presentation/src/main/kotlin/me/proton/pass/presentation/components/common/item/EmptyList.kt
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/components/common/item/EmptyList.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -28,6 +30,7 @@ fun EmptyList(modifier: Modifier = Modifier, emptyListMessage: String) {
     Column(
         modifier = modifier
             .fillMaxSize()
+            .verticalScroll(state = rememberScrollState())
             .padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally

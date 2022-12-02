e9c3952592ebdd8db62779eddb773bdf05cf9f16
Carlos Quintana <carlos.quintana@proton.ch>
2022-12-02 15:32:15 +0100

    (code) add @Stable to HomeFilterMode

--- pass/presentation/src/main/kotlin/me/proton/pass/presentation/home/HomeFilterMode.kt
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/home/HomeFilterMode.kt
@@ -1,30 +1,11 @@
 package me.proton.pass.presentation.home
 
-private const val ALL_ITEMS = "all_items"
-private const val LOGINS = "logins"
-private const val ALIASES = "aliases"
-private const val NOTES = "notes"
+import androidx.compose.runtime.Stable
 
+@Stable
 enum class HomeFilterMode {
     AllItems,
     Logins,
     Aliases,
     Notes;
-
-    fun value(): String = when (this) {
-        AllItems -> ALL_ITEMS
-        Logins -> LOGINS
-        Aliases -> ALIASES
-        Notes -> NOTES
-    }
-
-    companion object {
-        fun fromValue(value: String): HomeFilterMode = when (value) {
-            ALL_ITEMS -> AllItems
-            LOGINS -> Logins
-            ALIASES -> Aliases
-            NOTES -> Notes
-            else -> throw IllegalStateException("Unknown HomeFilterMode: $value")
-        }
-    }
 }

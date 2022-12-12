866800ad41b021c18e0d3875d0367beafc275297
Carlos Quintana <carlos.quintana@proton.ch>
2022-12-12 11:22:15 +0100

    (feature) allow to restore items from trash

--- pass/presentation/src/main/kotlin/me/proton/pass/presentation/trash/TrashScreen.kt
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/trash/TrashScreen.kt
@@ -32,6 +32,7 @@ fun TrashScreen(
     )
     val (selectedItem, setSelectedItem) = remember { mutableStateOf<ItemUiModel?>(null) }
     val (showClearTrashDialog, setShowClearTrashDialog) = rememberSaveable { mutableStateOf(false) }
+    val (showRestoreAllDialog, setShowRestoreAllDialog) = rememberSaveable { mutableStateOf(false) }
     val scope = rememberCoroutineScope()
 
     ProtonModalBottomSheetLayout(
@@ -56,6 +57,12 @@ fun TrashScreen(
                 TrashBottomSheetType.AllTrashActions -> TrashAllBottomSheetContents(
                     onEmptyTrash = {
                         setShowClearTrashDialog(true)
+                        scope.launch {
+                            bottomSheetState.hide()
+                        }
+                    },
+                    onRestoreAll = {
+                        setShowRestoreAllDialog(true)
                         scope.launch {
                             bottomSheetState.hide()
                         }
@@ -84,6 +91,11 @@ fun TrashScreen(
             show = showClearTrashDialog,
             onDismiss = { setShowClearTrashDialog(false) },
             onConfirm = { viewModel.clearTrash() }
+        )
+        ConfirmRestoreAllDialog(
+            show = showRestoreAllDialog,
+            onDismiss = { setShowRestoreAllDialog(false) },
+            onConfirm = { viewModel.restoreItems() }
         )
     }
 }

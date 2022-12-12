a4eb6538e994623cb0dc81d1d1cfcb8035fae26e
Carlos Quintana <carlos.quintana@proton.ch>
2022-12-12 16:02:43 +0100

    (paparazzi) update screenshots

--- pass/presentation/src/main/kotlin/me/proton/pass/presentation/trash/ConfirmRestoreAllDialog.kt
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/trash/ConfirmRestoreAllDialog.kt
@@ -1,10 +1,15 @@
 package me.proton.pass.presentation.trash
 
 import androidx.compose.material.AlertDialog
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import me.proton.core.compose.theme.ProtonTheme
+import me.proton.pass.commonui.api.ThemePreviewProvider
 import me.proton.pass.presentation.R
 
 @Composable
@@ -35,3 +40,18 @@ internal fun ConfirmRestoreAllDialog(
     )
 }
 
+@Preview
+@Composable
+fun ConfirmRestoreAllDialogPreview(
+    @PreviewParameter(ThemePreviewProvider::class) isDark: Boolean
+) {
+    ProtonTheme(isDark = isDark) {
+        Surface {
+            ConfirmRestoreAllDialog(
+                show = true,
+                onDismiss = {},
+                onConfirm = {}
+            )
+        }
+    }
+}

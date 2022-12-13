11da5e11c108b72e80d24898ed5b57705fbaf809
Carlos Quintana <carlos.quintana@proton.ch>
2022-12-13 12:14:46 +0100

    (test) fix tests

--- pass/autofill/implementation/src/test/kotlin/me/proton/pass/autofill/AuthViewModelTest.kt
+++ pass/autofill/implementation/src/test/kotlin/me/proton/pass/autofill/AuthViewModelTest.kt
@@ -6,8 +6,10 @@ import kotlinx.coroutines.test.runTest
 import me.proton.android.pass.biometry.BiometryAuthError
 import me.proton.android.pass.biometry.BiometryResult
 import me.proton.android.pass.biometry.BiometryStartupError
+import me.proton.android.pass.biometry.BiometryStatus
 import me.proton.android.pass.biometry.ContextHolder
 import me.proton.android.pass.biometry.TestBiometryManager
+import me.proton.android.pass.preferences.BiometricLockState
 import me.proton.android.pass.preferences.TestPreferenceRepository
 import me.proton.pass.common.api.None
 import me.proton.pass.presentation.auth.AuthStatus
@@ -30,6 +32,10 @@ class AuthViewModelTest {
     fun setUp() {
         preferenceRepository = TestPreferenceRepository()
         biometryManager = TestBiometryManager()
+
+        preferenceRepository.setBiometricLockState(BiometricLockState.Enabled)
+        biometryManager.setBiometryStatus(BiometryStatus.CanAuthenticate)
+
         viewModel = AuthViewModel(preferenceRepository, biometryManager)
     }
 
@@ -41,8 +47,41 @@ class AuthViewModelTest {
     }
 
     @Test
-    fun `biometry success emits success state`() = runTest {
+    fun `biometry success with enabled biometry emits success state`() = runTest {
         biometryManager.emitResult(BiometryResult.Success)
+
+        viewModel.init(ContextHolder(None))
+        viewModel.state.test {
+            assertThat(awaitItem()).isEqualTo(AuthStatus.Success)
+        }
+    }
+
+    @Test
+    fun `biometry preference is respected`() = runTest {
+        preferenceRepository.setBiometricLockState(BiometricLockState.Disabled)
+
+        // Set to failed so we can check it is not called
+        biometryManager.emitResult(BiometryResult.Failed)
+
+        viewModel.init(ContextHolder(None))
+        viewModel.state.test {
+            assertThat(awaitItem()).isEqualTo(AuthStatus.Success)
+        }
+    }
+
+    @Test
+    fun `unavailable biometry leads to auth success`() = runTest {
+        biometryManager.setBiometryStatus(BiometryStatus.NotAvailable)
+
+        viewModel.init(ContextHolder(None))
+        viewModel.state.test {
+            assertThat(awaitItem()).isEqualTo(AuthStatus.Success)
+        }
+    }
+
+    @Test
+    fun `not enrolled biometry leads to auth success`() = runTest {
+        biometryManager.setBiometryStatus(BiometryStatus.NotEnrolled)
 
         viewModel.init(ContextHolder(None))
         viewModel.state.test {

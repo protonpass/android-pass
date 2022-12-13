8f076c5a88087eb06d7395fa5b5ccb8a84a63557
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-12-13 13:12:22 +0100

    (fix) Sanitize url before trying to open and catch activity not found

---
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/utils/BrowserUtils.kt
@@ -0,0 +1,33 @@
+package me.proton.pass.presentation.utils
+
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import me.proton.android.pass.log.PassLogger
+import me.proton.pass.common.api.onError
+import me.proton.pass.common.api.onSuccess
+import me.proton.pass.presentation.UrlSanitizer
+
+object BrowserUtils {
+    const val TAG = "BrowserUtils"
+
+    fun openWebsite(context: Context, website: String) {
+        UrlSanitizer.sanitize(website)
+            .onSuccess {
+                try {
+                    val i = Intent(Intent.ACTION_VIEW).apply {
+                        data = Uri.parse(it)
+                    }
+                    context.startActivity(i)
+                } catch (e: ActivityNotFoundException) {
+                    val message = "Could not find a suitable activity"
+                    PassLogger.i(TAG, e = e, message)
+                }
+            }
+            .onError {
+                val message = "Could not find a suitable url"
+                PassLogger.i(TAG, e = it ?: Exception(message), message)
+            }
+    }
+}

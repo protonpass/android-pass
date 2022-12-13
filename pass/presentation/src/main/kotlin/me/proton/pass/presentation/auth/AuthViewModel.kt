54eb876adc387dea80a6d91d31a7fb223d5b8b2a
Carlos Quintana <carlos.quintana@proton.ch>
2022-12-13 10:27:34 +0100

    (fix) make autosave honor the biometry setting

--- pass/presentation/src/main/kotlin/me/proton/pass/presentation/auth/AuthViewModel.kt
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/auth/AuthViewModel.kt
@@ -6,6 +6,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
@@ -13,8 +14,10 @@ import kotlinx.coroutines.launch
 import me.proton.android.pass.biometry.BiometryAuthError
 import me.proton.android.pass.biometry.BiometryManager
 import me.proton.android.pass.biometry.BiometryResult
+import me.proton.android.pass.biometry.BiometryStatus
 import me.proton.android.pass.biometry.ContextHolder
 import me.proton.android.pass.log.PassLogger
+import me.proton.android.pass.preferences.BiometricLockState
 import me.proton.android.pass.preferences.HasAuthenticated
 import me.proton.android.pass.preferences.PreferenceRepository
 import me.proton.pass.common.api.asResultWithoutLoading
@@ -36,6 +39,26 @@ class AuthViewModel @Inject constructor(
         )
 
     fun init(context: ContextHolder) = viewModelScope.launch {
+        when (biometryManager.getBiometryStatus()) {
+            BiometryStatus.CanAuthenticate -> {
+                val biometricLockState = preferenceRepository.getBiometricLockState().first()
+                if (biometricLockState == BiometricLockState.Disabled) {
+                    // If there is biometry available, but the user does not have it enabled
+                    // we should proceed
+                    _state.update { AuthStatus.Success }
+                } else {
+                    // If there is biometry available and the user has it enabled, perform auth
+                    performAuth(context)
+                }
+            }
+            else -> {
+                // If there is no biometry available, emit success
+                _state.update { AuthStatus.Success }
+            }
+        }
+    }
+
+    private suspend fun performAuth(context: ContextHolder) {
         PassLogger.i(TAG, "Launching Biometry")
         biometryManager.launch(context)
             .map { result ->

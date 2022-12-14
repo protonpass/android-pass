29c0f239afac74ac2af8583af8792b026b2364bb
Carlos Quintana <carlos.quintana@proton.ch>
2022-12-14 16:11:14 +0100

    (refactor) use non-deprecated getApplicationInfo method

--- pass/data/impl/src/main/kotlin/me/proton/android/pass/data/impl/usecases/GetAppNameFromPackageNameImpl.kt
+++ pass/data/impl/src/main/kotlin/me/proton/android/pass/data/impl/usecases/GetAppNameFromPackageNameImpl.kt
@@ -2,6 +2,7 @@ package me.proton.android.pass.data.impl.usecases
 
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import dagger.hilt.android.qualifiers.ApplicationContext
 import me.proton.android.pass.data.api.usecases.GetAppNameFromPackageName
 import me.proton.pass.domain.entity.PackageName
@@ -13,7 +14,15 @@ class GetAppNameFromPackageNameImpl @Inject constructor(
 
     override fun invoke(packageName: PackageName): String {
         val packageManager = context.packageManager
-        val appInfo = packageManager.getApplicationInfo(packageName.packageName, PackageManager.GET_META_DATA)
+        val appInfo = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            packageManager.getApplicationInfo(
+                packageName.packageName,
+                PackageManager.ApplicationInfoFlags.of(PackageManager.GET_META_DATA.toLong())
+            )
+        } else {
+            packageManager.getApplicationInfo(packageName.packageName, PackageManager.GET_META_DATA)
+        }
+
         return packageManager.getApplicationLabel(appInfo).toString()
     }
 }

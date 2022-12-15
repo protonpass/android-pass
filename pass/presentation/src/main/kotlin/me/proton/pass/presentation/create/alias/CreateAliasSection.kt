e024e92a98ce6f5ef36820252448a1afb2183ed8
Carlos Quintana <carlos.quintana@proton.ch>
2022-12-15 16:07:52 +0100

    (fix) alias too long on creation

--- pass/presentation/src/main/kotlin/me/proton/pass/presentation/create/alias/CreateAliasSection.kt
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/create/alias/CreateAliasSection.kt
@@ -1,18 +1,29 @@
 package me.proton.pass.presentation.create.alias
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import me.proton.core.compose.theme.ProtonTheme
+import me.proton.pass.commonui.api.ThemePairPreviewProvider
 import me.proton.pass.presentation.R
+import me.proton.pass.presentation.components.previewproviders.CreateAliasSectionPreviewParameter
+import me.proton.pass.presentation.components.previewproviders.CreateAliasSectionPreviewProvider
 
 @Composable
 internal fun CreateAliasSection(
+    modifier: Modifier = Modifier,
     state: AliasItem,
     canEdit: Boolean,
     onAliasRequiredError: Boolean,
@@ -20,30 +31,58 @@ internal fun CreateAliasSection(
     onChange: (String) -> Unit,
     onSuffixClick: () -> Unit
 ) {
-    AliasInput(
-        value = state.alias,
-        onChange = onChange,
-        editable = canEdit,
-        onAliasRequiredError = onAliasRequiredError,
-        onInvalidAliasError = onInvalidAliasError
-    )
-    AliasSelector(
-        state = state,
-        modifier = Modifier.padding(top = 8.dp),
-        onClick = onSuffixClick
-    )
-    if (state.aliasToBeCreated != null) {
-        Row(modifier = Modifier.padding(top = 4.dp)) {
-            Text(
-                text = stringResource(R.string.alias_you_are_about_to_create_alias),
-                fontSize = 10.sp,
-                color = ProtonTheme.colors.textWeak
-            )
-            Text(
-                text = state.aliasToBeCreated,
-                fontSize = 10.sp,
-                color = ProtonTheme.colors.brandNorm,
-                modifier = Modifier.padding(start = 2.dp)
+    Column(modifier) {
+        AliasInput(
+            value = state.alias,
+            onChange = onChange,
+            editable = canEdit,
+            onAliasRequiredError = onAliasRequiredError,
+            onInvalidAliasError = onInvalidAliasError
+        )
+        AliasSelector(
+            state = state,
+            modifier = Modifier.padding(top = 8.dp),
+            onClick = onSuffixClick
+        )
+        if (state.aliasToBeCreated != null) {
+            Row(modifier = Modifier.padding(top = 4.dp)) {
+                val text = buildAnnotatedString {
+                    withStyle(style = SpanStyle(color = ProtonTheme.colors.textWeak)) {
+                        append(stringResource(R.string.alias_you_are_about_to_create_alias))
+                    }
+                    append(' ')
+                    withStyle(style = SpanStyle(color = ProtonTheme.colors.brandNorm)) {
+                        append(state.aliasToBeCreated)
+                    }
+                }
+                Text(
+                    text = text,
+                    fontSize = 10.sp
+                )
+            }
+        }
+    }
+}
+
+class ThemedCreateAliasSectionPreviewProvider :
+    ThemePairPreviewProvider<CreateAliasSectionPreviewParameter>(CreateAliasSectionPreviewProvider())
+
+@Preview
+@Composable
+fun CreateAliasSectionPreview(
+    @PreviewParameter(ThemedCreateAliasSectionPreviewProvider::class)
+    input: Pair<Boolean, CreateAliasSectionPreviewParameter>
+) {
+    val param = input.second
+    ProtonTheme(isDark = input.first) {
+        Surface {
+            CreateAliasSection(
+                state = param.aliasItem,
+                canEdit = param.canEdit,
+                onAliasRequiredError = param.onAliasRequiredError,
+                onInvalidAliasError = param.onInvalidAliasError,
+                onChange = {},
+                onSuffixClick = {}
             )
         }
     }

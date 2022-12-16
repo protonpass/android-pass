8a3bd6cff82792f4fea6a1df6b2a3f264f34a396
Victor Hidalgo <victor.hidalgo@proton.ch>
2022-12-16 10:18:27 +0100

    (feat) Track breadcrumbs with sentry

--- pass/presentation/src/alpha/kotlin/me/proton/pass/presentation/components/navigation/drawer/InternalDrawerItem.kt
+++ pass/presentation/src/alpha/kotlin/me/proton/pass/presentation/components/navigation/drawer/InternalDrawerItem.kt
@@ -1,30 +1,12 @@
 package me.proton.pass.presentation.components.navigation.drawer
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import kotlinx.coroutines.launch
-import me.proton.android.pass.log.InternalLogSharing
 
+@Suppress("UnusedPrivateMember", "OptionalUnit")
 @Composable
 fun InternalDrawerItem(
     modifier: Modifier = Modifier,
     closeDrawerAction: () -> Unit,
     onClick: () -> Unit
-) {
-    val coroutineScope = rememberCoroutineScope()
-    val context = LocalContext.current
-    NavigationDrawerListItem(
-        title = "(alpha) Share logs",
-        icon = me.proton.core.presentation.R.drawable.ic_proton_cog_wheel,
-        isSelected = false,
-        closeDrawerAction = closeDrawerAction,
-        modifier = modifier,
-        onClick = {
-            coroutineScope.launch {
-                InternalLogSharing.shareLogs("me.proton.android.pass.alpha", context)
-            }
-        }
-    )
-}
+) = Unit

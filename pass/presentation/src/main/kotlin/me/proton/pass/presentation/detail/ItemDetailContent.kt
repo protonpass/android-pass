cc518caa003340f4998b3a3bff0bdb89a9e617e3
Carlos Quintana <carlos.quintana@proton.ch>
2022-12-21 14:49:33 +0100

    (chore) do not show loading when navigating to detail

--- pass/presentation/src/main/kotlin/me/proton/pass/presentation/detail/ItemDetailContent.kt
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/detail/ItemDetailContent.kt
@@ -5,15 +5,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import me.proton.pass.common.api.Some
 import me.proton.pass.domain.ItemId
 import me.proton.pass.domain.ItemType
 import me.proton.pass.domain.ShareId
-import me.proton.pass.presentation.components.common.LoadingDialog
 import me.proton.pass.presentation.detail.alias.AliasDetail
 import me.proton.pass.presentation.detail.login.LoginDetail
 import me.proton.pass.presentation.detail.note.NoteDetail
-import me.proton.pass.presentation.uievents.IsLoadingState
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
@@ -24,16 +21,11 @@ fun ItemDetailContent(
     onEditClick: (ShareId, ItemId, ItemType) -> Unit
 ) {
     Box(modifier = modifier.fillMaxSize()) {
-        if (uiState.isLoading == IsLoadingState.Loading) {
-            LoadingDialog()
-        }
-
-        if (uiState.model is Some) {
-            val item = uiState.model.value.item
-            val itemName = uiState.model.value.name
+        if (uiState.model != null) {
+            val item = uiState.model.item
             val topBar: @Composable () -> Unit = {
                 ItemDetailTopBar(
-                    title = itemName,
+                    title = uiState.model.name,
                     onUpClick = onUpClick,
                     onEditClick = { onEditClick(item.shareId, item.id, item.itemType) }
                 )
--- pass/presentation/src/main/kotlin/me/proton/pass/presentation/detail/ItemDetailScreenUiState.kt
+++ pass/presentation/src/main/kotlin/me/proton/pass/presentation/detail/ItemDetailScreenUiState.kt
@@ -1,20 +1,15 @@
 package me.proton.pass.presentation.detail
 
 import androidx.compose.runtime.Immutable
-import me.proton.pass.common.api.None
-import me.proton.pass.common.api.Option
 import me.proton.pass.domain.Item
-import me.proton.pass.presentation.uievents.IsLoadingState
 
 @Immutable
 data class ItemDetailScreenUiState(
-    val model: Option<ItemModelUiState>,
-    val isLoading: IsLoadingState
+    val model: ItemModelUiState?
 ) {
     companion object {
-        val Loading = ItemDetailScreenUiState(
-            isLoading = IsLoadingState.Loading,
-            model = None
+        val Initial = ItemDetailScreenUiState(
+            model = null
         )
     }
 }

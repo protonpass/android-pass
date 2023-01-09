8515ea41a81c122774a2c07bbb3683e387a48308
Carlos Quintana <carlos.quintana@proton.ch>
2023-01-09 16:24:34 +0100

    build: convert crypto:api to jvm

--- pass/crypto/api/build.gradle.kts
+++ pass/crypto/api/build.gradle.kts
@@ -1,43 +1,10 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.jvm")
 }
-
-android {
-    compileSdk = libs.versions.compileSdk.get().toInt()
-    namespace = "me.proton.pass.crypto.api"
-
-    defaultConfig {
-        minSdk = libs.versions.minSdk.get().toInt()
-        targetSdk = libs.versions.targetSdk.get().toInt()
-    }
-
-    sourceSets {
-        getByName("main").java.srcDirs("src/main/kotlin")
-        getByName("test").java.srcDirs("src/test/kotlin")
-        getByName("androidTest").java.srcDirs("src/androidTest/kotlin")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = true
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
-}
-
 
 dependencies {
-    implementation(libs.core.cryptoCommon)
-    implementation(libs.core.user)
+    implementation(libs.core.key.domain)
+    implementation(libs.core.user.domain)
 
     implementation(projects.pass.domain)
     implementation(projects.pass.protos)

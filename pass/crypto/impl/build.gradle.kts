13bab80d9c28317d4b797abf14bb321734ba0e52
Carlos Quintana <carlos.quintana@proton.ch>
2023-01-09 16:44:40 +0100

    test: hardcode testInstrumentationRunner in crypto:impl

--- pass/crypto/impl/build.gradle.kts
+++ pass/crypto/impl/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        testInstrumentationRunner = Config.testInstrumentationRunner
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     compileOptions {

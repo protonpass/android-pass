5869f772582459bec5ba16243a38e65e2c04c7db
Carlos Quintana <carlos.quintana@proton.ch>
2023-01-10 10:39:29 +0100

    ci: fix git remote configuration

--- scripts/ci/generateScreenshotsIfNeeded.sh
+++ scripts/ci/generateScreenshotsIfNeeded.sh
@@ -6,6 +6,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 REPO_ROOT=$(echo "${SCRIPT_DIR}" | sed 's:scripts/ci::g')
 
 SCREENSHOTS_DIR="${REPO_ROOT}/pass/screenshot-tests/src/test/snapshots"
+AUTHENTICATED_REMOTE_NAME="authenticated"
+BRANCH_NAME="${CI_COMMIT_REF_NAME}"
 
 # Make sure we are on the repo root
 pushd $REPO_ROOT || exit 1
@@ -28,12 +30,15 @@ else
   git config --local user.email "pass@proton.local"
 
   # Set up gitlab remote
-  git remote add gitlab_origin "https://${GIT_SCREENSHOT_UPLOAD_USERNAME}:${GIT_SCREENSHOT_UPLOAD_TOKEN}@${CI_REPOSITORY_URL}"
+  git remote add "${AUTHENTICATED_REMOTE_NAME}" "https://${GIT_SCREENSHOT_UPLOAD_USERNAME}:${GIT_SCREENSHOT_UPLOAD_TOKEN}@${CI_SERVER_HOST}/${CI_PROJECT_PATH}.git"
+
+  # Create a branch with the proper name
+  git checkout -b "${BRANCH_NAME}" "${AUTHENTICATED_REMOTE_NAME}/${BRANCH_NAME}"
 
   # Add changes, create commit and push
   git add "${SCREENSHOTS_DIR}"
   git commit -m "test: Update screenshots (automatic commit)"
-  git push --set-upstream gitlab_origin "${CI_COMMIT_REF_NAME}"
+  git push --set-upstream "${AUTHENTICATED_REMOTE_NAME}" "${BRANCH_NAME}"
 
   echo "Changes have been pushed"
 fi

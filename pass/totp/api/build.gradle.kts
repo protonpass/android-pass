00281689550a036f4f77bb2e0e074d49b45a6366
Carlos Quintana <carlos.quintana@proton.ch>
2023-01-17 10:46:33 +0100

    build: add totp generation dependency

--- pass/totp/api/build.gradle.kts
+++ pass/totp/api/build.gradle.kts
@@ -1,3 +1,7 @@
 plugins {
     id("org.jetbrains.kotlin.jvm")
 }
+
+dependencies {
+    implementation(projects.pass.common.api)
+}
--- pass/totp/impl/build.gradle.kts
+++ pass/totp/impl/build.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
-    namespace = "proton.android.pass.pass.totp.impl"
+    namespace = "proton.android.pass.totp.impl"
     compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
@@ -12,6 +14,19 @@ android {
         targetSdk = libs.versions.targetSdk.get().toInt()
     }
 }
+
 dependencies {
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.onetimepassword)
+
+    implementation(projects.pass.common.api)
     implementation(projects.pass.totp.api)
+
+    implementation(libs.dagger.hilt.android)
+    kapt(libs.dagger.hilt.android.compiler)
+    kapt(libs.androidx.hilt.compiler)
+
+    testImplementation(projects.pass.commonTest)
+    testImplementation(libs.truth)
+    testImplementation(libs.junit)
 }

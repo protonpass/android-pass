bad82ba2b9be836475d6077e26cf821d43b09e97
Carlos Quintana <carlos.quintana@proton.ch>
2023-01-17 10:47:22 +0100

    feature: add TOTP calculator and Uri parser

---
+++ pass/totp/api/src/main/kotlin/proton/android/pass/totp/api/MalformedOtpUri.kt
@@ -0,0 +1,18 @@
+package proton.android.pass.totp.api
+
+import java.net.URISyntaxException
+
+sealed class MalformedOtpUri : IllegalArgumentException() {
+    data class InvalidUri(val throwable: URISyntaxException) : MalformedOtpUri()
+    data class InvalidScheme(val scheme: String) : MalformedOtpUri()
+    data class InvalidHost(val host: String) : MalformedOtpUri()
+    object MissingScheme : MalformedOtpUri()
+    object MissingHost : MalformedOtpUri()
+    object MissingLabel : MalformedOtpUri()
+    object MissingSecret : MalformedOtpUri()
+    data class InvalidAlgorithm(val algorithm: String) : MalformedOtpUri()
+    data class InvalidDigitCount(val digits: String) : MalformedOtpUri()
+    data class InvalidValidity(val validity: String) : MalformedOtpUri()
+}
+
+

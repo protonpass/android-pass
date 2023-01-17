2acf37b1f3e7263b5c0604c855add6bb8ada8064
Carlos Quintana <carlos.quintana@proton.ch>
2023-01-17 15:18:37 +0100

    test: fix ObserveActiveItems fake

--- pass/data/fakes/src/main/kotlin/proton/android/pass/data/fakes/usecases/TestObserveActiveItems.kt
+++ pass/data/fakes/src/main/kotlin/proton/android/pass/data/fakes/usecases/TestObserveActiveItems.kt
@@ -3,10 +3,11 @@ package proton.android.pass.data.fakes.usecases
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
+import proton.android.pass.common.api.Result
 import proton.android.pass.data.api.usecases.ItemTypeFilter
 import proton.android.pass.data.api.usecases.ObserveActiveItems
-import proton.android.pass.common.api.Result
 import proton.pass.domain.Item
+import proton.pass.domain.ShareSelection
 import javax.inject.Inject
 
 class TestObserveActiveItems @Inject constructor() : ObserveActiveItems {
@@ -14,7 +15,10 @@ class TestObserveActiveItems @Inject constructor() : ObserveActiveItems {
     private val activeItemsFlow: MutableSharedFlow<Result<List<Item>>> =
         MutableSharedFlow(replay = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST)
 
-    override fun invoke(filter: ItemTypeFilter): Flow<Result<List<Item>>> = activeItemsFlow
+    override fun invoke(
+        filter: ItemTypeFilter,
+        shareSelection: ShareSelection
+    ): Flow<Result<List<Item>>> = activeItemsFlow
 
     fun sendItemList(result: Result<List<Item>>) = activeItemsFlow.tryEmit(result)
 

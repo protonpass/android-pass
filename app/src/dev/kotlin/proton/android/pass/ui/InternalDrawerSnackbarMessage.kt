65eab172e1af4adc850f140e5070f7363681b0e8
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-01-17 12:00:52 +0100

    feat(manage-vault): temp ui to manage vaults

--- app/src/dev/kotlin/proton/android/pass/ui/InternalDrawerSnackbarMessage.kt
+++ app/src/dev/kotlin/proton/android/pass/ui/InternalDrawerSnackbarMessage.kt
@@ -12,7 +12,11 @@ enum class InternalDrawerSnackbarMessage(
 ): SnackbarMessage {
     PreferencesCleared(R.string.snackbar_preferences_cleared_success, SnackbarType.SUCCESS),
     PreferencesClearError(R.string.snackbar_preferences_cleared_error, SnackbarType.ERROR),
-    EmptyShareError(R.string.snackbar_empty_share, SnackbarType.ERROR),
+    CreateVaultSuccess(R.string.snackbar_create_vault_success, SnackbarType.SUCCESS),
+    CreateVaultError(R.string.snackbar_create_vault_error, SnackbarType.ERROR),
     DeleteVaultSuccess(R.string.snackbar_delete_vault_success, SnackbarType.SUCCESS),
-    DeleteVaultError(R.string.snackbar_delete_vault_error, SnackbarType.ERROR)
+    DeleteVaultError(R.string.snackbar_delete_vault_error, SnackbarType.ERROR),
+    CannotDeleteCurrentVault(R.string.snackbar_cannot_delete_current_vault_error, SnackbarType.ERROR),
+    ChangeVaultSuccess(R.string.snackbar_change_vault_success, SnackbarType.SUCCESS),
+    ChangeVaultError(R.string.snackbar_change_vault_error, SnackbarType.ERROR)
 }

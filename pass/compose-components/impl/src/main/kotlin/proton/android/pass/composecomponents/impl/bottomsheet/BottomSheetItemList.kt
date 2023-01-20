38e4964dd198dbad1f5c4a08dd7bd9c5c5bb8ca6
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-01-20 09:27:55 +0100

    fix: fix non scrollable components

--- pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/bottomsheet/BottomSheetItemList.kt
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/bottomsheet/BottomSheetItemList.kt
@@ -1,6 +1,8 @@
 package proton.android.pass.composecomponents.impl.bottomsheet
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import kotlinx.collections.immutable.ImmutableList
@@ -10,7 +12,7 @@ fun BottomSheetItemList(
     modifier: Modifier = Modifier,
     items: ImmutableList<BottomSheetItem>
 ) {
-    Column(modifier) {
+    Column(modifier.verticalScroll(rememberScrollState())) {
         items.forEach { item ->
             BottomSheetItemRow(
                 title = item.title,

ca51d9224c272fedbd86022876a0c5989233625e
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-01-24 12:44:29 +0100

    feat(manage-vault): handle multiple vault in alias creation

--- pass/feature-create-item/impl/src/main/kotlin/proton/android/pass/featurecreateitem/impl/alias/UpdateAlias.kt
+++ pass/feature-create-item/impl/src/main/kotlin/proton/android/pass/featurecreateitem/impl/alias/UpdateAlias.kt
@@ -12,12 +12,11 @@ import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import proton.pass.domain.ItemId
-import proton.pass.domain.ShareId
 import proton.android.pass.composecomponents.impl.dialogs.ConfirmMoveItemToTrashDialog
-import proton.android.pass.featurecreateitem.impl.alias.AliasSnackbarMessage.AliasUpdated
 import proton.android.pass.composecomponents.impl.uievents.IsLoadingState
 import proton.android.pass.featurecreateitem.impl.R
+import proton.pass.domain.ItemId
+import proton.pass.domain.ShareId
 
 @OptIn(ExperimentalLifecycleComposeApi::class)
 @ExperimentalMaterialApi
@@ -45,13 +44,10 @@ fun UpdateAlias(
             uiState = viewState,
             topBarTitle = R.string.title_edit_alias,
             canEdit = false,
-            canDelete = true,
+            isUpdate = true,
             isEditAllowed = viewState.isLoadingState == IsLoadingState.NotLoading,
             onUpClick = onUpClick,
-            onAliasCreated = { shareId, itemId, _ ->
-                viewModel.onEmitSnackbarMessage(AliasUpdated)
-                onSuccess(shareId, itemId)
-            },
+            onAliasCreated = { shareId, itemId, _ -> onSuccess(shareId, itemId) },
             onAliasDraftCreated = { _, _ -> },
             onSubmit = { viewModel.updateAlias() },
             onSuffixChange = { viewModel.onSuffixChange(it) },
@@ -59,8 +55,8 @@ fun UpdateAlias(
             onTitleChange = { viewModel.onTitleChange(it) },
             onNoteChange = { viewModel.onNoteChange(it) },
             onAliasChange = { viewModel.onAliasChange(it) },
-            onEmitSnackbarMessage = { viewModel.onEmitSnackbarMessage(it) },
-            onDeleteAlias = { setShowDeleteDialog(true) }
+            onDeleteAlias = { setShowDeleteDialog(true) },
+            onVaultSelect = {}
         )
 
         ConfirmMoveItemToTrashDialog(

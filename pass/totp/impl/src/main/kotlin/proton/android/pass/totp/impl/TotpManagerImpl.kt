924e2be321b1925018f6beb5edc0cbdcd8880071
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-01-27 17:37:12 +0100

    refactor(totp): observe code and time while it's valid

--- pass/totp/impl/src/main/kotlin/proton/android/pass/totp/impl/TotpManagerImpl.kt
+++ pass/totp/impl/src/main/kotlin/proton/android/pass/totp/impl/TotpManagerImpl.kt
@@ -4,6 +4,11 @@ import dev.turingcomplete.kotlinonetimepassword.HmacAlgorithm
 import dev.turingcomplete.kotlinonetimepassword.OtpAuthUriBuilder
 import dev.turingcomplete.kotlinonetimepassword.TimeBasedOneTimePasswordConfig
 import dev.turingcomplete.kotlinonetimepassword.TimeBasedOneTimePasswordGenerator
+import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.isActive
 import kotlinx.datetime.Clock
 import kotlinx.datetime.toJavaInstant
 import proton.android.pass.common.api.Result
@@ -48,5 +53,36 @@ class TotpManagerImpl @Inject constructor(
         return generator.generate(clock.now().toJavaInstant())
     }
 
+    override fun observeCode(spec: TotpSpec): Flow<Pair<String, Int>> {
+        val config = TimeBasedOneTimePasswordConfig(
+            timeStep = spec.validPeriodSeconds.toLong(),
+            timeStepUnit = TimeUnit.SECONDS,
+            codeDigits = spec.digits.digits,
+            hmacAlgorithm = when (spec.algorithm) {
+                TotpAlgorithm.Sha1 -> HmacAlgorithm.SHA1
+                TotpAlgorithm.Sha256 -> HmacAlgorithm.SHA256
+                TotpAlgorithm.Sha512 -> HmacAlgorithm.SHA512
+            }
+        )
+        val generator = TimeBasedOneTimePasswordGenerator(spec.secret.encodeToByteArray(), config)
+        return flow {
+            while (currentCoroutineContext().isActive) {
+                val timestamp = clock.now().toJavaInstant().toEpochMilli()
+                val code = generator.generate(timestamp)
+                val counter = generator.counter()
+                val endEpochMillis = generator.timeslotStart(counter + 1) - 1
+                while (generator.isValid(code)) {
+                    val millisValid = endEpochMillis - clock.now().toJavaInstant().toEpochMilli()
+                    emit(code to (millisValid / ONE_SECOND_MILLISECONDS).toInt())
+                    delay(ONE_SECOND_MILLISECONDS)
+                }
+            }
+        }
+    }
+
     override fun parse(uri: String): Result<TotpSpec> = OtpUriParser.parse(uri)
+
+    companion object {
+        private const val ONE_SECOND_MILLISECONDS = 1000L
+    }
 }

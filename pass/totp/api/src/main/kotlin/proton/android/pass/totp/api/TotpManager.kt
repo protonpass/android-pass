229d3c25cfffe06cf4d4bd97ad3b06fe06de2395
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-01-31 11:05:21 +0100

    fix: use base32 secret as input

--- pass/totp/api/src/main/kotlin/proton/android/pass/totp/api/TotpManager.kt
+++ pass/totp/api/src/main/kotlin/proton/android/pass/totp/api/TotpManager.kt
@@ -5,7 +5,6 @@ import proton.android.pass.common.api.Result
 
 interface TotpManager {
     fun generateUri(spec: TotpSpec): String
-    fun calculateCode(spec: TotpSpec): String
     fun observeCode(spec: TotpSpec): Flow<Pair<String, Int>>
     fun parse(uri: String): Result<TotpSpec>
 }

fa332dc4da4a195c3320530e9cd6e87bc0524603
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-02-02 11:46:16 +0100

    feat(copy-totp): copy totp and send notification when autofilling from activity

--- pass/autofill/impl/src/main/kotlin/proton/android/pass/autofill/entities/AutofillItem.kt
+++ pass/autofill/impl/src/main/kotlin/proton/android/pass/autofill/entities/AutofillItem.kt
@@ -3,7 +3,8 @@ package proton.android.pass.autofill.entities
 sealed class AutofillItem {
     data class Login(
         val username: String,
-        val password: String
+        val password: String,
+        val totp: String
     ) : AutofillItem()
 
     object Unknown : AutofillItem()

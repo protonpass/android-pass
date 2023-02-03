cf0f07e082e946bf064f45434603f68ea8c59100
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-02-03 09:55:37 +0100

    fix: handle share eviction on api error

--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/remote/RemoteEventDataSourceImpl.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/remote/RemoteEventDataSourceImpl.kt
@@ -2,10 +2,12 @@ package proton.android.pass.data.impl.remote
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import proton.android.pass.data.impl.responses.EventList
 import me.proton.core.domain.entity.UserId
 import me.proton.core.network.data.ApiProvider
+import me.proton.core.network.domain.ApiResult
+import proton.android.pass.data.api.errors.ShareNotAvailableError
 import proton.android.pass.data.impl.api.PasswordManagerApi
+import proton.android.pass.data.impl.responses.EventList
 import proton.pass.domain.ShareId
 import javax.inject.Inject
 
@@ -19,10 +21,18 @@ class RemoteEventDataSourceImpl @Inject constructor(
         emit(eventId)
     }
 
-    override fun getEvents(userId: UserId, shareId: ShareId, since: String): Flow<EventList> = flow {
-        val events = api.get<PasswordManagerApi>(userId).invoke {
-            getEvents(shareId.id, since)
-        }.valueOrThrow.events
-        emit(events)
+    override fun getEvents(userId: UserId, shareId: ShareId, since: String): Flow<EventList> =
+        flow {
+            val apiResult = api.get<PasswordManagerApi>(userId).invoke {
+                getEvents(shareId.id, since)
+            }
+            if (apiResult is ApiResult.Error.Http && apiResult.proton?.code == DELETED_SHARE_CODE) {
+                throw ShareNotAvailableError()
+            }
+            emit(apiResult.valueOrThrow.events)
+        }
+
+    companion object {
+        private const val DELETED_SHARE_CODE = 300_004
     }
 }

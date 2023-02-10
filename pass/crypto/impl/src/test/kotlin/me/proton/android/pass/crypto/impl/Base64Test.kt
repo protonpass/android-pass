292516fdb4e23164905803eab5d624795ba3e2ac
Carlos Quintana <carlos.quintana@proton.ch>
2023-02-10 11:54:14 +0100

    feat(crypto): add AES-GCM tag support

---
+++ pass/crypto/impl/src/test/kotlin/me/proton/android/pass/crypto/impl/Base64Test.kt
@@ -0,0 +1,31 @@
+package me.proton.android.pass.crypto.impl
+
+import org.apache.commons.codec.binary.Base64
+import org.junit.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
+
+class Base64Test {
+
+    @Test
+    fun worksAsExpected() {
+        assertContentEquals(
+            Base64.encodeBase64("Kotlin is awesome".encodeToByteArray()),
+            "S290bGluIGlzIGF3ZXNvbWU=".encodeToByteArray()
+        )
+        assertEquals(
+            "Kotlin is awesome",
+            String(Base64.decodeBase64("S290bGluIGlzIGF3ZXNvbWU="), Charsets.US_ASCII)
+        )
+    }
+
+    @Test
+    fun canEncodeDecode() {
+        val input = "this is a test"
+        val encoded = Base64.encodeBase64(input.encodeToByteArray())
+        val decoded = Base64.decodeBase64(encoded)
+        assertContentEquals(input.toByteArray(), decoded)
+        assertEquals(input, String(decoded, Charsets.US_ASCII))
+    }
+
+}

7c0de01345bc6983d3ecfa9440a6a463e83bb7f4
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-02-15 19:43:17 +0100

    feat: add detail transtion

--- pass/navigation/api/src/main/kotlin/proton/android/pass/navigation/api/NavUtils.kt
+++ pass/navigation/api/src/main/kotlin/proton/android/pass/navigation/api/NavUtils.kt
@@ -1,5 +1,8 @@
 package proton.android.pass.navigation.api
 
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavBackStackEntry
@@ -9,11 +12,19 @@ import com.google.accompanist.navigation.animation.composable
 @ExperimentalAnimationApi
 fun NavGraphBuilder.composable(
     navItem: NavItem,
+    enterTransition: (AnimatedContentScope<NavBackStackEntry>.() -> EnterTransition?)? = null,
+    exitTransition: (AnimatedContentScope<NavBackStackEntry>.() -> ExitTransition?)? = null,
+    popEnterTransition: (AnimatedContentScope<NavBackStackEntry>.() -> EnterTransition?)? = enterTransition,
+    popExitTransition: (AnimatedContentScope<NavBackStackEntry>.() -> ExitTransition?)? = exitTransition,
     content: @Composable (NavBackStackEntry) -> Unit
 ) {
     composable(
         route = navItem.route,
-        arguments = navItem.args
+        arguments = navItem.args,
+        enterTransition = enterTransition,
+        exitTransition = exitTransition,
+        popEnterTransition = popEnterTransition,
+        popExitTransition = popExitTransition
     ) {
         content(it)
     }

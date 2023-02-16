58594961680d4e20132df1923ec9e28a8218d875
Carlos Quintana <carlos.quintana@proton.ch>
2023-02-16 08:20:50 +0100

    feat(data): convert get shares call in a single one

--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/responses/GetSharesResponse.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/responses/GetSharesResponse.kt
@@ -8,21 +8,5 @@ data class GetSharesResponse(
     @SerialName("Code")
     val code: Int,
     @SerialName("Shares")
-    val shares: List<PartialShareResponse>
-)
-
-@Serializable
-data class PartialShareResponse(
-    @SerialName("ShareID")
-    val shareId: String,
-    @SerialName("VaultID")
-    val vaultId: String,
-    @SerialName("TargetType")
-    val shareType: Int,
-    @SerialName("TargetID")
-    val targetId: String,
-    @SerialName("Permission")
-    val permission: Int,
-    @SerialName("AcceptanceSignature")
-    val acceptanceSignature: String?
+    val shares: List<ShareResponse>
 )

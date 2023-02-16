c4367362c5721576d157bfa82573fac1e2d34209
Carlos Quintana <carlos.quintana@proton.ch>
2023-02-16 15:29:38 +0100

    test(data): Fix tests for ItemRepository

---
+++ pass/data/impl/src/test/kotlin/proton/android/pass/data/impl/fakes/TestItemKeyRepository.kt
@@ -0,0 +1,27 @@
+package proton.android.pass.data.impl.fakes
+
+import kotlinx.coroutines.flow.Flow
+import me.proton.core.domain.entity.UserId
+import me.proton.core.user.domain.entity.AddressId
+import proton.android.pass.data.impl.repositories.ItemKeyRepository
+import proton.android.pass.test.TestUtils.testFlow
+import proton.pass.domain.ItemId
+import proton.pass.domain.ShareId
+import proton.pass.domain.key.ItemKey
+import proton.pass.domain.key.ShareKey
+
+class TestItemKeyRepository : ItemKeyRepository {
+
+    private var getLatestItemKeyFlow = testFlow<Pair<ShareKey, ItemKey>>()
+
+    fun emitGetLatestItemKey(value: Pair<ShareKey, ItemKey>) {
+        getLatestItemKeyFlow.tryEmit(value)
+    }
+
+    override fun getLatestItemKey(
+        userId: UserId,
+        addressId: AddressId,
+        shareId: ShareId,
+        itemId: ItemId
+    ): Flow<Pair<ShareKey, ItemKey>> = getLatestItemKeyFlow
+}

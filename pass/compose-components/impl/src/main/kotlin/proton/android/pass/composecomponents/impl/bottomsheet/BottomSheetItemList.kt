f317099b79501aaca7674dcada9bbb204e2bb5be
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-02-16 11:24:44 +0100

    feat: add move to trash on login detail

--- pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/bottomsheet/BottomSheetItemList.kt
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/bottomsheet/BottomSheetItemList.kt
@@ -1,10 +1,12 @@
 package proton.android.pass.composecomponents.impl.bottomsheet
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import kotlinx.collections.immutable.ImmutableList
 
 @Composable
@@ -12,7 +14,11 @@ fun BottomSheetItemList(
     modifier: Modifier = Modifier,
     items: ImmutableList<BottomSheetItem>
 ) {
-    Column(modifier.verticalScroll(rememberScrollState())) {
+    Column(
+        modifier = modifier
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp, 24.dp)
+    ) {
         items.forEach { item ->
             BottomSheetItemRow(
                 title = item.title,
--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/TrashItemImpl.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/TrashItemImpl.kt
@@ -1,17 +1,32 @@
 package proton.android.pass.data.impl.usecases
 
-import proton.android.pass.data.api.repositories.ItemRepository
-import proton.android.pass.data.api.usecases.TrashItem
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 import me.proton.core.domain.entity.UserId
+import proton.android.pass.common.api.LoadingResult
+import proton.android.pass.data.api.repositories.ItemRepository
+import proton.android.pass.data.api.usecases.ObserveCurrentUser
+import proton.android.pass.data.api.usecases.TrashItem
 import proton.pass.domain.ItemId
 import proton.pass.domain.ShareId
 import javax.inject.Inject
 
 class TrashItemImpl @Inject constructor(
+    private val observeCurrentUser: ObserveCurrentUser,
     private val itemsRepository: ItemRepository
 ) : TrashItem {
 
-    override suspend fun invoke(userId: UserId, shareId: ShareId, itemId: ItemId) =
-        itemsRepository.trashItem(userId, shareId, itemId)
+    override suspend fun invoke(
+        userId: UserId?,
+        shareId: ShareId,
+        itemId: ItemId
+    ): LoadingResult<Unit> =
+        if (userId == null) {
+            observeCurrentUser()
+                .map { itemsRepository.trashItem(it.userId, shareId, itemId) }
+                .first()
+        } else {
+            itemsRepository.trashItem(userId, shareId, itemId)
+        }
 }
 

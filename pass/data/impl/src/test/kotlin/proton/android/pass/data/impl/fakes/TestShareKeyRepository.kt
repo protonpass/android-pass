d4d8e7b950c3cc7ebe35d30f06e8a8d6652fc8e8
Carlos Quintana <carlos.quintana@proton.ch>
2023-02-16 15:29:38 +0100

    test(data): Fix tests for ItemRepository

---
+++ pass/data/impl/src/test/kotlin/proton/android/pass/data/impl/fakes/TestShareKeyRepository.kt
@@ -0,0 +1,50 @@
+package proton.android.pass.data.impl.fakes
+
+import kotlinx.coroutines.flow.Flow
+import me.proton.core.domain.entity.UserId
+import me.proton.core.user.domain.entity.AddressId
+import proton.android.pass.data.impl.db.entities.ShareKeyEntity
+import proton.android.pass.data.impl.repositories.ShareKeyRepository
+import proton.android.pass.test.TestUtils.testFlow
+import proton.pass.domain.ShareId
+import proton.pass.domain.key.ShareKey
+
+class TestShareKeyRepository : ShareKeyRepository {
+
+    private var getShareKeysFlow = testFlow<List<ShareKey>>()
+    private var getLatestKeyForShareFlow = testFlow<ShareKey>()
+    private var getShareKeyForRotationFlow = testFlow<ShareKey?>()
+
+    fun emitGetShareKeys(value: List<ShareKey>) {
+        getShareKeysFlow.tryEmit(value)
+    }
+
+    fun emitGetLatestKeyForShare(value: ShareKey) {
+        getLatestKeyForShareFlow.tryEmit(value)
+    }
+
+    fun emitGetShareKeyForRotation(value: ShareKey?) {
+        getShareKeyForRotationFlow.tryEmit(value)
+    }
+
+    override fun getShareKeys(
+        userId: UserId,
+        addressId: AddressId,
+        shareId: ShareId,
+        forceRefresh: Boolean,
+        shouldStoreLocally: Boolean
+    ): Flow<List<ShareKey>> = getShareKeysFlow
+
+    override fun getLatestKeyForShare(shareId: ShareId): Flow<ShareKey> = getLatestKeyForShareFlow
+
+    override fun getShareKeyForRotation(
+        userId: UserId,
+        addressId: AddressId,
+        shareId: ShareId,
+        keyRotation: Long
+    ): Flow<ShareKey?> = getShareKeyForRotationFlow
+
+    override suspend fun saveShareKeys(shareKeyEntities: List<ShareKeyEntity>) {
+
+    }
+}

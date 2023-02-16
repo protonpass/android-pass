c81c2f321503687b87e48fd956eb0d7cdc845b9a
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-02-16 14:50:13 +0100

    feat: add alias move to trash and refactor alias detail loading

--- pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/alias/AliasDetailViewModel.kt
+++ pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/alias/AliasDetailViewModel.kt
@@ -17,17 +17,25 @@ import me.proton.core.accountmanager.domain.AccountManager
 import proton.android.pass.clipboard.api.ClipboardManager
 import proton.android.pass.common.api.LoadingResult
 import proton.android.pass.common.api.asResultWithoutLoading
+import proton.android.pass.common.api.onError
+import proton.android.pass.common.api.onSuccess
 import proton.android.pass.composecomponents.impl.uievents.IsLoadingState
+import proton.android.pass.composecomponents.impl.uievents.IsSentToTrashState
 import proton.android.pass.crypto.api.context.EncryptionContextProvider
 import proton.android.pass.data.api.repositories.AliasRepository
+import proton.android.pass.data.api.usecases.TrashItem
 import proton.android.pass.featureitemdetail.impl.DetailSnackbarMessages
 import proton.android.pass.featureitemdetail.impl.DetailSnackbarMessages.AliasCopiedToClipboard
+import proton.android.pass.featureitemdetail.impl.DetailSnackbarMessages.ItemMovedToTrash
+import proton.android.pass.featureitemdetail.impl.DetailSnackbarMessages.ItemNotMovedToTrash
 import proton.android.pass.log.api.PassLogger
 import proton.android.pass.notifications.api.SnackbarMessage
 import proton.android.pass.notifications.api.SnackbarMessageRepository
 import proton.pass.domain.AliasDetails
 import proton.pass.domain.Item
+import proton.pass.domain.ItemId
 import proton.pass.domain.ItemType
+import proton.pass.domain.ShareId
 import javax.inject.Inject
 
 @HiltViewModel
@@ -36,19 +44,24 @@ class AliasDetailViewModel @Inject constructor(
     private val accountManager: AccountManager,
     private val clipboardManager: ClipboardManager,
     private val snackbarMessageRepository: SnackbarMessageRepository,
-    private val encryptionContextProvider: EncryptionContextProvider
+    private val encryptionContextProvider: EncryptionContextProvider,
+    private val trashItem: TrashItem
 ) : ViewModel() {
 
-    private val loadingState: MutableStateFlow<IsLoadingState> =
+    private val isLoadingState: MutableStateFlow<IsLoadingState> =
         MutableStateFlow(IsLoadingState.Loading)
     private val modelState: MutableStateFlow<AliasUiModel?> = MutableStateFlow(null)
+    private val isItemSentToTrashState: MutableStateFlow<IsSentToTrashState> =
+        MutableStateFlow(IsSentToTrashState.NotSent)
 
-    val viewState: StateFlow<AliasDetailUiState> = combine(
-        loadingState,
+    val uiState: StateFlow<AliasDetailUiState> = combine(
+        isLoadingState,
+        isItemSentToTrashState,
         modelState
-    ) { loading, model ->
+    ) { loading, isItemSentToTrash, model ->
         AliasDetailUiState(
-            isLoadingState = loading,
+            isLoadingState = loading.value(),
+            isItemSentToTrash = isItemSentToTrash.value(),
             model = model
         )
     }
@@ -59,16 +72,24 @@ class AliasDetailViewModel @Inject constructor(
         )
 
     fun setItem(item: Item) = viewModelScope.launch {
-        loadingState.update { IsLoadingState.Loading }
-
         val userId = accountManager.getPrimaryUserId().first { userId -> userId != null }
         if (userId != null) {
+            modelState.update {
+                encryptionContextProvider.withEncryptionContext {
+                    AliasUiModel(
+                        title = decrypt(item.title),
+                        alias = (item.itemType as ItemType.Alias).aliasEmail,
+                        mailboxes = it?.mailboxes ?: emptyList(),
+                        note = decrypt(item.note)
+                    )
+                }
+            }
             aliasRepository.getAliasDetails(userId, item.shareId, item.id)
                 .asResultWithoutLoading()
                 .collect { onAliasDetails(it, item) }
         } else {
             showError("UserId is null", DetailSnackbarMessages.InitError, null)
-            loadingState.update { IsLoadingState.NotLoading }
+            isLoadingState.update { IsLoadingState.NotLoading }
         }
     }
 
@@ -98,7 +119,7 @@ class AliasDetailViewModel @Inject constructor(
                 // no-op
             }
         }
-        loadingState.update { IsLoadingState.NotLoading }
+        isLoadingState.update { IsLoadingState.NotLoading }
     }
 
     private suspend fun showError(
@@ -117,6 +138,20 @@ class AliasDetailViewModel @Inject constructor(
             }
             snackbarMessageRepository.emitSnackbarMessage(AliasCopiedToClipboard)
         }
+    }
+
+    fun onDelete(shareId: ShareId, itemId: ItemId) = viewModelScope.launch {
+        isLoadingState.update { IsLoadingState.Loading }
+        trashItem(shareId = shareId, itemId = itemId)
+            .onSuccess {
+                isItemSentToTrashState.update { IsSentToTrashState.Sent }
+                snackbarMessageRepository.emitSnackbarMessage(ItemMovedToTrash)
+            }
+            .onError {
+                snackbarMessageRepository.emitSnackbarMessage(ItemNotMovedToTrash)
+                PassLogger.d(TAG, it, "Could not delete item")
+            }
+        isLoadingState.update { IsLoadingState.NotLoading }
     }
 
     companion object {

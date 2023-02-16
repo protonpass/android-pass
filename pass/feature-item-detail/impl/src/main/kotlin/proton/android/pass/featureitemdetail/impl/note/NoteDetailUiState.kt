5b21212ad7f8c74ed313c17a074344a72f2530bb
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-02-16 12:33:15 +0100

    feat: add note detail move to trash

--- pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/note/NoteDetailUiState.kt
+++ pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/note/NoteDetailUiState.kt
@@ -2,12 +2,16 @@ package proton.android.pass.featureitemdetail.impl.note
 
 data class NoteDetailUiState(
     val title: String,
-    val note: String
+    val note: String,
+    val isLoading: Boolean,
+    val isItemSentToTrash: Boolean,
 ) {
     companion object {
         val Initial = NoteDetailUiState(
             title = "",
-            note = ""
+            note = "",
+            isLoading = false,
+            isItemSentToTrash = false
         )
     }
 }

d48deca6c6bff18f87129366ba85ef0240ef5dbb
Carlos Quintana <carlos.quintana@proton.ch>
2023-02-20 11:42:34 +0100

    refactor(common-ui): date formatting

--- pass/common-ui/api/src/main/java/proton/android/pass/commonui/api/DateFormatUtils.kt
+++ pass/common-ui/api/src/main/java/proton/android/pass/commonui/api/DateFormatUtils.kt
@@ -1,26 +1,34 @@
 package proton.android.pass.commonui.api
 
-import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDateTime
-import kotlinx.datetime.Month
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.minus
+import kotlinx.datetime.toJavaLocalDateTime
 import kotlinx.datetime.toLocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 object DateFormatUtils {
 
     @Composable
-    fun formatInstantText(now: Instant, toFormat: Instant): String {
+    fun formatInstantText(now: Instant, toFormat: Instant, locale: Locale = Locale.getDefault()): String {
         val formatted = formatInstant(now, toFormat, TimeZone.currentSystemDefault())
         return when (formatted) {
             is FormatResult.Date -> {
-                val month = stringResource(stringForMonth(formatted.month))
-
-                "${formatted.day} $month ${formatted.year} ${formatted.time}"
+                val pattern = stringResource(R.string.date_full_date_format_with_year)
+                DateTimeFormatter.ofPattern(pattern)
+                    .withLocale(locale)
+                    .format(formatted.localDateTime.toJavaLocalDateTime())
+            }
+            is FormatResult.DateOfSameYear -> {
+                val pattern = stringResource(R.string.date_full_date_format)
+                DateTimeFormatter.ofPattern(pattern)
+                    .withLocale(locale)
+                    .format(formatted.localDateTime.toJavaLocalDateTime())
             }
             is FormatResult.Today -> {
                 val today = stringResource(R.string.date_today)
@@ -38,41 +46,27 @@ object DateFormatUtils {
         toFormat: Instant,
         timeZone: TimeZone = TimeZone.UTC
     ): FormatResult {
-        val nowAsUtc = now.toLocalDateTime(timeZone)
-        val instantAsUtc = toFormat.toLocalDateTime(timeZone)
+        val nowAsLocal = now.toLocalDateTime(timeZone)
+        val instantAsLocal = toFormat.toLocalDateTime(timeZone)
 
         // If date is the same, it's a Today
-        if (nowAsUtc.date.equals(instantAsUtc.date)) {
-            return formatToday(instantAsUtc)
+        if (nowAsLocal.date.equals(instantAsLocal.date)) {
+            return formatToday(instantAsLocal)
         }
 
         // Check if the instant is the day before
         val nowMinusOneDay = now.minus(24, DateTimeUnit.HOUR).toLocalDateTime(timeZone)
-        if (nowMinusOneDay.date.equals(instantAsUtc.date)) {
-            return formatYesterday(instantAsUtc)
+        if (nowMinusOneDay.date.equals(instantAsLocal.date)) {
+            return formatYesterday(instantAsLocal)
         }
 
-        // It's neither today nor yesterday, format it as a regular date
-        return formatRegularDate(instantAsUtc)
+        // Check if the year is the same
+        return if (nowAsLocal.year == instantAsLocal.year) {
+            FormatResult.DateOfSameYear(instantAsLocal)
+        } else {
+            FormatResult.Date(localDateTime = instantAsLocal)
+        }
     }
-
-    @StringRes
-    fun stringForMonth(month: Month): Int =
-        when (month) {
-            Month.JANUARY -> R.string.month_january
-            Month.FEBRUARY -> R.string.month_february
-            Month.MARCH -> R.string.month_march
-            Month.APRIL -> R.string.month_april
-            Month.MAY -> R.string.month_may
-            Month.JUNE -> R.string.month_june
-            Month.JULY -> R.string.month_july
-            Month.AUGUST -> R.string.month_august
-            Month.SEPTEMBER -> R.string.month_september
-            Month.OCTOBER -> R.string.month_october
-            Month.NOVEMBER -> R.string.month_november
-            Month.DECEMBER -> R.string.month_december
-        }
-
 
     private fun formatToday(instant: LocalDateTime): FormatResult.Today =
         FormatResult.Today(extractHour(instant))
@@ -80,15 +74,6 @@ object DateFormatUtils {
 
     private fun formatYesterday(instant: LocalDateTime): FormatResult.Yesterday =
         FormatResult.Yesterday(extractHour(instant))
-
-    private fun formatRegularDate(instant: LocalDateTime): FormatResult.Date {
-        return FormatResult.Date(
-            day = instant.dayOfMonth,
-            month = instant.month,
-            year = instant.year,
-            time = extractHour(instant)
-        )
-    }
 
     private fun extractHour(instant: LocalDateTime): String {
         val hour = instant.hour.toString().padStart(2, '0')
@@ -99,11 +84,7 @@ object DateFormatUtils {
     sealed interface FormatResult {
         data class Today(val time: String) : FormatResult
         data class Yesterday(val time: String) : FormatResult
-        data class Date(
-            val day: Int,
-            val month: Month,
-            val year: Int,
-            val time: String
-        ) : FormatResult
+        data class DateOfSameYear(val localDateTime: LocalDateTime) : FormatResult
+        data class Date(val localDateTime: LocalDateTime) : FormatResult
     }
 }

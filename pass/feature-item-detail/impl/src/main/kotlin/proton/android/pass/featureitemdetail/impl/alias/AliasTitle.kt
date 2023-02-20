1d45680774de1b169fdd085c6a41dadf1fd2b8e6
Carlos Quintana <carlos.quintana@proton.ch>
2023-02-20 15:12:24 +0100

    feat(compose-components): adapt itemtype icons to new style

--- pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/alias/AliasTitle.kt
+++ pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/alias/AliasTitle.kt
@@ -3,21 +3,16 @@ package proton.android.pass.featureitemdetail.impl.alias
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
-import androidx.compose.material.Icon
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import me.proton.core.compose.theme.ProtonTheme
-import proton.android.pass.commonui.api.PassColors
 import proton.android.pass.commonui.api.ThemePreviewProvider
-import proton.android.pass.composecomponents.impl.container.Circle
-import proton.android.pass.featureitemdetail.impl.R
+import proton.android.pass.composecomponents.impl.item.icon.AliasIcon
 import proton.android.pass.featureitemdetail.impl.common.ItemTitleText
 
 @Composable
@@ -27,13 +22,7 @@ fun AliasTitle(modifier: Modifier = Modifier, title: String) {
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        Circle(backgroundColor = PassColors.GreenAccent, size = 60) {
-            Icon(
-                painter = painterResource(me.proton.core.presentation.R.drawable.ic_proton_alias),
-                contentDescription = stringResource(R.string.alias_title_icon_content_description),
-                tint = PassColors.GreenAccent
-            )
-        }
+        AliasIcon(size = 60)
         ItemTitleText(text = title)
     }
 }

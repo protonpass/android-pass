cfc6cbfb1222f6d1f2d6f073e626c831b5a88bad
Carlos Quintana <carlos.quintana@proton.ch>
2023-02-21 14:40:02 +0100

    refactor(commonui): remove unused color

--- pass/common-ui/api/src/main/java/proton/android/pass/commonui/api/PassColors.kt
+++ pass/common-ui/api/src/main/java/proton/android/pass/commonui/api/PassColors.kt
@@ -8,5 +8,4 @@ object PassColors {
     val YellowAccent = Color(0xFFFFCA8A)
     val GreenAccent = Color(0xFF6ABDB3)
     val RedAccent = Color(0xFFFC9C9F)
-    val AccentBrand = Color(0xFF1C1C30)
 }

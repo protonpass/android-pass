2f7ec59b3e7541fd7e1ec81dadc1425cebdcfb9a
Carlos Quintana <carlos.quintana@proton.ch>
2023-02-21 10:04:45 +0100

    feature(compose-components): add new BottomSheet style

--- pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/bottomsheet/BottomSheetItemList.kt
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/bottomsheet/BottomSheetItemList.kt
@@ -1,12 +1,11 @@
 package proton.android.pass.composecomponents.impl.bottomsheet
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import kotlinx.collections.immutable.ImmutableList
 
 @Composable
@@ -17,15 +16,18 @@ fun BottomSheetItemList(
     Column(
         modifier = modifier
             .verticalScroll(rememberScrollState())
-            .padding(16.dp, 24.dp)
     ) {
         items.forEach { item ->
-            BottomSheetItemRow(
-                title = item.title,
-                subtitle = item.subtitle,
-                icon = item.icon,
-                onClick = { item.onClick() }
-            )
+            if (item.isDivider) {
+                Divider()
+            } else {
+                BottomSheetItemRow(
+                    title = item.title,
+                    subtitle = item.subtitle,
+                    icon = item.icon,
+                    onClick = item.onClick?.let { { it.invoke() } }
+                )
+            }
         }
     }
 }

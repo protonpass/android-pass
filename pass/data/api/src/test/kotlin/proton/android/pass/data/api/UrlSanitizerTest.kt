179e37995eeba50b90d3d847c8eb9d9d8f47b9e3
Carlos Quintana <carlos.quintana@proton.ch>
2023-02-22 08:37:01 +0100

    refactor(data): convert UrlSanitizer to Kotlin results

--- pass/data/api/src/test/kotlin/proton/android/pass/data/api/UrlSanitizerTest.kt
+++ pass/data/api/src/test/kotlin/proton/android/pass/data/api/UrlSanitizerTest.kt
@@ -1,7 +1,6 @@
 package proton.android.pass.data.api
 
 import org.junit.Test
-import proton.android.pass.common.api.LoadingResult
 import proton.android.pass.data.api.url.UrlSanitizer
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
@@ -12,25 +11,27 @@ class UrlSanitizerTest {
     @Test
     fun `empty url should return error`() {
         val res = UrlSanitizer.sanitize("")
-        assertTrue(res is LoadingResult.Error)
-        assertNotNull(res.exception)
-        assertTrue(res.exception is IllegalArgumentException)
+        assertTrue(res.isFailure)
+
+        val exception = res.exceptionOrNull()
+        assertNotNull(exception)
+        assertTrue(exception is IllegalArgumentException)
     }
 
     @Test
     fun `url without scheme should have it added`() {
         val domain = "some.domain"
         val res = UrlSanitizer.sanitize(domain)
-        assertTrue(res is LoadingResult.Success)
-        assertEquals(res.data, "https://$domain")
+        assertTrue(res.isSuccess)
+        assertEquals(res.getOrThrow(), "https://$domain")
     }
 
     @Test
     fun `url with scheme should return success and not have it edited`() {
         val domain = "ssh://some.domain"
         val res = UrlSanitizer.sanitize(domain)
-        assertTrue(res is LoadingResult.Success)
-        assertEquals(res.data, domain)
+        assertTrue(res.isSuccess)
+        assertEquals(res.getOrThrow(), domain)
     }
 
     @Test
@@ -38,45 +39,47 @@ class UrlSanitizerTest {
         val domain = "some.domain"
         val path = "login"
         val res = UrlSanitizer.sanitize("$domain/$path")
-        assertTrue(res is LoadingResult.Success)
-        assertEquals(res.data, "https://$domain/$path")
+        assertTrue(res.isSuccess)
+        assertEquals(res.getOrThrow(), "https://$domain/$path")
     }
 
     @Test
     fun `url with empty scheme should return error`() {
         val domain = "://some.domain"
         val res = UrlSanitizer.sanitize(domain)
-        assertTrue(res is LoadingResult.Error)
+        assertTrue(res.isFailure)
     }
 
     @Test
     fun `url with spaces should return error`() {
         val domain = "https://url with spaces"
         val res = UrlSanitizer.sanitize(domain)
-        assertTrue(res is LoadingResult.Error)
+        assertTrue(res.isFailure)
     }
 
     @Test
     fun `getDomain empty url should return error`() {
         val res = UrlSanitizer.getDomain("")
-        assertTrue(res is LoadingResult.Error)
-        assertNotNull(res.exception)
-        assertTrue(res.exception is IllegalArgumentException)
+        assertTrue(res.isFailure)
+
+        val exception = res.exceptionOrNull()
+        assertNotNull(exception)
+        assertTrue(exception is IllegalArgumentException)
     }
 
     @Test
     fun `getDomain should be able to extract domain from url with scheme`() {
         val domain = "a.b.c.d"
         val res = UrlSanitizer.getDomain("https://$domain/e?f=g")
-        assertTrue(res is LoadingResult.Success)
-        assertEquals(res.data, domain)
+        assertTrue(res.isSuccess)
+        assertEquals(res.getOrThrow(), domain)
     }
 
     @Test
     fun `getDomain should be able to extract domain from url without scheme`() {
         val domain = "a.b.c.d"
         val res = UrlSanitizer.getDomain(domain)
-        assertTrue(res is LoadingResult.Success)
-        assertEquals(res.data, domain)
+        assertTrue(res.isSuccess)
+        assertEquals(res.getOrThrow(), domain)
     }
 }

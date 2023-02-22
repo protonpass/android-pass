05b552c90d33be88ad9ff32b72769d4b36359c56
Carlos Quintana <carlos.quintana@proton.ch>
2023-02-22 09:43:52 +0100

    fix: remove useless log

--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/RequestImageImpl.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/RequestImageImpl.kt
@@ -4,11 +4,9 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import me.proton.core.accountmanager.domain.AccountManager
-import org.apache.commons.codec.binary.Base64
 import proton.android.pass.data.api.url.UrlSanitizer
 import proton.android.pass.data.api.usecases.RequestImage
 import proton.android.pass.data.impl.remote.RemoteImageFetcher
-import proton.android.pass.log.api.PassLogger
 import javax.inject.Inject
 
 class RequestImageImpl @Inject constructor(
@@ -25,7 +23,6 @@ class RequestImageImpl @Inject constructor(
 
         val userId = requireNotNull(accountManager.getPrimaryUserId().first())
         val res = fetcher.fetchFavicon(userId, "something@$parsed").first()
-        PassLogger.i("Random", Base64.encodeBase64String(res))
         emit(res)
     }
 }

b8fb4176c0b882a61f963571042929577a7012d5
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-02-22 08:54:51 +0100

    fix: fix username screenshots and ime insets

--- pass/data/fakes/src/main/kotlin/proton/android/pass/data/fakes/usecases/TestObserveCurrentUser.kt
+++ pass/data/fakes/src/main/kotlin/proton/android/pass/data/fakes/usecases/TestObserveCurrentUser.kt
@@ -1,16 +1,15 @@
 package proton.android.pass.data.fakes.usecases
 
-import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import me.proton.core.user.domain.entity.User
+import proton.android.pass.common.api.FlowUtils.testFlow
 import proton.android.pass.data.api.usecases.ObserveCurrentUser
 import javax.inject.Inject
 
 class TestObserveCurrentUser @Inject constructor() : ObserveCurrentUser {
 
-    private val observeVaultsFlow: MutableSharedFlow<User> =
-        MutableSharedFlow(replay = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST)
+    private val observeVaultsFlow: MutableSharedFlow<User> = testFlow()
 
     fun sendUser(user: User) = observeVaultsFlow.tryEmit(user)
     override fun invoke(): Flow<User> = observeVaultsFlow

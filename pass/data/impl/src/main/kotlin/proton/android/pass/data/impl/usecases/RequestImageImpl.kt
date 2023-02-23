f4b56b855a15771c73847526afd3f3db7d4ad452
Carlos Quintana <carlos.quintana@proton.ch>
2023-02-23 16:27:08 +0100

    feat(image): Improve image caching

--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/RequestImageImpl.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/RequestImageImpl.kt
@@ -1,11 +1,12 @@
 package proton.android.pass.data.impl.usecases
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import me.proton.core.accountmanager.domain.AccountManager
 import proton.android.pass.data.api.url.UrlSanitizer
-import proton.android.pass.data.api.usecases.ImageResponse
+import proton.android.pass.data.api.usecases.ImageResponseResult
 import proton.android.pass.data.api.usecases.RequestImage
 import proton.android.pass.data.impl.remote.RemoteImageFetcher
 import javax.inject.Inject
@@ -14,10 +15,17 @@ class RequestImageImpl @Inject constructor(
     private val fetcher: RemoteImageFetcher,
     private val accountManager: AccountManager
 ) : RequestImage {
-    override fun invoke(domain: String): Flow<ImageResponse?> = flow {
+    override fun invoke(domain: String): Flow<ImageResponseResult> = flow {
         val parsed = UrlSanitizer.getDomain(domain).getOrThrow()
         val userId = requireNotNull(accountManager.getPrimaryUserId().first())
-        val res = fetcher.fetchFavicon(userId, "something@$parsed").first()
-        emit(res)
+        fetcher.fetchFavicon(userId, "no-reply@$parsed")
+            .catch { emit(ImageResponseResult.Error(it)) }
+            .collect {
+                if (it == null) {
+                    emit(ImageResponseResult.Empty)
+                } else {
+                    emit(ImageResponseResult.Data(it.content, it.mimeType))
+                }
+            }
     }
 }

4d981aa8c5d20b7b44b1feb107d718fd7a57931f
Carlos Quintana <carlos.quintana@proton.ch>
2023-02-24 08:52:03 +0100

    fix(data): handle invalid urls when requesting icon

--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/remote/RemoteImageFetcher.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/remote/RemoteImageFetcher.kt
@@ -7,6 +7,7 @@ import me.proton.core.network.data.ApiProvider
 import me.proton.core.network.data.ProtonErrorException
 import proton.android.pass.data.api.usecases.ImageResponse
 import proton.android.pass.data.impl.api.PasswordManagerApi
+import proton.android.pass.log.api.PassLogger
 import javax.inject.Inject
 
 interface RemoteImageFetcher {
@@ -24,9 +25,18 @@ class RemoteImageFetcherImpl @Inject constructor(
                 val mimeType = res.headers().get("Content-Type")
                 emit(ImageResponse(content = body, mimeType = mimeType))
             } catch (e: ProtonErrorException) {
-                if (e.response.code == HTTP_UNPROCESSABLE_CONTENT && e.protonData.code == PROTON_CODE_NOT_TRUSTED) {
-                    emit(null)
-                    return@invoke
+                if (e.response.code == HTTP_UNPROCESSABLE_CONTENT) {
+                    val protonCode = e.protonData.code
+                    if (protonCode == PROTON_CODE_NOT_TRUSTED) {
+                        PassLogger.d(TAG, "Received NotTrusted for domain $domain")
+                        emit(null)
+                        return@invoke
+                    }
+                    if (protonCode == PROTON_CODE_INVALID_ADDRESS) {
+                        PassLogger.d(TAG, "Received InvalidAddress for domain $domain")
+                        emit(null)
+                        return@invoke
+                    }
                 }
                 throw e
             }
@@ -36,5 +46,8 @@ class RemoteImageFetcherImpl @Inject constructor(
     companion object {
         private const val HTTP_UNPROCESSABLE_CONTENT = 422
         private const val PROTON_CODE_NOT_TRUSTED = 2011
+        private const val PROTON_CODE_INVALID_ADDRESS = -1
+
+        private const val TAG = "RemoteImageFetcherImpl"
     }
 }

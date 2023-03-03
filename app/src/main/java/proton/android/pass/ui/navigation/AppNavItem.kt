a9b321ed1434156d6fb5505dfde88c1c8862d177
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-03-03 09:49:33 +0100

    feat: add image picker ui and refactor navigation

--- app/src/main/java/proton/android/pass/ui/navigation/AppNavItem.kt
+++ app/src/main/java/proton/android/pass/ui/navigation/AppNavItem.kt
@@ -7,6 +7,7 @@ import proton.android.pass.navigation.api.AliasOptionalNavArgId
 import proton.android.pass.navigation.api.CommonNavArgId
 import proton.android.pass.navigation.api.CommonOptionalNavArgId
 import proton.android.pass.navigation.api.NavItem
+import proton.android.pass.navigation.api.toPath
 import proton.pass.domain.ItemId
 import proton.pass.domain.ShareId
 
@@ -15,22 +16,6 @@ object OnBoarding : NavItem(baseRoute = "onboarding")
 object Settings : NavItem(baseRoute = "settings", isTopLevel = true)
 
 object Trash : NavItem(baseRoute = "trash", isTopLevel = true)
-
-
-object CreateLogin : NavItem(
-    baseRoute = "login/create",
-    optionalArgIds = listOf(CommonOptionalNavArgId.ShareId)
-) {
-    fun createNavRoute(shareId: Option<ShareId>) = buildString {
-        append(baseRoute)
-        val map = mutableMapOf<String, Any>()
-        if (shareId is Some) {
-            map[CommonOptionalNavArgId.ShareId.key] = shareId.value.id
-        }
-        val path = map.toPath()
-        append(path)
-    }
-}
 
 object EditLogin : NavItem(
     baseRoute = "login/edit",
@@ -104,14 +89,3 @@ object ViewItem : NavItem(
     fun createNavRoute(shareId: ShareId, itemId: ItemId) =
         "$baseRoute/${shareId.id}/${itemId.id}"
 }
-
-object CreateTotp : NavItem(baseRoute = "totp/create")
-object CameraTotp : NavItem(baseRoute = "totp/camera")
-object PhotoPickerTotp : NavItem(baseRoute = "totp/photopicker")
-
-fun Map<String, Any>.toPath() = this
-    .map { "${it.key}=${it.value}" }
-    .joinToString(
-        prefix = "?",
-        separator = "&"
-    )
--- pass/autofill/impl/src/main/kotlin/proton/android/pass/autofill/ui/autofill/navigation/SelectItemGraph.kt
+++ pass/autofill/impl/src/main/kotlin/proton/android/pass/autofill/ui/autofill/navigation/SelectItemGraph.kt
@@ -5,9 +5,9 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.navigation.NavGraphBuilder
 import proton.android.pass.autofill.entities.AutofillAppState
 import proton.android.pass.autofill.entities.AutofillMappings
-import proton.android.pass.autofill.ui.autofill.CreateLogin
 import proton.android.pass.autofill.ui.autofill.SelectItem
 import proton.android.pass.autofill.ui.autofill.select.SelectItemScreen
+import proton.android.pass.featurecreateitem.impl.CreateLogin
 import proton.android.pass.navigation.api.AppNavigator
 import proton.android.pass.navigation.api.composable
 

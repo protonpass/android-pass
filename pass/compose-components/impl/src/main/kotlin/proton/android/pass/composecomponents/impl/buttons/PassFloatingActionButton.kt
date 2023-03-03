70b77043245cafa630fa20248a3453ebb53216c1
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-03-03 14:45:11 +0100

    feat: adapt fab colors

--- pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/buttons/PassFloatingActionButton.kt
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/buttons/PassFloatingActionButton.kt
@@ -5,12 +5,14 @@ import androidx.compose.material.Icon
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.compositeOver
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
-import me.proton.core.presentation.R
 import proton.android.pass.commonui.api.PassTheme
 import proton.android.pass.commonui.api.ThemePreviewProvider
+import proton.android.pass.composecomponents.impl.R
 
 @Composable
 fun PassFloatingActionButton(
@@ -19,11 +21,13 @@ fun PassFloatingActionButton(
 ) {
     FloatingActionButton(
         modifier = modifier,
+        backgroundColor = PassTheme.colors.accentPurpleWeakest.compositeOver(PassTheme.colors.backgroundNorm),
+        contentColor = PassTheme.colors.accentPurpleNorm,
         onClick = onClick
     ) {
         Icon(
-            painter = painterResource(R.drawable.ic_proton_plus),
-            contentDescription = null
+            painter = painterResource(me.proton.core.presentation.R.drawable.ic_proton_plus),
+            contentDescription = stringResource(R.string.add_item_icon_content_description),
         )
     }
 }

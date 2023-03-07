bccc78a73f3e6cc00d52fa72aff9538e3bdcf896
Carlos Quintana <carlos.quintana@proton.ch>
2023-03-07 09:28:14 +0100

    chore(profile): move settings to profile

--- pass/feature-profile/impl/src/main/kotlin/proton/android/pass/featureprofile/impl/ProfileGraph.kt
+++ pass/feature-profile/impl/src/main/kotlin/proton/android/pass/featureprofile/impl/ProfileGraph.kt
@@ -1,15 +1,14 @@
 package proton.android.pass.featureprofile.impl
 
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraphBuilder
 import proton.android.pass.composecomponents.impl.bottombar.BottomBar
 import proton.android.pass.composecomponents.impl.bottombar.BottomBarSelected
+import proton.android.pass.featuresettings.impl.SettingsScreen
 import proton.android.pass.navigation.api.NavItem
 import proton.android.pass.navigation.api.composable
 
@@ -19,6 +18,7 @@ object Profile : NavItem(baseRoute = "profile", isTopLevel = true)
 fun NavGraphBuilder.profileGraph(
     onListClick: () -> Unit,
     onCreateItemClick: () -> Unit,
+    onLogoutClick: () -> Unit
 ) {
     composable(Profile) {
         Scaffold(
@@ -32,9 +32,10 @@ fun NavGraphBuilder.profileGraph(
                 )
             }
         ) { padding ->
-            Column(modifier = Modifier.padding(padding)) {
-                Text("In development")
-            }
+            SettingsScreen(
+                modifier = Modifier.padding(padding),
+                onLogoutClick = onLogoutClick
+            )
         }
     }
 }

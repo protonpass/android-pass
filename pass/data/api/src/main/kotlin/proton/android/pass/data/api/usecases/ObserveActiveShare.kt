5f861344427f2406748d08a11eebb7d405be5521
Carlos Quintana <carlos.quintana@proton.ch>
2023-03-07 12:45:12 +0100

    refactor(data): refactor ObserveActiveShare to remove LoadingResult and return full share

--- pass/data/api/src/main/kotlin/proton/android/pass/data/api/usecases/ObserveActiveShare.kt
+++ pass/data/api/src/main/kotlin/proton/android/pass/data/api/usecases/ObserveActiveShare.kt
@@ -2,9 +2,8 @@ package proton.android.pass.data.api.usecases
 
 import kotlinx.coroutines.flow.Flow
 import me.proton.core.domain.entity.UserId
-import proton.android.pass.common.api.LoadingResult
-import proton.pass.domain.ShareId
+import proton.pass.domain.Share
 
 interface ObserveActiveShare {
-    operator fun invoke(userId: UserId? = null): Flow<LoadingResult<ShareId>>
+    operator fun invoke(userId: UserId? = null): Flow<Share>
 }

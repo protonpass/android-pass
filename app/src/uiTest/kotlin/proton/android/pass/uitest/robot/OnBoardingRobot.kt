f4d72c2822b5277a15fa0afba148e593e26bfb45
Neil Marietta <neil.marietta@protonmail.com>
2023-03-09 14:56:26 +0100

    feat: Added External Account Sign Up support.

---
+++ app/src/uiTest/kotlin/proton/android/pass/uitest/robot/OnBoardingRobot.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2023 Proton AG.
+ * This file is part of Proton Drive.
+ *
+ * Proton Drive is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Drive is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Drive.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.uitest.robot
+
+import me.proton.test.fusion.Fusion.node
+import proton.android.pass.featureonboarding.impl.OnBoardingScreenTestTag
+
+object OnBoardingRobot : Robot {
+
+    private val onBoardingScreen get() = node.withTag(OnBoardingScreenTestTag.screen)
+
+    fun onBoardingScreenDisplayed() {
+        onBoardingScreen.await { assertIsDisplayed() }
+    }
+}

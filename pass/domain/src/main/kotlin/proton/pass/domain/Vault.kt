23440dc55908309f0fdf865469351faa20288fad
Carlos Quintana <carlos.quintana@proton.ch>
2023-03-09 10:10:33 +0100

    chore(domain): add share icon and color to Share

--- pass/domain/src/main/kotlin/proton/pass/domain/Vault.kt
+++ pass/domain/src/main/kotlin/proton/pass/domain/Vault.kt
@@ -3,4 +3,6 @@ package proton.pass.domain
 data class Vault(
     val shareId: ShareId,
     val name: String,
+    val color: ShareColor = ShareColor.Color1,
+    val icon: ShareIcon = ShareIcon.Icon1
 )

baf644ae2cafe082af8d64e397cafebd22f6b01d
Carlos Quintana <carlos.quintana@proton.ch>
2023-03-09 10:15:05 +0100

    feat(app): add create vault bottomsheet to appgraph and allow to dismiss bottomsheet from appGraph

--- pass/common-ui-models/api/src/main/kotlin/proton/android/pass/commonuimodels/api/ShareUiModel.kt
+++ pass/common-ui-models/api/src/main/kotlin/proton/android/pass/commonuimodels/api/ShareUiModel.kt
@@ -1,15 +1,21 @@
 package proton.android.pass.commonuimodels.api
 
+import proton.pass.domain.ShareColor
+import proton.pass.domain.ShareIcon
 import proton.pass.domain.ShareId
 
 data class ShareUiModel(
     val id: ShareId,
-    val name: String
+    val name: String,
+    val color: ShareColor,
+    val icon: ShareIcon
 )
 
 data class ShareUiModelWithItemCount(
     val id: ShareId,
     val name: String,
     val activeItemCount: Long,
-    val trashedItemCount: Long
+    val trashedItemCount: Long,
+    val color: ShareColor,
+    val icon: ShareIcon
 )

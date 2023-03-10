d6843fa33136ec65f458ad769fe48898b966b12a
Carlos Quintana <carlos.quintana@proton.ch>
2023-03-10 08:53:36 +0100

    chore: remove vault management from dev drawer

--- pass/feature-vault/impl/src/main/kotlin/proton/android/pass/featurevault/impl/IsVaultCreated.kt
+++
@@ -1,9 +0,0 @@
-package proton.android.pass.featurevault.impl
-
-import androidx.compose.runtime.Stable
-
-@Stable
-sealed interface VaultSavedState {
-    object Unknown : VaultSavedState
-    object Success : VaultSavedState
-}

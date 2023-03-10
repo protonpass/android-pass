0bb22c0eecd986ea6ebc52800c797a57a441f729
Carlos Quintana <carlos.quintana@proton.ch>
2023-03-10 12:36:25 +0100

    chore: not use ActiveVaultIcon in Home

---
+++ pass/crypto/api/src/main/kotlin/proton/android/pass/crypto/api/extensions/ShareToVault.kt
@@ -0,0 +1,17 @@
+package proton.android.pass.crypto.api.extensions
+
+import proton.android.pass.common.api.Option
+import proton.android.pass.crypto.api.context.EncryptionContextProvider
+import proton.pass.domain.Share
+import proton.pass.domain.Vault
+import proton_pass_vault_v1.VaultV1
+
+fun Share.toVault(encryptionContextProvider: EncryptionContextProvider): Option<Vault> = content
+    .map {
+        val decrypted = encryptionContextProvider.withEncryptionContext {
+            decrypt(it)
+        }
+        val parsed = VaultV1.Vault.parseFrom(decrypted)
+        Vault(id, parsed.name, color, icon)
+    }
+

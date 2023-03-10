258662a32c8e8696ab683e666a30bf43001ff741
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-03-10 09:30:36 +0100

    feat: search toolbar new ui

---
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/topbar/iconbutton/BackArrowCircleIconButton.kt
@@ -0,0 +1,29 @@
+package proton.android.pass.composecomponents.impl.topbar.iconbutton
+
+import androidx.compose.material.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import proton.android.pass.composecomponents.impl.R
+import proton.android.pass.composecomponents.impl.container.Circle
+
+@Composable
+fun BackArrowCircleIconButton(
+    modifier: Modifier = Modifier,
+    color: Color,
+    onUpClick: () -> Unit
+) {
+    Circle(
+        modifier = modifier,
+        backgroundColor = color,
+        onClick = { onUpClick() }
+    ) {
+        Icon(
+            painter = painterResource(me.proton.core.presentation.R.drawable.ic_arrow_back),
+            contentDescription = stringResource(R.string.navigate_back_icon_content_description),
+            tint = color
+        )
+    }
+}

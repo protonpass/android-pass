16b776497089c11958328d85fb379be168aca9bf
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-03-12 18:12:33 +0100

    feat: add basic baseline profile

---
+++ benchmark/build.gradle.kts
@@ -0,0 +1,56 @@
+@file:Suppress("UnstableApiUsage")
+
+plugins {
+    id("com.android.test")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "proton.android.pass.benchmark"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        missingDimensionStrategy("version", "play")
+        missingDimensionStrategy("env", "prod")
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        create("benchmark") {
+            isDebuggable = true
+            signingConfig = getByName("debug").signingConfig
+            matchingFallbacks += listOf("release")
+        }
+    }
+
+    targetProjectPath = ":app"
+    experimentalProperties["android.experimental.self-instrumenting"] = true
+
+    testOptions {
+        managedDevices {
+            devices {
+                maybeCreate<com.android.build.api.dsl.ManagedVirtualDevice>("pixel2api31").apply {
+                    device = "Pixel 2"
+                    apiLevel = 31
+                    systemImageSource = "aosp"
+                }
+            }
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.test.espresso.core)
+    implementation(libs.androidx.test.junit)
+    implementation(libs.androidx.test.uiautomator)
+    implementation(libs.androidx.benchmark)
+}
+
+androidComponents {
+    beforeVariants(selector().all()) {
+        it.enabled = it.buildType == "benchmark"
+    }
+}

57c8355491f237f1056fe06490628ff5e435f365
Carlos Quintana <carlos.quintana@proton.ch>
2023-03-13 09:26:22 +0100

    fix(vault): handle errors in vault fetching

--- pass/feature-vault/impl/src/main/kotlin/proton/android/pass/featurevault/impl/VaultSnackbarMessage.kt
+++ pass/feature-vault/impl/src/main/kotlin/proton/android/pass/featurevault/impl/VaultSnackbarMessage.kt
@@ -15,4 +15,5 @@ enum class VaultSnackbarMessage(
     CreateVaultSuccess(R.string.snackbar_create_vault_success, SnackbarType.SUCCESS),
     CannotCreateMoreVaultsError(R.string.snackbar_cannot_create_more_vaults_error, SnackbarType.ERROR),
     CreateVaultError(R.string.snackbar_create_vault_error, SnackbarType.ERROR),
+    CannotRetrieveVaultError(R.string.snackbar_retrieve_vault_error, SnackbarType.ERROR)
 }

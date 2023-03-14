2b1d19ab194ef7b4bad687d1be52af6f1d187cf1
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-03-14 08:13:42 +0100

    fix: set login detail website max lines

--- pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/login/WebsiteSection.kt
+++ pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/login/WebsiteSection.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
@@ -67,7 +68,9 @@ fun WebsiteSection(
                         text = website,
                         color = PassTheme.colors.accentPurpleOpaque,
                         fontSize = 16.sp,
-                        fontWeight = FontWeight.W400
+                        fontWeight = FontWeight.W400,
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis
                     )
                 }
             }

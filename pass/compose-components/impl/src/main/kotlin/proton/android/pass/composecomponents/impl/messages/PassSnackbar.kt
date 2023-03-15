d249bb86e95eff88f0198ba47f03647537ec5ac8
Carlos Quintana <carlos.quintana@proton.ch>
2023-03-15 12:45:30 +0100

    feat(compose-components): add dismiss button to snackbar

--- pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/messages/PassSnackbar.kt
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/messages/PassSnackbar.kt
@@ -1,21 +1,70 @@
 package proton.android.pass.composecomponents.impl.messages
 
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Snackbar
+import androidx.compose.material.SnackbarData
 import androidx.compose.material.SnackbarDuration
 import androidx.compose.material.SnackbarHostState
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import proton.android.pass.notifications.api.SnackbarType
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import me.proton.core.compose.component.ProtonSnackbarHost
 import me.proton.core.compose.component.ProtonSnackbarHostState
 import me.proton.core.compose.component.ProtonSnackbarType
+import me.proton.core.compose.theme.ProtonTheme
+import proton.android.pass.commonui.api.PassTheme
+import proton.android.pass.commonui.api.ThemePairPreviewProvider
+import proton.android.pass.notifications.api.SnackbarType
+
+@Composable
+fun PassSnackbar(
+    modifier: Modifier = Modifier,
+    type: ProtonSnackbarType,
+    snackbarData: SnackbarData
+) {
+    Snackbar(
+        modifier = modifier,
+        content = { Text(snackbarData.message) },
+        action = {
+            IconButton(
+                onClick = { snackbarData.performAction() },
+                content = {
+                    Icon(
+                        painter = painterResource(me.proton.core.presentation.R.drawable.ic_proton_cross_small),
+                        contentDescription = null,
+                        tint = ProtonTheme.colors.textInverted
+                    )
+                }
+            )
+        },
+        actionOnNewLine = false,
+        shape = ProtonTheme.shapes.medium,
+        backgroundColor = when (type) {
+            ProtonSnackbarType.SUCCESS -> ProtonTheme.colors.notificationSuccess
+            ProtonSnackbarType.WARNING -> ProtonTheme.colors.notificationWarning
+            ProtonSnackbarType.ERROR -> ProtonTheme.colors.notificationError
+            ProtonSnackbarType.NORM -> ProtonTheme.colors.notificationNorm
+        },
+        contentColor = ProtonTheme.colors.textInverted,
+        elevation = 6.dp,
+    )
+}
+
 
 @Composable
 fun PassSnackbarHost(modifier: Modifier = Modifier, snackbarHostState: PassSnackbarHostState) {
     ProtonSnackbarHost(
         modifier = modifier,
-        hostState = snackbarHostState.protonSnackbarHostState
+        hostState = snackbarHostState.protonSnackbarHostState,
+        snackbar = { PassSnackbar(type = snackbarHostState.protonSnackbarHostState.type, snackbarData = it) }
     )
 }
 
@@ -46,4 +95,30 @@ class PassSnackbarHostState(
         actionLabel = actionLabel,
         duration = duration
     )
+}
+
+private val previewSnackbarData = object : SnackbarData {
+    override val actionLabel: String? = null
+    override val duration: SnackbarDuration = SnackbarDuration.Indefinite
+    override val message: String = "This is a snackbar"
+    override fun dismiss() = Unit
+    override fun performAction() = Unit
+}
+
+class ThemeSnackbarPreviewProvider :
+    ThemePairPreviewProvider<ProtonSnackbarType>(SnackbarTypePreviewProvider())
+
+@Preview
+@Composable
+fun PassSnackbarPreview(
+    @PreviewParameter(ThemeSnackbarPreviewProvider::class) input: Pair<Boolean, ProtonSnackbarType>
+) {
+    PassTheme(isDark = input.first) {
+        Surface {
+            PassSnackbar(
+                type = input.second,
+                snackbarData = previewSnackbarData
+            )
+        }
+    }
 }

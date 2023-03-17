e5ee658580c0a86b0561de5f8df27b15ffc3def4
Carlos Quintana <carlos.quintana@proton.ch>
2023-03-17 12:36:27 +0100

    feat(home): display trash icon when trash is selected

--- pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/icon/TrashVaultIcon.kt
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/icon/TrashVaultIcon.kt
@@ -1,17 +1,39 @@
 package proton.android.pass.composecomponents.impl.icon
 
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import proton.android.pass.commonui.api.PassTheme
+import proton.android.pass.commonui.api.ThemePreviewProvider
 
 @Composable
 fun TrashVaultIcon(
     modifier: Modifier = Modifier,
+    size: Int = 40,
+    iconSize: Int = 20,
+    onClick: (() -> Unit)? = null
 ) {
     VaultIcon(
         modifier = modifier,
         backgroundColor = PassTheme.colors.textDisabled,
         iconColor = PassTheme.colors.textWeak,
-        icon = me.proton.core.presentation.R.drawable.ic_proton_trash
+        iconSize = iconSize,
+        size = size,
+        icon = me.proton.core.presentation.R.drawable.ic_proton_trash,
+        onClick = onClick
     )
+}
+
+@Preview
+@Composable
+fun TrashVaultIconPreview(
+    @PreviewParameter(ThemePreviewProvider::class) isDark: Boolean
+) {
+    PassTheme(isDark = isDark) {
+        Surface {
+            TrashVaultIcon()
+        }
+    }
 }

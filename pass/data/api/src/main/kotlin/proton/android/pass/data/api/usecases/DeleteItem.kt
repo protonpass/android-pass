e2db9e1c3ed4ff1d4f6d282b4e59fd6bae964414
Carlos Quintana <carlos.quintana@proton.ch>
2023-03-20 09:49:10 +0100

    refactor(data): move trash use-cases to suspend instead of flow

--- pass/data/api/src/main/kotlin/proton/android/pass/data/api/usecases/DeleteItem.kt
+++ pass/data/api/src/main/kotlin/proton/android/pass/data/api/usecases/DeleteItem.kt
@@ -1,10 +1,9 @@
 package proton.android.pass.data.api.usecases
 
-import kotlinx.coroutines.flow.Flow
 import me.proton.core.domain.entity.UserId
 import proton.pass.domain.ItemId
 import proton.pass.domain.ShareId
 
 interface DeleteItem {
-    operator fun invoke(userId: UserId? = null, shareId: ShareId, itemId: ItemId): Flow<Unit>
+    suspend operator fun invoke(userId: UserId? = null, shareId: ShareId, itemId: ItemId)
 }

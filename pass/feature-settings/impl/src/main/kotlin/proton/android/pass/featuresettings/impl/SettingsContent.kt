7532f4b246ee0121f37b98db760357f3917183e5
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-03-21 11:14:32 +0100

    feat: add settings design

--- pass/feature-settings/impl/src/main/kotlin/proton/android/pass/featuresettings/impl/SettingsContent.kt
+++ pass/feature-settings/impl/src/main/kotlin/proton/android/pass/featuresettings/impl/SettingsContent.kt
@@ -1,82 +1,81 @@
 package proton.android.pass.featuresettings.impl
 
-import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Scaffold
-import androidx.compose.material.ScaffoldState
-import androidx.compose.material.rememberModalBottomSheetState
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import kotlinx.coroutines.launch
+import androidx.compose.ui.unit.dp
 import me.proton.core.compose.component.appbar.ProtonTopAppBar
-import proton.android.pass.composecomponents.impl.bottomsheet.PassModalBottomSheetLayout
+import proton.android.pass.commonui.api.PassTheme
+import proton.android.pass.commonui.api.PassTypography
 import proton.android.pass.composecomponents.impl.loading.LoadingDialog
-import proton.android.pass.composecomponents.impl.topbar.TopBarTitleView
+import proton.android.pass.composecomponents.impl.topbar.iconbutton.BackArrowCircleIconButton
 import proton.android.pass.composecomponents.impl.uievents.IsLoadingState
-import proton.android.pass.preferences.ThemePreference
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SettingsContent(
     modifier: Modifier = Modifier,
-    scaffoldState: ScaffoldState = rememberScaffoldState(),
     state: SettingsUiState,
-    onThemeChange: (ThemePreference) -> Unit,
-    onCopyToClipboardChange: (Boolean) -> Unit,
+    onSelectThemeClick: () -> Unit,
     onForceSyncClick: () -> Unit,
     onAppVersionClick: (String) -> Unit,
     onReportProblemClick: () -> Unit,
-    onLogoutClick: () -> Unit
+    onLogoutClick: () -> Unit,
+    onPrivacyClick: () -> Unit,
+    onTermsClick: () -> Unit,
+    onUpClick: () -> Unit
 ) {
-    val bottomSheetState = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
-    val scope = rememberCoroutineScope()
-
-    BackHandler(
-        enabled = bottomSheetState.isVisible
-    ) {
-        scope.launch { bottomSheetState.hide() }
-    }
-
-    PassModalBottomSheetLayout(
-        sheetState = bottomSheetState,
-        sheetContent = {
-            ThemeSelectionBottomSheetContents(
-                onThemeSelected = { theme ->
-                    scope.launch {
-                        bottomSheetState.hide()
-                        onThemeChange(theme)
-                    }
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            ProtonTopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(R.string.title_settings),
+                        style = PassTypography.hero
+                    )
+                },
+                navigationIcon = {
+                    BackArrowCircleIconButton(
+                        modifier = modifier.padding(12.dp, 4.dp),
+                        color = PassTheme.colors.accentPurpleOpaque,
+                        onUpClick = onUpClick
+                    )
                 }
             )
         }
-    ) {
-        Scaffold(
-            scaffoldState = scaffoldState,
-            topBar = {
-                ProtonTopAppBar(
-                    title = {
-                        TopBarTitleView(
-                            title = stringResource(id = R.string.title_settings)
-                        )
-                    }
-                )
-            }
-        ) { contentPadding ->
-            if (state.isLoadingState == IsLoadingState.Loading) {
-                LoadingDialog()
-            }
-            Settings(
-                modifier = modifier.padding(contentPadding),
-                state = state,
-                onCopyToClipboardChange = onCopyToClipboardChange,
-                onForceSyncClick = onForceSyncClick,
+    ) { contentPadding ->
+        if (state.isLoadingState == IsLoadingState.Loading) {
+            LoadingDialog()
+        }
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .padding(contentPadding)
+                .padding(16.dp)
+        ) {
+            PreferencesSection(
+                theme = state.themePreference,
+                onSelectThemeClick = onSelectThemeClick,
+                onClipboardClick = { }
+            )
+            AboutSection(
+                onPrivacyClick = onPrivacyClick,
+                onTermsClick = onTermsClick
+            )
+            ApplicationSection(onViewLogsClick = {}, onForceSyncClick = onForceSyncClick)
+            AppSection(
+                appVersion = state.appVersion,
                 onAppVersionClick = onAppVersionClick,
-                onReportProblemClick = onReportProblemClick,
+                onReportProblemClick = onReportProblemClick
+            )
+            AccountSection(
+                currentAccount = state.currentAccount,
                 onLogoutClick = onLogoutClick
             )
         }

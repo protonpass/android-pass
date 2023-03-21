f130da69b5b777827efba0c0076b9ec2002b7788
Carlos Quintana <carlos.quintana@proton.ch>
2023-03-21 16:44:22 +0100

    feat(telemetry): add telemetry module

---
+++ pass/telemetry/impl/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    namespace = "proton.android.pass.telemetry.impl"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+    }
+}
+dependencies {
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.core.utilKotlin)
+
+    implementation(libs.dagger.hilt.android)
+    kapt(libs.dagger.hilt.android.compiler)
+    kapt(libs.androidx.hilt.compiler)
+
+    implementation(projects.pass.data.api)
+    implementation(projects.pass.log.api)
+    implementation(projects.pass.telemetry.api)
+
+    testImplementation(libs.core.test.kotlin)
+    testImplementation(libs.junit)
+    testImplementation(libs.kotlinTest)
+    testImplementation(libs.truth)
+    testImplementation(libs.turbine)
+
+    testImplementation(projects.pass.commonTest)
+    testImplementation(projects.pass.data.fakes)
+}

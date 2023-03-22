6fbed2166ea5eb051bb15cb1b5c923d18c034d58
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-03-22 09:57:19 +0100

    fix: revert wrong removal of sync initializer code

--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/sync/SyncInitializer.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/sync/SyncInitializer.kt
@@ -4,12 +4,19 @@ import android.content.Context
 import androidx.startup.Initializer
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
+import dagger.hilt.android.EntryPointAccessors
 import dagger.hilt.components.SingletonComponent
 
 class SyncInitializer : Initializer<Unit> {
 
     override fun create(context: Context) {
-
+        EntryPointAccessors
+            .fromApplication(
+                context.applicationContext,
+                SyncInitializerEntryPoint::class.java
+            )
+            .syncManager()
+            .start()
     }
 
     override fun dependencies(): List<Class<out Initializer<*>>> = listOf()

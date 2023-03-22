932a70b6228e3f3d03725a01fc001013bd4925a3
Carlos Quintana <carlos.quintana@proton.ch>
2023-03-22 14:15:46 +0100

    feat(home): emit telemetry events

--- pass/telemetry/api/src/main/kotlin/proton/android/pass/telemetry/api/TelemetryEvent.kt
+++ pass/telemetry/api/src/main/kotlin/proton/android/pass/telemetry/api/TelemetryEvent.kt
@@ -1,10 +1,21 @@
 package proton.android.pass.telemetry.api
 
+import proton.pass.domain.ItemType
+
 enum class EventItemType(val itemTypeName: String) {
     Login("login"),
     Note("note"),
     Alias("alias"),
-    Password("password")
+    Password("password");
+
+    companion object {
+        fun from(itemType: ItemType): EventItemType = when (itemType) {
+            ItemType.Password -> Password
+            is ItemType.Alias -> Alias
+            is ItemType.Note -> Note
+            is ItemType.Login -> Login
+        }
+    }
 }
 
 @Suppress("UnnecessaryAbstractClass")

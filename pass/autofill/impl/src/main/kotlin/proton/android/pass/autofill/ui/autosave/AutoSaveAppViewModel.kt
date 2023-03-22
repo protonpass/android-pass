e70516beb932b5ae3125f0201e6dcc30091554b4
Carlos Quintana <carlos.quintana@proton.ch>
2023-03-22 12:15:42 +0100

    feat(telemetry): autosave events

--- pass/autofill/impl/src/main/kotlin/proton/android/pass/autofill/ui/autosave/AutoSaveAppViewModel.kt
+++ pass/autofill/impl/src/main/kotlin/proton/android/pass/autofill/ui/autosave/AutoSaveAppViewModel.kt
@@ -7,13 +7,21 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.runBlocking
+import proton.android.pass.autofill.AutosaveDisplay
+import proton.android.pass.autofill.AutosaveDone
 import proton.android.pass.preferences.UserPreferencesRepository
+import proton.android.pass.telemetry.api.TelemetryManager
 import javax.inject.Inject
 
 @HiltViewModel
 class AutoSaveAppViewModel @Inject constructor(
-    private val preferenceRepository: UserPreferencesRepository
+    private val preferenceRepository: UserPreferencesRepository,
+    private val telemetryManager: TelemetryManager
 ) : ViewModel() {
+
+    init {
+        telemetryManager.sendEvent(AutosaveDisplay)
+    }
 
     val state = preferenceRepository.getThemePreference()
         .stateIn(
@@ -21,4 +29,8 @@ class AutoSaveAppViewModel @Inject constructor(
             started = SharingStarted.WhileSubscribed(5_000),
             initialValue = runBlocking { preferenceRepository.getThemePreference().first() }
         )
+
+    fun onItemAutoSaved() {
+        telemetryManager.sendEvent(AutosaveDone)
+    }
 }

78bfe8c189b68b833c9e4b78fe431bb82e4a0308
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-03-23 12:28:03 +0100

    fix: fix vault selector chevron color and alignment

--- pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/form/VaultSelector.kt
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/form/VaultSelector.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import me.proton.core.compose.theme.ProtonTheme
 import proton.android.pass.commonui.api.PassTheme
 import proton.android.pass.commonui.api.PassTypography
 import proton.android.pass.composecomponents.impl.R
@@ -59,9 +61,12 @@ fun VaultSelector(
                 color = PassTheme.colors.textNorm,
             )
         }
-        Icon(
-            painter = painterResource(me.proton.core.presentation.R.drawable.ic_proton_chevron_down),
-            contentDescription = null
-        )
+        IconButton(onClick = onVaultClicked) {
+            Icon(
+                painter = painterResource(me.proton.core.presentation.R.drawable.ic_proton_chevron_down),
+                contentDescription = null,
+                tint = ProtonTheme.colors.iconWeak
+            )
+        }
     }
 }

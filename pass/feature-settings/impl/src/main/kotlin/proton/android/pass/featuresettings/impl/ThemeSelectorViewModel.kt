3f9505df31b8d439d552f7aa9aa7ea72370f08fb
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-03-23 09:38:12 +0100

    feat: add theme selector design

--- pass/feature-settings/impl/src/main/kotlin/proton/android/pass/featuresettings/impl/ThemeSelectorViewModel.kt
+++ pass/feature-settings/impl/src/main/kotlin/proton/android/pass/featuresettings/impl/ThemeSelectorViewModel.kt
@@ -3,7 +3,11 @@ package proton.android.pass.featuresettings.impl
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import proton.android.pass.featuresettings.impl.SettingsSnackbarMessage.ErrorPerformingOperation
 import proton.android.pass.log.api.PassLogger
 import proton.android.pass.notifications.api.SnackbarMessageRepository
@@ -16,6 +20,13 @@ class ThemeSelectorViewModel @Inject constructor(
     private val preferencesRepository: UserPreferencesRepository,
     private val snackbarMessageRepository: SnackbarMessageRepository
 ) : ViewModel() {
+
+    val state = preferencesRepository.getThemePreference()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000L),
+            initialValue = runBlocking { preferencesRepository.getThemePreference().first() }
+        )
 
     fun onThemePreferenceChange(theme: ThemePreference) = viewModelScope.launch {
         PassLogger.d(TAG, "Changing theme to $theme")

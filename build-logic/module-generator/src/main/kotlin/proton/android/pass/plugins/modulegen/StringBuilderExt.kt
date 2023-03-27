70fc58bc0dee83b0d6e72d0d455049b28cf67a08
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-03-27 11:18:50 +0200

    refactor: Cleanup module generator task

--- build-logic/module-generator/src/main/kotlin/proton/android/pass/plugins/modulegen/StringBuilderExt.kt
+++ build-logic/module-generator/src/main/kotlin/proton/android/pass/plugins/modulegen/StringBuilderExt.kt
@@ -1,66 +1,36 @@
 package proton.android.pass.plugins.modulegen
 
-
-fun StringBuilder.appendConfiguration(configuration: Configuration): StringBuilder =
-    when (configuration) {
-        Configuration.API -> appendJvmPlugin()
-        Configuration.IMPL -> appendAndroidLibraryPlugin().appendLibraryDependency()
-        Configuration.FAKES -> appendAndroidLibraryPlugin().appendLibraryDependency()
-    }
-
-private fun StringBuilder.appendJvmPlugin(): StringBuilder = append(
+fun StringBuilder.appendJvmPlugin(): StringBuilder = append(
     """
-    plugins {
-        id("org.jetbrains.kotlin.jvm")
-    }
-    
-""".trimIndent()
+        plugins {
+            id("org.jetbrains.kotlin.jvm")
+        }   
+    """.trimIndent()
 )
 
-private fun StringBuilder.appendAndroidLibraryPlugin(): StringBuilder = append(
+fun StringBuilder.appendAndroidLibraryPlugin(namespace: String): StringBuilder = append(
     """
-    plugins {
-        id("com.android.library")
-        id("org.jetbrains.kotlin.android")
-    }
-    
-    android {
-        namespace = "&s2"
-        compileSdk = libs.versions.compileSdk.get().toInt()
-
-        defaultConfig {
-            minSdk = libs.versions.minSdk.get().toInt()
-            targetSdk = libs.versions.targetSdk.get().toInt()
+        plugins {
+            id("com.android.library")
+            id("org.jetbrains.kotlin.android")
         }
-    }
-    
-""".trimIndent()
+        
+        android {
+            namespace = "$namespace"
+            compileSdk = libs.versions.compileSdk.get().toInt()
+            
+            defaultConfig {
+                minSdk = libs.versions.minSdk.get().toInt()
+                targetSdk = libs.versions.targetSdk.get().toInt()
+            }
+        }
+    """.trimIndent()
 )
 
-private fun StringBuilder.appendLibraryDependency(): StringBuilder = append(
+fun StringBuilder.appendLibraryDependency(projectAccessor: String): StringBuilder = append(
     """
-    dependencies {
-        implementation(projects.&s1.api)
-    }
-    
-""".trimIndent()
+        dependencies {
+            implementation(projects.$projectAccessor.api)
+        }
+    """.trimIndent()
 )
-
-fun String.convertToProjectAccessor(): String {
-    val builder = StringBuilder()
-    var shouldCapitalizeNext = false
-    for (c in this) {
-        if (c == '-') {
-            shouldCapitalizeNext = true
-            continue
-        }
-
-        if (shouldCapitalizeNext) {
-            builder.append(c.uppercase())
-            shouldCapitalizeNext = false
-        } else {
-            builder.append(c)
-        }
-    }
-    return builder.toString()
-}

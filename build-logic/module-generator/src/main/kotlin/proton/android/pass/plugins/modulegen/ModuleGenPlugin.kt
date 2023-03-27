498bccea50f71125dcc8ed69890f76b5324f5cd4
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-03-27 11:18:50 +0200

    refactor: Cleanup module generator task

--- build-logic/module-generator/src/main/kotlin/proton/android/pass/plugins/modulegen/ModuleGenPlugin.kt
+++ build-logic/module-generator/src/main/kotlin/proton/android/pass/plugins/modulegen/ModuleGenPlugin.kt
@@ -5,6 +5,9 @@ import org.gradle.api.Project
 
 class ModuleGenPlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        target.tasks.register("genModule", ModuleGenTask::class.java)
+        target.tasks.register("generateModules", ModuleGenTask::class.java) {
+            description = "Task to auto-generate modules following project architecture."
+            group = "Android"
+        }
     }
 }
--- build-logic/module-generator/src/main/kotlin/proton/android/pass/plugins/modulegen/ModuleGenTask.kt
+++ build-logic/module-generator/src/main/kotlin/proton/android/pass/plugins/modulegen/ModuleGenTask.kt
@@ -6,159 +6,152 @@ import org.gradle.api.Project
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.options.Option
+import java.io.File
 
 open class ModuleGenTask : DefaultTask() {
 
-    private var moduleInput: String? = null
-    private var configurationInput: String? = null
-
+    @Input
     @Option(
         option = "module",
         description = "Module path to be generated"
     )
-    open fun setModule(moduleInput: String?) {
-        this.moduleInput = moduleInput
-    }
+    lateinit var moduleInput: String
 
+    @Input
     @Option(
         option = "conf",
-        description = "Configurations to be generated. api,impl,fakes"
+        description = "Configurations to be generated. api, impl, fakes"
     )
-    open fun setConf(configurationInput: String?) {
-        this.configurationInput = configurationInput
-    }
+    lateinit var configurationInput: String
 
-    @Input
-    open fun getModule(): String? = moduleInput
-
-    @Input
-    open fun getConf(): String? = configurationInput
-
-    @Suppress("ThrowsCount")
     @TaskAction
     fun generate() {
-        val module = moduleInput ?: throw GradleException("Module input cannot be null")
-        val configurationList =
-            configurationInput ?: throw GradleException("Configurations input cannot be null")
-
-        val moduleRegex = "[^-a-z]".toRegex()
-        val moduleList: List<String> = sanitizeModuleInput(module, moduleRegex)
-        val detectedConfList: List<Configuration> = detectConfigurations(configurationList)
+        val moduleList: List<String> = sanitizeModuleInput(moduleInput)
         if (moduleList.isEmpty()) {
-            throw GradleException("Detected an empty module path")
+            throw GradleException("Invalid module path: $moduleInput")
         }
+        val detectedConfList: Set<Configuration> = detectConfigurations(configurationInput)
         if (detectedConfList.isEmpty()) {
-            throw GradleException("Couldn't detect a proper configuration")
+            throw GradleException("Invalid configuration: $configurationInput")
         }
 
         logger.lifecycle("Sanitized module path: :${moduleList.joinToString(":")}")
         logger.lifecycle("Detected configurations: $detectedConfList")
 
         with(project) {
-            generateDirs(moduleList, detectedConfList)
+            generateDirs(moduleList, detectedConfList, ROOT_PACKAGE_NAME)
             generateManifest(moduleList, detectedConfList)
-            generateBuildGradle(moduleList, detectedConfList)
+            generateBuildGradle(moduleList, detectedConfList, ROOT_PACKAGE_NAME)
             generateModuleSettings(moduleList, detectedConfList)
         }
     }
 
+    private fun sanitizeModuleInput(path: String): List<String> {
+        require(path.isNotBlank()) { "Module path is blank" }
+        val moduleRegex = "[^-a-z]".toRegex()
+        return path.split(":")
+            .map { segment -> moduleRegex.replace(segment.lowercase(), "") }
+            .filter(String::isNotEmpty)
+    }
+
+    private fun detectConfigurations(input: String): Set<Configuration> = input
+        .split(",")
+        .map { conf -> Configuration.valueOf(conf.uppercase()) }
+        .toSet()
+
     private fun Project.generateManifest(
         modulePath: List<String>,
-        detectedConfList: List<Configuration>
+        configurationSet: Set<Configuration>
     ) {
-        detectedConfList.forEach { conf ->
-            when (conf) {
-                Configuration.IMPL,
-                Configuration.FAKES -> {
-                    val lcConfiguration = conf.name.lowercase()
-                    val dir = modulePath.joinToString("/")
-                    val manifestFile = "AndroidManifest.xml"
-                    val manifestContent = """
-                        <?xml version="1.0" encoding="utf-8"?>
-                        <manifest/>
-                        
-                    """.trimIndent()
-                    file("$dir/$lcConfiguration/src/main/$manifestFile").writeText(manifestContent)
-                }
-                else -> Unit
+        require(modulePath.isEmpty()) { "Module path is empty" }
+        if (configurationSet.none { it == Configuration.IMPL || it == Configuration.FAKES }) {
+            return
+        }
+        val manifestTemplate = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<manifest/>\n"
+        configurationSet.forEach { conf ->
+            val lcConfiguration = conf.name.lowercase()
+            val dir = modulePath.joinToString(File.separator)
+            val manifestFilePath = "$dir/$lcConfiguration/src/main/AndroidManifest.xml"
+            file(manifestFilePath).writeText(manifestTemplate)
+        }
+    }
+
+    private fun Project.generateDirs(
+        modulePath: List<String>,
+        configurationSet: Set<Configuration>,
+        rootPackageName: String
+    ) {
+        val modulePathString = modulePath.joinToString(File.separator)
+        val subPackage = modulePath.joinToString(".")
+        val rootPackagePath = rootPackageName.replace('.', '/')
+        for (configuration in configurationSet) {
+            val lcConfiguration = configuration.name.lowercase()
+            val configurationPath = "$rootPackagePath/$subPackage/$lcConfiguration".replace("-", "")
+            mkdir("$modulePathString/$lcConfiguration/src/main/kotlin/$configurationPath")
+            if (configuration == Configuration.IMPL) {
+                mkdir("$modulePathString/$lcConfiguration/src/androidTest/kotlin/$configurationPath")
+                mkdir("$modulePathString/$lcConfiguration/src/test/kotlin/$configurationPath")
             }
         }
     }
 
-    private fun sanitizeModuleInput(path: String, regex: Regex): List<String> = path.split(":")
-        .map { segment -> regex.replace(segment.lowercase(), "") }
-        .filter(String::isNotEmpty)
-
-    private fun detectConfigurations(input: String): List<Configuration> = input
-        .split(",")
-        .map { conf -> Configuration.valueOf(conf.uppercase()) }
-
-    private fun Project.generateDirs(
-        modulePath: List<String>,
-        configurationList: List<Configuration>
-    ) {
-        val dir = modulePath.joinToString("/")
-        val subpackage = modulePath.joinToString(".")
-        configurationList
-            .forEach { configuration ->
-                val lcConfiguration = configuration.name.lowercase()
-                val configurationPath = "$ROOT_PACKAGE_NAME.$subpackage.$lcConfiguration"
-                    .replace('.', '/')
-                    .replace("-", "")
-                mkdir("$dir/$lcConfiguration/src/main/kotlin/$configurationPath")
-                if (configuration == Configuration.IMPL) {
-                    mkdir("$dir/$lcConfiguration/src/androidTest/kotlin/$configurationPath")
-                    mkdir("$dir/$lcConfiguration/src/test/kotlin/$configurationPath")
-                }
-            }
-    }
-
     private fun Project.generateBuildGradle(
         moduleList: List<String>,
-        configurationList: List<Configuration>
+        configurationSet: Set<Configuration>,
+        rootPackageName: String
     ) {
-        val dir = moduleList.joinToString("/")
+        val dir = moduleList.joinToString(File.separator)
         val subpackage = moduleList.joinToString(".")
         val subpackageWithoutHyphens = subpackage.replace("-", "")
         val asProjectAccessor = subpackage.convertToProjectAccessor()
-        configurationList
-            .map { configuration ->
-                val lcConfiguration = configuration.name.lowercase()
-                val stringBuilder = StringBuilder()
-                stringBuilder.appendConfiguration(configuration)
-                configuration to stringBuilder.toString()
-                    .replace("&s1", asProjectAccessor)
-                    .replace("&s2", "$ROOT_PACKAGE_NAME.$subpackageWithoutHyphens.$lcConfiguration")
+        val namespace = "$rootPackageName.$subpackageWithoutHyphens"
+
+        for (configuration in configurationSet) {
+            val lcConfiguration = configuration.name.lowercase()
+
+            val configString = when (configuration) {
+                Configuration.API -> buildString {
+                    appendJvmPlugin()
+                    appendLine()
+                }
+                Configuration.IMPL,
+                Configuration.FAKES -> buildString {
+                    appendAndroidLibraryPlugin(namespace)
+                    appendLine()
+                    appendLine()
+                    appendLibraryDependency(asProjectAccessor)
+                    appendLine()
+                }
             }
-            .forEach { pair ->
-                file("$dir/${pair.first.name.lowercase()}/build.gradle.kts")
-                    .writeText(pair.second)
-            }
+
+            file("$dir/${lcConfiguration}/build.gradle.kts").writeText(configString)
+        }
     }
+
+    private fun String.convertToProjectAccessor(): String =
+        replace(Regex("-([a-z])")) { it.groupValues[1].uppercase() }
 
     private fun Project.generateModuleSettings(
         moduleList: List<String>,
-        configurationList: List<Configuration>
+        configurationSet: Set<Configuration>
     ) {
         val settingsFile = "settings.gradle.kts"
         val includePrefix = "include("
-        val fileLines = file(settingsFile).readLines()
-        val firstIncludeIndex = fileLines.indexOfFirst { it.startsWith(includePrefix) }
-        val includeList = fileLines.filter { it.startsWith(includePrefix) }.toMutableSet()
-        val includeListSize = includeList.size
-
+        val lines = file(settingsFile).readLines()
+        val firstIncludeIndex = lines.indexOfFirst { it.startsWith(includePrefix) }
+        if (firstIncludeIndex == -1) {
+            throw IllegalStateException("Could not find $includePrefix in $settingsFile")
+        }
+        val existingIncludes = lines.filter { it.startsWith(includePrefix) }.toSet()
         val includeModulesPath = moduleList.joinToString(":")
-        configurationList
-            .forEach { configuration ->
-                val lcConfiguration = configuration.name.lowercase()
-                val include = "$includePrefix\":$includeModulesPath:$lcConfiguration\")"
-                if (!includeList.contains(include)) {
-                    includeList.add(include)
-                }
-            }
-        val output = fileLines.take(firstIncludeIndex) +
-            includeList.sorted() +
-            fileLines.takeLast(fileLines.size - (firstIncludeIndex + includeListSize))
+        val newIncludes = configurationSet.map { configuration ->
+            val lcConfiguration = configuration.name.lowercase()
+            "$includePrefix\":$includeModulesPath:$lcConfiguration\")"
+        }.toSet()
+        val includes = existingIncludes + newIncludes
+        val output = lines.take(firstIncludeIndex) +
+            includes.sorted() +
+            lines.takeLast(lines.size - (firstIncludeIndex + includes.size))
         file(settingsFile).writeText(output.joinToString(separator = "\n", postfix = "\n"))
     }
 

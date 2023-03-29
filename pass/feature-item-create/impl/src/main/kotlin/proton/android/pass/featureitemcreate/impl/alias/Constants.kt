7dafcc0cc42d553f01297199a96a82c1d6e374df
Carlos Quintana <carlos.quintana@proton.ch>
2023-03-29 16:41:24 +0200

    refactor(item-create): store draft alias on memory

---
+++ pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/alias/Constants.kt
@@ -0,0 +1,3 @@
+package proton.android.pass.featureitemcreate.impl.alias
+
+const val KEY_DRAFT_ALIAS = "draft_alias"
--- pass/feature-item-create/impl/src/test/kotlin/proton/android/pass/featureitemcreate/impl/alias/CreateAliasViewModelTest.kt
+++ pass/feature-item-create/impl/src/test/kotlin/proton/android/pass/featureitemcreate/impl/alias/CreateAliasViewModelTest.kt
@@ -2,6 +2,7 @@ package proton.android.pass.featureitemcreate.impl.alias
 
 import app.cash.turbine.test
 import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.test.runTest
 import me.proton.core.domain.entity.UserId
@@ -11,10 +12,13 @@ import org.junit.Test
 import proton.android.pass.common.api.LoadingResult
 import proton.android.pass.composecomponents.impl.uievents.IsLoadingState
 import proton.android.pass.data.api.errors.CannotCreateMoreAliasesError
+import proton.android.pass.data.fakes.repositories.TestDraftRepository
 import proton.android.pass.data.fakes.usecases.TestCreateAlias
 import proton.android.pass.data.fakes.usecases.TestObserveAliasOptions
 import proton.android.pass.data.fakes.usecases.TestObserveVaultsWithItemCount
 import proton.android.pass.featureitemcreate.impl.ItemCreate
+import proton.android.pass.navigation.api.AliasOptionalNavArgId
+import proton.android.pass.navigation.api.CommonNavArgId
 import proton.android.pass.notifications.fakes.TestSnackbarDispatcher
 import proton.android.pass.telemetry.api.EventItemType
 import proton.android.pass.telemetry.fakes.TestTelemetryManager
@@ -41,6 +45,7 @@ class CreateAliasViewModelTest {
     private lateinit var createAlias: TestCreateAlias
     private lateinit var snackbarRepository: TestSnackbarDispatcher
     private lateinit var telemetryManager: TestTelemetryManager
+    private lateinit var draftRepository: TestDraftRepository
 
     @Before
     fun setUp() {
@@ -52,6 +57,7 @@ class CreateAliasViewModelTest {
         createAlias = TestCreateAlias()
         snackbarRepository = TestSnackbarDispatcher()
         telemetryManager = TestTelemetryManager()
+        draftRepository = TestDraftRepository()
     }
 
 
@@ -153,6 +159,33 @@ class CreateAliasViewModelTest {
     }
 
     @Test
+    fun `emits success when draft alias is stored successfully`() = runTest {
+        viewModel = createAliasViewModel(isDraft = true)
+        setupAliasOptions()
+        createAlias.setResult(LoadingResult.Success(TestItem.random()))
+        setupContentsForCreation()
+
+        viewModel.aliasUiState.test { awaitItem() }
+        viewModel.createAlias(TestShare.create().id)
+        viewModel.aliasUiState.test {
+            val item = awaitItem()
+
+            assertThat(item.isLoadingState).isEqualTo(IsLoadingState.NotLoading)
+            assertThat(item.isAliasDraftSavedState).isInstanceOf(AliasDraftSavedState.Success::class.java)
+        }
+
+        // No telemetry events should be emitted
+        val events = telemetryManager.getMemory()
+        assertThat(events).isEmpty()
+
+        // Draft should be stored
+        val draft = draftRepository.get<AliasItem>(KEY_DRAFT_ALIAS).first().value()
+        assertThat(draft).isNotNull()
+        assertThat(draft!!.title).isEqualTo(TEST_ALIAS_TITLE)
+        assertThat(draft.prefix).isEqualTo(TEST_ALIAS_PREFIX)
+    }
+
+    @Test
     fun `spaces in title are properly formatted`() = runTest {
         viewModel = createAliasViewModel()
         setupAliasOptions()
@@ -184,14 +217,16 @@ class CreateAliasViewModelTest {
             createAlias = createAlias,
             snackbarDispatcher = snackbarRepository,
             savedStateHandle = TestSavedStateHandle.create().apply {
-                set("shareId", "123")
-                set("isDraft", isDraft)
+                set(CommonNavArgId.ShareId.key, "123")
                 title?.let {
-                    set("aliasTitle", title)
+                    set(AliasOptionalNavArgId.Title.key, title)
                 }
             },
-            telemetryManager = telemetryManager
-        )
+            telemetryManager = telemetryManager,
+            draftRepository = draftRepository
+        ).apply {
+            setDraftStatus(isDraft)
+        }
 
     private fun setupContentsForCreation() {
         viewModel.aliasItemState.update {
@@ -202,8 +237,8 @@ class CreateAliasViewModelTest {
             )
         }
 
-        viewModel.onTitleChange("title")
-        viewModel.onPrefixChange("alias")
+        viewModel.onTitleChange(TEST_ALIAS_TITLE)
+        viewModel.onPrefixChange(TEST_ALIAS_PREFIX)
         viewModel.onSuffixChange(suffix)
         viewModel.onMailboxesChanged(
             listOf(
@@ -237,5 +272,10 @@ class CreateAliasViewModelTest {
                 mailboxes = listOf(mailbox.toDomain())
             )
         )
+    }
+
+    companion object {
+        private const val TEST_ALIAS_TITLE = "title"
+        private const val TEST_ALIAS_PREFIX = "prefix"
     }
 }

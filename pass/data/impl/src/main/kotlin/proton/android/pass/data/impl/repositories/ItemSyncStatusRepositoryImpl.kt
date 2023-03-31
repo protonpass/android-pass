b9b93cc30ea06c5a47bbaac16f729234582eae53
Carlos Quintana <carlos.quintana@proton.ch>
2023-03-31 13:13:35 +0200

    feat(home): observe initial load status on home

--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/repositories/ItemSyncStatusRepositoryImpl.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/repositories/ItemSyncStatusRepositoryImpl.kt
@@ -3,18 +3,22 @@ package proton.android.pass.data.impl.repositories
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.onStart
 import proton.android.pass.data.api.repositories.ItemSyncStatus
 import proton.android.pass.data.api.repositories.ItemSyncStatusRepository
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class ItemSyncStatusRepositoryImpl @Inject constructor() : ItemSyncStatusRepository {
 
-    private val syncStatus = MutableSharedFlow<ItemSyncStatus>()
+    private val syncStatus = MutableSharedFlow<ItemSyncStatus>(replay = 1)
+    private val observedSyncStatus = syncStatus.onStart { emit(ItemSyncStatus.Synced) }
 
     override fun emit(status: ItemSyncStatus) {
         syncStatus.tryEmit(status)
     }
 
-    override fun observeSyncStatus(): Flow<ItemSyncStatus> = syncStatus.distinctUntilChanged()
+    override fun observeSyncStatus(): Flow<ItemSyncStatus> = observedSyncStatus.distinctUntilChanged()
 
 }

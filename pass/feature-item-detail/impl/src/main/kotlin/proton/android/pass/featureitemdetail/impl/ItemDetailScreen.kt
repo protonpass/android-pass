94abc281f4d3c70b3b05b5661bac37b567c40d10
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-03-31 19:39:40 +0200

    refactor: item detail to no use item object

--- pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/ItemDetailScreen.kt
+++ pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/ItemDetailScreen.kt
@@ -1,6 +1,7 @@
 package proton.android.pass.featureitemdetail.impl
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -20,6 +21,10 @@ fun ItemDetailScreen(
     viewModel: ItemDetailViewModel = hiltViewModel()
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    LaunchedEffect(uiState.itemTypeUiState) {
+        viewModel.sendItemReadEvent(uiState.itemTypeUiState)
+    }
 
     ItemDetailContent(
         modifier = modifier,

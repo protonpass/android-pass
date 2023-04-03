88ed70e26f2d9dc2c17d2252d61061a3de57ef52
Carlos Quintana <carlos.quintana@proton.ch>
2023-04-03 09:21:11 +0200

    feat(item-create): add confirmation dialog when closing edit screens

--- pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/alias/UpdateAlias.kt
+++ pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/alias/UpdateAlias.kt
@@ -1,15 +1,22 @@
 package proton.android.pass.featureitemcreate.impl.alias
 
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import proton.android.pass.composecomponents.impl.dialogs.ConfirmCloseDialog
 import proton.android.pass.composecomponents.impl.uievents.IsLoadingState
 import proton.android.pass.featureitemcreate.impl.R
 import proton.pass.domain.ItemId
@@ -26,27 +33,46 @@ fun UpdateAlias(
     viewModel: UpdateAliasViewModel = hiltViewModel()
 ) {
     val viewState by viewModel.aliasUiState.collectAsStateWithLifecycle()
+    var showConfirmDialog by rememberSaveable { mutableStateOf(false) }
+    BackHandler {
+        showConfirmDialog = !showConfirmDialog
+    }
+
     LaunchedEffect(viewState.closeScreenEvent) {
         if (viewState.closeScreenEvent is CloseScreenEvent.Close) {
             onUpClick()
         }
     }
 
-    AliasContent(
-        modifier = modifier,
-        uiState = viewState,
-        topBarActionName = stringResource(id = R.string.action_save),
-        canEdit = false,
-        isEditAllowed = viewState.isLoadingState == IsLoadingState.NotLoading,
-        showVaultSelector = false,
-        onUpClick = onUpClick,
-        onAliasCreated = { shareId, itemId, _ -> onSuccess(shareId, itemId) },
-        onSubmit = { viewModel.updateAlias() },
-        onSuffixChange = { viewModel.onSuffixChange(it) },
-        onMailboxesChanged = { viewModel.onMailboxesChanged(it) },
-        onTitleChange = { viewModel.onTitleChange(it) },
-        onNoteChange = { viewModel.onNoteChange(it) },
-        onPrefixChange = { viewModel.onPrefixChange(it) },
-        onVaultSelect = {}
-    )
+    Box(
+        modifier = modifier.fillMaxSize()
+    ) {
+        AliasContent(
+            uiState = viewState,
+            topBarActionName = stringResource(id = R.string.action_save),
+            canEdit = false,
+            isEditAllowed = viewState.isLoadingState == IsLoadingState.NotLoading,
+            showVaultSelector = false,
+            onUpClick = { showConfirmDialog = true },
+            onAliasCreated = { shareId, itemId, _ -> onSuccess(shareId, itemId) },
+            onSubmit = { viewModel.updateAlias() },
+            onSuffixChange = { viewModel.onSuffixChange(it) },
+            onMailboxesChanged = { viewModel.onMailboxesChanged(it) },
+            onTitleChange = { viewModel.onTitleChange(it) },
+            onNoteChange = { viewModel.onNoteChange(it) },
+            onPrefixChange = { viewModel.onPrefixChange(it) },
+            onVaultSelect = {}
+        )
+
+        ConfirmCloseDialog(
+            show = showConfirmDialog,
+            onCancel = {
+                showConfirmDialog = false
+            },
+            onConfirm = {
+                showConfirmDialog = false
+                onUpClick()
+            }
+        )
+    }
 }

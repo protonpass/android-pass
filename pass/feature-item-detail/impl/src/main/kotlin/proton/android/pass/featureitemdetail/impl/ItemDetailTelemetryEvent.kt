c8d04cf599594bc23b870da70acad088c1c4cb7d
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-04-03 15:41:24 +0200

    feat: permanently delete from note detail

--- pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/ItemDetailTelemetryEvent.kt
+++ pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/ItemDetailTelemetryEvent.kt
@@ -3,6 +3,9 @@ package proton.android.pass.featureitemdetail.impl
 import proton.android.pass.telemetry.api.EventItemType
 import proton.android.pass.telemetry.api.TelemetryEvent
 
+data class ItemDelete(val itemType: EventItemType) : TelemetryEvent("item.deletion") {
+    override fun dimensions(): Map<String, String> = mapOf("type" to itemType.itemTypeName)
+}
 data class ItemRead(val itemType: EventItemType) : TelemetryEvent("item.read") {
     override fun dimensions(): Map<String, String> = mapOf("type" to itemType.itemTypeName)
 }

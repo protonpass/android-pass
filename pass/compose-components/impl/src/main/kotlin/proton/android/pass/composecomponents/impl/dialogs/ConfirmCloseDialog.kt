7cf01f3cd1f882ebdbcc0c833342251205d86634
Carlos Quintana <carlos.quintana@proton.ch>
2023-04-03 08:21:18 +0200

    feat(compose-components): implement confirmation dialog

---
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/dialogs/ConfirmCloseDialog.kt
@@ -0,0 +1,22 @@
+package proton.android.pass.composecomponents.impl.dialogs
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import proton.android.pass.composecomponents.impl.R
+
+@Composable
+fun ConfirmCloseDialog(
+    modifier: Modifier = Modifier,
+    onCancel: () -> Unit,
+    onConfirm: () -> Unit
+) {
+    ConfirmDialog(
+        modifier = modifier,
+        title = stringResource(R.string.confirm_close_dialog_title),
+        message = stringResource(R.string.confirm_close_dialog_message),
+        state = true,
+        onDismiss = onCancel,
+        onConfirm = { onConfirm() }
+    )
+}

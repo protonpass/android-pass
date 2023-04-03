035c3d500b117550ddaf1317d5124ce09909dd87
Carlos Quintana <carlos.quintana@proton.ch>
2023-04-03 16:04:01 +0200

    feat(home): new empty vault screen

--- pass/feature-home/impl/src/main/kotlin/proton/android/pass/featurehome/impl/NavHomeContent.kt
+++ pass/feature-home/impl/src/main/kotlin/proton/android/pass/featurehome/impl/NavHomeContent.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import proton.android.pass.common.api.Option
 import proton.android.pass.common.api.Some
+import proton.android.pass.commonuimodels.api.ItemTypeUiState
 import proton.pass.domain.ShareId
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -14,7 +15,7 @@ import proton.pass.domain.ShareId
 fun NavHomeContent(
     state: NavHomeUiState,
     homeScreenNavigation: HomeScreenNavigation,
-    onAddItemClick: (Option<ShareId>) -> Unit,
+    onAddItemClick: (Option<ShareId>, ItemTypeUiState) -> Unit,
     onCreateVaultClick: () -> Unit,
     onEditVaultClick: (ShareId) -> Unit,
     onDeleteVaultClick: (ShareId) -> Unit

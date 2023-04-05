055c9f8e7f68f6eef1343da7dc101d6360a8e1f8
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-04-05 12:19:11 +0200

    feat: add new associate and autofill dialog ui

--- pass/autofill/impl/src/main/kotlin/proton/android/pass/autofill/ui/autofill/select/AssociateAutofillItemDialog.kt
+++ pass/autofill/impl/src/main/kotlin/proton/android/pass/autofill/ui/autofill/select/AssociateAutofillItemDialog.kt
@@ -1,30 +1,40 @@
 package proton.android.pass.autofill.ui.autofill.select
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Card
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import kotlinx.datetime.Clock
 import me.proton.core.compose.theme.ProtonTheme
-import me.proton.core.compose.theme.defaultStrong
+import me.proton.core.compose.theme.default
+import me.proton.core.compose.theme.headline
 import proton.android.pass.autofill.service.R
+import proton.android.pass.commonui.api.PassTheme
+import proton.android.pass.commonui.api.ThemePreviewProvider
 import proton.android.pass.commonuimodels.api.ItemUiModel
+import proton.pass.domain.ItemId
+import proton.pass.domain.ItemType
+import proton.pass.domain.ShareId
 
 @Composable
 internal fun AssociateAutofillItemDialog(
     modifier: Modifier = Modifier,
     itemUiModel: ItemUiModel?,
-    appName: String?,
-    webDomain: String?,
     onAssociateAndAutofill: (ItemUiModel) -> Unit,
     onAutofill: (ItemUiModel) -> Unit,
     onDismiss: () -> Unit
@@ -34,40 +44,84 @@ internal fun AssociateAutofillItemDialog(
     Dialog(
         onDismissRequest = onDismiss,
     ) {
-        Card {
+        Card(backgroundColor = PassTheme.colors.backgroundNorm) {
             Column(
                 modifier = modifier.padding(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
-                val associated = webDomain ?: appName ?: ""
                 Text(
+                    text = stringResource(R.string.autofill_dialog_associate_title),
+                    style = ProtonTheme.typography.headline
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    modifier = Modifier.padding(horizontal = 8.dp),
                     text = stringResource(
                         R.string.autofill_associate_web_app_name_dialog_title,
-                        associated,
                         itemUiModel.name
                     ),
-                    style = ProtonTheme.typography.defaultStrong
+                    style = ProtonTheme.typography.default
                 )
-                Spacer(modifier = Modifier.height(16.dp))
-                Button(
-                    modifier = Modifier.fillMaxWidth(),
+                DialogButton(
+                    modifier = Modifier.align(Alignment.End),
+                    text = stringResource(R.string.autofill_dialog_associate_and_autofill),
                     onClick = { onAssociateAndAutofill(itemUiModel) }
-                ) {
-                    Text(text = stringResource(R.string.autofill_dialog_associate_and_autofill))
-                }
-                Button(
-                    modifier = Modifier.fillMaxWidth(),
+                )
+                DialogButton(
+                    modifier = Modifier.align(Alignment.End),
+                    text = stringResource(R.string.autofill_dialog_just_autofill),
                     onClick = { onAutofill(itemUiModel) }
-                ) {
-                    Text(text = stringResource(R.string.autofill_dialog_just_autofill))
-                }
-                Button(
-                    modifier = Modifier.fillMaxWidth(),
+                )
+                DialogButton(
+                    modifier = Modifier.align(Alignment.End),
+                    text = stringResource(R.string.autofill_dialog_cancel),
                     onClick = onDismiss
-                ) {
-                    Text(text = stringResource(R.string.autofill_dialog_cancel))
-                }
+                )
             }
+        }
+    }
+}
+
+@Composable
+fun DialogButton(modifier: Modifier = Modifier, text: String, onClick: () -> Unit) {
+    Button(
+        modifier = modifier,
+        elevation = null,
+        colors = ButtonDefaults.buttonColors(Color.Transparent),
+        onClick = onClick
+    ) {
+        Text(
+            text = text,
+            style = ProtonTheme.typography.default,
+            color = PassTheme.colors.interactionNormMajor1
+        )
+    }
+}
+
+@Preview
+@Composable
+fun AssociateAutofillItemDialogPreview(
+    @PreviewParameter(ThemePreviewProvider::class) isDark: Boolean
+) {
+    PassTheme(isDark = isDark) {
+        Surface {
+            AssociateAutofillItemDialog(
+                itemUiModel = ItemUiModel(
+                    id = ItemId(id = "ferri"),
+                    shareId = ShareId(id = "rutrum"),
+                    name = "Willie Lowe",
+                    note = "repudiandae",
+                    itemType = ItemType.Password,
+                    state = 6128,
+                    createTime = Clock.System.now(),
+                    modificationTime = Clock.System.now(),
+                    lastAutofillTime = null
+                ),
+                onAssociateAndAutofill = {},
+                onAutofill = {},
+                onDismiss = {}
+            )
         }
     }
 }

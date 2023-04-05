2fdd20f0c37edcb982e8b72ab4019f5b45df0ddb
Carlos Quintana <carlos.quintana@proton.ch>
2023-04-05 14:23:57 +0200

    chore(design): fix paddings for chevrons and X in edit

--- pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/login/PasswordInput.kt
+++ pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/login/PasswordInput.kt
@@ -42,7 +42,7 @@ internal fun PasswordInput(
     }
 
     ProtonTextField(
-        modifier = modifier.padding(0.dp, 16.dp),
+        modifier = modifier.padding(start = 0.dp, top = 16.dp, end = 4.dp, bottom = 16.dp),
         value = value,
         editable = isEditAllowed,
         textStyle = ProtonTheme.typography.default(isEditAllowed),

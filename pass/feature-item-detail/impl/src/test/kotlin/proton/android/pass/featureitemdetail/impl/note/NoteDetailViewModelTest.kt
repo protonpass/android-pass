96c3b2a23e678fb176e909e3ffed4e32e2b2a64e
Carlos Quintana <carlos.quintana@proton.ch>
2023-04-14 17:06:04 +0200

    test: add NoteDetailViewModel tests

---
+++ pass/feature-item-detail/impl/src/test/kotlin/proton/android/pass/featureitemdetail/impl/note/NoteDetailViewModelTest.kt
@@ -0,0 +1,240 @@
+package proton.android.pass.featureitemdetail.impl.note
+
+import app.cash.turbine.test
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import kotlinx.datetime.Clock
+import me.proton.core.crypto.common.keystore.EncryptedByteArray
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import proton.android.pass.common.api.LoadingResult
+import proton.android.pass.common.api.None
+import proton.android.pass.crypto.fakes.context.TestEncryptionContextProvider
+import proton.android.pass.data.fakes.usecases.TestDeleteItem
+import proton.android.pass.data.fakes.usecases.TestGetItemById
+import proton.android.pass.data.fakes.usecases.TestRestoreItem
+import proton.android.pass.data.fakes.usecases.TestTrashItem
+import proton.android.pass.featureitemdetail.impl.DetailSnackbarMessages
+import proton.android.pass.featureitemdetail.impl.ItemDelete
+import proton.android.pass.navigation.api.CommonNavArgId
+import proton.android.pass.notifications.fakes.TestSnackbarDispatcher
+import proton.android.pass.telemetry.api.EventItemType
+import proton.android.pass.telemetry.fakes.TestTelemetryManager
+import proton.android.pass.test.MainDispatcherRule
+import proton.android.pass.test.TestSavedStateHandle
+import proton.pass.domain.Item
+import proton.pass.domain.ItemId
+import proton.pass.domain.ItemType
+import proton.pass.domain.ShareId
+
+class NoteDetailViewModelTest {
+
+    @get:Rule
+    val dispatcher = MainDispatcherRule()
+
+    private lateinit var instance: NoteDetailViewModel
+
+    private lateinit var snackbarDispatcher: TestSnackbarDispatcher
+    private lateinit var telemetryManager: TestTelemetryManager
+    private lateinit var getItemById: TestGetItemById
+    private lateinit var trashItem: TestTrashItem
+    private lateinit var deleteItem: TestDeleteItem
+    private lateinit var restoreItem: TestRestoreItem
+    private lateinit var encryptionContextProvider: TestEncryptionContextProvider
+
+    @Before
+    fun setup() {
+        snackbarDispatcher = TestSnackbarDispatcher()
+        telemetryManager = TestTelemetryManager()
+        getItemById = TestGetItemById()
+        trashItem = TestTrashItem()
+        deleteItem = TestDeleteItem()
+        restoreItem = TestRestoreItem()
+        encryptionContextProvider = TestEncryptionContextProvider()
+        instance = NoteDetailViewModel(
+            snackbarDispatcher = snackbarDispatcher,
+            telemetryManager = telemetryManager,
+            getItemById = getItemById,
+            savedStateHandle = TestSavedStateHandle.create().apply {
+                set(CommonNavArgId.ShareId.key, SHARE_ID)
+                set(CommonNavArgId.ItemId.key, ITEM_ID)
+            },
+            encryptionContextProvider = encryptionContextProvider,
+            trashItem = trashItem,
+            deleteItem = deleteItem,
+            restoreItem = restoreItem
+        )
+    }
+
+    @Test
+    fun `sends initial state`() = runTest {
+        instance.state.test {
+            assertThat(awaitItem()).isEqualTo(NoteDetailUiState.NotInitialised)
+        }
+    }
+
+    @Test
+    fun `move to trash success`() = runTest {
+        val item = initialSetup()
+
+        instance.state.test {
+            val value = awaitItem() as NoteDetailUiState.Success
+            assertThat(value.isItemSentToTrash).isFalse()
+        }
+
+        trashItem.setResult(LoadingResult.Success(Unit))
+        instance.onMoveToTrash(item.shareId, item.id)
+
+        instance.state.test {
+            val value = awaitItem() as NoteDetailUiState.Success
+            assertThat(value.isItemSentToTrash).isTrue()
+        }
+
+        val message = snackbarDispatcher.snackbarMessage.first().value()
+        assertThat(message).isEqualTo(DetailSnackbarMessages.ItemMovedToTrash)
+    }
+
+    @Test
+    fun `move to trash error`() = runTest {
+        val item = initialSetup()
+
+        instance.state.test {
+            val value = awaitItem() as NoteDetailUiState.Success
+            assertThat(value.isItemSentToTrash).isFalse()
+        }
+
+        trashItem.setResult(LoadingResult.Error(IllegalStateException("test")))
+        instance.onMoveToTrash(item.shareId, item.id)
+
+        instance.state.test {
+            val value = awaitItem() as NoteDetailUiState.Success
+            assertThat(value.isItemSentToTrash).isFalse()
+        }
+
+        val message = snackbarDispatcher.snackbarMessage.first().value()
+        assertThat(message).isEqualTo(DetailSnackbarMessages.ItemNotMovedToTrash)
+    }
+
+    @Test
+    fun `restore from trash success`() = runTest {
+        val item = initialSetup()
+
+        instance.state.test {
+            val value = awaitItem() as NoteDetailUiState.Success
+            assertThat(value.isRestoredFromTrash).isFalse()
+        }
+
+        instance.onItemRestore(item.shareId, item.id)
+        instance.state.test {
+            val value = awaitItem() as NoteDetailUiState.Success
+            assertThat(value.isRestoredFromTrash).isTrue()
+        }
+
+        val message = snackbarDispatcher.snackbarMessage.first().value()
+        assertThat(message).isEqualTo(DetailSnackbarMessages.ItemRestored)
+    }
+
+    @Test
+    fun `restore from trash error`() = runTest {
+        val item = initialSetup()
+
+        instance.state.test {
+            val value = awaitItem() as NoteDetailUiState.Success
+            assertThat(value.isRestoredFromTrash).isFalse()
+        }
+
+        restoreItem.setResult(Result.failure(IllegalStateException("test")))
+        instance.onItemRestore(item.shareId, item.id)
+        instance.state.test {
+            val value = awaitItem() as NoteDetailUiState.Success
+            assertThat(value.isRestoredFromTrash).isFalse()
+        }
+
+        val message = snackbarDispatcher.snackbarMessage.first().value()
+        assertThat(message).isEqualTo(DetailSnackbarMessages.ItemNotRestored)
+    }
+
+    @Test
+    fun `permanently delete success`() = runTest {
+        val item = initialSetup()
+
+        instance.state.test {
+            val value = awaitItem() as NoteDetailUiState.Success
+            assertThat(value.isPermanentlyDeleted).isFalse()
+        }
+
+        instance.onPermanentlyDelete(item.shareId, item.id, item.itemType)
+        instance.state.test {
+            val value = awaitItem() as NoteDetailUiState.Success
+            assertThat(value.isPermanentlyDeleted).isTrue()
+        }
+
+        val message = snackbarDispatcher.snackbarMessage.first().value()
+        assertThat(message).isEqualTo(DetailSnackbarMessages.ItemPermanentlyDeleted)
+
+        val memory = telemetryManager.getMemory()
+        assertThat(memory.size).isEqualTo(1)
+        assertThat(memory[0]).isEqualTo(ItemDelete(EventItemType.Note))
+    }
+
+    @Test
+    fun `permanently delete error`() = runTest {
+        val item = initialSetup()
+
+        instance.state.test {
+            val value = awaitItem() as NoteDetailUiState.Success
+            assertThat(value.isPermanentlyDeleted).isFalse()
+        }
+
+        deleteItem.setResult(Result.failure(IllegalStateException("test")))
+        instance.onPermanentlyDelete(item.shareId, item.id, item.itemType)
+        instance.state.test {
+            val value = awaitItem() as NoteDetailUiState.Success
+            assertThat(value.isPermanentlyDeleted).isFalse()
+        }
+
+        val message = snackbarDispatcher.snackbarMessage.first().value()
+        assertThat(message).isEqualTo(DetailSnackbarMessages.ItemNotPermanentlyDeleted)
+
+        val memory = telemetryManager.getMemory()
+        assertThat(memory).isEmpty()
+    }
+
+    private fun initialSetup(note: String = "note"): Item {
+        val item = createEncryptedItem(note)
+        getItemById.emitValue(LoadingResult.Success(item))
+        return item
+    }
+
+    private fun createEncryptedItem(
+        title: String = "item-title",
+        note: String = "item-note",
+    ): Item {
+        val now = Clock.System.now()
+        return encryptionContextProvider.withEncryptionContext {
+            Item(
+                id = ItemId(ITEM_ID),
+                itemUuid = "uuid",
+                revision = 0,
+                shareId = ShareId(SHARE_ID),
+                itemType = ItemType.Note(note),
+                title = encrypt(title),
+                note = encrypt(note),
+                content = EncryptedByteArray(byteArrayOf()),
+                packageInfoSet = emptySet(),
+                state = 0,
+                modificationTime = now,
+                createTime = now,
+                lastAutofillTime = None
+            )
+        }
+    }
+
+    companion object {
+        private const val SHARE_ID = "share-id"
+        private const val ITEM_ID = "item-id"
+    }
+
+}

eaf4a7a029f50a54edd24ea196b6cdd593caa934
Carlos Quintana <carlos.quintana@proton.ch>
2023-04-14 15:20:56 +0200

    test: add ItemDetailViewModel test

--- pass/feature-item-detail/impl/build.gradle.kts
+++ pass/feature-item-detail/impl/build.gradle.kts
@@ -71,4 +71,5 @@ dependencies {
     testImplementation(projects.pass.crypto.fakes)
     testImplementation(projects.pass.data.fakes)
     testImplementation(projects.pass.notifications.fakes)
+    testImplementation(projects.pass.telemetry.fakes)
 }

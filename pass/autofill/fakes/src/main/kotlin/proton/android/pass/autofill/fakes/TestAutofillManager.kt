14d2d4b3ed58a3840e7592ffe48f761285657016
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-04-16 20:03:09 +0200

    test: add onboarding tests

--- pass/autofill/fakes/src/main/kotlin/proton/android/pass/autofill/fakes/TestAutofillManager.kt
+++ pass/autofill/fakes/src/main/kotlin/proton/android/pass/autofill/fakes/TestAutofillManager.kt
@@ -1,15 +1,17 @@
 package proton.android.pass.autofill.fakes
 
-import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import proton.android.pass.autofill.api.AutofillManager
+import proton.android.pass.autofill.api.AutofillStatus
 import proton.android.pass.autofill.api.AutofillSupportedStatus
+import javax.inject.Inject
 
-class TestAutofillManager : AutofillManager {
-    private val statusFlow = MutableSharedFlow<AutofillSupportedStatus>(
-        replay = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST
-    )
+class TestAutofillManager @Inject constructor() : AutofillManager {
+
+    private val statusFlow: MutableStateFlow<AutofillSupportedStatus> =
+        MutableStateFlow(AutofillSupportedStatus.Supported(AutofillStatus.EnabledByOtherService))
+
     override fun getAutofillStatus(): Flow<AutofillSupportedStatus> = statusFlow
 
     fun emitStatus(status: AutofillSupportedStatus) {

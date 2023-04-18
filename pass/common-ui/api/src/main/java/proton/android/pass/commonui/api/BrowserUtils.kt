6452abcdacae53fd17547eedde3fb2f29456eec4
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-04-18 18:14:40 +0200

    fix: avoid sanitising url on website open

--- pass/common-ui/api/src/main/java/proton/android/pass/commonui/api/BrowserUtils.kt
+++ pass/common-ui/api/src/main/java/proton/android/pass/commonui/api/BrowserUtils.kt
@@ -4,27 +4,20 @@ import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import proton.android.pass.data.api.url.UrlSanitizer
 import proton.android.pass.log.api.PassLogger
 
 object BrowserUtils {
     const val TAG = "BrowserUtils"
 
     fun openWebsite(context: Context, website: String) {
-        UrlSanitizer.sanitize(website)
-            .onSuccess {
-                try {
-                    val i = Intent(Intent.ACTION_VIEW).apply {
-                        data = Uri.parse(it)
-                    }
-                    context.startActivity(i)
-                } catch (e: ActivityNotFoundException) {
-                    val message = "Could not find a suitable activity"
-                    PassLogger.i(TAG, e, message)
-                }
+        try {
+            val i = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse(website)
             }
-            .onFailure {
-                PassLogger.i(TAG, it, "Could not find a suitable url")
-            }
+            context.startActivity(i)
+        } catch (e: ActivityNotFoundException) {
+            val message = "Could not find a suitable activity"
+            PassLogger.i(TAG, e, message)
+        }
     }
 }

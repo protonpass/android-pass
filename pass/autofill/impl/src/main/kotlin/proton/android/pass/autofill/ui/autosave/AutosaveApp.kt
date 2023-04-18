eb96b044977fe78b96d9b579794ccf35729d2d9b
Carlos Quintana <carlos.quintana@proton.ch>
2023-04-18 12:40:44 +0200

    fix(autofill): do not show snackbar from autosave/autofill in main app

--- pass/autofill/impl/src/main/kotlin/proton/android/pass/autofill/ui/autosave/AutosaveApp.kt
+++ pass/autofill/impl/src/main/kotlin/proton/android/pass/autofill/ui/autosave/AutosaveApp.kt
@@ -16,6 +16,8 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.google.accompanist.insets.ProvideWindowInsets
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import me.proton.core.compose.theme.isNightMode
+import proton.android.pass.autofill.ui.SnackBarLaunchedEffect
+import proton.android.pass.autofill.ui.SnackBarViewModel
 import proton.android.pass.commonui.api.PassTheme
 import proton.android.pass.composecomponents.impl.messages.PassSnackbarHost
 import proton.android.pass.composecomponents.impl.messages.rememberPassSnackbarHostState
@@ -27,10 +29,19 @@ fun AutoSaveApp(
     modifier: Modifier = Modifier,
     arguments: AutoSaveArguments,
     onAutoSaveSuccess: () -> Unit,
-    onAutoSaveCancel: () -> Unit
+    onAutoSaveCancel: () -> Unit,
+    snackBarViewModel: SnackBarViewModel = hiltViewModel()
 ) {
     val scaffoldState = rememberScaffoldState()
     val passSnackbarHostState = rememberPassSnackbarHostState(scaffoldState.snackbarHostState)
+
+    val snackbarState by snackBarViewModel.state.collectAsStateWithLifecycle()
+    SnackBarLaunchedEffect(
+        snackBarMessage = snackbarState.value(),
+        passSnackBarHostState = passSnackbarHostState,
+        onSnackBarMessageDelivered = { snackBarViewModel.onSnackbarMessageDelivered() }
+    )
+
     val viewModel = hiltViewModel<AutoSaveAppViewModel>()
     val state by viewModel.state.collectAsStateWithLifecycle()
     val isDark = when (state) {

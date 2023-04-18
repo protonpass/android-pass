8e3d6470441770259826a1b168dda5501bb22359
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-04-18 16:33:35 +0200

    feat: add small vault icon versions

--- pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/extension/ShareIconMapper.kt
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/extension/ShareIconMapper.kt
@@ -40,3 +40,39 @@ fun ShareIcon.toResource(): Int = when (this) {
     ShareIcon.Icon29 -> R.drawable.ic_alien
     ShareIcon.Icon30 -> R.drawable.ic_car
 }
+
+@Suppress("ComplexMethod")
+@Composable
+@DrawableRes
+fun ShareIcon.toSmallResource(): Int = when (this) {
+    ShareIcon.Icon1 -> R.drawable.ic_home_small
+    ShareIcon.Icon2 -> R.drawable.ic_work_small
+    ShareIcon.Icon3 -> R.drawable.ic_gift_small
+    ShareIcon.Icon4 -> R.drawable.ic_shop_small
+    ShareIcon.Icon5 -> R.drawable.ic_heart_small
+    ShareIcon.Icon6 -> R.drawable.ic_bear_small
+    ShareIcon.Icon7 -> R.drawable.ic_circles_small
+    ShareIcon.Icon8 -> R.drawable.ic_flower_small
+    ShareIcon.Icon9 -> R.drawable.ic_group_small
+    ShareIcon.Icon10 -> R.drawable.ic_pacman_small
+    ShareIcon.Icon11 -> R.drawable.ic_shopping_cart_small
+    ShareIcon.Icon12 -> R.drawable.ic_leaf_small
+    ShareIcon.Icon13 -> R.drawable.ic_shield_small
+    ShareIcon.Icon14 -> R.drawable.ic_basketball_small
+    ShareIcon.Icon15 -> R.drawable.ic_credit_card_small
+    ShareIcon.Icon16 -> R.drawable.ic_fish_small
+    ShareIcon.Icon17 -> R.drawable.ic_smile_small
+    ShareIcon.Icon18 -> R.drawable.ic_lock_small
+    ShareIcon.Icon19 -> R.drawable.ic_mushroom_small
+    ShareIcon.Icon20 -> R.drawable.ic_star_small
+    ShareIcon.Icon21 -> R.drawable.ic_fire_small
+    ShareIcon.Icon22 -> R.drawable.ic_wallet_small
+    ShareIcon.Icon23 -> R.drawable.ic_bookmark_small
+    ShareIcon.Icon24 -> R.drawable.ic_cream_small
+    ShareIcon.Icon25 -> R.drawable.ic_laptop_small
+    ShareIcon.Icon26 -> R.drawable.ic_json_small
+    ShareIcon.Icon27 -> R.drawable.ic_book_small
+    ShareIcon.Icon28 -> R.drawable.ic_box_small
+    ShareIcon.Icon29 -> R.drawable.ic_atom_small
+    ShareIcon.Icon30 -> R.drawable.ic_cheque_small
+}

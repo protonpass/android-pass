768ba53435e5051a7e8460bd08e34decfadabe67
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-04-19 15:29:34 +0200

    fix: update clipboard copy

--- pass/preferences/api/src/main/java/proton/android/pass/preferences/ClearClipboardPreference.kt
+++ pass/preferences/api/src/main/java/proton/android/pass/preferences/ClearClipboardPreference.kt
@@ -1,18 +1,18 @@
 package proton.android.pass.preferences
 
 private const val CLIPBOARD_NEVER = 1
-private const val CLIPBOARD_69S = 2
+private const val CLIPBOARD_60S = 2
 private const val CLIPBOARD_180S = 3
 
 enum class ClearClipboardPreference {
     Never,
-    S69,
+    S60,
     S180;
 
     fun value(): Int =
         when (this) {
             Never -> CLIPBOARD_NEVER
-            S69 -> CLIPBOARD_69S
+            S60 -> CLIPBOARD_60S
             S180 -> CLIPBOARD_180S
         }
 
@@ -20,9 +20,9 @@ enum class ClearClipboardPreference {
         fun from(value: Int): ClearClipboardPreference =
             when (value) {
                 CLIPBOARD_NEVER -> Never
-                CLIPBOARD_69S -> S69
+                CLIPBOARD_60S -> S60
                 CLIPBOARD_180S -> S180
-                else -> S69
+                else -> S60
             }
     }
 }

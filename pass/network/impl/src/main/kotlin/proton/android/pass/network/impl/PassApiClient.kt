d5d36dc025051a8468f4a13392ee39c0684cc5d0
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-04-19 16:56:34 +0200

    fix: use enableDebugLogging properly with each build type

--- pass/network/impl/src/main/kotlin/proton/android/pass/network/impl/PassApiClient.kt
+++ pass/network/impl/src/main/kotlin/proton/android/pass/network/impl/PassApiClient.kt
@@ -1,15 +1,15 @@
 package proton.android.pass.network.impl
 
 import android.os.Build
+import me.proton.core.network.domain.ApiClient
 import proton.android.pass.appconfig.api.AppConfig
 import proton.android.pass.log.api.PassLogger
-import me.proton.core.network.domain.ApiClient
 import java.util.Locale
 import javax.inject.Inject
 
 class PassApiClient @Inject constructor(appConfig: AppConfig) : ApiClient {
     override val appVersionHeader: String = "android-pass@${appConfig.versionName}"
-    override val enableDebugLogging: Boolean = true
+    override val enableDebugLogging: Boolean = appConfig.isDebug
     override val shouldUseDoh: Boolean = false
     override val userAgent: String = StringBuilder()
         .append("ProtonPass/${appConfig.versionName}")

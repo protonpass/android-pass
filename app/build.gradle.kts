c2c9fe2575031eef123013daed28390dc8f5cd71
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-04-19 15:44:52 +0200

    bump: version 1.0.2 → 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ val sentryDSN: String? = privateProperties.getProperty("SENTRY_DSN")
 val proxyToken: String? = privateProperties.getProperty("PROXY_TOKEN")
 
 val jobId: Int = System.getenv("CI_JOB_ID")?.take(3)?.toInt() ?: 0
-val appVersionName: String = "1.0.2"
+val appVersionName: String = "1.0.3"
 val appVersionCode: Int = versionCode(appVersionName)
 val archivesBaseName = "ProtonPass-$appVersionName"
 
--- pass/autofill/test-app/build.gradle.kts
+++ pass/autofill/test-app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("kotlin-android")
 }
 
-val appVersionName = "1.0.2"
+val appVersionName = "1.0.3"
 val appVersionCode = versionCode(appVersionName)
 val archivesBaseName = "AutofillTestApp-$appVersionName"
 

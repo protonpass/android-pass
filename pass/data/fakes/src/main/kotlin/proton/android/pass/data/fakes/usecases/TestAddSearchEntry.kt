27d55013b2b01e333f0ce518737f0269fbc15e33
Carlos Quintana <carlos.quintana@proton.ch>
2023-04-21 09:35:00 +0200

    test(data): add missing data fakes

---
+++ pass/data/fakes/src/main/kotlin/proton/android/pass/data/fakes/usecases/TestAddSearchEntry.kt
@@ -0,0 +1,19 @@
+package proton.android.pass.data.fakes.usecases
+
+import proton.android.pass.data.api.usecases.searchentry.AddSearchEntry
+import proton.pass.domain.ItemId
+import proton.pass.domain.ShareId
+import javax.inject.Inject
+
+class TestAddSearchEntry @Inject constructor(): AddSearchEntry {
+
+    private var result: Result<Unit> = Result.success(Unit)
+
+    fun setResult(value: Result<Unit>) {
+        result = value
+    }
+
+    override suspend fun invoke(shareId: ShareId, itemId: ItemId) {
+        result.getOrThrow()
+    }
+}

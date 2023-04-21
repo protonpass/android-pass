70e65bba9e5922e9eff19508f72b035c68e4d0cd
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-04-21 09:19:54 +0200

    style: apply kapt last to avoid warnings

--- pass/feature-auth/impl/build.gradle.kts
+++ pass/feature-auth/impl/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.kapt")
     id("dagger.hilt.android.plugin")
     id("com.google.devtools.ksp")
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
--- pass/feature-onboarding/impl/build.gradle.kts
+++ pass/feature-onboarding/impl/build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
-    id("org.jetbrains.kotlin.kapt")
     id("dagger.hilt.android.plugin")
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {

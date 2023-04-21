938508631b530cd110d3cababb96acb645ad1a6b
Carlos Quintana <carlos.quintana@proton.ch>
2023-04-21 16:35:54 +0200

    test(feature-home): more Home tests

---
+++ pass/common-test/src/main/kotlin/proton/android/pass/test/UiTestHelpers.kt
@@ -0,0 +1,22 @@
+package proton.android.pass.test
+
+import androidx.compose.ui.test.SemanticsMatcher
+import androidx.compose.ui.test.junit4.ComposeContentTestRule
+
+private const val WAIT_UNTIL_TIMEOUT = 1_000L
+
+fun ComposeContentTestRule.waitUntilNodeCount(
+    matcher: SemanticsMatcher,
+    count: Int,
+    timeoutMillis: Long = WAIT_UNTIL_TIMEOUT
+) {
+    waitUntil(timeoutMillis) {
+        onAllNodes(matcher).fetchSemanticsNodes().size == count
+    }
+}
+
+fun ComposeContentTestRule.waitUntilExists(
+    matcher: SemanticsMatcher,
+    timeoutMillis: Long = WAIT_UNTIL_TIMEOUT
+) = waitUntilNodeCount(matcher, 1, timeoutMillis)
+

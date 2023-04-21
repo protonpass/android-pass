f971664c447b1179198edf20bddd172406a8e92c
Carlos Quintana <carlos.quintana@proton.ch>
2023-04-21 09:35:00 +0200

    test(data): add missing data fakes

---
+++ pass/data/fakes/src/main/kotlin/proton/android/pass/data/fakes/usecases/TestObserveSearchEntry.kt
@@ -0,0 +1,22 @@
+package proton.android.pass.data.fakes.usecases
+
+import kotlinx.coroutines.flow.Flow
+import proton.android.pass.common.api.FlowUtils.testFlow
+import proton.android.pass.data.api.SearchEntry
+import proton.android.pass.data.api.usecases.searchentry.ObserveSearchEntry
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class TestObserveSearchEntry @Inject constructor(): ObserveSearchEntry {
+
+    private val flow = testFlow<List<SearchEntry>>()
+
+    fun emit(value: List<SearchEntry>) {
+        flow.tryEmit(value)
+    }
+
+    override fun invoke(
+        searchEntrySelection: ObserveSearchEntry.SearchEntrySelection
+    ): Flow<List<SearchEntry>> = flow
+}

c1da4bc667b2dbd1a0b6a10efff5432057774ad9
Carlos Quintana <carlos.quintana@proton.ch>
2023-04-24 09:52:53 +0200

    fix(item-detail): TOTP time text color

--- pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/common/SectionSubtitle.kt
+++ pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/common/SectionSubtitle.kt
@@ -5,7 +5,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.AnnotatedString
 import me.proton.core.compose.theme.ProtonTheme
-import me.proton.core.compose.theme.default
+import me.proton.core.compose.theme.defaultNorm
 
 @Composable
 fun SectionSubtitle(
@@ -15,6 +15,6 @@ fun SectionSubtitle(
     Text(
         modifier = modifier,
         text = text,
-        style = ProtonTheme.typography.default
+        style = ProtonTheme.typography.defaultNorm
     )
 }

a5099683357962c926ee12b4899f5ae58009b4c9
Carlos Quintana <carlos.quintana@proton.ch>
2023-04-24 16:46:36 +0200

    test: fix unit tests

--- pass/data/fakes/src/main/kotlin/proton/android/pass/data/fakes/usecases/TestGetItemByIdWithVault.kt
+++ pass/data/fakes/src/main/kotlin/proton/android/pass/data/fakes/usecases/TestGetItemByIdWithVault.kt
@@ -1,6 +1,7 @@
 package proton.android.pass.data.fakes.usecases
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import proton.android.pass.common.api.FlowUtils.testFlow
 import proton.android.pass.data.api.usecases.GetItemByIdWithVault
 import proton.android.pass.data.api.usecases.ItemWithVaultInfo
@@ -10,14 +11,16 @@ import javax.inject.Inject
 
 class TestGetItemByIdWithVault @Inject constructor() : GetItemByIdWithVault {
 
-    private val result = testFlow<ItemWithVaultInfo>()
+    private val result = testFlow<Result<ItemWithVaultInfo>>()
 
-    fun emitValue(value: ItemWithVaultInfo) {
+    fun emitValue(value: Result<ItemWithVaultInfo>) {
         result.tryEmit(value)
     }
 
     override fun invoke(
         shareId: ShareId,
         itemId: ItemId
-    ): Flow<ItemWithVaultInfo> = result
+    ): Flow<ItemWithVaultInfo> = result.map {
+        it.getOrThrow()
+    }
 }

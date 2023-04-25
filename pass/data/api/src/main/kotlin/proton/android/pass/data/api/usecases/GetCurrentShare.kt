afc12bb6a2bc734fe353584a62308728c23e8478
Carlos Quintana <carlos.quintana@proton.ch>
2023-04-25 16:13:13 +0200

    refactor(data): remove unused GetCurrentShare use-case

--- pass/data/api/src/main/kotlin/proton/android/pass/data/api/usecases/GetCurrentShare.kt
+++
@@ -1,9 +0,0 @@
-package proton.android.pass.data.api.usecases
-
-import me.proton.core.domain.entity.UserId
-import proton.android.pass.common.api.LoadingResult
-import proton.pass.domain.Share
-
-interface GetCurrentShare {
-    suspend operator fun invoke(userId: UserId): LoadingResult<List<Share>>
-}

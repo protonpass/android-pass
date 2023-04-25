0179b27250d756db49a45eb925cba1b9dc39be6c
Carlos Quintana <carlos.quintana@proton.ch>
2023-04-25 09:16:31 +0200

    refactor: remove LoadingResult form ObserveVaultsWithItemCount and fix crash in offline edit

--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/ObserveVaultsWithItemCountImpl.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/ObserveVaultsWithItemCountImpl.kt
@@ -2,10 +2,7 @@ package proton.android.pass.data.impl.usecases
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
-import proton.android.pass.common.api.LoadingResult
-import proton.android.pass.common.api.asLoadingResult
 import proton.android.pass.data.api.repositories.ItemRepository
 import proton.android.pass.data.api.repositories.ShareItemCount
 import proton.android.pass.data.api.usecases.ObserveVaults
@@ -21,29 +18,22 @@ class ObserveVaultsWithItemCountImpl @Inject constructor(
     private val itemRepository: ItemRepository
 ) : ObserveVaultsWithItemCount {
 
-    override fun invoke(): Flow<LoadingResult<List<VaultWithItemCount>>> = observeVaults()
-        .asLoadingResult()
-        .flatMapLatest { result ->
-            when (result) {
-                LoadingResult.Loading -> flowOf(LoadingResult.Loading)
-                is LoadingResult.Error -> flowOf(LoadingResult.Error(result.exception))
-                is LoadingResult.Success -> observeItemCounts(result.data)
-            }
-        }
+    override fun invoke(): Flow<List<VaultWithItemCount>> = observeVaults()
+        .flatMapLatest { result -> observeItemCounts(result) }
 
     private fun observeItemCounts(
         vaultList: List<Vault>
-    ): Flow<LoadingResult<List<VaultWithItemCount>>> = itemRepository.observeItemCount(
+    ): Flow<List<VaultWithItemCount>> = itemRepository.observeItemCount(
         shareIds = vaultList.map { it.shareId }
     ).map { count -> mapVaults(vaultList, count) }
 
     private fun mapVaults(
         vaultList: List<Vault>,
         count: Map<ShareId, ShareItemCount>
-    ): LoadingResult<List<VaultWithItemCount>> {
+    ): List<VaultWithItemCount> {
         val res = vaultList.map { vault ->
             val itemsForShare = count[vault.shareId]
-                ?: return LoadingResult.Error(IllegalStateException("Could not find ItemCount for share"))
+                ?: throw IllegalStateException("Could not find ItemCount for share")
 
             VaultWithItemCount(
                 vault = vault,
@@ -51,7 +41,7 @@ class ObserveVaultsWithItemCountImpl @Inject constructor(
                 trashedItemCount = itemsForShare.trashedItems
             )
         }.sorted()
-        return LoadingResult.Success(res)
+        return res
     }
 
 }

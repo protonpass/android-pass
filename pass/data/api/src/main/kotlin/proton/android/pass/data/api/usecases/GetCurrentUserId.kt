7f858da41b221d51fb823ace35153fb53b1616a8
Carlos Quintana <carlos.quintana@proton.ch>
2023-04-26 09:04:50 +0200

    refactor: remove unused GetCurrentUserId use-case

--- pass/data/api/src/main/kotlin/proton/android/pass/data/api/usecases/GetCurrentUserId.kt
+++
@@ -1,9 +0,0 @@
-package proton.android.pass.data.api.usecases
-
-import me.proton.core.domain.entity.UserId
-import proton.android.pass.common.api.LoadingResult
-
-interface GetCurrentUserId {
-    suspend operator fun invoke(): LoadingResult<UserId>
-}
-

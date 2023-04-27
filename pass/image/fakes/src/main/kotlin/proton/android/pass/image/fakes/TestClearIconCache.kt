83c06ed98bb4dd511f9f26d35733918f6096161a
Carlos Quintana <carlos.quintana@proton.ch>
2023-04-27 09:10:00 +0200

    test: add Settings screen UI tests

--- pass/image/fakes/src/main/kotlin/proton/android/pass/image/fakes/TestClearIconCache.kt
+++ pass/image/fakes/src/main/kotlin/proton/android/pass/image/fakes/TestClearIconCache.kt
@@ -2,16 +2,22 @@ package proton.android.pass.image.fakes
 
 import proton.android.pass.image.api.ClearIconCache
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class TestClearIconCache @Inject constructor() : ClearIconCache {
 
     private var result: Result<Unit> = Result.success(Unit)
+    private var invoked = false
 
     fun setResult(value: Result<Unit>) {
         result = value
     }
 
+    fun invoked() = invoked
+
     override suspend fun invoke() {
+        invoked = true
         result.getOrThrow()
     }
 }

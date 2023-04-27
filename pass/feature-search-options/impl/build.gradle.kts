2432c1adf329b4f87d197cb7a8ae5a4422ec12ef
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-04-27 16:09:14 +0200

    refactor: move sorting ui code to separate module

---
+++ pass/feature-search-options/impl/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("dagger.hilt.android.plugin")
+    id("org.jetbrains.kotlin.kapt")
+}
+
+android {
+    namespace = "proton.android.pass.featuresearchoptions"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+    
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.androidx.compose.compiler.get()
+    }
+}
+
+androidComponents.beforeVariants { variant ->
+    variant.enableAndroidTest = false
+}
+
+dependencies {
+    api(projects.pass.featureSearchOptions.api)
+
+    implementation(libs.kotlinx.collections)
+    implementation(libs.core.presentation.compose)
+    implementation(libs.core.presentation)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.runtime)
+
+    implementation(libs.dagger.hilt.android)
+    kapt(libs.dagger.hilt.android.compiler)
+    kapt(libs.androidx.hilt.compiler)
+
+    debugImplementation(libs.showkase)
+    kspDebug(libs.showkaseProcessor)
+
+    debugImplementation(libs.androidx.compose.uiTooling)
+    implementation(libs.androidx.compose.uiToolingPreview)
+    debugImplementation(libs.androidx.compose.uiTestManifest)
+
+    implementation(projects.pass.navigation.api)
+
+    implementation(projects.pass.commonUi.api)
+    implementation(projects.pass.composeComponents.impl)
+}
---
+++ pass/feature-search-options/impl/src/main/kotlin/proton/android/featuresearchoptions/impl/SortingBottomSheetViewModel.kt
@@ -0,0 +1,30 @@
+package proton.android.featuresearchoptions.impl
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import proton.android.pass.featuresearchoptions.api.SearchOptionsRepository
+import proton.android.pass.featuresearchoptions.api.SearchSortingType
+import proton.android.pass.featuresearchoptions.api.SortingOption
+import proton.android.pass.navigation.api.SortingTypeNavArgId
+import javax.inject.Inject
+
+@HiltViewModel
+class SortingBottomSheetViewModel @Inject constructor(
+    private val searchOptionsRepository: SearchOptionsRepository,
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+    private val sortingType: SearchSortingType =
+        SearchSortingType.valueOf(requireNotNull(savedStateHandle.get<String>(SortingTypeNavArgId.key)))
+    val state: StateFlow<SearchSortingType> = MutableStateFlow(sortingType)
+        .stateIn(viewModelScope, SharingStarted.Eagerly, sortingType)
+
+    fun onSortingTypeChanged(searchSortingType: SearchSortingType) {
+        searchOptionsRepository.setSearchOption(SortingOption(searchSortingType))
+    }
+}

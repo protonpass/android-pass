f58cca0419233be5ad9c18bdb27402b275e2c707
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-05-02 15:55:26 +0200

    fix: avoid apply pending event cancellation

--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/sync/SyncManagerImpl.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/sync/SyncManagerImpl.kt
@@ -4,6 +4,7 @@ import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.WorkManager
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.isActive
@@ -12,18 +13,22 @@ import me.proton.core.accountmanager.domain.AccountManager
 import me.proton.core.eventmanager.domain.work.EventWorkerManager
 import me.proton.core.presentation.app.AppLifecycleProvider
 import me.proton.core.util.kotlin.CoroutineScopeProvider
+import proton.android.pass.data.api.usecases.ApplyPendingEvents
 import proton.android.pass.log.api.PassLogger
 import javax.inject.Inject
+import javax.inject.Singleton
 import kotlin.time.Duration
 
+@Singleton
 class SyncManagerImpl @Inject constructor(
     private val scopeProvider: CoroutineScopeProvider,
     private val workManager: WorkManager,
+    private val applyPendingEvents: ApplyPendingEvents,
     appLifecycleProvider: AppLifecycleProvider,
     accountManager: AccountManager
 ) : SyncManager {
 
-    private val state = combine(
+    private val state: Flow<SyncState> = combine(
         accountManager.getPrimaryUserId(),
         appLifecycleProvider.state
     ) { userId, appLifecycle ->
@@ -44,13 +49,17 @@ class SyncManagerImpl @Inject constructor(
                         AppLifecycleProvider.State.Background -> EventWorkerManager.REPEAT_INTERVAL_BACKGROUND
                         AppLifecycleProvider.State.Foreground -> EventWorkerManager.REPEAT_INTERVAL_FOREGROUND
                     }
-                    val immediateDuration = Duration.ZERO
                     when (it.appLifecycle) {
                         AppLifecycleProvider.State.Foreground ->
                             while (currentCoroutineContext().isActive) {
-                                enqueueWorker(immediateDuration)
+                                runCatching {
+                                    applyPendingEvents()
+                                }.onFailure { t ->
+                                    PassLogger.w(TAG, t, "Apply pending events error")
+                                }
                                 delay(initialDelay)
                             }
+
                         AppLifecycleProvider.State.Background -> enqueueWorker(initialDelay)
                     }
                 } else {

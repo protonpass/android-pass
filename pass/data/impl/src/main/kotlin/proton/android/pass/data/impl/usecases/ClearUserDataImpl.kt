a287acd5173825aee7bdf8fb3197191eb0fd0524
Carlos Quintana <carlos.quintana@proton.ch>
2023-05-02 09:06:58 +0200

    chore: clear user data on account status change

--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/ClearUserDataImpl.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/ClearUserDataImpl.kt
@@ -1,5 +1,7 @@
 package proton.android.pass.data.impl.usecases
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import me.proton.core.domain.entity.UserId
 import proton.android.pass.data.api.repositories.ShareRepository
 import proton.android.pass.data.api.usecases.ClearUserData
@@ -9,7 +11,7 @@ class ClearUserDataImpl @Inject constructor(
     private val shareRepository: ShareRepository
 ) : ClearUserData {
 
-    override suspend fun invoke(userId: UserId) {
+    override suspend fun invoke(userId: UserId) = withContext(Dispatchers.IO) {
         shareRepository.deleteSharesForUser(userId)
     }
 

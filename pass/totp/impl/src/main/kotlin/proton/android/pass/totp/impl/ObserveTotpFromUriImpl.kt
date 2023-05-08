1c9d05fdbd4570a5c9b01e32937691574c47df15
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-05-08 15:33:09 +0200

    fix: fix crash on incorrect totp uri

--- pass/totp/impl/src/main/kotlin/proton/android/pass/totp/impl/ObserveTotpFromUriImpl.kt
+++ pass/totp/impl/src/main/kotlin/proton/android/pass/totp/impl/ObserveTotpFromUriImpl.kt
@@ -1,8 +1,7 @@
 package proton.android.pass.totp.impl
 
 import kotlinx.coroutines.flow.Flow
-import proton.android.pass.common.api.flatMap
-import proton.android.pass.log.api.PassLogger
+import kotlinx.coroutines.flow.emptyFlow
 import proton.android.pass.totp.api.ObserveTotpFromUri
 import proton.android.pass.totp.api.TotpManager
 import javax.inject.Inject
@@ -11,20 +10,10 @@ class ObserveTotpFromUriImpl @Inject constructor(
     private val totpManager: TotpManager
 ) : ObserveTotpFromUri {
 
-    override fun invoke(uri: String): Result<Flow<TotpManager.TotpWrapper>> =
-        totpManager.parse(uri)
-            .onFailure {
-                PassLogger.d(TAG, it, "Failed to parse TOTP uri")
-                PassLogger.w(TAG, "Failed to parse TOTP uri")
-            }
-            .flatMap { spec ->
-                runCatching {
-                    totpManager.observeCode(spec)
-                }.onFailure { PassLogger.w(TAG, it, "Failed to observe TOTP code") }
-            }
-
-
-    companion object {
-        private const val TAG = "ObserveTotpFromUriImpl"
+    override fun invoke(uri: String): Flow<TotpManager.TotpWrapper> {
+        val parsed = totpManager.parse(uri)
+        val spec = parsed.getOrNull()
+        spec ?: return emptyFlow()
+        return totpManager.observeCode(spec)
     }
 }

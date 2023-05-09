fd218d0400960059dc292a565490dcad81670402
Carlos Quintana <carlos.quintana@proton.ch>
2023-05-09 16:44:28 +0200

    test: add edit totp test for UpdateLovinViewModel

--- pass/totp/fakes/src/main/kotlin/proton/android/pass/totp/fakes/TestTotpManager.kt
+++ pass/totp/fakes/src/main/kotlin/proton/android/pass/totp/fakes/TestTotpManager.kt
@@ -5,8 +5,16 @@ import kotlinx.coroutines.flow.emptyFlow
 import proton.android.pass.totp.api.TotpManager
 import proton.android.pass.totp.api.TotpSpec
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class TestTotpManager @Inject constructor() : TotpManager {
+
+    private var parseResult: Result<TotpSpec> = Result.failure(NotImplementedError())
+
+    fun setParseResult(result: Result<TotpSpec>) {
+        parseResult = result
+    }
 
     override fun generateUri(spec: TotpSpec): String = ""
 
@@ -14,5 +22,5 @@ class TestTotpManager @Inject constructor() : TotpManager {
 
     override fun observeCode(spec: TotpSpec): Flow<TotpManager.TotpWrapper> = emptyFlow()
 
-    override fun parse(uri: String): Result<TotpSpec> = Result.failure(NotImplementedError())
+    override fun parse(uri: String): Result<TotpSpec> = parseResult
 }

5be522665188b7a552de0870f216c698063ec53e
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-05-10 15:28:05 +0200

    feat: change plan entity to hold plan info

---
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/db/dao/PlanDao.kt
@@ -0,0 +1,40 @@
+package proton.android.pass.data.impl.db.dao
+
+import androidx.room.ColumnInfo
+import androidx.room.Dao
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+import me.proton.core.data.room.db.BaseDao
+import proton.android.pass.data.impl.db.entities.PlanEntity
+
+@Dao
+abstract class PlanDao : BaseDao<PlanEntity>() {
+    @Query(
+        """
+        SELECT * FROM ${PlanEntity.TABLE}
+        WHERE ${PlanEntity.Columns.USER_ID} = :userId
+        LIMIT 1
+        """
+    )
+    abstract fun observeUserPlan(userId: String): Flow<PlanEntity>
+
+    @Query(
+        """
+        SELECT ${PlanEntity.Columns.TYPE}, 
+        ${PlanEntity.Columns.INTERNAL_NAME}, 
+        ${PlanEntity.Columns.DISPLAY_NAME} FROM ${PlanEntity.TABLE}
+        WHERE ${PlanEntity.Columns.USER_ID} = :userId
+        LIMIT 1
+        """
+    )
+    abstract fun observeUserPlanType(userId: String): Flow<PlanTypeFields>
+}
+
+data class PlanTypeFields(
+    @ColumnInfo(name = PlanEntity.Columns.TYPE)
+    val type: String,
+    @ColumnInfo(name = PlanEntity.Columns.INTERNAL_NAME)
+    val internalName: String,
+    @ColumnInfo(name = PlanEntity.Columns.DISPLAY_NAME)
+    val displayName: String
+)
--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/db/entities/PlanEntity.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/db/entities/PlanEntity.kt
@@ -6,20 +6,26 @@ import androidx.room.ForeignKey
 import me.proton.core.user.data.entity.UserEntity
 
 @Entity(
-    tableName = PlanLimitsEntity.TABLE,
-    primaryKeys = [PlanLimitsEntity.Columns.USER_ID],
+    tableName = PlanEntity.TABLE,
+    primaryKeys = [PlanEntity.Columns.USER_ID],
     foreignKeys = [
         ForeignKey(
             entity = UserEntity::class,
             parentColumns = [ExternalColumns.USER_ID],
-            childColumns = [PlanLimitsEntity.Columns.USER_ID],
+            childColumns = [PlanEntity.Columns.USER_ID],
             onDelete = ForeignKey.CASCADE
         )
     ]
 )
-data class PlanLimitsEntity(
+data class PlanEntity(
     @ColumnInfo(name = Columns.USER_ID)
     val userId: String,
+    @ColumnInfo(name = Columns.TYPE, defaultValue = "free")
+    val type: String,
+    @ColumnInfo(name = Columns.INTERNAL_NAME, defaultValue = "")
+    val internalName: String,
+    @ColumnInfo(name = Columns.DISPLAY_NAME, defaultValue = "")
+    val displayName: String,
     @ColumnInfo(name = Columns.VAULT_LIMIT)
     val vaultLimit: Int,
     @ColumnInfo(name = Columns.ALIAS_LIMIT)
@@ -29,12 +35,15 @@ data class PlanLimitsEntity(
 ) {
     object Columns {
         const val USER_ID = "user_id"
+        const val TYPE = "type"
+        const val INTERNAL_NAME = "internal_name"
+        const val DISPLAY_NAME = "display_name"
         const val VAULT_LIMIT = "vault_limit"
         const val ALIAS_LIMIT = "alias_limit"
         const val TOTP_LIMIT = "totp_limit"
     }
 
     companion object {
-        const val TABLE = "PlanLimitsEntity"
+        const val TABLE = "PlanEntity"
     }
 }

40ad853df77ead73577f8ccfcf9acb66f3746b05
Carlos Quintana <carlos.quintana@proton.ch>
2023-05-11 11:02:32 +0200

    chore(item-create): remove duplicated code

--- pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/generatepassword/GeneratePasswordUiState.kt
+++
@@ -1,7 +0,0 @@
-package proton.android.pass.composecomponents.impl.generatepassword
-
-data class GeneratePasswordUiState(
-    val password: String,
-    val length: Int,
-    val hasSpecialCharacters: Boolean
-)

95226c4abb4a7d0e0494388cbb63619c0cecf818
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-05-17 09:26:36 +0200

    chore: remove iap ff usage in upgrade info

--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/ObserveUpgradeInfoImpl.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/ObserveUpgradeInfoImpl.kt
@@ -1,20 +1,18 @@
 package proton.android.pass.data.impl.usecases
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
 import me.proton.core.payment.domain.PaymentManager
-import proton.android.pass.common.api.combineN
-import proton.android.pass.data.api.usecases.ObserveUpgradeInfo
 import proton.android.pass.data.api.usecases.ObserveCurrentUser
 import proton.android.pass.data.api.usecases.ObserveItemCount
 import proton.android.pass.data.api.usecases.ObserveMFACount
+import proton.android.pass.data.api.usecases.ObserveUpgradeInfo
 import proton.android.pass.data.api.usecases.ObserveVaultCount
 import proton.android.pass.data.api.usecases.UpgradeInfo
 import proton.android.pass.data.impl.repositories.PlanRepository
-import proton.android.pass.preferences.FeatureFlags
-import proton.android.pass.preferences.FeatureFlagsPreferencesRepository
 import proton.pass.domain.PlanType
 import javax.inject.Inject
 
@@ -24,26 +22,24 @@ class ObserveUpgradeInfoImpl @Inject constructor(
     private val observeItemCount: ObserveItemCount,
     private val paymentManager: PaymentManager,
     private val planRepository: PlanRepository,
-    private val featureFlagsPreferencesRepository: FeatureFlagsPreferencesRepository,
     private val observeVaultCount: ObserveVaultCount
 ) : ObserveUpgradeInfo {
     override fun invoke(forceRefresh: Boolean): Flow<UpgradeInfo> = observeCurrentUser()
         .distinctUntilChanged()
         .flatMapLatest { user ->
-            combineN(
+            combine(
                 planRepository.sendUserAccessAndObservePlan(
                     userId = user.userId,
                     forceRefresh = forceRefresh
                 ),
-                featureFlagsPreferencesRepository.get<Boolean>(FeatureFlags.IAP_ENABLED),
                 flowOf(paymentManager.isUpgradeAvailable()),
                 observeMFACount(),
                 observeItemCount(itemState = null),
                 observeVaultCount(user.userId)
-            ) { plan, iapEnabled, isUpgradeAvailable, mfaCount, itemCount, vaultCount ->
+            ) { plan, isUpgradeAvailable, mfaCount, itemCount, vaultCount ->
                 val isPaid = plan.planType is PlanType.Paid
                 UpgradeInfo(
-                    isUpgradeAvailable = iapEnabled && isUpgradeAvailable && !isPaid,
+                    isUpgradeAvailable = isUpgradeAvailable && !isPaid,
                     plan = plan.copy(
                         vaultLimit = plan.vaultLimit.takeIf { it >= 0 } ?: Int.MAX_VALUE,
                         aliasLimit = plan.aliasLimit.takeIf { it >= 0 } ?: Int.MAX_VALUE,

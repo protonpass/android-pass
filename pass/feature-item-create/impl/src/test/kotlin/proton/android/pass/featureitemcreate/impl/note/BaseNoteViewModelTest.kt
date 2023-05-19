88a99a989b9d673ead4722f9b6cdf894ba30a7e1
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-05-19 12:58:07 +0200

    feat: control primary vault selection on note create

--- pass/feature-item-create/impl/src/test/kotlin/proton/android/pass/featureitemcreate/impl/note/BaseNoteViewModelTest.kt
+++ pass/feature-item-create/impl/src/test/kotlin/proton/android/pass/featureitemcreate/impl/note/BaseNoteViewModelTest.kt
@@ -6,34 +6,31 @@ import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import proton.android.pass.data.fakes.usecases.TestObserveVaultsWithItemCount
-import proton.android.pass.featureitemcreate.impl.note.CreateUpdateNoteUiState.Companion.Initial
+import proton.android.pass.featureitemcreate.impl.note.BaseNoteUiState.Companion.Initial
+import proton.android.pass.notifications.fakes.TestSnackbarDispatcher
 import proton.android.pass.test.MainDispatcherRule
 import proton.android.pass.test.TestSavedStateHandle
-import proton.pass.domain.ShareId
-import proton.pass.domain.Vault
-import proton.pass.domain.VaultWithItemCount
 
 internal class BaseNoteViewModelTest {
 
     @get:Rule
     val dispatcherRule = MainDispatcherRule()
 
-    private lateinit var observeVaults: TestObserveVaultsWithItemCount
+    private lateinit var snackbarDispatcher: TestSnackbarDispatcher
     private lateinit var baseNoteViewModel: BaseNoteViewModel
 
     @Before
     fun setUp() {
-        observeVaults = TestObserveVaultsWithItemCount()
+        snackbarDispatcher = TestSnackbarDispatcher()
         baseNoteViewModel = object : BaseNoteViewModel(
-            observeVaults,
+            snackbarDispatcher,
             TestSavedStateHandle.create()
         ) {}
     }
 
     @Test
     fun `should start with the initial state`() = runTest {
-        baseNoteViewModel.noteUiState.test {
+        baseNoteViewModel.baseNoteUiState.test {
             assertThat(awaitItem()).isEqualTo(Initial)
         }
     }
@@ -41,9 +38,8 @@ internal class BaseNoteViewModelTest {
     @Test
     fun `when the title has changed, the state should hold it`() = runTest {
         val titleInput = "Title Changed"
-        givenAVaultList()
         baseNoteViewModel.onTitleChange(titleInput)
-        baseNoteViewModel.noteUiState.test {
+        baseNoteViewModel.baseNoteUiState.test {
             assertThat(awaitItem().noteItem)
                 .isEqualTo(Initial.noteItem.copy(title = titleInput))
         }
@@ -52,25 +48,10 @@ internal class BaseNoteViewModelTest {
     @Test
     fun `when the note has changed, the state should hold it`() = runTest {
         val noteInput = "Note Changed"
-        givenAVaultList()
         baseNoteViewModel.onNoteChange(noteInput)
-        baseNoteViewModel.noteUiState.test {
+        baseNoteViewModel.baseNoteUiState.test {
             assertThat(awaitItem().noteItem)
                 .isEqualTo(Initial.noteItem.copy(note = noteInput))
         }
-    }
-
-    private fun givenAVaultList() {
-        observeVaults.sendResult(
-            Result.success(
-                listOf(
-                    VaultWithItemCount(
-                        vault = Vault(ShareId("ShareId"), "name", isPrimary = false),
-                        activeItemCount = 1,
-                        trashedItemCount = 0
-                    )
-                )
-            )
-        )
     }
 }

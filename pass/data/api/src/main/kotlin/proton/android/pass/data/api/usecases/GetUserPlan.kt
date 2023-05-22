bb15023a4998fb7bb2617115c99597c27070e1b3
Carlos Quintana <carlos.quintana@proton.ch>
2023-05-22 16:16:14 +0200

    feat(data): offer to invoke GetUserPlan without specifying UserId

--- pass/data/api/src/main/kotlin/proton/android/pass/data/api/usecases/GetUserPlan.kt
+++ pass/data/api/src/main/kotlin/proton/android/pass/data/api/usecases/GetUserPlan.kt
@@ -5,5 +5,6 @@ import me.proton.core.domain.entity.UserId
 import proton.pass.domain.Plan
 
 interface GetUserPlan {
+    operator fun invoke(): Flow<Plan>
     operator fun invoke(userId: UserId): Flow<Plan>
 }
--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/GetUserPlanImpl.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/GetUserPlanImpl.kt
@@ -1,6 +1,9 @@
 package proton.android.pass.data.impl.usecases
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.flatMapLatest
+import me.proton.core.accountmanager.domain.AccountManager
 import me.proton.core.domain.entity.UserId
 import proton.android.pass.data.api.usecases.GetUserPlan
 import proton.android.pass.data.impl.repositories.PlanRepository
@@ -8,8 +11,13 @@ import proton.pass.domain.Plan
 import javax.inject.Inject
 
 class GetUserPlanImpl @Inject constructor(
+    private val accountManager: AccountManager,
     private val planRepository: PlanRepository
 ) : GetUserPlan {
+
+    override fun invoke(): Flow<Plan> = accountManager.getPrimaryUserId()
+        .filterNotNull()
+        .flatMapLatest { invoke(it) }
 
     override fun invoke(userId: UserId): Flow<Plan> = planRepository.observePlan(userId)
 }

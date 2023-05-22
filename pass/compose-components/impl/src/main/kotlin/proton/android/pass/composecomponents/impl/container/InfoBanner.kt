86671820b0916ed80b8f1e8e32d75bfc1651d027
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-05-22 14:21:37 +0200

    feat: add upgrade ui from vault selection

--- pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/container/InfoBanner.kt
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/container/InfoBanner.kt
@@ -1,6 +1,7 @@
 package proton.android.pass.composecomponents.impl.container
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -10,27 +11,42 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import proton.android.pass.commonui.api.PassTheme
 import proton.android.pass.commonui.api.ThemePreviewProvider
+import proton.android.pass.commonui.api.applyIf
 
 @Composable
 fun InfoBanner(
     modifier: Modifier = Modifier,
     backgroundColor: Color,
-    text: String
+    text: AnnotatedString,
+    onClick: (() -> Unit)? = null
 ) {
     Text(
         modifier = modifier
             .clip(RoundedCornerShape(8.dp))
+            .applyIf(onClick != null, ifTrue = { clickable { onClick?.invoke() } })
             .fillMaxWidth()
             .background(backgroundColor)
             .padding(16.dp),
         text = text,
         color = PassTheme.colors.textNorm,
     )
+}
+
+@Composable
+fun InfoBanner(
+    modifier: Modifier = Modifier,
+    backgroundColor: Color,
+    text: String,
+    onClick: (() -> Unit)? = null
+) {
+    InfoBanner(modifier, backgroundColor, AnnotatedString(text), onClick)
 }
 
 @Preview
@@ -42,7 +58,7 @@ fun InfoBannerPreview(
         Surface {
             InfoBanner(
                 backgroundColor = PassTheme.colors.loginInteractionNormMinor1,
-                text = "This is an info banner"
+                text = buildAnnotatedString { append("This is an info banner") }
             )
         }
     }

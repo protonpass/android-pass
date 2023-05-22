9f18e153445cd71e9c97638fbdea7678f7bbfbf6
Carlos Quintana <carlos.quintana@proton.ch>
2023-05-22 13:23:09 +0200

    fix(data): use local plan info for detecting if totp can be shown

--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/db/dao/PlanDao.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/db/dao/PlanDao.kt
@@ -1,6 +1,5 @@
 package proton.android.pass.data.impl.db.dao
 
-import androidx.room.ColumnInfo
 import androidx.room.Dao
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
@@ -17,26 +16,4 @@ abstract class PlanDao : BaseDao<PlanEntity>() {
         """
     )
     abstract fun observeUserPlan(userId: String): Flow<PlanEntity>
-
-    @Query(
-        """
-        SELECT ${PlanEntity.Columns.TYPE}, 
-        ${PlanEntity.Columns.INTERNAL_NAME}, 
-        ${PlanEntity.Columns.DISPLAY_NAME} FROM ${PlanEntity.TABLE}
-        WHERE ${PlanEntity.Columns.USER_ID} = :userId
-        LIMIT 1
-        """
-    )
-    abstract fun observeUserPlanType(userId: String): Flow<PlanTypeFields>
 }
-
-data class PlanTypeFields(
-    @ColumnInfo(name = PlanEntity.Columns.TYPE)
-    val type: String,
-    @ColumnInfo(name = PlanEntity.Columns.INTERNAL_NAME)
-    val internalName: String,
-    @ColumnInfo(name = PlanEntity.Columns.DISPLAY_NAME)
-    val displayName: String,
-    @ColumnInfo(name = PlanEntity.Columns.TRIAL_END)
-    val trialEnd: Long?
-)
--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/GetUserPlanImpl.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/GetUserPlanImpl.kt
@@ -4,12 +4,12 @@ import kotlinx.coroutines.flow.Flow
 import me.proton.core.domain.entity.UserId
 import proton.android.pass.data.api.usecases.GetUserPlan
 import proton.android.pass.data.impl.repositories.PlanRepository
-import proton.pass.domain.PlanType
+import proton.pass.domain.Plan
 import javax.inject.Inject
 
 class GetUserPlanImpl @Inject constructor(
     private val planRepository: PlanRepository
 ) : GetUserPlan {
 
-    override fun invoke(userId: UserId): Flow<PlanType> = planRepository.observePlanType(userId)
+    override fun invoke(userId: UserId): Flow<Plan> = planRepository.observePlan(userId)
 }

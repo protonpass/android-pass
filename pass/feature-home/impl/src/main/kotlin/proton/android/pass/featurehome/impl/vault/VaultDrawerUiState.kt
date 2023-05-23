bc5418fb90861473fc7fb056880cbb10a7acab4d
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-05-23 15:05:31 +0200

    fix: select all vaults on current vault delete

--- pass/feature-home/impl/src/main/kotlin/proton/android/pass/featurehome/impl/vault/VaultDrawerUiState.kt
+++ pass/feature-home/impl/src/main/kotlin/proton/android/pass/featurehome/impl/vault/VaultDrawerUiState.kt
@@ -3,12 +3,10 @@ package proton.android.pass.featurehome.impl.vault
 import androidx.compose.runtime.Immutable
 import kotlinx.collections.immutable.ImmutableList
 import proton.android.pass.commonuimodels.api.ShareUiModelWithItemCount
-import proton.android.pass.data.api.ItemCountSummary
 import proton.android.pass.featurehome.impl.HomeVaultSelection
 
 @Immutable
 data class VaultDrawerUiState(
-    val itemCountSummary: ItemCountSummary,
     val vaultSelection: HomeVaultSelection,
     val shares: ImmutableList<ShareUiModelWithItemCount>,
     val totalTrashedItems: Long

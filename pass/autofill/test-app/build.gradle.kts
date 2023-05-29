add887189326a4ef0c5b403abe00cf0cb835f762
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-05-29 10:12:29 +0200

    bump: version 1.3.1 → 1.3.2

--- pass/autofill/test-app/build.gradle.kts
+++ pass/autofill/test-app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("kotlin-android")
 }
 
-val appVersionName = "1.3.1"
+val appVersionName = "1.3.2"
 val appVersionCode = versionCode(appVersionName)
 val archivesBaseName = "AutofillTestApp-$appVersionName"
 

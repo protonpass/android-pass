3e2968730e217e6410ad1cfebc7c41e853e88fc5
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-05-30 17:14:38 +0200

    fix: set maximum bottomsheet subtitle lines

--- pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/bottomsheet/BottomSheetItemSubtitle.kt
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/bottomsheet/BottomSheetItemSubtitle.kt
@@ -4,6 +4,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import me.proton.core.compose.theme.ProtonTheme
 import me.proton.core.compose.theme.defaultSmallWeak
 
@@ -11,12 +12,15 @@ import me.proton.core.compose.theme.defaultSmallWeak
 fun BottomSheetItemSubtitle(
     modifier: Modifier = Modifier,
     text: String,
-    textcolor: Color = Color.Unspecified
+    color: Color = Color.Unspecified,
+    maxLines: Int = 5
 ) {
     Text(
         modifier = modifier,
         text = text,
         style = ProtonTheme.typography.defaultSmallWeak,
-        color = textcolor
+        color = color,
+        maxLines = maxLines,
+        overflow = TextOverflow.Ellipsis
     )
 }

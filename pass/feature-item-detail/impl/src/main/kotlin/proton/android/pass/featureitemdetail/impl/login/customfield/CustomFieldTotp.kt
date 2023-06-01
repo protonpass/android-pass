8b79e77b9f93fd42d016b8a5d9cca076b8715505
Carlos Quintana <carlos.quintana@proton.ch>
2023-06-01 09:21:45 +0200

    feat(item-detail): view custom fields in detail (except totp)

---
+++ pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/login/customfield/CustomFieldTotp.kt
@@ -0,0 +1,12 @@
+package proton.android.pass.featureitemdetail.impl.login.customfield
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CustomFieldTotp(
+    modifier: Modifier = Modifier,
+) {
+    Text(modifier = modifier, text = "To be done")
+}

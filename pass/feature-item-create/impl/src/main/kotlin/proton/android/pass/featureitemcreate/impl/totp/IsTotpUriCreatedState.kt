1d766dca9d5e63281bd64650064e9d3374c370ac
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-06-02 11:54:31 +0200

    feat: support camera totp on custom totp fields

--- pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/totp/IsTotpUriCreatedState.kt
+++
@@ -1,9 +0,0 @@
-package proton.android.pass.featureitemcreate.impl.totp
-
-import androidx.compose.runtime.Stable
-
-@Stable
-sealed interface IsTotpUriCreatedState {
-    object Unknown : IsTotpUriCreatedState
-    data class Success(val totpUri: String) : IsTotpUriCreatedState
-}

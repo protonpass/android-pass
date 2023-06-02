97366775e38a78c637d695a76d1c25e34d827ce7
Carlos Quintana <carlos.quintana@proton.ch>
2023-06-02 12:10:12 +0200

    feat(home): animate spotlight switch

--- pass/feature-home/impl/src/main/kotlin/proton/android/pass/featurehome/impl/onboardingtips/OnBoardingTipContent.kt
+++ pass/feature-home/impl/src/main/kotlin/proton/android/pass/featurehome/impl/onboardingtips/OnBoardingTipContent.kt
@@ -5,14 +5,17 @@ import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.DismissDirection
 import androidx.compose.material.DismissValue
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.rememberDismissState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import kotlinx.collections.immutable.ImmutableSet
+import kotlinx.coroutines.launch
 import proton.android.pass.featurehome.impl.onboardingtips.OnBoardingTipPage.AUTOFILL
 import proton.android.pass.featurehome.impl.onboardingtips.OnBoardingTipPage.TRIAL
 
@@ -24,13 +27,14 @@ fun OnBoardingTipContent(
     onClick: (OnBoardingTipPage) -> Unit,
     onDismiss: (OnBoardingTipPage) -> Unit
 ) {
-    AnimatedVisibility(
-        visible = tipsSetToShow.isNotEmpty(),
-        enter = expandVertically(),
-        exit = shrinkVertically()
-    ) {
-        Box(modifier = modifier.padding(16.dp)) {
-            if (tipsSetToShow.contains(AUTOFILL)) {
+    val scope = rememberCoroutineScope()
+    Box(modifier = modifier) {
+        AnimatedVisibility(
+            visible = tipsSetToShow.contains(AUTOFILL),
+            enter = expandVertically(),
+            exit = shrinkVertically()
+        ) {
+            Box(modifier = modifier.padding(16.dp)) {
                 val dismissState = rememberDismissState(
                     confirmStateChange = {
                         if (it != DismissValue.Default) {
@@ -42,11 +46,23 @@ fun OnBoardingTipContent(
                 SwipeToDismiss(state = dismissState, background = {}) {
                     AutofillCard(
                         onClick = { onClick(AUTOFILL) },
-                        onDismiss = { onDismiss(AUTOFILL) }
+                        onDismiss = {
+                            scope.launch {
+                                dismissState.dismiss(DismissDirection.EndToStart)
+                                onDismiss(AUTOFILL)
+                            }
+                        }
                     )
                 }
             }
-            if (tipsSetToShow.contains(TRIAL)) {
+        }
+
+        AnimatedVisibility(
+            visible = tipsSetToShow.contains(TRIAL),
+            enter = expandVertically(),
+            exit = shrinkVertically()
+        ) {
+            Box(modifier = modifier.padding(16.dp)) {
                 val dismissState = rememberDismissState(
                     confirmStateChange = {
                         if (it != DismissValue.Default) {
@@ -58,7 +74,12 @@ fun OnBoardingTipContent(
                 SwipeToDismiss(state = dismissState, background = {}) {
                     TrialCard(
                         onClick = { onClick(TRIAL) },
-                        onDismiss = { onDismiss(TRIAL) }
+                        onDismiss = {
+                            scope.launch {
+                                dismissState.dismiss(DismissDirection.EndToStart)
+                                onDismiss(TRIAL)
+                            }
+                        }
                     )
                 }
             }

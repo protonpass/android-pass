c728cffee3f0e86e14416148c0d14456fadd8a29
Carlos Quintana <carlos.quintana@proton.ch>
2023-06-13 10:14:15 +0200

    refactor: move auth to NeedsBiometricAuthImpl and refactor usages so they are aware of the lifecycle

--- app/src/main/java/proton/android/pass/ui/PassApp.kt
+++ app/src/main/java/proton/android/pass/ui/PassApp.kt
@@ -5,12 +5,16 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.google.accompanist.insets.ProvideWindowInsets
-import proton.android.pass.commonui.api.OnResumeCallback
+import proton.android.pass.commonui.api.LifecycleEffect
 import proton.android.pass.commonui.api.PassTheme
 import proton.android.pass.composecomponents.impl.theme.SystemUIEffect
 import proton.android.pass.composecomponents.impl.theme.isDark
@@ -27,11 +31,21 @@ fun PassApp(
 
     SystemUIEffect(isDark = isDark)
 
-    OnResumeCallback { isFirstTime ->
-        if (!isFirstTime) {
-            appViewModel.onAppResumed()
+    val currentConfig = LocalConfiguration.current
+    var orientation by rememberSaveable { mutableStateOf(currentConfig.orientation) }
+
+    LifecycleEffect(
+        onResume = {
+            if (currentConfig.orientation != orientation) {
+                appViewModel.onRotate()
+                orientation = currentConfig.orientation
+            }
+
+        },
+        onStop = {
+            appViewModel.onStop()
         }
-    }
+    )
 
     PassTheme(isDark = isDark) {
         ProvideWindowInsets {
@@ -43,7 +57,7 @@ fun PassApp(
                 appUiState = appUiState,
                 onNavigate = onNavigate,
                 onSnackbarMessageDelivered = { appViewModel.onSnackbarMessageDelivered() },
-                onAuthPerformed = { appViewModel.onAuthPerformed() }
+                onAuthPerformed = { }
             )
         }
     }

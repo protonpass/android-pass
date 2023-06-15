05852667ce94919b364f7721a28a49a8add42a3d
Carlos Quintana <carlos.quintana@proton.ch>
2023-06-15 17:15:48 +0200

    feat(preferences): credit card feature flag

--- pass/feature-feature-flags/impl/src/main/kotlin/proton/android/pass/featurefeatureflags/impl/FeatureFlagsViewModel.kt
+++ pass/feature-feature-flags/impl/src/main/kotlin/proton/android/pass/featurefeatureflags/impl/FeatureFlagsViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import proton.android.pass.preferences.FeatureFlag
@@ -16,8 +16,15 @@ class FeatureFlagsViewModel @Inject constructor(
     private val ffRepository: FeatureFlagsPreferencesRepository
 ) : ViewModel() {
 
-    val state = ffRepository.get<Boolean>(FeatureFlag.CUSTOM_FIELDS_ENABLED)
-        .map { mapOf(FeatureFlag.CUSTOM_FIELDS_ENABLED to it) }
+    val state = combine(
+        ffRepository.get<Boolean>(FeatureFlag.CUSTOM_FIELDS_ENABLED),
+        ffRepository.get<Boolean>(FeatureFlag.CREDIT_CARDS_ENABLED),
+    ) { customFields, creditCards ->
+        mapOf(
+            FeatureFlag.CUSTOM_FIELDS_ENABLED to customFields,
+            FeatureFlag.CREDIT_CARDS_ENABLED to creditCards,
+        )
+    }
         .stateIn(viewModelScope, SharingStarted.Eagerly, emptyMap())
 
     fun <T> override(featureFlag: FeatureFlag, value: T) = viewModelScope.launch {
--- pass/preferences/impl/src/main/java/proton/android/pass/preferences/FeatureFlagsPreferencesRepositoryImpl.kt
+++ pass/preferences/impl/src/main/java/proton/android/pass/preferences/FeatureFlagsPreferencesRepositoryImpl.kt
@@ -9,10 +9,12 @@ import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.runBlocking
 import me.proton.android.pass.preferences.BoolFlagPrefProto
+import me.proton.android.pass.preferences.BooleanPrefProto
 import me.proton.core.accountmanager.domain.AccountManager
 import me.proton.core.featureflag.domain.FeatureFlagManager
 import me.proton.core.featureflag.domain.entity.FeatureId
 import proton.android.pass.log.api.PassLogger
+import proton.android.pass.preferences.FeatureFlag.CREDIT_CARDS_ENABLED
 import proton.android.pass.preferences.FeatureFlag.CUSTOM_FIELDS_ENABLED
 import java.io.IOException
 import javax.inject.Inject
@@ -28,45 +30,55 @@ class FeatureFlagsPreferencesRepositoryImpl @Inject constructor(
 
     override fun <T> get(featureFlag: FeatureFlag): Flow<T> =
         when (featureFlag) {
-            CUSTOM_FIELDS_ENABLED -> {
-                val key = featureFlag.key
-                if (key != null) {
-                    accountManager.getPrimaryUserId()
-                        .filterNotNull()
-                        .flatMapLatest { userId ->
-                            featureFlagManager.observe(userId = userId, featureId = FeatureId(key))
-                        }
-                        .flatMapLatest { ff ->
-                            dataStore.data
-                                .catch { exception -> handleExceptions(exception) }
-                                .map { preferences ->
-                                    fromBooleanPrefProto(
-                                        preferences.customFieldsEnabled.value,
-                                        ff?.value ?: false
-                                    ) as T
-                                }
-                        }
-                } else {
-                    dataStore.data
-                        .catch { exception -> handleExceptions(exception) }
-                        .map { fromBooleanPrefProto(it.customFieldsEnabled.value) as T }
-                }
-            }
+            CUSTOM_FIELDS_ENABLED -> getFeatureFlag(featureFlag.key) { customFieldsEnabled.value }
+            CREDIT_CARDS_ENABLED -> getFeatureFlag(featureFlag.key) { creditCardsEnabled.value }
         }
 
     override fun <T> set(featureFlag: FeatureFlag, value: T?): Result<Unit> =
         when (featureFlag) {
-            CUSTOM_FIELDS_ENABLED -> runCatching {
-                runBlocking {
-                    dataStore.updateData { prefs ->
-                        prefs.toBuilder()
-                            .setCustomFieldsEnabled(boolFlagPrefProto(value))
-                            .build()
-                    }
-                }
-                return@runCatching
+            CUSTOM_FIELDS_ENABLED -> setFeatureFlag {
+                setCustomFieldsEnabled(boolFlagPrefProto(value))
+            }
+            CREDIT_CARDS_ENABLED -> setFeatureFlag {
+                setCreditCardsEnabled(boolFlagPrefProto(value))
             }
         }
+
+    private fun <T> getFeatureFlag(key: String?, prefGetter: FeatureFlagsPreferences.() -> BooleanPrefProto): Flow<T> =
+        if (key != null) {
+            accountManager.getPrimaryUserId()
+                .filterNotNull()
+                .flatMapLatest { userId ->
+                    featureFlagManager.observe(userId = userId, featureId = FeatureId(key))
+                }
+                .flatMapLatest { ff ->
+                    dataStore.data
+                        .catch { exception -> handleExceptions(exception) }
+                        .map { preferences ->
+                            fromBooleanPrefProto(
+                                prefGetter(preferences),
+                                ff?.value ?: false
+                            ) as T
+                        }
+                }
+        } else {
+            dataStore.data
+                .catch { exception -> handleExceptions(exception) }
+                .map {
+                    fromBooleanPrefProto(prefGetter(it)) as T
+                }
+        }
+
+    private fun setFeatureFlag(setter: FeatureFlagsPreferences.Builder.() -> Unit) = runCatching {
+        runBlocking {
+            dataStore.updateData { prefs ->
+                val a = prefs.toBuilder()
+                setter(a)
+                a.build()
+            }
+        }
+        return@runCatching
+    }
 
     private fun <T> boolFlagPrefProto(
         value: T?

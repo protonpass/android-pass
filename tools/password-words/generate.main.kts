e16c8c368b3fb3a63a4dbd895a2569631f7edcce
Carlos Quintana <carlos.quintana@proton.ch>
2023-06-15 12:38:03 +0200

    fix: dangling references to scripts directory

--- tools/password-words/generate.main.kts
+++ tools/password-words/generate.main.kts
@@ -4,7 +4,7 @@ import java.nio.file.Paths
 
 val PROJECT_ROOT_DIR: File = __FILE__.absoluteFile // this script
     .parentFile // public-suffix-dir
-    .parentFile // scripts
+    .parentFile // tools
     .parentFile // ProtonPass
 
 val OUTPUT_FILE = Paths.get(PROJECT_ROOT_DIR.absolutePath, "pass", "password", "api", "src", "main", "kotlin", "proton", "android", "pass", "password", "api", "WordList.kt")

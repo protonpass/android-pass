7c7c3b06c5fb3ecddb98fdb2d5da3d170787af8c
Carlos Quintana <carlos.quintana@proton.ch>
2023-06-19 16:23:26 +0200

    chore: add previews for CreditCardRow

--- pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/item/CreditCardRowPreviewProvider.kt
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/item/CreditCardRowPreviewProvider.kt
@@ -1,13 +1,54 @@
 package proton.android.pass.composecomponents.impl.item
 
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import kotlinx.datetime.Clock
 import proton.android.pass.commonuimodels.api.ItemUiModel
+import proton.pass.domain.CreditCardType
+import proton.pass.domain.HiddenState
+import proton.pass.domain.ItemContents
+import proton.pass.domain.ItemId
+import proton.pass.domain.ShareId
 
 class CreditCardRowPreviewProvider : PreviewParameterProvider<CreditCardRowParameter> {
     override val values: Sequence<CreditCardRowParameter>
-        get() = sequence {
+        get() = sequenceOf(
+            with(
+                title = "A credit card",
+                note = "Example credit card",
+                number = "0000000000000000"
+            ),
+            with(
+                title = "A credit card",
+                note = "Example credit card",
+                number = "0000000000000000",
+                highlight = "credit"
+            )
+        )
 
-        }
+    companion object {
+        private fun with(title: String, note: String = "", number: String, highlight: String = "") =
+            CreditCardRowParameter(
+                model = ItemUiModel(
+                    id = ItemId("123"),
+                    shareId = ShareId("456"),
+                    contents = ItemContents.CreditCard(
+                        title = title,
+                        note = note,
+                        number = number,
+                        cardHolder = "Name",
+                        expirationDate = "2030-01",
+                        pin = HiddenState.Concealed(""),
+                        cvv = HiddenState.Concealed(""),
+                        type = CreditCardType.Visa
+                    ),
+                    state = 0,
+                    createTime = Clock.System.now(),
+                    modificationTime = Clock.System.now(),
+                    lastAutofillTime = Clock.System.now()
+                ),
+                highlight = highlight
+            )
+    }
 }
 
 data class CreditCardRowParameter(

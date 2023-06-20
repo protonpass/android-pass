0d474e91c4219c156c56fd71bb10333471a903f2
Carlos Quintana <carlos.quintana@proton.ch>
2023-06-20 16:16:48 +0200

    feat(item-detail): add support for credit card

---
+++ pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/creditcard/CreditCardDetailViewModel.kt
@@ -0,0 +1,332 @@
+/*
+ * Copyright (c) 2023 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.featureitemdetail.impl.creditcard
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import proton.android.pass.clipboard.api.ClipboardManager
+import proton.android.pass.common.api.LoadingResult
+import proton.android.pass.common.api.asLoadingResult
+import proton.android.pass.common.api.combineN
+import proton.android.pass.common.api.flatMap
+import proton.android.pass.common.api.getOrNull
+import proton.android.pass.common.api.map
+import proton.android.pass.commonui.api.SavedStateHandleProvider
+import proton.android.pass.commonui.api.require
+import proton.android.pass.commonui.api.toUiModel
+import proton.android.pass.commonuimodels.api.ItemUiModel
+import proton.android.pass.composecomponents.impl.uievents.IsLoadingState
+import proton.android.pass.composecomponents.impl.uievents.IsPermanentlyDeletedState
+import proton.android.pass.composecomponents.impl.uievents.IsRestoredFromTrashState
+import proton.android.pass.composecomponents.impl.uievents.IsSentToTrashState
+import proton.android.pass.crypto.api.context.EncryptionContext
+import proton.android.pass.crypto.api.context.EncryptionContextProvider
+import proton.android.pass.crypto.api.toEncryptedByteArray
+import proton.android.pass.data.api.usecases.CanPerformPaidAction
+import proton.android.pass.data.api.usecases.DeleteItem
+import proton.android.pass.data.api.usecases.GetItemByIdWithVault
+import proton.android.pass.data.api.usecases.RestoreItem
+import proton.android.pass.data.api.usecases.TrashItem
+import proton.android.pass.featureitemdetail.impl.DetailSnackbarMessages
+import proton.android.pass.featureitemdetail.impl.ItemDelete
+import proton.android.pass.log.api.PassLogger
+import proton.android.pass.navigation.api.CommonNavArgId
+import proton.android.pass.notifications.api.SnackbarDispatcher
+import proton.android.pass.telemetry.api.EventItemType
+import proton.android.pass.telemetry.api.TelemetryManager
+import proton.pass.domain.HiddenState
+import proton.pass.domain.ItemContents
+import proton.pass.domain.ItemId
+import proton.pass.domain.ShareId
+import proton.pass.domain.Vault
+import javax.inject.Inject
+
+@HiltViewModel
+class CreditCardDetailViewModel @Inject constructor(
+    private val snackbarDispatcher: SnackbarDispatcher,
+    private val clipboardManager: ClipboardManager,
+    private val encryptionContextProvider: EncryptionContextProvider,
+    private val trashItem: TrashItem,
+    private val deleteItem: DeleteItem,
+    private val restoreItem: RestoreItem,
+    private val telemetryManager: TelemetryManager,
+    canPerformPaidAction: CanPerformPaidAction,
+    getItemByIdWithVault: GetItemByIdWithVault,
+    savedStateHandle: SavedStateHandleProvider
+) : ViewModel() {
+
+    private val shareId: ShareId =
+        ShareId(savedStateHandle.get().require(CommonNavArgId.ShareId.key))
+    private val itemId: ItemId =
+        ItemId(savedStateHandle.get().require(CommonNavArgId.ItemId.key))
+
+    private val isLoadingState: MutableStateFlow<IsLoadingState> =
+        MutableStateFlow(IsLoadingState.NotLoading)
+    private val isItemSentToTrashState: MutableStateFlow<IsSentToTrashState> =
+        MutableStateFlow(IsSentToTrashState.NotSent)
+    private val isPermanentlyDeletedState: MutableStateFlow<IsPermanentlyDeletedState> =
+        MutableStateFlow(IsPermanentlyDeletedState.NotDeleted)
+    private val isRestoredFromTrashState: MutableStateFlow<IsRestoredFromTrashState> =
+        MutableStateFlow(IsRestoredFromTrashState.NotRestored)
+
+    private val fieldVisibilityFlow: MutableStateFlow<FieldVisibility> = MutableStateFlow(FieldVisibility())
+
+    private val canPerformPaidActionFlow = canPerformPaidAction().asLoadingResult()
+
+    private data class FieldVisibility(
+        val cardNumber: Boolean = false,
+        val cvv: Boolean = false,
+    )
+
+    private data class CreditCardItemInfo(
+        val itemUiModel: ItemUiModel,
+        val cardNumberState: CardNumberState,
+        val vault: Vault,
+        val hasMoreThanOneVault: Boolean
+    )
+
+    private val itemInfoFlow: Flow<LoadingResult<CreditCardItemInfo>> = combine(
+        getItemByIdWithVault(shareId, itemId).take(1).asLoadingResult(),
+        canPerformPaidActionFlow,
+        fieldVisibilityFlow
+    ) { detailsResult, paidActionResult, fieldVisibility ->
+        paidActionResult.flatMap { canShowFields ->
+            detailsResult.map { details ->
+                val (itemUiModel, cardNumber) = encryptionContextProvider.withEncryptionContext {
+                    val model = details.item.toUiModel(this)
+                    var contents = model.contents as ItemContents.CreditCard
+
+                    val cardNumber = if (fieldVisibility.cardNumber) {
+                        CardNumberState.Visible(contents.number)
+                    } else {
+                        val start = contents.number.take(4)
+                        val end = contents.number.takeLast(4)
+                        CardNumberState.Masked("$start •••• •••• $end")
+                    }
+
+                    contents = contents.copy(
+                        expirationDate = adaptExpirationDate(contents.expirationDate)
+                    )
+
+                    if (canShowFields) {
+                        val cvv = if (fieldVisibility.cvv) {
+                            HiddenState.Revealed(
+                                encrypted = contents.cvv.encrypted,
+                                clearText = decrypt(contents.cvv.encrypted)
+                            )
+                        } else {
+                            encryptedOrEmpty(contents.cvv, this)
+                        }
+                        contents = contents.copy(cvv = cvv)
+                    }
+
+                    model.copy(contents = contents) to cardNumber
+                }
+
+                CreditCardItemInfo(
+                    itemUiModel = itemUiModel,
+                    vault = details.vault,
+                    cardNumberState = cardNumber,
+                    hasMoreThanOneVault = details.hasMoreThanOneVault
+                )
+            }
+        }
+
+    }.distinctUntilChanged()
+
+    val uiState: StateFlow<CreditCardDetailUiState> = combineN(
+        itemInfoFlow,
+        isLoadingState,
+        isItemSentToTrashState,
+        isPermanentlyDeletedState,
+        isRestoredFromTrashState,
+        canPerformPaidActionFlow,
+    ) { itemDetails, isLoading, isItemSentToTrash, isPermanentlyDeleted,
+        isRestoredFromTrash, canPerformPaidActionResult ->
+        when (itemDetails) {
+            is LoadingResult.Error -> {
+                snackbarDispatcher(DetailSnackbarMessages.InitError)
+                CreditCardDetailUiState.Error
+            }
+
+            LoadingResult.Loading -> CreditCardDetailUiState.NotInitialised
+            is LoadingResult.Success -> {
+                val details = itemDetails.data
+                val vault = if (details.hasMoreThanOneVault) {
+                    details.vault
+                } else {
+                    null
+                }
+
+                val isPaid = canPerformPaidActionResult.getOrNull() == true
+                val canMigrate = when {
+                    isPaid -> true
+                    vault?.isPrimary == true -> false
+                    else -> true
+                }
+
+                CreditCardDetailUiState.Success(
+                    itemContent = CreditCardDetailUiState.ItemContent(
+                        model = details.itemUiModel,
+                        cardNumber = details.cardNumberState,
+                    ),
+                    vault = vault,
+                    isLoading = isLoading.value(),
+                    isItemSentToTrash = isItemSentToTrash.value(),
+                    isPermanentlyDeleted = isPermanentlyDeleted.value(),
+                    isRestoredFromTrash = isRestoredFromTrash.value(),
+                    canMigrate = canMigrate,
+                )
+            }
+        }
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000L),
+        initialValue = CreditCardDetailUiState.NotInitialised
+    )
+
+    fun onMoveToTrash(shareId: ShareId, itemId: ItemId) = viewModelScope.launch {
+        isLoadingState.update { IsLoadingState.Loading }
+        runCatching { trashItem(shareId = shareId, itemId = itemId) }
+            .onSuccess {
+                isItemSentToTrashState.update { IsSentToTrashState.Sent }
+                snackbarDispatcher(DetailSnackbarMessages.ItemMovedToTrash)
+            }
+            .onFailure {
+                snackbarDispatcher(DetailSnackbarMessages.ItemNotMovedToTrash)
+                PassLogger.d(TAG, it, "Could not delete item")
+            }
+        isLoadingState.update { IsLoadingState.NotLoading }
+    }
+
+    fun onPermanentlyDelete(itemUiModel: ItemUiModel) =
+        viewModelScope.launch {
+            isLoadingState.update { IsLoadingState.Loading }
+            runCatching {
+                deleteItem(shareId = itemUiModel.shareId, itemId = itemUiModel.id)
+            }.onSuccess {
+                telemetryManager.sendEvent(ItemDelete(EventItemType.from(itemUiModel.contents)))
+                isPermanentlyDeletedState.update { IsPermanentlyDeletedState.Deleted }
+                snackbarDispatcher(DetailSnackbarMessages.ItemPermanentlyDeleted)
+                PassLogger.i(TAG, "Item deleted successfully")
+            }.onFailure {
+                snackbarDispatcher(DetailSnackbarMessages.ItemNotPermanentlyDeleted)
+                PassLogger.i(TAG, it, "Could not delete item")
+            }
+            isLoadingState.update { IsLoadingState.NotLoading }
+        }
+
+    fun onItemRestore(shareId: ShareId, itemId: ItemId) = viewModelScope.launch {
+        isLoadingState.update { IsLoadingState.Loading }
+        runCatching {
+            restoreItem(shareId = shareId, itemId = itemId)
+        }.onSuccess {
+            isRestoredFromTrashState.update { IsRestoredFromTrashState.Restored }
+            PassLogger.i(TAG, "Item restored successfully")
+            snackbarDispatcher(DetailSnackbarMessages.ItemRestored)
+        }.onFailure {
+            PassLogger.i(TAG, it, "Error restoring item")
+            snackbarDispatcher(DetailSnackbarMessages.ItemNotRestored)
+        }
+        isLoadingState.update { IsLoadingState.NotLoading }
+    }
+
+    fun copyCardHolderName() = viewModelScope.launch {
+        modelFromState()?.let {
+            clipboardManager.copyToClipboard(it.cardHolder)
+            snackbarDispatcher(DetailSnackbarMessages.CardHolderCopiedToClipboard)
+        }
+    }
+
+    fun copyCvv() = viewModelScope.launch {
+        modelFromState()?.let {
+            val cvv = when (val content = it.cvv) {
+                is HiddenState.Empty -> return@let
+                is HiddenState.Concealed -> {
+                    encryptionContextProvider.withEncryptionContext {
+                        decrypt(content.encrypted)
+                    }
+                }
+                is HiddenState.Revealed -> content.clearText
+            }
+            clipboardManager.copyToClipboard(cvv, isSecure = true)
+            snackbarDispatcher(DetailSnackbarMessages.CardVerificationNumberCopiedToClipboard)
+        }
+    }
+
+    fun copyNumber() = viewModelScope.launch {
+        modelFromState()?.let {
+            clipboardManager.copyToClipboard(it.number)
+            snackbarDispatcher(DetailSnackbarMessages.CardNumberCopiedToClipboard)
+        }
+    }
+
+    fun toggleCvv() = viewModelScope.launch {
+        fieldVisibilityFlow.update { it.copy(cvv = !it.cvv) }
+    }
+
+    fun toggleNumber() = viewModelScope.launch {
+        fieldVisibilityFlow.update { it.copy(cardNumber = !it.cardNumber) }
+    }
+
+    private fun modelFromState(): ItemContents.CreditCard? {
+        val state = uiState.value
+        return if (state is CreditCardDetailUiState.Success) {
+            state.itemContent.model.contents as? ItemContents.CreditCard
+        } else {
+            null
+        }
+    }
+
+    private fun encryptedOrEmpty(state: HiddenState, context: EncryptionContext): HiddenState {
+        val isEmpty = context.decrypt(state.encrypted.toEncryptedByteArray()).isEmpty()
+        return if (isEmpty) {
+            HiddenState.Empty(context.encrypt(""))
+        } else {
+            state
+        }
+    }
+
+    private fun adaptExpirationDate(input: String): String {
+        val parts = input.split("-")
+        return if (parts.size == 2) {
+            val year = parts[0]
+            val month = parts[1].padStart(2, '0')
+            "$month / $year"
+        } else {
+            input
+        }
+    }
+
+    companion object {
+        private const val TAG = "CreditCardDetailViewModel"
+    }
+
+}
---
+++ pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/creditcard/CreditCardTitle.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2023 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.featureitemdetail.impl.creditcard
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import proton.android.pass.commonui.api.PassTheme
+import proton.android.pass.composecomponents.impl.item.icon.CreditCardIcon
+import proton.android.pass.featureitemdetail.impl.common.ItemTitleInput
+import proton.android.pass.featureitemdetail.impl.common.ItemTitleText
+import proton.android.pass.featureitemdetail.impl.common.ThemeItemTitleProvider
+import proton.android.pass.featureitemdetail.impl.common.VaultNameSubtitle
+import proton.pass.domain.Vault
+
+@Composable
+fun CreditCardTitle(
+    modifier: Modifier = Modifier,
+    title: String,
+    vault: Vault?
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        CreditCardIcon(size = 60, shape = PassTheme.shapes.squircleMediumLargeShape)
+        Column(modifier = Modifier.fillMaxWidth()) {
+            ItemTitleText(text = title)
+            VaultNameSubtitle(vault = vault)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AliasTitlePreview(
+    @PreviewParameter(ThemeItemTitleProvider::class) input: Pair<Boolean, ItemTitleInput>
+) {
+    PassTheme(isDark = input.first) {
+        Surface {
+            CreditCardTitle(
+                title = input.second.title,
+                vault = input.second.vault
+            )
+        }
+    }
+}
+

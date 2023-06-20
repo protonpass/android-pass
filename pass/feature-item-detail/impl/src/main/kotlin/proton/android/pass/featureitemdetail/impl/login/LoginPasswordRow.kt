ba798e9080e854c6ad38758140ba184ad07e1eda
Carlos Quintana <carlos.quintana@proton.ch>
2023-06-20 16:16:34 +0200

    refactor(item-detail): refactor HiddenContentRow for making it reusable

--- pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/login/LoginPasswordRow.kt
+++ pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/login/LoginPasswordRow.kt
@@ -19,29 +19,16 @@
 package proton.android.pass.featureitemdetail.impl.login
 
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import me.proton.core.compose.theme.ProtonTheme
 import proton.android.pass.commonui.api.PassTheme
-import proton.android.pass.commonui.api.asAnnotatedString
 import proton.android.pass.commonui.api.toPasswordAnnotatedString
-import proton.android.pass.composecomponents.impl.container.Circle
-import proton.android.pass.composecomponents.impl.item.SectionTitle
 import proton.android.pass.featureitemdetail.impl.R
-import proton.android.pass.featureitemdetail.impl.common.SectionSubtitle
+import proton.android.pass.featureitemdetail.impl.common.HiddenContentRow
 import proton.pass.domain.HiddenState
 import me.proton.core.presentation.R as CoreR
 
@@ -52,77 +39,37 @@ internal fun LoginPasswordRow(
     modifier: Modifier = Modifier,
     passwordHiddenState: HiddenState,
     label: String = stringResource(R.string.field_password),
-    @DrawableRes iconRes: Int = me.proton.core.presentation.R.drawable.ic_proton_key,
+    @DrawableRes iconRes: Int = CoreR.drawable.ic_proton_key,
     onTogglePasswordClick: () -> Unit,
     onCopyPasswordClick: () -> Unit
 ) {
-    val (sectionContent, icon, actionContent) = when (passwordHiddenState) {
-        is HiddenState.Concealed -> LoginPasswordUIState(
-            sectionContent = "•".repeat(CHAR_AMOUNT),
-            icon = CoreR.drawable.ic_proton_eye,
-            actionContent = R.string.action_reveal_password
-        )
-
-        is HiddenState.Revealed -> LoginPasswordUIState(
-            sectionContent = passwordHiddenState.clearText,
-            icon = CoreR.drawable.ic_proton_eye_slash,
-            actionContent = R.string.action_conceal_password
-        )
-
-        is HiddenState.Empty -> LoginPasswordUIState(
-            sectionContent = "",
-            icon = null,
-            actionContent = null
-        )
+    val (sectionContent, isContentVisible) = when (passwordHiddenState) {
+        is HiddenState.Concealed -> "•".repeat(CHAR_AMOUNT) to false
+        is HiddenState.Revealed -> passwordHiddenState.clearText to true
+        is HiddenState.Empty -> "" to false
     }
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .clickable { onCopyPasswordClick() }
-            .padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Icon(
-            painter = painterResource(iconRes),
-            contentDescription = null,
-            tint = PassTheme.colors.loginInteractionNorm
-        )
-        Spacer(modifier = Modifier.width(16.dp))
-        Column(modifier = Modifier.weight(1f)) {
-            SectionTitle(text = label)
-            Spacer(modifier = Modifier.height(8.dp))
-            when (passwordHiddenState) {
-                is HiddenState.Empty -> {}
-                is HiddenState.Concealed -> SectionSubtitle(text = sectionContent.asAnnotatedString())
-                is HiddenState.Revealed -> {
-                    SectionSubtitle(
-                        text = sectionContent.toPasswordAnnotatedString(
-                            digitColor = ProtonTheme.colors.notificationError,
-                            symbolColor = ProtonTheme.colors.notificationSuccess,
-                            letterColor = ProtonTheme.colors.textNorm
-                        )
-                    )
-                }
 
-            }
-        }
-        if (icon != null && actionContent != null) {
-            Circle(
-                backgroundColor = PassTheme.colors.loginInteractionNormMinor1,
-                onClick = { onTogglePasswordClick() }
-            ) {
-                Icon(
-                    painter = painterResource(icon),
-                    contentDescription = stringResource(actionContent),
-                    tint = PassTheme.colors.loginInteractionNormMajor2
-                )
-            }
-        }
-    }
+    HiddenContentRow(
+        modifier = modifier,
+        isContentVisible = isContentVisible,
+        sectionContent = sectionContent.toPasswordAnnotatedString(
+            digitColor = ProtonTheme.colors.notificationError,
+            symbolColor = ProtonTheme.colors.notificationSuccess,
+            letterColor = ProtonTheme.colors.textNorm
+        ),
+        label = label,
+        icon = {
+            Icon(
+                painter = painterResource(iconRes),
+                contentDescription = null,
+                tint = PassTheme.colors.loginInteractionNorm
+            )
+        },
+        toggleIconBackground = PassTheme.colors.loginInteractionNormMinor1,
+        toggleIconForeground = PassTheme.colors.loginInteractionNormMajor2,
+        revealAction = stringResource(R.string.action_reveal_password),
+        concealAction = stringResource(R.string.action_conceal_password),
+        onRowClick = onCopyPasswordClick,
+        onToggleClick = onTogglePasswordClick
+    )
 }
-
-data class LoginPasswordUIState(
-    val sectionContent: String,
-    val icon: Int?,
-    val actionContent: Int?
-)

074f9b80a8af8e5e49f0bdde2d37da1ebc0c3337
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-06-20 11:07:55 +0200

    feat: start new create credit card screen

---
+++ pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/creditcard/CardNumberInput.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2023 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.featureitemcreate.impl.creditcard
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import me.proton.core.compose.theme.ProtonTheme
+import me.proton.core.compose.theme.defaultNorm
+import proton.android.pass.composecomponents.impl.form.ProtonTextField
+import proton.android.pass.composecomponents.impl.form.ProtonTextFieldLabel
+import proton.android.pass.composecomponents.impl.form.ProtonTextFieldPlaceHolder
+import proton.android.pass.composecomponents.impl.form.SmallCrossIconButton
+import proton.android.pass.featureitemcreate.impl.R
+
+@Composable
+internal fun CardNumberInput(
+    modifier: Modifier = Modifier,
+    value: String,
+    onChange: (String) -> Unit
+) {
+    ProtonTextField(
+        modifier = modifier.padding(start = 0.dp, top = 16.dp, end = 4.dp, bottom = 16.dp),
+        value = value,
+        onChange = onChange,
+        moveToNextOnEnter = true,
+        textStyle = ProtonTheme.typography.defaultNorm,
+        editable = true,
+        label = { ProtonTextFieldLabel(text = stringResource(id = R.string.field_card_number_title)) },
+        placeholder = { ProtonTextFieldPlaceHolder(text = stringResource(id = R.string.field_card_number_hint)) },
+        leadingIcon = {
+            Icon(
+                painter = painterResource(me.proton.core.presentation.R.drawable.ic_proton_credit_card),
+                contentDescription = null,
+                tint = ProtonTheme.colors.iconWeak
+            )
+        },
+        trailingIcon = {
+            if (value.isNotEmpty()) {
+                SmallCrossIconButton(enabled = true) { onChange("") }
+            }
+        }
+    )
+}

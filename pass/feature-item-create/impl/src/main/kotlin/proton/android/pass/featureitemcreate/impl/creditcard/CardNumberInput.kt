270ae6dfa9eb8a875f00bbf2261b4351ad1577fd
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-06-20 13:02:27 +0200

    feat: add credit cards field restrictions

--- pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/creditcard/CardNumberInput.kt
+++ pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/creditcard/CardNumberInput.kt
@@ -19,14 +19,24 @@
 package proton.android.pass.featureitemcreate.impl.creditcard
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Icon
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import me.proton.core.compose.theme.ProtonTheme
 import me.proton.core.compose.theme.defaultNorm
+import proton.android.pass.commonui.api.PassTheme
+import proton.android.pass.commonui.api.ThemePreviewProvider
 import proton.android.pass.composecomponents.impl.form.ProtonTextField
 import proton.android.pass.composecomponents.impl.form.ProtonTextFieldLabel
 import proton.android.pass.composecomponents.impl.form.ProtonTextFieldPlaceHolder
@@ -45,6 +55,20 @@ internal fun CardNumberInput(
         onChange = onChange,
         moveToNextOnEnter = true,
         textStyle = ProtonTheme.typography.defaultNorm,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        visualTransformation = { text ->
+            if (text.isEmpty())
+                return@ProtonTextField TransformedText(text, OffsetMapping.Identity)
+            val split = text.chunked(4)
+            TransformedText(
+                AnnotatedString(split.joinToString(" ")),
+                object : OffsetMapping {
+                    override fun originalToTransformed(offset: Int): Int = offset + split.size - 1
+
+                    override fun transformedToOriginal(offset: Int): Int = offset - split.size + 1
+                }
+            )
+        },
         editable = true,
         label = { ProtonTextFieldLabel(text = stringResource(id = R.string.field_card_number_title)) },
         placeholder = { ProtonTextFieldPlaceHolder(text = stringResource(id = R.string.field_card_number_hint)) },
@@ -61,4 +85,19 @@ internal fun CardNumberInput(
             }
         }
     )
+}
+
+@Preview
+@Composable
+fun CardNumberInputPreview(
+    @PreviewParameter(ThemePreviewProvider::class) input: Boolean
+) {
+    PassTheme(isDark = input) {
+        Surface {
+            CardNumberInput(
+                value = "1234567891234567",
+                onChange = {}
+            )
+        }
+    }
 }

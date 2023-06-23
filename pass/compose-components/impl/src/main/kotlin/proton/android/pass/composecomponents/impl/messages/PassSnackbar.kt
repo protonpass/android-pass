df8d2efefece88a0035e737f8443de551adb3b27
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-06-23 13:01:30 +0200

    refactor: proper handling of snackbar actions

--- pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/messages/PassSnackbar.kt
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/messages/PassSnackbar.kt
@@ -18,6 +18,7 @@
 
 package proton.android.pass.composecomponents.impl.messages
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -27,6 +28,7 @@ import androidx.compose.material.SnackbarDuration
 import androidx.compose.material.SnackbarHostState
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
@@ -53,16 +55,24 @@ fun PassSnackbar(
         modifier = modifier.padding(12.dp),
         content = { Text(snackbarData.message) },
         action = {
-            IconButton(
-                onClick = { snackbarData.performAction() },
-                content = {
-                    Icon(
-                        painter = painterResource(me.proton.core.presentation.R.drawable.ic_proton_cross_small),
-                        contentDescription = null,
-                        tint = ProtonTheme.colors.textInverted
-                    )
+            Row {
+                val label = snackbarData.actionLabel
+                if (!label.isNullOrBlank()) {
+                    TextButton(onClick = { snackbarData.performAction() }) {
+                        Text(text = label)
+                    }
                 }
-            )
+                IconButton(
+                    onClick = { snackbarData.dismiss() },
+                    content = {
+                        Icon(
+                            painter = painterResource(me.proton.core.presentation.R.drawable.ic_proton_cross_small),
+                            contentDescription = null,
+                            tint = ProtonTheme.colors.textInverted
+                        )
+                    }
+                )
+            }
         },
         actionOnNewLine = false,
         shape = ProtonTheme.shapes.medium,
@@ -83,7 +93,12 @@ fun PassSnackbarHost(modifier: Modifier = Modifier, snackbarHostState: PassSnack
     ProtonSnackbarHost(
         modifier = modifier,
         hostState = snackbarHostState.protonSnackbarHostState,
-        snackbar = { PassSnackbar(type = snackbarHostState.protonSnackbarHostState.type, snackbarData = it) }
+        snackbar = {
+            PassSnackbar(
+                type = snackbarHostState.protonSnackbarHostState.type,
+                snackbarData = it
+            )
+        }
     )
 }
 

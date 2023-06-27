bf23175e4a80412e97b1906b9745cec22a23c43e
Carlos Quintana <carlos.quintana@proton.ch>
2023-06-27 10:55:55 +0200

    chore: create AutofillDebug feature flag

--- pass/feature-feature-flags/impl/src/main/kotlin/proton/android/pass/featurefeatureflags/impl/FeatureFlagsViewModel.kt
+++ pass/feature-feature-flags/impl/src/main/kotlin/proton/android/pass/featurefeatureflags/impl/FeatureFlagsViewModel.kt
@@ -22,7 +22,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import proton.android.pass.preferences.FeatureFlag
@@ -34,12 +34,15 @@ class FeatureFlagsViewModel @Inject constructor(
     private val ffRepository: FeatureFlagsPreferencesRepository
 ) : ViewModel() {
 
-    val state = ffRepository.get<Boolean>(FeatureFlag.CREDIT_CARDS_ENABLED)
-        .map { creditCards ->
-            mapOf(
-                FeatureFlag.CREDIT_CARDS_ENABLED to creditCards,
-            )
-        }
+    val state = combine(
+        ffRepository.get<Boolean>(FeatureFlag.CREDIT_CARDS_ENABLED),
+        ffRepository.get<Boolean>(FeatureFlag.AUTOFILL_DEBUG_MODE),
+    ) { creditCards, autofillDebug ->
+        mapOf(
+            FeatureFlag.CREDIT_CARDS_ENABLED to creditCards,
+            FeatureFlag.AUTOFILL_DEBUG_MODE to autofillDebug,
+        )
+    }
         .stateIn(viewModelScope, SharingStarted.Eagerly, emptyMap())
 
     fun <T> override(featureFlag: FeatureFlag, value: T) = viewModelScope.launch {

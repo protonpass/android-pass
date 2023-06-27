259fdd6e828da20e6fa930005fa77bb353f5683b
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-06-27 09:05:20 +0200

    fix: auth back navigation not triggering

--- app/src/main/java/proton/android/pass/App.kt
+++ app/src/main/java/proton/android/pass/App.kt
@@ -22,7 +22,10 @@ import android.app.Application
 import coil.ImageLoader
 import coil.ImageLoaderFactory
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.runBlocking
 import proton.android.pass.initializer.MainInitializer
+import proton.android.pass.preferences.HasAuthenticated
+import proton.android.pass.preferences.UserPreferencesRepository
 import javax.inject.Inject
 import javax.inject.Provider
 
@@ -30,11 +33,16 @@ import javax.inject.Provider
 class App : Application(), ImageLoaderFactory {
     @Inject
     lateinit var imageLoader: Provider<ImageLoader>
+    @Inject
+    lateinit var preferenceRepository: UserPreferencesRepository
 
     override fun newImageLoader(): ImageLoader = imageLoader.get()
 
     override fun onCreate() {
         super.onCreate()
         MainInitializer.init(this)
+        runBlocking {
+            preferenceRepository.setHasAuthenticated(HasAuthenticated.NotAuthenticated)
+        }
     }
 }

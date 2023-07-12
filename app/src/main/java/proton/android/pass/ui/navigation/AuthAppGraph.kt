0358fb30ad1b4a5041a0562cbbac66a95ba90fcb
Carlos Quintana <carlos.quintana@proton.ch>
2023-07-12 09:02:00 +0200

    chore(app): create auth graph with Auth screen and sign out dialog

---
+++ app/src/main/java/proton/android/pass/ui/navigation/AuthAppGraph.kt
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2023 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.ui.navigation
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.navigation.NavGraphBuilder
+import proton.android.pass.featureaccount.impl.AccountNavigation
+import proton.android.pass.featureaccount.impl.ConfirmSignOutDialog
+import proton.android.pass.featureaccount.impl.SignOutDialog
+import proton.android.pass.featureauth.impl.Auth
+import proton.android.pass.featureauth.impl.AuthNavigation
+import proton.android.pass.featureauth.impl.AuthScreen
+import proton.android.pass.navigation.api.AppNavigator
+import proton.android.pass.navigation.api.composable
+import proton.android.pass.navigation.api.dialog
+import proton.android.pass.ui.AppNavigation
+
+@OptIn(ExperimentalAnimationApi::class)
+fun NavGraphBuilder.authAppGraph(
+    appNavigator: AppNavigator,
+    onNavigate: (AppNavigation) -> Unit
+) {
+    composable(Auth) {
+        AuthScreen(
+            canLogout = true,
+            navigation = {
+                when (it) {
+                    AuthNavigation.Dismissed,
+                    AuthNavigation.Back -> onNavigate(AppNavigation.Finish)
+
+                    AuthNavigation.Success,
+                    AuthNavigation.Failed -> {
+                    }
+                    AuthNavigation.SignOut -> {
+                        appNavigator.navigate(SignOutDialog)
+                    }
+                    AuthNavigation.ForceSignOut -> {
+                        onNavigate(AppNavigation.SignOut(null))
+                    }
+                }
+            }
+        )
+    }
+
+    dialog(SignOutDialog) {
+        ConfirmSignOutDialog(
+            onNavigate = {
+                when (it) {
+                    AccountNavigation.Back -> appNavigator.onBackClick()
+                    AccountNavigation.ConfirmSignOut -> onNavigate(AppNavigation.SignOut())
+                    AccountNavigation.DismissDialog -> appNavigator.onBackClick()
+                    AccountNavigation.SignOut -> {}
+                    AccountNavigation.Subscription -> {}
+                    AccountNavigation.Upgrade -> {}
+                }
+            }
+        )
+    }
+}

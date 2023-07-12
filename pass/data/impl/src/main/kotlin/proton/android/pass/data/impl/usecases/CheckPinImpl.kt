bd859b6bf8ad68ad0325612d5cd5580eea40aecd
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-07-12 16:23:03 +0200

    chore: add pin usecases

---
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/CheckPinImpl.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2023 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.data.impl.usecases
+
+import android.content.Context
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import me.proton.core.crypto.common.keystore.EncryptedByteArray
+import proton.android.pass.crypto.api.context.EncryptionContextProvider
+import proton.android.pass.data.api.usecases.CheckPin
+import proton.android.pass.data.impl.util.PinFileConfig
+import java.io.File
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class CheckPinImpl @Inject constructor(
+    @ApplicationContext private val context: Context,
+    private val encryptionContextProvider: EncryptionContextProvider
+) : CheckPin {
+    override suspend fun invoke(pin: ByteArray): Boolean = withContext(Dispatchers.IO) {
+        val file = File(context.dataDir, PinFileConfig.FILE_NAME)
+        val encrypted = EncryptedByteArray(file.readBytes())
+        val decryptedData = encryptionContextProvider.withEncryptionContext { decrypt(encrypted) }
+        decryptedData.contentEquals(pin)
+    }
+}

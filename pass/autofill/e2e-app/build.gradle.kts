464c96dd6bcf1f4a00bc341cac9e194af57bda50
Carlos Quintana <carlos.quintana@proton.ch>
2023-07-13 16:47:28 +0200

    bump: version 1.6.2 → 1.6.3

--- pass/autofill/e2e-app/build.gradle.kts
+++ pass/autofill/e2e-app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("org.jetbrains.kotlin.kapt")
 }
 
-val appVersionName: String = "1.6.2"
+val appVersionName: String = "1.6.3"
 val appVersionCode: Int = versionCode(appVersionName)
 val archivesBaseName = "AutofillE2EApp-$appVersionName"
 

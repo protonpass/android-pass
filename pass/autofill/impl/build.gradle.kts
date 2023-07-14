e691a83238791f223be393c23144d0179e41a728
Carlos Quintana <carlos.quintana@proton.ch>
2023-07-14 15:23:27 +0200

    test: fix ui test

--- pass/autofill/impl/build.gradle.kts
+++ pass/autofill/impl/build.gradle.kts
@@ -137,6 +137,7 @@ dependencies {
     androidTestImplementation(projects.pass.biometry.fakes)
     androidTestImplementation(projects.pass.clipboard.fakes)
     androidTestImplementation(projects.pass.commonTest)
+    androidTestImplementation(projects.pass.common.fakes)
     androidTestImplementation(projects.pass.commonUi.fakes)
     androidTestImplementation(projects.pass.crypto.fakes)
     androidTestImplementation(projects.pass.data.fakes)

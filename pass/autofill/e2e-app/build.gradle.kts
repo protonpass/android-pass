6e9ea77cf95aed1ba762ce12605bd84030fe22c4
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-07-21 12:08:22 +0200

    bump: version 1.7.0 → 1.7.1

--- pass/autofill/e2e-app/build.gradle.kts
+++ pass/autofill/e2e-app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("org.jetbrains.kotlin.kapt")
 }
 
-val appVersionName: String = "1.7.0"
+val appVersionName: String = "1.7.1"
 val appVersionCode: Int = versionCode(appVersionName)
 val archivesBaseName = "AutofillE2EApp-$appVersionName"
 

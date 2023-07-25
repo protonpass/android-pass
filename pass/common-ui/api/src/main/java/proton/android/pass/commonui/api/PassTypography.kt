a70c8903b4dc6acd7baa23cb0ba02ca936f30194
Carlos Quintana <carlos.quintana@proton.ch>
2023-07-25 14:50:43 +0200

    chore: add body1Regular typography

--- pass/common-ui/api/src/main/java/proton/android/pass/commonui/api/PassTypography.kt
+++ pass/common-ui/api/src/main/java/proton/android/pass/commonui/api/PassTypography.kt
@@ -21,6 +21,7 @@ package proton.android.pass.commonui.api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import me.proton.core.compose.theme.ProtonTheme
@@ -43,6 +44,15 @@ object PassTypography {
     val heroWeak: TextStyle
         @Composable get() = heroWeak()
 
+    private val body1Base: TextStyle
+        @Composable get() = TextStyle(
+            fontSize = 16.sp,
+            lineHeight = 24.sp,
+            fontWeight = FontWeight(400),
+            textAlign = TextAlign.Center,
+            letterSpacing = 0.5.sp,
+        )
+
     private val body3Base: TextStyle
         @Composable get() = TextStyle(
             fontSize = 14.sp,
@@ -50,6 +60,9 @@ object PassTypography {
             letterSpacing = 0.02.em,
             lineHeight = 20.sp
         )
+
+    val body1Regular: TextStyle
+        @Composable get() = body1Regular()
 
     val body3Regular: TextStyle
         @Composable get() = body3Regular()
@@ -73,6 +86,10 @@ object PassTypography {
     @Composable
     fun heroWeak(enabled: Boolean = true): TextStyle =
         heroRegular.copy(color = ProtonTheme.colors.textWeak(enabled))
+
+    @Composable
+    fun body1Regular(enabled: Boolean = true): TextStyle = body1Base
+        .copy(color = ProtonTheme.colors.textNorm(enabled))
 
     @Composable
     fun body3Regular(enabled: Boolean = true): TextStyle = body3Base

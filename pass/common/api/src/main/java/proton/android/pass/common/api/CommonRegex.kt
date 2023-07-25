6e2cb6b2a2ba86f8f2b7c572ed907d503e9398dd
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-07-25 08:04:58 +0200

    test: add regex and viewmodel tests

--- pass/common/api/src/main/java/proton/android/pass/common/api/CommonRegex.kt
+++ pass/common/api/src/main/java/proton/android/pass/common/api/CommonRegex.kt
@@ -20,4 +20,8 @@ package proton.android.pass.common.api
 
 object CommonRegex {
     val NON_DIGIT_REGEX: Regex = "\\D".toRegex()
+
+    @Suppress("MaxLineLength")
+    val EMAIL_VALIDATION_REGEX: Regex =
+        """(?:[a-z0-9!#${'$'}%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#${'$'}%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])""".toRegex()
 }

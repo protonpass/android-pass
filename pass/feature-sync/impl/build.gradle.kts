7fc393bd5a203a15e64c737f1c96e0716a3a60ea
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-07-31 15:06:30 +0200

    feat: add sync dialog

---
+++ pass/feature-sync/impl/build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("dagger.hilt.android.plugin")
+    id("com.google.devtools.ksp")
+    id("org.jetbrains.kotlin.kapt")
+}
+
+android {
+    namespace = "proton.android.pass.featuresync.impl"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+    
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.androidx.compose.compiler.get()
+    }
+}
+
+androidComponents.beforeVariants { variant ->
+    variant.enableAndroidTest = false
+}
+
+dependencies {
+    implementation(libs.core.presentation.compose)
+    implementation(libs.core.presentation)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.runtime)
+    implementation(libs.kotlinx.collections)
+
+    implementation(libs.dagger.hilt.android)
+    kapt(libs.dagger.hilt.android.compiler)
+    kapt(libs.androidx.hilt.compiler)
+
+    debugImplementation(libs.androidx.compose.uiTooling)
+    implementation(libs.androidx.compose.uiToolingPreview)
+    debugImplementation(libs.androidx.compose.uiTestManifest)
+
+    debugImplementation(libs.showkase)
+    kspDebug(libs.showkaseProcessor)
+
+    implementation(projects.pass.common.api)
+    implementation(projects.pass.commonUi.api)
+    implementation(projects.pass.composeComponents.impl)
+    implementation(projects.pass.domain)
+    implementation(projects.pass.data.api)
+    implementation(projects.pass.navigation.api)
+}
---
+++ pass/feature-sync/impl/src/main/kotlin/proton/android/pass/featuresync/impl/SyncDialogViewModel.kt
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2023 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.featuresync.impl
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.collections.immutable.ImmutableMap
+import kotlinx.collections.immutable.persistentMapOf
+import kotlinx.collections.immutable.toPersistentMap
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import proton.android.pass.common.api.asLoadingResult
+import proton.android.pass.common.api.getOrNull
+import proton.android.pass.data.api.repositories.ItemSyncStatus
+import proton.android.pass.data.api.repositories.ItemSyncStatusRepository
+import proton.android.pass.data.api.usecases.ObserveVaults
+import proton.pass.domain.ShareId
+import proton.pass.domain.Vault
+import javax.inject.Inject
+
+@HiltViewModel
+class SyncDialogViewModel @Inject constructor(
+    syncStatusRepository: ItemSyncStatusRepository,
+    observeVaults: ObserveVaults
+) : ViewModel() {
+
+    private val observeFinishEvents: Flow<Boolean> = syncStatusRepository.observeSyncStatus()
+        .map { it is ItemSyncStatus.CompletedSyncing }
+
+    val state: StateFlow<SyncDialogUiState> = combine(
+        syncStatusRepository.observeAccSyncStatus(),
+        observeFinishEvents,
+        observeVaults().asLoadingResult()
+    ) { accSync, isFinished, vaultsResult ->
+        val syncItemMap = vaultsResult
+            .getOrNull()
+            ?.associate { it.shareId to it }
+            ?.toMutableMap()
+            ?.map { entry ->
+                val syncItem = accSync[entry.key]
+                entry.key to SyncItem(
+                    vault = entry.value,
+                    current = syncItem?.current ?: -1,
+                    total = syncItem?.total ?: -1
+                )
+            }
+            ?.toMap()
+            ?.toPersistentMap()
+            ?: persistentMapOf()
+
+        SyncDialogUiState(
+            syncItemMap = syncItemMap,
+            isFinished = isFinished
+        )
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = SyncDialogUiState(persistentMapOf(), false)
+    )
+}
+
+data class SyncDialogUiState(
+    val syncItemMap: ImmutableMap<ShareId, SyncItem>,
+    val isFinished: Boolean
+)
+
+data class SyncItem(
+    val vault: Vault,
+    val current: Int,
+    val total: Int
+)

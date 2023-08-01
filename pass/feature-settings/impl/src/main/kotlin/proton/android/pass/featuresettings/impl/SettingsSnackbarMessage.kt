69bf1822aad84bd7aa4fbb4d7a829c3f7ff6e78a
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-08-01 12:47:16 +0200

    refactor: fix ci and code cleanup

--- pass/feature-settings/impl/src/main/kotlin/proton/android/pass/featuresettings/impl/SettingsSnackbarMessage.kt
+++ pass/feature-settings/impl/src/main/kotlin/proton/android/pass/featuresettings/impl/SettingsSnackbarMessage.kt
@@ -28,8 +28,6 @@ enum class SettingsSnackbarMessage(
     override val isClipboard: Boolean = false
 ) : SnackbarMessage {
     ErrorPerformingOperation(R.string.settings_error_performing_operation, SnackbarType.ERROR),
-    ErrorPerformingSync(R.string.settings_force_sync_error, SnackbarType.ERROR),
-    SyncSuccessful(R.string.settings_force_sync_success, SnackbarType.SUCCESS),
     ChangePrimaryVaultSuccess(R.string.settings_primary_vault_change_success, SnackbarType.SUCCESS),
     ChangePrimaryVaultError(R.string.settings_primary_vault_change_error, SnackbarType.ERROR),
     ClearIconCacheSuccess(R.string.settings_clear_icon_cache_success, SnackbarType.SUCCESS),

dd9a78b8d8d5e9cede33fa8e11d5c3f2812b4c45
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-08-02 13:05:00 +0200

    chore: add new conditions on in app review check

---
+++ pass/in-app-review/impl/src/test/kotlin/proton/android/pass/inappreview/impl/InAppReviewManagerImplTest.kt
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 2023 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.inappreview.impl
+
+import app.cash.turbine.test
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.test.runTest
+import kotlinx.datetime.Clock
+import org.junit.Before
+import org.junit.Test
+import proton.android.pass.data.fakes.usecases.TestGetUserPlan
+import proton.android.pass.inappreview.api.InAppReviewManager
+import proton.android.pass.inappreview.impl.InAppReviewManagerImpl.Companion.FREE_USER_ITEM_AUTOFILL_TRIGGER
+import proton.android.pass.inappreview.impl.InAppReviewManagerImpl.Companion.FREE_USER_ITEM_CREATED_TRIGGER
+import proton.android.pass.inappreview.impl.InAppReviewManagerImpl.Companion.PAID_USER_ITEM_AUTOFILL_TRIGGER
+import proton.android.pass.inappreview.impl.InAppReviewManagerImpl.Companion.PAID_USER_ITEM_CREATED_TRIGGER
+import proton.android.pass.inappreview.impl.InAppReviewManagerImpl.Companion.TIMES_USED
+import proton.android.pass.preferences.AppUsageConfig
+import proton.android.pass.preferences.TestInternalSettingsRepository
+import proton.pass.domain.Plan
+import proton.pass.domain.PlanLimit
+import proton.pass.domain.PlanType
+
+class InAppReviewManagerImplTest {
+
+    private lateinit var getUserPlan: TestGetUserPlan
+    private lateinit var internalSettingsRepository: TestInternalSettingsRepository
+    private lateinit var inAppReviewManager: InAppReviewManager
+
+    @Before
+    fun setUp() {
+        getUserPlan = TestGetUserPlan()
+        internalSettingsRepository = TestInternalSettingsRepository()
+        inAppReviewManager = InAppReviewManagerImpl(
+            getUserPlan = getUserPlan,
+            internalSettingsRepository = internalSettingsRepository
+        )
+    }
+
+    @Test
+    fun `shouldRequestReview returns false when conditions are not met for free user`() = runTest {
+        inAppReviewManager.shouldRequestReview().test {
+            assertThat(awaitItem()).isFalse()
+        }
+    }
+
+    @Test
+    fun `shouldRequestReview returns false when conditions are not met for paid user`() = runTest {
+        setPaidPlan()
+
+        inAppReviewManager.shouldRequestReview().test {
+            assertThat(awaitItem()).isFalse()
+        }
+    }
+
+    @Test
+    fun `shouldRequestReview returns false when review has been already triggered`() = runTest {
+        internalSettingsRepository.setInAppReviewTriggered(true)
+
+        inAppReviewManager.shouldRequestReview().test {
+            assertThat(awaitItem()).isFalse()
+        }
+    }
+
+    @Test
+    fun `shouldRequestReview returns true when item created is reached for free user`() = runTest {
+        internalSettingsRepository.setItemCreateCount(FREE_USER_ITEM_CREATED_TRIGGER)
+
+        inAppReviewManager.shouldRequestReview().test {
+            assertThat(awaitItem()).isTrue()
+        }
+    }
+
+    @Test
+    fun `shouldRequestReview returns true when item created is reached for paid user`() = runTest {
+        internalSettingsRepository.setItemCreateCount(PAID_USER_ITEM_CREATED_TRIGGER)
+        setPaidPlan()
+
+        inAppReviewManager.shouldRequestReview().test {
+            assertThat(awaitItem()).isTrue()
+        }
+    }
+
+    @Test
+    fun `shouldRequestReview returns true when item autofill is reached for free user`() = runTest {
+        internalSettingsRepository.setItemAutofillCount(FREE_USER_ITEM_AUTOFILL_TRIGGER)
+
+        inAppReviewManager.shouldRequestReview().test {
+            assertThat(awaitItem()).isTrue()
+        }
+    }
+
+    @Test
+    fun `shouldRequestReview returns true when item autofill is reached for paid user`() = runTest {
+        internalSettingsRepository.setItemAutofillCount(PAID_USER_ITEM_AUTOFILL_TRIGGER)
+        setPaidPlan()
+
+        inAppReviewManager.shouldRequestReview().test {
+            assertThat(awaitItem()).isTrue()
+        }
+    }
+
+    @Test
+    fun `shouldRequestReview returns true when items launched is reached `() = runTest {
+        internalSettingsRepository.setAppUsage(AppUsageConfig(TIMES_USED, Clock.System.now()))
+
+        inAppReviewManager.shouldRequestReview().test {
+            assertThat(awaitItem()).isTrue()
+        }
+    }
+
+    private fun setPaidPlan() {
+        getUserPlan.setResult(
+            Result.success(
+                Plan(
+                    planType = PlanType.Paid("", ""),
+                    hideUpgrade = false,
+                    vaultLimit = PlanLimit.Limited(10),
+                    aliasLimit = PlanLimit.Limited(10),
+                    totpLimit = PlanLimit.Limited(10),
+                    updatedAt = 0
+                )
+            )
+        )
+    }
+}

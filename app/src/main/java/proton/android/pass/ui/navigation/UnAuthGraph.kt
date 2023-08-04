38bea3e04d6e577c874785df89a1643a41b63d1c
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-08-04 16:37:23 +0200

    refactor: add auth nested graphs

--- app/src/main/java/proton/android/pass/ui/navigation/UnAuthGraph.kt
+++ app/src/main/java/proton/android/pass/ui/navigation/UnAuthGraph.kt
@@ -20,6 +20,7 @@ package proton.android.pass.ui.navigation
 
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.navigation
 import proton.android.pass.featureaccount.impl.AccountNavigation
 import proton.android.pass.featureaccount.impl.ConfirmSignOutDialog
 import proton.android.pass.featureaccount.impl.SignOutDialog
@@ -35,54 +36,61 @@ import proton.android.pass.navigation.api.composable
 import proton.android.pass.navigation.api.dialog
 import proton.android.pass.ui.AppNavigation
 
+private const val UN_AUTH_GRAPH = "un_auth_graph"
+
 @OptIn(ExperimentalAnimationApi::class)
 fun NavGraphBuilder.unAuthGraph(
     appNavigator: AppNavigator,
     onNavigate: (AppNavigation) -> Unit
 ) {
-    composable(Auth) {
-        AuthScreen(
-            canLogout = true,
-            navigation = {
-                when (it) {
-                    AuthNavigation.Dismissed,
-                    AuthNavigation.Back -> onNavigate(AppNavigation.Finish)
+    navigation(
+        route = UN_AUTH_GRAPH,
+        startDestination = Auth.route
+    ) {
+        composable(Auth) {
+            AuthScreen(
+                canLogout = true,
+                navigation = {
+                    when (it) {
+                        AuthNavigation.Dismissed,
+                        AuthNavigation.Back -> onNavigate(AppNavigation.Finish)
 
-                    AuthNavigation.Success,
-                    AuthNavigation.Failed -> {
+                        AuthNavigation.Success,
+                        AuthNavigation.Failed -> {
+                        }
+
+                        AuthNavigation.SignOut -> appNavigator.navigate(SignOutDialog)
+                        AuthNavigation.ForceSignOut -> onNavigate(AppNavigation.SignOut())
+                        AuthNavigation.EnterPin -> appNavigator.navigate(EnterPin)
                     }
-
-                    AuthNavigation.SignOut -> appNavigator.navigate(SignOutDialog)
-                    AuthNavigation.ForceSignOut -> onNavigate(AppNavigation.SignOut())
-                    AuthNavigation.EnterPin -> appNavigator.navigate(EnterPin)
                 }
-            }
-        )
-    }
+            )
+        }
 
-    dialog(SignOutDialog) {
-        ConfirmSignOutDialog(
-            onNavigate = {
-                when (it) {
-                    AccountNavigation.Back -> appNavigator.navigateBack()
-                    AccountNavigation.ConfirmSignOut -> onNavigate(AppNavigation.SignOut())
-                    AccountNavigation.DismissDialog -> appNavigator.navigateBack()
-                    AccountNavigation.SignOut -> {}
-                    AccountNavigation.Subscription -> {}
-                    AccountNavigation.Upgrade -> {}
+        dialog(SignOutDialog) {
+            ConfirmSignOutDialog(
+                onNavigate = {
+                    when (it) {
+                        AccountNavigation.Back -> appNavigator.navigateBack()
+                        AccountNavigation.ConfirmSignOut -> onNavigate(AppNavigation.SignOut())
+                        AccountNavigation.DismissDialog -> appNavigator.navigateBack()
+                        AccountNavigation.SignOut -> {}
+                        AccountNavigation.Subscription -> {}
+                        AccountNavigation.Upgrade -> {}
+                    }
                 }
-            }
-        )
-    }
+            )
+        }
 
-    bottomSheet(EnterPin) {
-        EnterPinBottomsheet(
-            onNavigate = {
-                when (it) {
-                    EnterPinNavigation.Success -> appNavigator.navigateBack()
-                    EnterPinNavigation.ForceSignOut -> onNavigate(AppNavigation.SignOut())
+        bottomSheet(EnterPin) {
+            EnterPinBottomsheet(
+                onNavigate = {
+                    when (it) {
+                        EnterPinNavigation.Success -> appNavigator.navigateBack()
+                        EnterPinNavigation.ForceSignOut -> onNavigate(AppNavigation.SignOut())
+                    }
                 }
-            }
-        )
+            )
+        }
     }
 }

691ae697745064095fe6b95c6fd69772a30ce279
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-08-07 11:27:52 +0200

    chore: expose autofill tools

--- tools/ci/setupSentryProperties.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-REPO_ROOT=$(echo "${SCRIPT_DIR}" | sed 's:tools/ci::g')
-
-echo "${SENTRY_PROPERTIES_B64}" | base64 -d > "${REPO_ROOT}/sentry.properties"

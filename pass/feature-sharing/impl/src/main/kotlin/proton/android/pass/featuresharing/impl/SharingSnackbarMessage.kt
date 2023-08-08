e5dd8db2b4785a332ea67d9b7f6d02664e75393b
Carlos Quintana <carlos.quintana@proton.ch>
2023-08-08 09:44:38 +0200

    feat(sharing): implement confirm transfer ownership dialog

--- pass/feature-sharing/impl/src/main/kotlin/proton/android/pass/featuresharing/impl/SharingSnackbarMessage.kt
+++ pass/feature-sharing/impl/src/main/kotlin/proton/android/pass/featuresharing/impl/SharingSnackbarMessage.kt
@@ -45,5 +45,8 @@ enum class SharingSnackbarMessage(
     CancelInviteError(R.string.sharing_snackbar_cancel_invite_error, SnackbarType.ERROR),
     ResendInviteSuccess(R.string.sharing_snackbar_resend_invite_success, SnackbarType.NORM),
     ResendInviteError(R.string.sharing_snackbar_resend_invite_error, SnackbarType.ERROR),
-    TooManyInvitesSentError(R.string.sharing_snackbar_too_many_invites_sent, SnackbarType.ERROR)
+    TooManyInvitesSentError(R.string.sharing_snackbar_too_many_invites_sent, SnackbarType.ERROR),
+
+    TransferOwnershipSuccess(R.string.sharing_snackbar_transfer_ownership_success, SnackbarType.NORM),
+    TransferOwnershipError(R.string.sharing_snackbar_transfer_ownership_error, SnackbarType.ERROR),
 }

12136f8b776176ae182148782cd424e371df9d6c
Carlos Quintana <carlos.quintana@proton.ch>
2023-08-08 10:58:22 +0200

    chore(domain): add method for checking if user has update permission

--- pass/domain/src/main/kotlin/proton/pass/domain/SharePermission.kt
+++ pass/domain/src/main/kotlin/proton/pass/domain/SharePermission.kt
@@ -30,3 +30,6 @@ data class SharePermission(val value: Int) {
 fun SharePermission.hasFlag(flag: SharePermissionFlag): Boolean = value.hasFlag(flag.value)
 fun SharePermission.flags(): List<SharePermissionFlag> =
     SharePermissionFlag.map.values.filter { hasFlag(it) }.toList()
+
+fun SharePermission.canUpdate(): Boolean =
+    hasFlag(SharePermissionFlag.Admin) or hasFlag(SharePermissionFlag.Update)

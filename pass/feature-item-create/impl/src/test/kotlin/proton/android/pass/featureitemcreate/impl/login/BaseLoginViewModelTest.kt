e0a9a57fd580d898f06c9e43e4991853dff2f0c5
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-08-10 11:41:46 +0200

    test: refactor login viewmodel tests

--- pass/feature-item-create/impl/src/test/kotlin/proton/android/pass/featureitemcreate/impl/login/BaseLoginViewModelTest.kt
+++ pass/feature-item-create/impl/src/test/kotlin/proton/android/pass/featureitemcreate/impl/login/BaseLoginViewModelTest.kt
@@ -18,7 +18,6 @@
 
 package proton.android.pass.featureitemcreate.impl.login
 
-import app.cash.turbine.test
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
@@ -26,16 +25,17 @@ import org.junit.Rule
 import org.junit.Test
 import proton.android.pass.account.fakes.TestAccountManager
 import proton.android.pass.clipboard.fakes.TestClipboardManager
+import proton.android.pass.commonui.fakes.TestSavedStateHandleProvider
 import proton.android.pass.crypto.fakes.context.TestEncryptionContext
 import proton.android.pass.crypto.fakes.context.TestEncryptionContextProvider
 import proton.android.pass.data.fakes.repositories.TestDraftRepository
 import proton.android.pass.data.fakes.usecases.TestObserveCurrentUser
 import proton.android.pass.data.fakes.usecases.TestObserveUpgradeInfo
+import proton.android.pass.featureitemcreate.impl.common.UIHiddenState
 import proton.android.pass.notifications.fakes.TestSnackbarDispatcher
 import proton.android.pass.test.MainDispatcherRule
 import proton.android.pass.test.domain.TestUser
 import proton.android.pass.totp.fakes.TestTotpManager
-import proton.pass.domain.HiddenState
 
 internal class BaseLoginViewModelTest {
 
@@ -46,11 +46,6 @@ internal class BaseLoginViewModelTest {
     private lateinit var clipboardManager: TestClipboardManager
     private lateinit var observeCurrentUser: TestObserveCurrentUser
     private lateinit var baseLoginViewModel: BaseLoginViewModel
-
-    private val initial = BaseLoginUiState.create(
-        password = HiddenState.Empty(TestEncryptionContext.encrypt("")),
-        primaryTotp = HiddenState.Empty(TestEncryptionContext.encrypt(""))
-    )
 
     @Before
     fun setUp() {
@@ -65,40 +60,23 @@ internal class BaseLoginViewModelTest {
             draftRepository = TestDraftRepository(),
             observeCurrentUser = observeCurrentUser,
             observeUpgradeInfo = TestObserveUpgradeInfo(),
-            encryptionContextProvider = TestEncryptionContextProvider()
+            encryptionContextProvider = TestEncryptionContextProvider(),
+            savedStateHandleProvider = TestSavedStateHandleProvider()
         ) {}
-    }
-
-    @Test
-    fun `should start with the initial state`() = runTest {
-        baseLoginViewModel.baseLoginUiState.test {
-            assertThat(awaitItem()).isEqualTo(
-                initial.copy(
-                    totpUiState = TotpUiState.Success,
-                    customFieldsState = CustomFieldsState.Disabled,
-                )
-            )
-        }
     }
 
     @Test
     fun `when the title has changed the state should hold it`() = runTest {
         val titleInput = "Title Changed"
         baseLoginViewModel.onTitleChange(titleInput)
-        baseLoginViewModel.baseLoginUiState.test {
-            assertThat(awaitItem().contents)
-                .isEqualTo(initial.contents.copy(title = titleInput))
-        }
+        assertThat(baseLoginViewModel.loginItemFormState.title).isEqualTo(titleInput)
     }
 
     @Test
     fun `when the username has changed the state should hold it`() = runTest {
         val usernameInput = "Username Changed"
         baseLoginViewModel.onUsernameChange(usernameInput)
-        baseLoginViewModel.baseLoginUiState.test {
-            assertThat(awaitItem().contents)
-                .isEqualTo(initial.contents.copy(username = usernameInput))
-        }
+        assertThat(baseLoginViewModel.loginItemFormState.username).isEqualTo(usernameInput)
     }
 
     @Test
@@ -106,51 +84,33 @@ internal class BaseLoginViewModelTest {
         val passwordInput = "Password Changed"
         val encryptedPassword = TestEncryptionContext.encrypt("Password Changed")
         baseLoginViewModel.onPasswordChange(passwordInput)
-        baseLoginViewModel.baseLoginUiState.test {
-            assertThat(awaitItem().contents)
-                .isEqualTo(
-                    initial.contents.copy(
-                        password = HiddenState.Revealed(encryptedPassword, passwordInput)
-                    )
-                )
-        }
+        assertThat(baseLoginViewModel.loginItemFormState.password)
+            .isEqualTo(UIHiddenState.Revealed(encryptedPassword, passwordInput))
     }
 
     @Test
     fun `when the note has changed the state should hold it`() = runTest {
         val noteInput = "Note Changed"
         baseLoginViewModel.onNoteChange(noteInput)
-        baseLoginViewModel.baseLoginUiState.test {
-            assertThat(awaitItem().contents)
-                .isEqualTo(initial.contents.copy(note = noteInput))
-        }
+        assertThat(baseLoginViewModel.loginItemFormState.note).isEqualTo(noteInput)
     }
 
     @Test
     fun `when a website has been changed the state should change it`() = runTest {
         val url = "proton.me"
         baseLoginViewModel.onWebsiteChange(url, 0)
-        baseLoginViewModel.baseLoginUiState.test {
-            assertThat(awaitItem().contents)
-                .isEqualTo(initial.contents.copy(urls = listOf(url)))
-        }
+        assertThat(baseLoginViewModel.loginItemFormState.urls).isEqualTo(listOf(url))
     }
 
     @Test
     fun `when a website has been added the state should add it`() = runTest {
         baseLoginViewModel.onAddWebsite()
-        baseLoginViewModel.baseLoginUiState.test {
-            assertThat(awaitItem().contents)
-                .isEqualTo(initial.contents.copy(urls = listOf("", "")))
-        }
+        assertThat(baseLoginViewModel.loginItemFormState.urls).isEqualTo(listOf("", ""))
     }
 
     @Test
     fun `when a website has been removed the state should remove it`() = runTest {
         baseLoginViewModel.onRemoveWebsite(0)
-        baseLoginViewModel.baseLoginUiState.test {
-            assertThat(awaitItem().contents)
-                .isEqualTo(initial.contents.copy(urls = emptyList()))
-        }
+        assertThat(baseLoginViewModel.loginItemFormState.urls).isEqualTo(emptyList<String>())
     }
 }

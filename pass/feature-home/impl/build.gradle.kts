74cd1113306327855706ce0976726072629a49c7
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-08-10 09:13:12 +0200

    build: make common-ui-models an android module

--- pass/feature-home/impl/build.gradle.kts
+++ pass/feature-home/impl/build.gradle.kts
@@ -80,6 +80,7 @@ dependencies {
     implementation(projects.pass.composeComponents.impl)
     implementation(projects.pass.crypto.api)
     implementation(projects.pass.data.api)
+    implementation(projects.pass.domain)
     implementation(projects.pass.log.api)
     implementation(projects.pass.navigation.api)
     implementation(projects.pass.notifications.api)

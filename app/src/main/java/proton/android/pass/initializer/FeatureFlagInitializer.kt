c0bbf16769525335ba5d528b5878ce3a91a4f7f7
Neil Marietta <neil.marietta@protonmail.com>
2023-08-24 13:39:11 +0200

    chore: Integrated FeatureFlagRefreshStarter (unleash).

---
+++ app/src/main/java/proton/android/pass/initializer/FeatureFlagInitializer.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2023 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.initializer
+
+import android.content.Context
+import androidx.startup.Initializer
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.android.EntryPointAccessors
+import dagger.hilt.components.SingletonComponent
+import me.proton.core.featureflag.data.FeatureFlagRefreshStarter
+
+class FeatureFlagInitializer : Initializer<Unit> {
+
+    override fun create(context: Context) {
+        EntryPointAccessors.fromApplication(
+            context.applicationContext,
+            FeatureFlagInitializerEntryPoint::class.java
+        ).featureFlagRefreshStarter().start()
+    }
+
+    override fun dependencies(): List<Class<out Initializer<*>?>> = listOf(
+        WorkManagerInitializer::class.java
+    )
+
+    @EntryPoint
+    @InstallIn(SingletonComponent::class)
+    interface FeatureFlagInitializerEntryPoint {
+        fun featureFlagRefreshStarter(): FeatureFlagRefreshStarter
+    }
+}

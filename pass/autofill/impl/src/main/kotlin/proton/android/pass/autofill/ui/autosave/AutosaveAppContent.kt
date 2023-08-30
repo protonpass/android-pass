c1e846ab1f3072fe2b534730ca3183d49eb1c4f2
Carlos Quintana <carlos.quintana@proton.ch>
2023-08-30 10:55:24 +0200

    fix(autosave): use graph instead of route as initialDestination on autosave graph

--- pass/autofill/impl/src/main/kotlin/proton/android/pass/autofill/ui/autosave/AutosaveAppContent.kt
+++ pass/autofill/impl/src/main/kotlin/proton/android/pass/autofill/ui/autosave/AutosaveAppContent.kt
@@ -32,7 +32,7 @@ import com.google.accompanist.navigation.material.ExperimentalMaterialNavigation
 import kotlinx.coroutines.launch
 import proton.android.pass.composecomponents.impl.bottomsheet.PassModalBottomSheetLayout
 import proton.android.pass.featureauth.impl.AUTH_GRAPH
-import proton.android.pass.featureitemcreate.impl.login.CreateLogin
+import proton.android.pass.featureitemcreate.impl.login.CREATE_LOGIN_GRAPH
 import proton.android.pass.navigation.api.rememberAppNavigator
 import proton.android.pass.navigation.api.rememberBottomSheetNavigator
 
@@ -60,7 +60,7 @@ fun AutosaveAppContent(
         AnimatedNavHost(
             modifier = modifier.defaultMinSize(minHeight = 200.dp),
             navController = appNavigator.navController,
-            startDestination = if (needsAuth) AUTH_GRAPH else CreateLogin.route,
+            startDestination = if (needsAuth) AUTH_GRAPH else CREATE_LOGIN_GRAPH,
         ) {
             autosaveActivityGraph(
                 appNavigator = appNavigator,

fd0f6b68523cffdae9e7f1cdbee9d34ef8b87de5
Carlos Quintana <carlos.quintana@proton.ch>
2023-09-08 09:54:33 +0200

    feat(sharing): add support for owner in member list

--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/responses/GetShareMembersResponse.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/responses/GetShareMembersResponse.kt
@@ -39,6 +39,8 @@ data class ShareMemberResponse(
     val userName: String,
     @SerialName("UserEmail")
     val userEmail: String,
+    @SerialName("Owner")
+    val owner: Boolean? = false,
     @SerialName("TargetType")
     val targetType: Int,
     @SerialName("TargetID")

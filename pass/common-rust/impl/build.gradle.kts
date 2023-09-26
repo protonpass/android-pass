cf2a0042423ed64ee3e6528234cde7783cbdb3b5
Carlos Quintana <carlos.quintana@proton.ch>
2023-09-26 12:27:53 +0200

    feat: add rust library into the project

---
+++ pass/common-rust/impl/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("dagger.hilt.android.plugin")
+    id("org.jetbrains.kotlin.kapt")
+}
+
+android {
+    namespace = "proton.android.pass.commonrust.impl"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+    
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+
+//        ndk {
+//            abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86_64")
+//        }
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+}
+
+androidComponents.beforeVariants { variant ->
+    variant.enableAndroidTest = false
+}
+
+dependencies {
+    api(projects.pass.commonRust.api)
+
+    implementation(files("libs/lib-release.aar"))
+
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.java.jna) {
+        artifact {
+            type = "aar"
+        }
+    }
+
+    implementation(libs.dagger.hilt.android)
+    kapt(libs.dagger.hilt.android.compiler)
+    kapt(libs.androidx.hilt.compiler)
+}

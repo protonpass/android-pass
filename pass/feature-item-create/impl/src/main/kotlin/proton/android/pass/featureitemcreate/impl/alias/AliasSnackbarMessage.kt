8c8e6d8b2db5bb7d4ef2da97245903d51f11a0f9
Carlos Quintana <carlos.quintana@proton.ch>
2023-10-02 11:26:37 +0200

    chore: fix l10n issues

--- pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/alias/AliasSnackbarMessage.kt
+++ pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/alias/AliasSnackbarMessage.kt
@@ -28,7 +28,6 @@ enum class AliasSnackbarMessage(
     override val type: SnackbarType,
     override val isClipboard: Boolean = false
 ) : SnackbarMessage {
-    EmptyShareIdError(R.string.create_alias_empty_share_id, SnackbarType.ERROR),
     InitError(R.string.create_alias_init_error, SnackbarType.ERROR),
     ItemCreationError(R.string.create_alias_item_creation_error, SnackbarType.ERROR),
     CannotCreateMoreAliasesError(R.string.create_alias_cannot_create_more_aliases_error, SnackbarType.ERROR),
@@ -37,6 +36,5 @@ enum class AliasSnackbarMessage(
     ItemUpdateError(R.string.create_alias_item_update_error, SnackbarType.ERROR),
     AliasCreated(R.string.alias_created, SnackbarType.SUCCESS),
     AliasUpdated(R.string.changes_saved, SnackbarType.SUCCESS),
-    AliasMovedToTrash(R.string.alias_moved_to_trash, SnackbarType.SUCCESS),
     CannotRetrieveAliasOptions(R.string.cannot_retrieve_alias_options, SnackbarType.ERROR)
 }

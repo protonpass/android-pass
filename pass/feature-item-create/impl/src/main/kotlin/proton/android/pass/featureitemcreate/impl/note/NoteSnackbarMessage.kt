ccc34335e755e1f59b5f03cff4c9f50fed518e31
Carlos Quintana <carlos.quintana@proton.ch>
2023-10-02 11:26:37 +0200

    chore: fix l10n issues

--- pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/note/NoteSnackbarMessage.kt
+++ pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/note/NoteSnackbarMessage.kt
@@ -28,12 +28,9 @@ enum class NoteSnackbarMessage(
     override val type: SnackbarType,
     override val isClipboard: Boolean = false
 ) : SnackbarMessage {
-    EmptyShareIdError(R.string.create_note_empty_share_id, SnackbarType.ERROR),
     InitError(R.string.create_note_init_error, SnackbarType.ERROR),
     ItemCreationError(R.string.create_note_item_creation_error, SnackbarType.ERROR),
     ItemUpdateError(R.string.create_note_item_update_error, SnackbarType.ERROR),
     NoteCreated(R.string.note_created, SnackbarType.SUCCESS),
     NoteUpdated(R.string.changes_saved, SnackbarType.SUCCESS),
-    NoteMovedToTrash(R.string.edit_note_snackbar_note_moved_to_trash, SnackbarType.SUCCESS),
-    NoteMovedToTrashError(R.string.edit_note_snackbar_note_moved_to_trash_error, SnackbarType.ERROR)
 }

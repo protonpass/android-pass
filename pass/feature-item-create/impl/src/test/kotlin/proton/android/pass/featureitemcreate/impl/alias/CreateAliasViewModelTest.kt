74b3686875fafe5398c0de5bd7400c14f29e94d3
Carlos Quintana <carlos.quintana@proton.ch>
2023-10-09 15:37:55 +0200

    feat(item-create): take removePrimaryVault into account when selecting vault

--- pass/feature-item-create/impl/src/test/kotlin/proton/android/pass/featureitemcreate/impl/alias/CreateAliasViewModelTest.kt
+++ pass/feature-item-create/impl/src/test/kotlin/proton/android/pass/featureitemcreate/impl/alias/CreateAliasViewModelTest.kt
@@ -44,6 +44,8 @@ import proton.android.pass.inappreview.fakes.TestInAppReviewTriggerMetrics
 import proton.android.pass.navigation.api.AliasOptionalNavArgId
 import proton.android.pass.navigation.api.CommonNavArgId
 import proton.android.pass.notifications.fakes.TestSnackbarDispatcher
+import proton.android.pass.preferences.FeatureFlag
+import proton.android.pass.preferences.TestFeatureFlagsPreferenceRepository
 import proton.android.pass.telemetry.api.EventItemType
 import proton.android.pass.telemetry.fakes.TestTelemetryManager
 import proton.android.pass.test.MainDispatcherRule
@@ -251,7 +253,10 @@ class CreateAliasViewModelTest {
             draftRepository = draftRepository,
             inAppReviewTriggerMetrics = TestInAppReviewTriggerMetrics(),
             encryptionContextProvider = TestEncryptionContextProvider(),
-            aliasPrefixValidator = TestAliasPrefixValidator()
+            aliasPrefixValidator = TestAliasPrefixValidator(),
+            ffRepo = TestFeatureFlagsPreferenceRepository().apply {
+                set(FeatureFlag.REMOVE_PRIMARY_VAULT, false)
+            }
         ).apply {
             setDraftStatus(isDraft)
         }

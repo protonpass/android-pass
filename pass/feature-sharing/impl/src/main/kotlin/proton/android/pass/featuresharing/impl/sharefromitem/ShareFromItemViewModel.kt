ee860190e328adf0c3f427c76bb5975dc7d769a8
Carlos Quintana <carlos.quintana@proton.ch>
2023-10-26 17:00:47 +0200

    feat(sharing): do not display create and move to shared vault if user cannot do it

--- pass/feature-sharing/impl/src/main/kotlin/proton/android/pass/featuresharing/impl/sharefromitem/ShareFromItemViewModel.kt
+++ pass/feature-sharing/impl/src/main/kotlin/proton/android/pass/featuresharing/impl/sharefromitem/ShareFromItemViewModel.kt
@@ -21,39 +21,62 @@ package proton.android.pass.featuresharing.impl.sharefromitem
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import proton.android.pass.common.api.LoadingResult
+import proton.android.pass.common.api.asLoadingResult
+import proton.android.pass.common.api.getOrNull
 import proton.android.pass.common.api.some
 import proton.android.pass.commonui.api.SavedStateHandleProvider
 import proton.android.pass.commonui.api.require
 import proton.android.pass.data.api.usecases.GetVaultWithItemCountById
+import proton.android.pass.data.api.usecases.ObserveVaults
+import proton.android.pass.data.api.usecases.capabilities.CanCreateVault
 import proton.android.pass.navigation.api.CommonNavArgId
 import proton.pass.domain.ItemId
 import proton.pass.domain.ShareId
+import proton.pass.domain.canCreate
+import proton.pass.domain.toPermissions
 import javax.inject.Inject
 
 @HiltViewModel
 class ShareFromItemViewModel @Inject constructor(
     savedState: SavedStateHandleProvider,
-    getVaultWithItemCount: GetVaultWithItemCountById
+    observeVaults: ObserveVaults,
+    getVaultWithItemCount: GetVaultWithItemCountById,
+    canCreateVault: CanCreateVault
 ) : ViewModel() {
 
     private val shareId: ShareId = ShareId(savedState.get().require(CommonNavArgId.ShareId.key))
     private val itemId: ItemId = ItemId(savedState.get().require(CommonNavArgId.ItemId.key))
 
-    val state: StateFlow<ShareFromItemUiState> = getVaultWithItemCount(shareId = shareId)
-        .map { vault ->
-            ShareFromItemUiState(
-                vault = vault.some(),
-                itemId = itemId
-            )
+    private val canMoveToSharedVaultFlow: Flow<LoadingResult<Boolean>> = observeVaults()
+        .map { vaults ->
+            vaults.any { vault ->
+                vault.shareId != shareId && vault.shared && vault.role.toPermissions().canCreate()
+            }
         }
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(5_000L),
-            initialValue = ShareFromItemUiState.Initial(itemId)
+        .asLoadingResult()
+
+    val state: StateFlow<ShareFromItemUiState> = combine(
+        getVaultWithItemCount(shareId = shareId),
+        canMoveToSharedVaultFlow,
+        canCreateVault()
+    ) { vault, canMoveToSharedVault, showCreateVault ->
+        ShareFromItemUiState(
+            vault = vault.some(),
+            itemId = itemId,
+            showMoveToSharedVault = canMoveToSharedVault.getOrNull() ?: false,
+            showCreateVault = showCreateVault
         )
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000L),
+        initialValue = ShareFromItemUiState.Initial(itemId)
+    )
 
 }

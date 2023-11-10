ece9945affee02cedd890f76f08f923b2c09dc0f
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-11-10 15:49:39 +0100

    chore: add autofill manager sample activity

---
+++ pass/autofill/test-app/src/main/java/proton/android/pass/autofill/sample/autofillmanageractivity/AutofillManagerLoginActivity.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2023 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.autofill.sample.autofillmanageractivity
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.InputType
+import android.view.View
+import android.view.autofill.AutofillManager
+import androidx.appcompat.app.AppCompatActivity
+import proton.android.pass.autofill.sample.LoginResultActivity
+import proton.android.pass.autofill.sample.databinding.ActivitySimpleLoginBinding
+
+class AutofillManagerLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivitySimpleLoginBinding.inflate(layoutInflater)
+
+        binding.loginButton.setOnClickListener {
+            startActivity(Intent(this, LoginResultActivity::class.java))
+            finish()
+        }
+        val autofillManager = getSystemService(AutofillManager::class.java)
+
+        binding.loginEditText.onFocusChangeListener = View.OnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                autofillManager.requestAutofill(v)
+            }
+        }
+        binding.passwordEditText.onFocusChangeListener = View.OnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                autofillManager.requestAutofill(v)
+            }
+        }
+        binding.showPasswordButton.setOnClickListener {
+            if (binding.passwordEditText.inputType == InputType.TYPE_TEXT_VARIATION_PASSWORD) {
+                binding.passwordEditText.inputType = InputType.TYPE_CLASS_TEXT
+            } else {
+                binding.passwordEditText.inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
+            }
+        }
+
+        setContentView(binding.root)
+    }
+}

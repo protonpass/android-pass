a74c1b252d371a4d00629b4e63284e79ac53d2c1
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-11-13 10:27:53 +0100

    fix: fix totp crash on wrong uri

--- pass/totp/impl/src/main/kotlin/proton/android/pass/totp/impl/GetTotpCodeFromUriImpl.kt
+++ pass/totp/impl/src/main/kotlin/proton/android/pass/totp/impl/GetTotpCodeFromUriImpl.kt
@@ -19,6 +19,7 @@
 package proton.android.pass.totp.impl
 
 import kotlinx.coroutines.flow.first
+import proton.android.pass.common.api.flatMap
 import proton.android.pass.totp.api.GetTotpCodeFromUri
 import proton.android.pass.totp.api.TotpManager
 import javax.inject.Inject
@@ -28,5 +29,5 @@ class GetTotpCodeFromUriImpl @Inject constructor(
 ) : GetTotpCodeFromUri {
     override suspend fun invoke(uri: String): Result<String> =
         totpManager.parse(uri)
-            .map { totpManager.observeCode(it).first().code }
+            .flatMap { runCatching { totpManager.observeCode(it).first().code } }
 }

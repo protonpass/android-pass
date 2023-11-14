3dc1b082e47ef6282b689a218998f203e9547fb2
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-11-14 14:12:32 +0100

    chore: avoid disabling autofill

--- pass/autofill/impl/src/main/kotlin/proton/android/pass/autofill/ui/autofill/AutofillActivityViewModel.kt
+++ pass/autofill/impl/src/main/kotlin/proton/android/pass/autofill/ui/autofill/AutofillActivityViewModel.kt
@@ -36,7 +36,6 @@ import kotlinx.coroutines.launch
 import me.proton.core.accountmanager.domain.AccountManager
 import proton.android.pass.account.api.AccountOrchestrators
 import proton.android.pass.account.api.Orchestrator
-import proton.android.pass.autofill.api.AutofillManager
 import proton.android.pass.autofill.entities.AndroidAutofillFieldId
 import proton.android.pass.autofill.entities.AutofillAppState
 import proton.android.pass.autofill.entities.AutofillItem
@@ -78,7 +77,6 @@ class AutofillActivityViewModel @Inject constructor(
     private val internalSettingsRepository: InternalSettingsRepository,
     private val accountManager: AccountManager,
     private val toastManager: ToastManager,
-    private val autofillManager: AutofillManager,
     needsBiometricAuth: NeedsBiometricAuth,
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
@@ -198,8 +196,6 @@ class AutofillActivityViewModel @Inject constructor(
             .onFailure {
                 PassLogger.w(TAG, it, "Error clearing preferences")
             }
-
-        autofillManager.disableAutofill()
 
         closeScreenFlow.update { true }
     }

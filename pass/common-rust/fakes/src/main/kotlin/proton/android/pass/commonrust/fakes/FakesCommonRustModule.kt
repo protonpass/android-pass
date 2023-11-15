8eae9547a8319d6cc40cdb5dd152c63912d46ab6
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-11-15 12:15:42 +0100

    test: add missing fake

--- pass/common-rust/fakes/src/main/kotlin/proton/android/pass/commonrust/fakes/FakesCommonRustModule.kt
+++ pass/common-rust/fakes/src/main/kotlin/proton/android/pass/commonrust/fakes/FakesCommonRustModule.kt
@@ -26,6 +26,7 @@ import proton.android.pass.commonrust.api.AliasPrefixValidator
 import proton.android.pass.commonrust.api.EmailValidator
 import proton.android.pass.commonrust.api.NewUserInviteSignatureBodyCreator
 import proton.android.pass.commonrust.api.PasswordGenerator
+import proton.android.pass.commonrust.api.PasswordScorer
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -44,4 +45,7 @@ abstract class FakesCommonRustModule {
 
     @Binds
     abstract fun bindPasswordGenerator(impl: TestPasswordGenerator): PasswordGenerator
+
+    @Binds
+    abstract fun bindPasswordScorer(impl: TestPasswordScorer): PasswordScorer
 }

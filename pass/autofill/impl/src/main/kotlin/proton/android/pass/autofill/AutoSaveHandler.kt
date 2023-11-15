ed7c395171a4e3ae52dad354e7b56a0f9a528255
Carlos Quintana <carlos.quintana@proton.ch>
2023-11-15 16:49:00 +0100

    feat(autofill): implement proper autosave

--- pass/autofill/impl/src/main/kotlin/proton/android/pass/autofill/AutoSaveHandler.kt
+++ pass/autofill/impl/src/main/kotlin/proton/android/pass/autofill/AutoSaveHandler.kt
@@ -25,6 +25,10 @@ import android.service.autofill.SaveRequest
 import proton.android.pass.autofill.Utils.getApplicationPackageName
 import proton.android.pass.autofill.Utils.getWindowNodes
 import proton.android.pass.autofill.entities.SaveInformation
+import proton.android.pass.autofill.entities.SaveItemType
+import proton.android.pass.autofill.extensions.MultiStepUtils
+import proton.android.pass.autofill.extensions.MultiStepUtils.getPasswordFromState
+import proton.android.pass.autofill.extensions.MultiStepUtils.getUsernameFromState
 import proton.android.pass.autofill.service.R
 import proton.android.pass.autofill.ui.autosave.AutoSaveActivity
 import proton.android.pass.autofill.ui.autosave.LinkedAppInfo
@@ -33,17 +37,39 @@ import proton.android.pass.common.api.Option
 import proton.android.pass.common.api.Some
 import proton.android.pass.commonui.api.AndroidUtils.getApplicationName
 import proton.android.pass.data.api.url.UrlSanitizer
+import proton.android.pass.log.api.PassLogger
 
 object AutoSaveHandler {
+
+    private const val TAG = "AutoSaveHandler"
+
     fun handleOnSave(context: Context, request: SaveRequest, callback: SaveCallback) {
+        val usernameFromState = request.clientState?.getUsernameFromState()
+        val passwordFromState = request.clientState?.getPasswordFromState()
+
+        val usernameField = getField(usernameFromState, request)
+        val passwordField = getField(passwordFromState, request)
+
         val windowNode = getWindowNodes(request.fillContexts).lastOrNull()
         if (windowNode?.rootViewNode == null) {
+            PassLogger.w(TAG, "WindowNode.rootViewNode is null")
+            callback.onFailure(context.getString(R.string.error_cant_find_matching_fields))
+            return
+        }
+
+        if (usernameField == null && passwordField == null) {
+            PassLogger.w(TAG, "Username and password fields were null")
             callback.onFailure(context.getString(R.string.error_cant_find_matching_fields))
             return
         }
 
         runCatching {
-            saveCredentials(context, windowNode)
+            saveCredentials(
+                context = context,
+                windowNode = windowNode,
+                usernameField = usernameField,
+                passwordField = passwordField
+            )
         }.onSuccess {
             callback.onSuccess()
         }.onFailure {
@@ -51,16 +77,66 @@ object AutoSaveHandler {
         }
     }
 
+    @Suppress("ReturnCount")
+    private fun getField(field: MultiStepUtils.SaveFieldInfo?, request: SaveRequest): AssistStructure.ViewNode? {
+        if (field == null) return null
+        val fillContext = request.fillContexts.firstOrNull { it.requestId == field.sessionId }
+        if (fillContext == null) {
+            PassLogger.w(TAG, "No fill context found for session id ${field.sessionId}")
+            return null
+        }
+
+        val structure = fillContext.structure
+        val windowNodes = if (structure.windowNodeCount > 0) {
+            (0 until structure.windowNodeCount).map { structure.getWindowNodeAt(it) }
+        } else {
+            return null
+        }
+
+        windowNodes.forEach { windowNode ->
+            val child = windowNode.rootViewNode.findChildById(field.fieldId)
+            if (child != null) {
+                return child
+            }
+        }
+
+        PassLogger.w(TAG, "Could not find child with id ${field.fieldId}")
+        return null
+    }
+
+
     private fun saveCredentials(
         context: Context,
-        windowNode: AssistStructure.WindowNode
+        windowNode: AssistStructure.WindowNode,
+        usernameField: AssistStructure.ViewNode?,
+        passwordField: AssistStructure.ViewNode?
     ) {
         val assistInfo = AssistNodeTraversal().traverse(windowNode.rootViewNode)
 
         val packageName = getApplicationPackageName(windowNode)
         val itemTitle = getItemTitle(context, packageName, assistInfo.url)
 
-        val saveInformation = SaveFieldExtractor.extract(assistInfo.fields)
+        val usernameValue = usernameField?.autofillValue?.textValue
+        val passwordValue = passwordField?.autofillValue?.textValue
+
+        val saveInfo = when {
+            usernameValue != null && passwordValue != null -> {
+                SaveInformation(
+                    itemType = SaveItemType.Login(
+                        identity = usernameValue.toString(),
+                        password = passwordValue.toString()
+                    )
+                )
+            }
+            usernameValue != null && passwordValue == null -> {
+                SaveInformation(SaveItemType.SingleValue(usernameValue.toString()))
+            }
+            usernameValue == null && passwordValue != null -> {
+                SaveInformation(SaveItemType.SingleValue(passwordValue.toString()))
+            }
+            else -> return
+        }
+
         val linkedAppInfo = if (BROWSERS.contains(packageName)) {
             null
         } else {
@@ -68,16 +144,13 @@ object AutoSaveHandler {
             LinkedAppInfo(packageName, appName)
         }
 
-        // We should handle what happens if there are multiple credentials
-        saveInformation.firstOrNull()?.let {
-            launchSaveCredentialScreen(
-                context = context,
-                saveInformation = it,
-                title = itemTitle,
-                website = assistInfo.url,
-                linkedAppInfo = linkedAppInfo
-            )
-        }
+        launchSaveCredentialScreen(
+            context = context,
+            saveInformation = saveInfo,
+            title = itemTitle,
+            website = assistInfo.url,
+            linkedAppInfo = linkedAppInfo
+        )
     }
 
     private fun getItemTitle(context: Context, packageName: String, url: Option<String>): String =
@@ -92,7 +165,6 @@ object AutoSaveHandler {
         } else {
             getApplicationName(context, packageName).value() ?: ""
         }
-
 
     private fun launchSaveCredentialScreen(
         context: Context,

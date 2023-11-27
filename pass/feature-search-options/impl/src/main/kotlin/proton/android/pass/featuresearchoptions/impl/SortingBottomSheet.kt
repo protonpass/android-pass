dac0829cdf7f752bfe295cfd27a7b44601b880cc
Carlos Quintana <carlos.quintana@proton.ch>
2023-11-27 08:56:22 +0100

    chore(featuresearchoptions): fix package path

--- pass/feature-search-options/impl/src/main/kotlin/proton/android/pass/featuresearchoptions/impl/SortingBottomSheet.kt
+++ pass/feature-search-options/impl/src/main/kotlin/proton/android/pass/featuresearchoptions/impl/SortingBottomSheet.kt
@@ -24,7 +24,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import proton.android.featuresearchoptions.impl.SortingBottomSheetContents
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable

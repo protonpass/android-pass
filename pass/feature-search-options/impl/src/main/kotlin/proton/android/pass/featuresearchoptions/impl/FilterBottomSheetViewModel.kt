03a53455ccfced91f07754da1b595a894e8c36c9
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-11-29 16:12:03 +0100

    feat: add count to filter bottomsheet

--- pass/feature-search-options/impl/src/main/kotlin/proton/android/pass/featuresearchoptions/impl/FilterBottomSheetViewModel.kt
+++ pass/feature-search-options/impl/src/main/kotlin/proton/android/pass/featuresearchoptions/impl/FilterBottomSheetViewModel.kt
@@ -21,27 +21,62 @@ package proton.android.pass.featuresearchoptions.impl
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.zip
+import proton.android.pass.data.api.ItemCountSummary
+import proton.android.pass.data.api.usecases.ObserveItemCount
+import proton.android.pass.domain.ItemState
 import proton.android.pass.featuresearchoptions.api.FilterOption
 import proton.android.pass.featuresearchoptions.api.HomeSearchOptionsRepository
 import proton.android.pass.featuresearchoptions.api.SearchFilterType
+import proton.android.pass.featuresearchoptions.api.VaultSelectionOption
 import javax.inject.Inject
 
 @HiltViewModel
 class FilterBottomSheetViewModel @Inject constructor(
     private val homeSearchOptionsRepository: HomeSearchOptionsRepository,
+    observeItemCount: ObserveItemCount
 ) : ViewModel() {
 
-    val state: StateFlow<SearchFilterType> =
-        homeSearchOptionsRepository.observeFilterOption()
-            .map { it.searchFilterType }
-            .stateIn(viewModelScope, SharingStarted.Eagerly, SearchFilterType.All)
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val state: StateFlow<FilterOptionsUIState> = homeSearchOptionsRepository.observeSearchOptions()
+        .flatMapLatest {
+            when (val vault = it.vaultSelectionOption) {
+                VaultSelectionOption.AllVaults -> observeItemCount()
+                VaultSelectionOption.Trash -> observeItemCount(
+                    itemState = ItemState.Trashed
+                )
+
+                is VaultSelectionOption.Vault -> observeItemCount(
+                    selectedShareId = vault.shareId
+                )
+            }.zip(flowOf(it)) { itemCount, searchOptions ->
+                itemCount to searchOptions
+            }
+        }
+        .map { (summary, options) ->
+            SuccessFilterOptionsUIState(
+                filterType = options.filterOption.searchFilterType,
+                summary = summary
+            )
+        }
+        .stateIn(viewModelScope, SharingStarted.Eagerly, EmptyFilterOptionsUIState)
 
     fun onFilterTypeChanged(searchFilterType: SearchFilterType) {
         val value = FilterOption(searchFilterType)
         homeSearchOptionsRepository.setFilterOption(value)
     }
 }
+
+sealed interface FilterOptionsUIState
+object EmptyFilterOptionsUIState : FilterOptionsUIState
+data class SuccessFilterOptionsUIState(
+    val filterType: SearchFilterType,
+    val summary: ItemCountSummary
+) : FilterOptionsUIState

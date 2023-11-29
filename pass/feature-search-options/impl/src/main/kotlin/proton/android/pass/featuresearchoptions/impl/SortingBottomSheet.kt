3bb4fc2766671c4786af02514c68634fc086b59d
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-11-29 15:27:53 +0100

    fix: add backhandlers to bottomsheets

--- pass/feature-search-options/impl/src/main/kotlin/proton/android/pass/featuresearchoptions/impl/SortingBottomSheet.kt
+++ pass/feature-search-options/impl/src/main/kotlin/proton/android/pass/featuresearchoptions/impl/SortingBottomSheet.kt
@@ -18,6 +18,7 @@
 
 package proton.android.pass.featuresearchoptions.impl
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -34,6 +35,7 @@ fun SortingBottomSheet(
     viewModel: SortingBottomSheetViewModel = hiltViewModel()
 ) {
     val sortingType by viewModel.state.collectAsStateWithLifecycle()
+    BackHandler { onNavigateEvent(SearchOptionsNavigation.Dismiss) }
     SortingBottomSheetContents(
         modifier = modifier,
         sortingType = sortingType,

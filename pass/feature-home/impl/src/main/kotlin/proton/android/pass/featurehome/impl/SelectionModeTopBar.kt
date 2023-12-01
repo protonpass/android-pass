eb13799417c843765c9c1ef46cdcc25cf70d3b31
Carlos Quintana <carlos.quintana@proton.ch>
2023-12-01 08:23:30 +0100

    feat(home): add back and counter to bulk topbar

--- pass/feature-home/impl/src/main/kotlin/proton/android/pass/featurehome/impl/SelectionModeTopBar.kt
+++ pass/feature-home/impl/src/main/kotlin/proton/android/pass/featurehome/impl/SelectionModeTopBar.kt
@@ -21,25 +21,43 @@ package proton.android.pass.featurehome.impl
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import me.proton.core.compose.component.appbar.ProtonTopAppBar
 import me.proton.core.presentation.R
 import proton.android.pass.commonui.api.PassTheme
+import proton.android.pass.commonui.api.ThemedBooleanPreviewProvider
 import proton.android.pass.featuresearchoptions.api.VaultSelectionOption
 
 @Composable
 fun SelectionModeTopBar(
     modifier: Modifier = Modifier,
     homeVaultSelection: VaultSelectionOption,
+    selectedItemCount: Int,
     onEvent: (HomeUiEvent) -> Unit
 ) {
     ProtonTopAppBar(
         modifier = modifier.padding(vertical = 12.dp),
-        title = { },
+        title = {
+            Text(
+                text = selectedItemCount.toString(),
+                color = PassTheme.colors.textNorm
+            )
+        },
         navigationIcon = {
+            IconButton(onClick = { onEvent(HomeUiEvent.StopBulk) }) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_arrow_back),
+                    contentDescription = null,
+                    tint = PassTheme.colors.textNorm
+                )
+            }
         },
         actions = {
             if (homeVaultSelection == VaultSelectionOption.Trash) {
@@ -47,14 +65,14 @@ fun SelectionModeTopBar(
                     Icon(
                         painter = painterResource(R.drawable.ic_proton_clock_rotate_left),
                         contentDescription = null,
-                        tint = PassTheme.colors.textWeak
+                        tint = PassTheme.colors.textNorm
                     )
                 }
                 IconButton(onClick = { onEvent(HomeUiEvent.PermanentlyDeleteItemsActionClick) }) {
                     Icon(
                         painter = painterResource(R.drawable.ic_proton_trash_cross),
                         contentDescription = null,
-                        tint = PassTheme.colors.textWeak
+                        tint = PassTheme.colors.textNorm
                     )
                 }
             } else {
@@ -62,17 +80,34 @@ fun SelectionModeTopBar(
                     Icon(
                         painter = painterResource(R.drawable.ic_proton_folder_arrow_in),
                         contentDescription = null,
-                        tint = PassTheme.colors.textWeak
+                        tint = PassTheme.colors.textNorm
                     )
                 }
                 IconButton(onClick = { onEvent(HomeUiEvent.DeleteItemsActionClick) }) {
                     Icon(
                         painter = painterResource(R.drawable.ic_proton_trash),
                         contentDescription = null,
-                        tint = PassTheme.colors.textWeak
+                        tint = PassTheme.colors.textNorm
                     )
                 }
             }
         }
     )
+}
+
+@Preview
+@Composable
+fun SelectionModeTopBarPreview(
+    @PreviewParameter(ThemedBooleanPreviewProvider::class) input: Pair<Boolean, Boolean>
+) {
+    val selection = if (input.second) VaultSelectionOption.Trash else VaultSelectionOption.AllVaults
+    PassTheme(isDark = input.first) {
+        Surface {
+            SelectionModeTopBar(
+                homeVaultSelection = selection,
+                selectedItemCount = 2,
+                onEvent = {}
+            )
+        }
+    }
 }

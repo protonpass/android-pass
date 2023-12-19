f91db7073c1eb16a402d0958c157d63d948b2e14
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-12-19 09:49:16 +0100

    chore: add apply pending logs

--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/ApplyPendingEventsImpl.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/ApplyPendingEventsImpl.kt
@@ -22,10 +22,13 @@ import androidx.work.WorkManager
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.withContext
 import me.proton.core.domain.entity.UserId
 import me.proton.core.user.domain.entity.AddressId
+import me.proton.core.user.domain.entity.User
 import me.proton.core.user.domain.extension.primary
 import me.proton.core.user.domain.repository.UserAddressRepository
 import proton.android.pass.crypto.api.context.EncryptionContextProvider
@@ -34,6 +37,7 @@ import proton.android.pass.data.api.errors.ShareNotAvailableError
 import proton.android.pass.data.api.repositories.ItemRepository
 import proton.android.pass.data.api.repositories.ItemSyncStatus
 import proton.android.pass.data.api.repositories.ItemSyncStatusRepository
+import proton.android.pass.data.api.repositories.RefreshSharesResult
 import proton.android.pass.data.api.repositories.ShareRepository
 import proton.android.pass.data.api.repositories.SyncMode
 import proton.android.pass.data.api.usecases.ApplyPendingEvents
@@ -44,11 +48,11 @@ import proton.android.pass.data.impl.extensions.toPendingEvent
 import proton.android.pass.data.impl.repositories.EventRepository
 import proton.android.pass.data.impl.responses.EventList
 import proton.android.pass.data.impl.work.FetchItemsWorker
-import proton.android.pass.log.api.PassLogger
 import proton.android.pass.domain.ShareColor
 import proton.android.pass.domain.ShareIcon
 import proton.android.pass.domain.ShareId
 import proton.android.pass.domain.entity.NewVault
+import proton.android.pass.log.api.PassLogger
 import javax.inject.Inject
 
 class ApplyPendingEventsImpl @Inject constructor(
@@ -68,27 +72,72 @@ class ApplyPendingEventsImpl @Inject constructor(
             val user = observeCurrentUser().first()
             val address = requireNotNull(addressRepository.getAddresses(user.userId).primary())
             val refreshSharesResult = shareRepository.refreshShares(user.userId)
+
             if (refreshSharesResult.allShareIds.isEmpty()) {
-                PassLogger.d(TAG, "Received an empty list of shares, creating default vault")
-                itemSyncStatusRepository.setMode(SyncMode.Background)
-                createDefaultVault(user.userId)
-                itemSyncStatusRepository.emit(ItemSyncStatus.CompletedSyncing(false))
+                handleSharesWhenEmpty(user)
             } else {
-                enqueueRefreshItems(refreshSharesResult.newShareIds)
-                refreshSharesResult.allShareIds.subtract(refreshSharesResult.newShareIds)
-                    .map { share ->
-                        async {
-                            try {
-                                applyPendingEvents(address.addressId, user.userId, share)
-                            } catch (e: ShareNotAvailableError) {
-                                PassLogger.d(TAG, e, "Deleting share not available")
-                                shareRepository.deleteVault(user.userId, share)
-                            }
-                        }
-                    }
-                    .awaitAll()
+                handleExistingShares(user.userId, address.addressId, refreshSharesResult)
             }
         }
+    }
+
+    private suspend fun handleSharesWhenEmpty(user: User) {
+        PassLogger.i(TAG, "Received an empty list of shares, creating default vault")
+        itemSyncStatusRepository.setMode(SyncMode.Background)
+        createDefaultVault(user.userId)
+        itemSyncStatusRepository.emit(ItemSyncStatus.CompletedSyncing(false))
+    }
+
+    private suspend fun handleExistingShares(
+        userId: UserId,
+        addressId: AddressId,
+        refreshSharesResult: RefreshSharesResult
+    ) {
+        PassLogger.i(TAG, "Received a list of shares, applying pending events")
+        enqueueRefreshItems(refreshSharesResult.newShareIds)
+        withContext(Dispatchers.IO) {
+            val existingShares =
+                refreshSharesResult.allShareIds.subtract(refreshSharesResult.newShareIds)
+            val results = existingShares.map { share ->
+                async {
+                    runCatching {
+                        PassLogger.d(TAG, "Applying pending events for share id: $share")
+                        applyPendingEvents(addressId, userId, share)
+                    }
+                        .onFailure {
+                            PassLogger.w(TAG, "Error applying pending events for share id: $share")
+                            PassLogger.w(TAG, it)
+                            if (it is ShareNotAvailableError) {
+                                onShareNotAvailable(userId, share)
+                            }
+                        }
+                }
+            }.awaitAll()
+            val errors = results.filter { it.isFailure }.mapNotNull { it.exceptionOrNull() }
+                .filterNot { it is ShareNotAvailableError }
+            if (errors.isNotEmpty()) {
+                PassLogger.i(TAG, "Error applying pending events")
+            } else {
+                PassLogger.i(TAG, "Applied pending events")
+            }
+        }
+    }
+
+    private suspend fun onShareNotAvailable(
+        userId: UserId,
+        shareId: ShareId
+    ) {
+        PassLogger.i(TAG, "Deleting share not available")
+        runCatching {
+            shareRepository.deleteVault(userId, shareId)
+        }
+            .onSuccess {
+                PassLogger.i(TAG, "Deleted unavailable share id: $shareId")
+            }
+            .onFailure { t ->
+                PassLogger.w(TAG, "Error deleting unavailable share id: $shareId")
+                PassLogger.w(TAG, t)
+            }
     }
 
     private suspend fun createDefaultVault(userId: UserId) {
@@ -103,9 +152,10 @@ class ApplyPendingEventsImpl @Inject constructor(
         runCatching {
             createVault(userId, vault)
         }.onSuccess {
-            PassLogger.d(TAG, "Default vault created")
+            PassLogger.i(TAG, "Default vault created")
         }.onFailure {
-            PassLogger.d(TAG, it, "Error creating default vault")
+            PassLogger.w(TAG, "Error creating default vault")
+            PassLogger.w(TAG, it)
         }
     }
 
@@ -149,6 +199,7 @@ class ApplyPendingEventsImpl @Inject constructor(
     private fun enqueueRefreshItems(shares: Set<ShareId>) {
         if (shares.isNotEmpty()) {
             val request = FetchItemsWorker.getRequestFor(shares.toList())
+            PassLogger.i(TAG, "Enqueuing FetchItemsWorker")
             workManager.enqueue(request)
         }
     }

2e7af42dfe4ef539c941909a9a1030efe1d0af99
Alexander Talledo <alexander.talledo@proton.ch>
2023-12-21 10:03:11 +0100

    test: fix compilation errors
    Signed-off-by: Alexander Talledo <alexander.talledo@proton.ch>

--- pass/common-rust/impl/src/test/kotlin/proton/android/pass/commonrust/impl/passwords/strengths/RustPasswordStrengthMapperTest.kt
+++ pass/common-rust/impl/src/test/kotlin/proton/android/pass/commonrust/impl/passwords/strengths/RustPasswordStrengthMapperTest.kt
@@ -22,6 +22,7 @@ import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
+import proton.android.pass.common.api.PasswordStrength
 import proton.android.pass.commonrust.PasswordScore
 
 @RunWith(Parameterized::class)

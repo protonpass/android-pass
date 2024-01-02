aa8059cb462bd6852bfe66c5e30dc1ecf8e3571e
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-01-02 17:35:28 +0100

    fix: avoid displaying move to trash in item detail items without permissions

--- pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/note/NoteTopBarOptionsBottomSheetContents.kt
+++ pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/note/NoteTopBarOptionsBottomSheetContents.kt
@@ -16,30 +16,39 @@
  * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package proton.android.pass.featureitemdetail.impl.common
+package proton.android.pass.featureitemdetail.impl.note
 
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import kotlinx.collections.immutable.toPersistentList
 import proton.android.pass.commonui.api.PassTheme
-import proton.android.pass.commonui.api.ThemePairPreviewProvider
 import proton.android.pass.commonui.api.bottomSheet
 import proton.android.pass.composecomponents.impl.bottomsheet.BottomSheetItem
+import proton.android.pass.composecomponents.impl.bottomsheet.BottomSheetItemIcon
 import proton.android.pass.composecomponents.impl.bottomsheet.BottomSheetItemList
+import proton.android.pass.composecomponents.impl.bottomsheet.BottomSheetItemTitle
 import proton.android.pass.composecomponents.impl.bottomsheet.withDividers
+import proton.android.pass.featureitemdetail.impl.R
+import proton.android.pass.featureitemdetail.impl.common.ThemedTopBarOptionsPreviewProvider
+import proton.android.pass.featureitemdetail.impl.common.TopBarOptionsParameters
+import proton.android.pass.featureitemdetail.impl.common.migrate
+import proton.android.pass.featureitemdetail.impl.common.moveToTrash
 
 @Composable
-fun TopBarOptionsBottomSheetContents(
+fun NoteTopBarOptionsBottomSheetContents(
     modifier: Modifier = Modifier,
     canMigrate: Boolean,
     canMoveToTrash: Boolean,
     onMigrate: () -> Unit,
-    onMoveToTrash: () -> Unit
+    onMoveToTrash: () -> Unit,
+    onCopyNote: () -> Unit = {}
 ) {
     val items = mutableListOf<BottomSheetItem>()
+    items.add(copyNote(onClick = onCopyNote))
     if (canMigrate) {
         items.add(migrate(onClick = onMigrate))
     }
@@ -52,17 +61,29 @@ fun TopBarOptionsBottomSheetContents(
     )
 }
 
-class ThemedTopBarOptionsPreviewProvider :
-    ThemePairPreviewProvider<TopBarOptionsParameters>(TopBarOptionsParametersPreviewProvider())
+private fun copyNote(onClick: () -> Unit): BottomSheetItem =
+    object : BottomSheetItem {
+        override val title: @Composable () -> Unit
+            get() = { BottomSheetItemTitle(text = stringResource(R.string.note_detail_copy_note_to_clipboard)) }
+        override val subtitle: @Composable (() -> Unit)?
+            get() = null
+        override val leftIcon: @Composable (() -> Unit)
+            get() = { BottomSheetItemIcon(iconId = me.proton.core.presentation.R.drawable.ic_proton_squares) }
+        override val endIcon: (@Composable () -> Unit)?
+            get() = null
+        override val onClick: () -> Unit
+            get() = { onClick() }
+        override val isDivider = false
+    }
 
 @Preview
 @Composable
-fun TopBarOptionsBottomSheetContentsPreview(
+fun NoteTopBarOptionsBSContentsPreview(
     @PreviewParameter(ThemedTopBarOptionsPreviewProvider::class) input: Pair<Boolean, TopBarOptionsParameters>
 ) {
     PassTheme(isDark = input.first) {
         Surface {
-            TopBarOptionsBottomSheetContents(
+            NoteTopBarOptionsBottomSheetContents(
                 canMigrate = input.second.canMigrate,
                 canMoveToTrash = input.second.canMoveToTrash,
                 onMigrate = {},

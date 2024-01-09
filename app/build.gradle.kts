0dc264e03e4b31c31c7d6689202a12cfd8e75811
Mateusz Armatys <mateusz.armatys@proton.ch>
2024-01-09 14:02:15 +0100

    test: Minor improvement for `AccountRecoveryFlowTest`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -248,6 +248,8 @@ android {
         resources.excludes.add("META-INF/licenses/**")
         resources.excludes.add("META-INF/AL2.0")
         resources.excludes.add("META-INF/LGPL2.1")
+        resources.excludes.add("META-INF/LICENSE.md")
+        resources.excludes.add("META-INF/LICENSE-notice.md")
     }
 
     kapt {

15f57139d7d970a77a0bed69f272194eb0620de0
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-01-10 14:08:24 +0100

    test: remove preview

--- pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/item/header/ItemListHeader.kt
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/item/header/ItemListHeader.kt
@@ -21,14 +21,9 @@ package proton.android.pass.composecomponents.impl.item.header
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import proton.android.pass.commonui.api.PassTheme
-import proton.android.pass.commonui.api.ThemedBooleanPreviewProvider
 
 @Composable
 fun ItemListHeader(
@@ -43,20 +38,5 @@ fun ItemListHeader(
     ) {
         countContent()
         sortingContent()
-    }
-}
-
-@Preview
-@Composable
-fun ItemListHeaderPreview(
-    @PreviewParameter(ThemedBooleanPreviewProvider::class) input: Pair<Boolean, Boolean>
-) {
-    PassTheme(isDark = input.first) {
-        Surface {
-            ItemListHeader(
-                countContent = {},
-                sortingContent = {},
-            )
-        }
     }
 }

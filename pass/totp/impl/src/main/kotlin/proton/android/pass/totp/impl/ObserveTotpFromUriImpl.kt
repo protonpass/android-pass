1ef3be7bfcfb2ab8b4f2715aa268b2b02ee7e44e
Carlos Quintana <carlos.quintana@proton.ch>
2024-01-11 12:15:53 +0100

    chore: adapt totp to new rust library usage

--- pass/totp/impl/src/main/kotlin/proton/android/pass/totp/impl/ObserveTotpFromUriImpl.kt
+++ pass/totp/impl/src/main/kotlin/proton/android/pass/totp/impl/ObserveTotpFromUriImpl.kt
@@ -19,7 +19,6 @@
 package proton.android.pass.totp.impl
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import proton.android.pass.totp.api.ObserveTotpFromUri
 import proton.android.pass.totp.api.TotpManager
 import javax.inject.Inject
@@ -28,10 +27,6 @@ class ObserveTotpFromUriImpl @Inject constructor(
     private val totpManager: TotpManager
 ) : ObserveTotpFromUri {
 
-    override fun invoke(uri: String): Flow<TotpManager.TotpWrapper> = totpManager.parse(uri)
-        .fold(
-            onSuccess = { totpManager.observeCode(it) },
-            onFailure = { flow { throw it } }
-        )
+    override fun invoke(uri: String): Flow<TotpManager.TotpWrapper> = totpManager.observeCode(uri)
 }
 

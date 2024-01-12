089b0f28bef3c4285c6e9c5db7604b26067ed081
Carlos Quintana <carlos.quintana@proton.ch>
2024-01-12 15:34:48 +0100

    feat(data): allow to invite multiple users with a single invocation

--- pass/data/impl/src/test/kotlin/proton/android/pass/data/impl/fakes/TestRemoteInviteDataSource.kt
+++ pass/data/impl/src/test/kotlin/proton/android/pass/data/impl/fakes/TestRemoteInviteDataSource.kt
@@ -21,16 +21,9 @@ package proton.android.pass.data.impl.fakes
 import me.proton.core.domain.entity.UserId
 import proton.android.pass.data.impl.remote.RemoteInviteDataSource
 import proton.android.pass.data.impl.requests.AcceptInviteRequest
-<<<<<<< HEAD
-import proton.android.pass.data.impl.requests.CreateInviteRequest
-import proton.android.pass.data.impl.requests.CreateInvitesRequest
-import proton.android.pass.data.impl.requests.CreateNewUserInviteRequest
 import proton.android.pass.data.impl.requests.CreateNewUserInvitesRequest
 import proton.android.pass.data.impl.responses.InviteRecommendationResponse
-=======
 import proton.android.pass.data.impl.requests.CreateInvitesRequest
-import proton.android.pass.data.impl.requests.CreateNewUserInvitesRequest
->>>>>>> 4448ab378 (test: fix unit tests)
 import proton.android.pass.data.impl.responses.PendingInviteResponse
 import proton.android.pass.data.impl.responses.ShareResponse
 import proton.android.pass.domain.InviteToken

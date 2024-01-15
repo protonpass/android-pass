5528498fb9518873fca6c95897770eb0d5952f9a
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-01-15 15:32:30 +0100

    chore: add family to generate password

--- pass/feature-password/impl/src/main/kotlin/proton/android/pass/featurepassword/impl/bottomsheet/GeneratePasswordViewContent.kt
+++ pass/feature-password/impl/src/main/kotlin/proton/android/pass/featurepassword/impl/bottomsheet/GeneratePasswordViewContent.kt
@@ -27,6 +27,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
@@ -65,7 +66,7 @@ fun GeneratePasswordViewContent(
             )
             Text(
                 text = annotatedString,
-                style = ProtonTheme.typography.subheadlineNorm,
+                style = ProtonTheme.typography.subheadlineNorm.copy(fontFamily = FontFamily.Monospace),
             )
 
             ProtonPasswordStrengthItem(passwordStrength = state.passwordStrength)

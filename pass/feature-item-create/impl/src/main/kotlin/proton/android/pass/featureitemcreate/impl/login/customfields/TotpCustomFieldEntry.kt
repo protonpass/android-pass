ddf263f0e9bea3ecc25f84905849199319824795
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-01-15 15:10:39 +0100

    feat: support monospace in passwords secrets

--- pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/login/customfields/TotpCustomFieldEntry.kt
+++ pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/login/customfields/TotpCustomFieldEntry.kt
@@ -27,6 +27,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
@@ -81,7 +82,7 @@ fun TotpCustomFieldEntry(
         isError = isError,
         errorMessage = errorMessage,
         moveToNextOnEnter = true,
-        textStyle = ProtonTheme.typography.defaultNorm(canEdit),
+        textStyle = ProtonTheme.typography.defaultNorm(canEdit).copy(fontFamily = FontFamily.Monospace),
         onFocusChange = { onFocusChange(LoginCustomField.CustomFieldTOTP(index), it) },
         label = { ProtonTextFieldLabel(text = content.label, isError = isError) },
         placeholder = {

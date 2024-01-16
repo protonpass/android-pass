6b983c204f216ce6df6864209279ee219addfce5
Carlos Quintana <carlos.quintana@proton.ch>
2024-01-16 17:04:25 +0100

    feat(sharing): invite multiple users set permissions ui

---
+++ pass/feature-sharing/impl/src/main/kotlin/proton/android/pass/featuresharing/impl/sharingpermissions/SharingPermissionItem.kt
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.featuresharing.impl.sharingpermissions
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import proton.android.pass.commonui.api.PassTheme
+import proton.android.pass.commonui.api.body3Norm
+import proton.android.pass.commonui.api.body3Weak
+import proton.android.pass.composecomponents.impl.R
+import proton.android.pass.composecomponents.impl.container.CircleTextIcon
+import proton.android.pass.featuresharing.impl.extensions.toStringResource
+
+@Composable
+fun SharingPermissionItem(
+    modifier: Modifier = Modifier,
+    address: AddressPermissionUiState,
+    onPermissionChangeClick: () -> Unit
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        CircleTextIcon(
+            text = address.address,
+            backgroundColor = PassTheme.colors.interactionNormMinor1,
+            textColor = PassTheme.colors.interactionNormMajor2,
+            shape = PassTheme.shapes.squircleMediumShape
+        )
+
+        Column(
+            modifier = modifier.weight(1f),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            Text(
+                text = address.address,
+                style = PassTheme.typography.body3Norm()
+            )
+
+            Text(
+                text = stringResource(address.permission.toStringResource()),
+                style = PassTheme.typography.body3Weak()
+            )
+        }
+
+        IconButton(
+            onClick = onPermissionChangeClick,
+            modifier = Modifier.size(24.dp)
+        ) {
+            Icon(
+                imageVector = ImageVector.vectorResource(R.drawable.ic_three_dots_vertical_24),
+                contentDescription = stringResource(id = R.string.action_content_description_menu),
+                tint = PassTheme.colors.textHint
+            )
+        }
+    }
+}
---
+++ pass/feature-sharing/impl/src/main/kotlin/proton/android/pass/featuresharing/impl/sharingpermissions/bottomsheet/SharingPermissionsBottomSheetViewModel.kt
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.featuresharing.impl.sharingpermissions.bottomsheet
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import proton.android.pass.commonui.api.SavedStateHandleProvider
+import proton.android.pass.commonui.api.require
+import proton.android.pass.data.api.repositories.BulkInviteRepository
+import proton.android.pass.featuresharing.impl.extensions.toShareRole
+import proton.android.pass.featuresharing.impl.sharingpermissions.SharingType
+import javax.inject.Inject
+
+@HiltViewModel
+class SharingPermissionsBottomSheetViewModel @Inject constructor(
+    private val bulkInviteRepository: BulkInviteRepository,
+    private val savedStateHandleProvider: SavedStateHandleProvider
+) : ViewModel() {
+
+    private val mode = getMode()
+
+    private val eventFlow: MutableStateFlow<SharingPermissionsBottomSheetEvent> =
+        MutableStateFlow(SharingPermissionsBottomSheetEvent.Unknown)
+
+    val state: StateFlow<SharingPermissionsBottomSheetUiState> = combine(
+        eventFlow,
+        bulkInviteRepository.observeAddresses()
+    ) { event, addresses ->
+        SharingPermissionsBottomSheetUiState(
+            event = event,
+            mode = mode.toUi(),
+            displayRemove = when (mode) {
+                is SharingPermissionMode.SetAll -> false
+                is SharingPermissionMode.SetOne -> { addresses.size > 1 }
+            }
+        )
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000L),
+        initialValue = SharingPermissionsBottomSheetUiState.Initial(mode = mode.toUi())
+    )
+
+    fun onPermissionSelected(sharingType: SharingType) = viewModelScope.launch {
+        when (mode) {
+            is SharingPermissionMode.SetAll -> {
+                bulkInviteRepository.setAllPermissions(sharingType.toShareRole())
+            }
+            is SharingPermissionMode.SetOne -> {
+                bulkInviteRepository.setPermission(
+                    address = mode.email,
+                    permission = sharingType.toShareRole()
+                )
+            }
+        }
+        eventFlow.update { SharingPermissionsBottomSheetEvent.Close }
+    }
+
+    fun onDeleteUser() = viewModelScope.launch {
+        when (mode) {
+            is SharingPermissionMode.SetAll -> {
+                throw IllegalStateException("Cannot delete user in SetAll mode")
+            }
+            is SharingPermissionMode.SetOne -> {
+                bulkInviteRepository.removeAddress(mode.email)
+            }
+        }
+        eventFlow.update { SharingPermissionsBottomSheetEvent.Close }
+    }
+
+    fun clearEvent() = viewModelScope.launch {
+        eventFlow.update { SharingPermissionsBottomSheetEvent.Unknown }
+    }
+
+    private fun getMode(): SharingPermissionMode {
+        val savedState = savedStateHandleProvider.get()
+        val mode = savedState.require<String>(EditPermissionsModeNavArgId.key)
+        val modeAsEnum = EditPermissionsMode.values().firstOrNull { it.name == mode }
+        return when (modeAsEnum) {
+            null -> throw IllegalArgumentException("Unknown mode: $mode")
+            EditPermissionsMode.SingleUser -> {
+                val email = savedState.require<String>(EmailNavArgId.key)
+                val permission = savedState.require<String>(PermissionNavArgId.key)
+                val permissionAsEnum = SharingType.values().firstOrNull { it.name == permission }
+                when (permissionAsEnum) {
+                    null -> throw IllegalArgumentException("Unknown permission: $permission")
+                    else -> SharingPermissionMode.SetOne(email, permissionAsEnum)
+                }
+            }
+            EditPermissionsMode.AllUsers -> SharingPermissionMode.SetAll
+        }
+    }
+
+    private fun SharingPermissionMode.toUi() = when (this) {
+        SharingPermissionMode.SetAll -> SharingPermissionsEditMode.All
+        is SharingPermissionMode.SetOne -> SharingPermissionsEditMode.EditOne(
+            email = email,
+            sharingType = currentPermission
+        )
+    }
+
+    sealed interface SharingPermissionMode {
+        object SetAll : SharingPermissionMode
+        data class SetOne(
+            val email: String,
+            val currentPermission: SharingType
+        ) : SharingPermissionMode
+    }
+
+}

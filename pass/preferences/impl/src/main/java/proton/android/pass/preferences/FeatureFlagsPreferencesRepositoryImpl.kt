24c492a285318de33f54472439a347685c59fc1a
Alexander Talledo <alexander.talledo@proton.ch>
2024-01-17 17:33:11 +0100

    refactor: use core unleash implementation

--- pass/preferences/impl/src/main/java/proton/android/pass/preferences/FeatureFlagsPreferencesRepositoryImpl.kt
+++ pass/preferences/impl/src/main/java/proton/android/pass/preferences/FeatureFlagsPreferencesRepositoryImpl.kt
@@ -27,7 +27,9 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.runBlocking
 import me.proton.android.pass.preferences.BoolFlagPrefProto
 import me.proton.android.pass.preferences.BooleanPrefProto
-import proton.android.pass.data.api.repositories.FeatureFlagRepository
+import me.proton.core.accountmanager.domain.AccountManager
+import me.proton.core.featureflag.domain.entity.FeatureId
+import me.proton.core.featureflag.domain.repository.FeatureFlagRepository
 import proton.android.pass.log.api.PassLogger
 import proton.android.pass.preferences.FeatureFlag.AUTOFILL_DEBUG_MODE
 import proton.android.pass.preferences.FeatureFlag.CREDIT_CARD_AUTOFILL
@@ -41,8 +43,9 @@ import javax.inject.Singleton
 @Suppress("UNCHECKED_CAST")
 @Singleton
 class FeatureFlagsPreferencesRepositoryImpl @Inject constructor(
+    private val accountManager: AccountManager,
     private val featureFlagManager: FeatureFlagRepository,
-    private val dataStore: DataStore<FeatureFlagsPreferences>
+    private val dataStore: DataStore<FeatureFlagsPreferences>,
 ) : FeatureFlagsPreferencesRepository {
 
     override fun <T> get(featureFlag: FeatureFlag): Flow<T> =
@@ -59,30 +62,43 @@ class FeatureFlagsPreferencesRepositoryImpl @Inject constructor(
             AUTOFILL_DEBUG_MODE -> setFeatureFlag {
                 autofillDebugModeEnabled = boolFlagPrefProto(value)
             }
+
             SHARING_NEW_USERS -> setFeatureFlag {
                 sharingNewUsersEnabled = boolFlagPrefProto(value)
             }
+
             SHARING_V1 -> setFeatureFlag {
                 sharingV1Enabled = boolFlagPrefProto(value)
             }
+
             CREDIT_CARD_AUTOFILL -> setFeatureFlag {
                 creditCardAutofillEnabled = boolFlagPrefProto(value)
             }
+
             PINNING_V1 -> setFeatureFlag {
                 pinningV1Enabled = boolFlagPrefProto(value)
             }
         }
 
-    private fun <T> getFeatureFlag(key: String?, prefGetter: FeatureFlagsPreferences.() -> BooleanPrefProto): Flow<T> =
+    private fun <T> getFeatureFlag(
+        key: String?,
+        prefGetter: FeatureFlagsPreferences.() -> BooleanPrefProto
+    ): Flow<T> =
         if (key != null) {
-            featureFlagManager.isFeatureEnabled(key)
-                .flatMapLatest { ff ->
+            accountManager.getPrimaryUserId()
+                .flatMapLatest { userId ->
+                    featureFlagManager.observe(
+                        userId = userId,
+                        featureId = FeatureId(id = key),
+                    )
+                }
+                .flatMapLatest { featureFlag ->
                     dataStore.data
                         .catch { exception -> handleExceptions(exception) }
                         .map { preferences ->
                             fromBooleanPrefProto(
                                 pref = prefGetter(preferences),
-                                default = ff
+                                default = featureFlag?.value ?: false,
                             ) as T
                         }
                 }

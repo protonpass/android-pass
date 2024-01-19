0af3d5b02f6cbc9b80eb3d5380d541f20cb9459b
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-01-19 12:04:42 +0100

    feat: add manage account url

--- app/src/main/java/proton/android/pass/ui/navigation/AppGraph.kt
+++ app/src/main/java/proton/android/pass/ui/navigation/AppGraph.kt
@@ -442,7 +442,6 @@ fun NavGraphBuilder.appGraph(
                 AccountNavigation.SignOut -> appNavigator.navigate(SignOutDialog)
                 AccountNavigation.Subscription -> onNavigate(AppNavigation.Subscription)
                 AccountNavigation.Upgrade -> onNavigate(AppNavigation.Upgrade)
-                AccountNavigation.ManageAccount -> TODO()
             }
         }
     )

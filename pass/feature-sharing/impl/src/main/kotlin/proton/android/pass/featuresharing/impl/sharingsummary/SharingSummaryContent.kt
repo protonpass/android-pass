ff1deae9953b664c3ceb4f7020bde5e4ed5ac7aa
Carlos Quintana <carlos.quintana@proton.ch>
2024-01-19 09:11:35 +0100

    feat(sharing): implement multiple addresses in SharingSummary

--- pass/feature-sharing/impl/src/main/kotlin/proton/android/pass/featuresharing/impl/sharingsummary/SharingSummaryContent.kt
+++ pass/feature-sharing/impl/src/main/kotlin/proton/android/pass/featuresharing/impl/sharingsummary/SharingSummaryContent.kt
@@ -20,9 +20,10 @@ package proton.android.pass.featuresharing.impl.sharingsummary
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -31,6 +32,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import proton.android.pass.commonui.api.PassTheme
 import proton.android.pass.commonui.api.body3Norm
+import proton.android.pass.commonui.api.body3Weak
 import proton.android.pass.commonui.api.heroNorm
 import proton.android.pass.composecomponents.impl.buttons.LoadingCircleButton
 import proton.android.pass.composecomponents.impl.topbar.BackArrowTopAppBar
@@ -67,21 +69,36 @@ fun SharingSummaryContent(
                 }
             )
         }
-    ) {
-        Column(
+    ) { padding ->
+        LazyColumn(
             modifier = Modifier
-                .padding(it)
+                .padding(padding)
                 .padding(16.dp)
-                .verticalScroll(rememberScrollState()),
-            verticalArrangement = Arrangement.spacedBy(24.dp)
         ) {
-            Text(
-                text = stringResource(R.string.share_summary_title),
-                style = PassTheme.typography.heroNorm()
-            )
-            ShareToEmailSection(email = state.email)
-            VaultRowSection(vaultWithItemCount = state.vaultWithItemCount)
-            PermissionSummarySection(sharingType = state.sharingType)
+            item {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(24.dp)
+                ) {
+                    Text(
+                        text = stringResource(R.string.share_summary_title),
+                        style = PassTheme.typography.heroNorm()
+                    )
+                    VaultRowSection(vaultWithItemCount = state.vaultWithItemCount)
+
+                    Text(
+                        text = stringResource(R.string.share_with_title),
+                        style = PassTheme.typography.body3Weak()
+                    )
+                }
+            }
+
+            items(items = state.addresses, key = { it.address }) { address ->
+                AddressRowSection(
+                    modifier = Modifier.padding(vertical = 12.dp),
+                    address = address
+                )
+            }
         }
     }
 }

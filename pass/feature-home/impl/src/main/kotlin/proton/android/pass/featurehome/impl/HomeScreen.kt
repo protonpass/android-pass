a82fe4ba8b905929a1db95df0d3ca53050ac9c64
Alexander Talledo <alexander.talledo@proton.ch>
2024-01-22 14:44:31 +0100

    feat: support alias item pinning from home screen

--- pass/feature-home/impl/src/main/kotlin/proton/android/pass/featurehome/impl/HomeScreen.kt
+++ pass/feature-home/impl/src/main/kotlin/proton/android/pass/featurehome/impl/HomeScreen.kt
@@ -279,6 +279,18 @@ fun HomeScreen(
                             homeViewModel.copyToClipboard(it, HomeClipboardType.Alias)
                         }
                     },
+                    onPinned = remember {
+                        { shareId, itemId ->
+                            scope.launch { bottomSheetState.hide() }
+                            homeViewModel.pinItem(shareId, itemId)
+                        }
+                    },
+                    onUnpinned = remember {
+                        { shareId, itemId ->
+                            scope.launch { bottomSheetState.hide() }
+                            homeViewModel.unpinItem(shareId, itemId)
+                        }
+                    },
                     onEdit = remember {
                         { shareId, itemId ->
                             scope.launch { bottomSheetState.hide() }

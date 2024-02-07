e56bf7e297ccdd0857f013f59377767ba5bbcea1
Alexander Talledo <alexander.talledo@proton.ch>
2024-02-07 18:39:35 +0100

    chore: smooth TOTP progress indicator transitions

--- pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/login/totp/TotpProgress.kt
+++ pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/login/totp/TotpProgress.kt
@@ -18,11 +18,16 @@
 
 package proton.android.pass.featureitemdetail.impl.login.totp
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,21 +38,59 @@ import me.proton.core.compose.theme.defaultSmallNorm
 import proton.android.pass.commonui.api.PassTheme
 import proton.android.pass.commonui.api.ThemePreviewProvider
 
+private const val PROGRESS_ANIMATION_LABEL = "TOTP Progress Animation"
+private const val PROGRESS_ANIMATION_DURATION_MILLIS = 1_000
+
+private const val PROGRESS_COLOR_ANIMATION_LABEL = "TOTP Progress Color Animation"
+private const val PROGRESS_COLOR_ANIMATION_DURATION_MILLIS = 500
+
+private const val PROGRESS_LIMIT_1 = 0f
+private const val PROGRESS_LIMIT_2 = 0.20f
+private const val PROGRESS_LIMIT_3 = 0.33f
+
 @Composable
 fun TotpProgress(
     modifier: Modifier = Modifier,
     remainingSeconds: Int,
-    totalSeconds: Int
+    totalSeconds: Int,
 ) {
-    Box(modifier = modifier, contentAlignment = Alignment.Center) {
-        CircularProgressIndicator(
-            progress = remainingSeconds.toFloat() / totalSeconds.toFloat(),
-            color = ProtonTheme.colors.notificationSuccess,
-            strokeWidth = 3.dp
+    val currentProgress = remainingSeconds.toFloat() / totalSeconds.toFloat()
+
+    val animatedProgress by animateFloatAsState(
+        label = PROGRESS_ANIMATION_LABEL,
+        targetValue = currentProgress,
+        animationSpec = tween(
+            durationMillis = PROGRESS_ANIMATION_DURATION_MILLIS,
+            easing = LinearEasing,
+        ),
+    )
+
+    val animatedProgressColor by animateColorAsState(
+        label = PROGRESS_COLOR_ANIMATION_LABEL,
+        targetValue = when (currentProgress) {
+            in PROGRESS_LIMIT_2..PROGRESS_LIMIT_3 -> ProtonTheme.colors.notificationWarning
+            in PROGRESS_LIMIT_1..PROGRESS_LIMIT_2 -> ProtonTheme.colors.notificationError
+            else -> ProtonTheme.colors.notificationSuccess
+        },
+        animationSpec = tween(
+            durationMillis = PROGRESS_COLOR_ANIMATION_DURATION_MILLIS,
+            easing = LinearEasing,
         )
+    )
+
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center,
+    ) {
+        CircularProgressIndicator(
+            progress = animatedProgress,
+            color = animatedProgressColor,
+            strokeWidth = 3.dp,
+        )
+
         Text(
             text = remainingSeconds.toString(),
-            style = ProtonTheme.typography.defaultSmallNorm
+            style = ProtonTheme.typography.defaultSmallNorm,
         )
     }
 }

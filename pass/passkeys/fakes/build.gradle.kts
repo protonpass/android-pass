359aa59b434c7523ad007981644a3089085ba8ad
Carlos Quintana <carlos.quintana@proton.ch>
2024-02-20 15:07:12 +0100

    chore(passkeys): add fakes module for passkeys

---
+++ pass/passkeys/fakes/build.gradle.kts
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("dagger.hilt.android.plugin")
+    id("org.jetbrains.kotlin.kapt")
+}
+
+android {
+    compileSdk = libs.versions.compileSdk.get().toInt()
+    namespace = "proton.android.pass.passkeys.fakes"
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+androidComponents.beforeVariants { variant ->
+    variant.enableAndroidTest = false
+}
+
+dependencies {
+    api(projects.pass.passkeys.api)
+
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.datetime)
+
+    implementation(projects.pass.common.api)
+    implementation(projects.pass.domain)
+
+    implementation(libs.dagger.hilt.android)
+    kapt(libs.dagger.hilt.android.compiler)
+    kapt(libs.androidx.hilt.compiler)
+}
---
+++ pass/passkeys/fakes/src/main/kotlin/proton/android/pass/passkeys/fakes/FakesPasskeysModule.kt
@@ -0,0 +1,24 @@
+package proton.android.pass.passkeys.fakes
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import proton.android.pass.passkeys.api.AuthenticateWithPasskey
+import proton.android.pass.passkeys.api.GeneratePasskey
+import proton.android.pass.passkeys.api.ParseCreatePasskeyRequest
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class FakesPasskeysModule {
+
+    @Binds
+    abstract fun bindAuthenticateWithPasskey(impl: TestAuthenticateWithPasskey): AuthenticateWithPasskey
+
+    @Binds
+    abstract fun bindGeneratePasskey(impl: TestGeneratePasskey): GeneratePasskey
+
+    @Binds
+    abstract fun bindParseCreatePasskeyRequest(impl: TestParseCreatePasskeyRequest): ParseCreatePasskeyRequest
+}
+

3838bc3cf5ac6aafb9e9a1be0ba8c668cb749c07
Carlos Quintana <carlos.quintana@proton.ch>
2024-02-27 15:20:38 +0100

    chore: update to latest proto and library version + use new required fields

--- pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/login/UIPasskeyContent.kt
+++ pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/login/UIPasskeyContent.kt
@@ -37,7 +37,7 @@ data class UIPasskeyContent(
     val createTime: Int,
     val note: String,
     val credentialId: ByteArray,
-    val userHandle: ByteArray
+    val userHandle: ByteArray?
 ) : Parcelable {
 
     @Suppress("ReturnCount")

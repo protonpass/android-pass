a0b5e3dc0ac00ffd4212b13be9d481e95198cc60
Carlos Quintana <carlos.quintana@proton.ch>
2024-02-29 14:36:34 +0100

    feat(data): offer use-cases for observing organization settings and check if addresses can be invited

---
+++ pass/domain/src/main/kotlin/proton/android/pass/domain/OrganizationSettings.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.domain
+
+enum class OrganizationShareMode(val value: Int) {
+    Unrestricted(0),
+    OrganizationOnly(1);
+
+    companion object {
+        fun fromValue(value: Int): OrganizationShareMode = values()
+            .firstOrNull { it.value == value }
+            ?: Unrestricted
+    }
+}
+
+sealed interface OrganizationSettings {
+    object NotAnOrganization : OrganizationSettings
+    data class Organization(
+        val canUpdate: Boolean,
+        val shareMode: OrganizationShareMode
+    ) : OrganizationSettings
+}

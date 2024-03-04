e9f30a48b63e7e46a2702a2094d403dfcd69f23b
Alexander Talledo <alexander.talledo@proton.ch>
2024-03-04 18:39:14 +0100

    fix: fixed copied content when plain custom field is clicked

--- pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/item/details/sections/login/PassLoginItemDetailCustomFieldsSection.kt
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/item/details/sections/login/PassLoginItemDetailCustomFieldsSection.kt
@@ -64,7 +64,7 @@ internal fun PassLoginItemDetailCustomFieldsSection(
                         itemColors = itemColors,
                         onClick = {
                             onSectionClick(
-                                customField.title,
+                                customField.content,
                                 ItemDetailsFieldType.Plain.CustomField,
                             )
                         },

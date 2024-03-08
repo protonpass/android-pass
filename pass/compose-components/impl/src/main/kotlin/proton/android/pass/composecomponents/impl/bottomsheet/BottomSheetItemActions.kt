e6e6a464b7dbcf3ae24a360eb0fa4eb0ca1e2518
Alexander Talledo <alexander.talledo@proton.ch>
2024-03-08 13:15:26 +0100

    fix: fixed pass plus icon

--- pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/bottomsheet/BottomSheetItemActions.kt
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/bottomsheet/BottomSheetItemActions.kt
@@ -18,10 +18,12 @@
 
 package proton.android.pass.composecomponents.impl.bottomsheet
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import proton.android.pass.composecomponents.impl.R
@@ -108,8 +110,11 @@ fun viewHistory(
 
     override val endIcon: @Composable (() -> Unit)
         get() = {
-            if (isFreePlan) {
-                BottomSheetItemIcon(iconId = CompR.drawable.ic_pass_plus)
+            if (!isFreePlan) {
+                Image(
+                    painter = painterResource(id = CompR.drawable.ic_pass_plus),
+                    contentDescription = null
+                )
             }
         }
 

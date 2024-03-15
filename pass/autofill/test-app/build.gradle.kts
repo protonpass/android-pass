08df7d18b4e9c38eee722882330133a17d7bc3e5
Carlos Quintana <carlos.quintana@proton.ch>
2024-03-15 14:41:45 +0100

    bump: version 1.18.3 → 1.19.0

--- pass/autofill/test-app/build.gradle.kts
+++ pass/autofill/test-app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("kotlin-android")
 }
 
-val appVersionName = "1.18.3"
+val appVersionName = "1.19.0"
 val appVersionCode = versionCode(appVersionName)
 val archivesBaseName = "AutofillTestApp-$appVersionName"
 

cb8347732d247c713d27e0997a7527dc0c4bfa5d
Alexander Talledo <alexander.talledo@proton.ch>
2024-03-19 10:20:12 +0100

    test: added item migration unit tests

--- pass/crypto/impl/src/test/kotlin/proton/android/pass/crypto/impl/usecases/MigrateItemsImplTest.kt
+++ pass/crypto/impl/src/test/kotlin/proton/android/pass/crypto/impl/usecases/MigrateItemsImplTest.kt
@@ -18,57 +18,146 @@
 
 package proton.android.pass.crypto.impl.usecases
 
+import com.google.common.truth.Truth.assertThat
 import me.proton.core.crypto.common.keystore.EncryptedByteArray
+import org.junit.Before
 import org.junit.Test
 import proton.android.pass.crypto.api.Base64
 import proton.android.pass.crypto.api.EncryptionKey
+import proton.android.pass.crypto.api.usecases.EncryptedMigrateContent
+import proton.android.pass.crypto.api.usecases.ItemMigrationContent
+import proton.android.pass.crypto.api.usecases.ItemMigrationHistoryContent
+import proton.android.pass.crypto.api.usecases.ItemMigrationPayload
+import proton.android.pass.crypto.api.usecases.MigrateItem
 import proton.android.pass.crypto.fakes.context.TestEncryptionContextProvider
 import proton.android.pass.crypto.fakes.utils.TestUtils
 import proton.android.pass.domain.ItemContents
 import proton_pass_item_v1.ItemV1
-import kotlin.test.assertEquals
+import kotlin.random.Random
 
-class MigrateItemsImplTest {
+internal class MigrateItemsImplTest {
 
     private val encryptionContextProvider = TestEncryptionContextProvider()
     private val createItem = CreateItemImpl(encryptionContextProvider)
 
+    private lateinit var migrateItem: MigrateItem
+
+    @Before
+    internal fun setUp() {
+        migrateItem = MigrateItemImpl(encryptionContextProvider)
+    }
+
     @Test
-    fun testCanMigrateItem() {
-        // GIVEN
-        val contents = ItemContents.Note(
+    internal fun `WHEN migrating an item THEN item content should be migrated`() {
+        val sourceShareKey = TestUtils.createShareKey().first
+        val itemContents = ItemContents.Note(
             title = proton.android.pass.test.TestUtils.randomString(),
             note = proton.android.pass.test.TestUtils.randomString()
         )
-        val (sourceShareKey, _) = TestUtils.createShareKey()
+        val item = createItem.create(sourceShareKey, itemContents)
         val (destinationShareKey, decryptedDestinationShareKey) = TestUtils.createShareKey()
-        val item = createItem.create(sourceShareKey, contents)
-
-        val encryptedItemContents = Base64.decodeBase64(item.request.content)
-
-        // WHEN
-        val instance = MigrateItemImpl(encryptionContextProvider)
-        val output = instance.migrate(
-            destinationKey = destinationShareKey,
-            encryptedItemContents = EncryptedByteArray(encryptedItemContents),
-            contentFormatVersion = item.request.contentFormatVersion
+        val payload = ItemMigrationPayloadMother.create(
+            itemContent = ItemMigrationContentMother.create(
+                encryptedItemContents = EncryptedByteArray(Base64.decodeBase64(item.request.content)),
+                contentFormatVersion = item.request.contentFormatVersion
+            )
         )
 
-        // THEN
-        assertEquals(output.contentFormatVersion, item.request.contentFormatVersion)
-        assertEquals(output.keyRotation, destinationShareKey.rotation)
+        val encryptedMigrateItemBody = migrateItem.migrate(destinationShareKey, payload)
 
-        val decryptedItemKey =
-            encryptionContextProvider.withEncryptionContext(decryptedDestinationShareKey) {
-                EncryptionKey(decrypt(EncryptedByteArray(Base64.decodeBase64(output.itemKey))))
-            }
+        assertThat(encryptedMigrateItemBody.item.contentFormatVersion).isEqualTo(item.request.contentFormatVersion)
+        assertThat(encryptedMigrateItemBody.item.keyRotation).isEqualTo(destinationShareKey.rotation)
+        assertThat(encryptedMigrateItemBody.history).isEmpty()
 
-        val decryptedContent = encryptionContextProvider.withEncryptionContext(decryptedItemKey) {
-            decrypt(EncryptedByteArray(Base64.decodeBase64(output.content)))
-        }
-
-        val parsed = ItemV1.Item.parseFrom(decryptedContent)
-        assertEquals(parsed.metadata.name, contents.title)
-        assertEquals(parsed.metadata.note, contents.note)
+        val decryptedContent = decryptEncryptedItemContent(
+            encryptionKey = decryptedDestinationShareKey,
+            encryptedMigrateContent = encryptedMigrateItemBody.item
+        )
+        val expectedItem = ItemV1.Item.parseFrom(decryptedContent)
+        assertThat(expectedItem.metadata.name).isEqualTo(itemContents.title)
+        assertThat(expectedItem.metadata.note).isEqualTo(itemContents.note)
     }
+
+    @Test
+    internal fun `WHEN migrating an item THEN item history should be migrated`() {
+        val sourceShareKey = TestUtils.createShareKey().first
+        val itemContents = ItemContents.Note(
+            title = proton.android.pass.test.TestUtils.randomString(),
+            note = proton.android.pass.test.TestUtils.randomString()
+        )
+        val item = createItem.create(sourceShareKey, itemContents)
+        val itemRevision = Random.nextLong()
+        val (destinationShareKey, decryptedDestinationShareKey) = TestUtils.createShareKey()
+        val payload = ItemMigrationPayloadMother.create(
+            historyContents = listOf(
+                ItemMigrationHistoryContentMother.create(
+                    revision = itemRevision,
+                    itemContent = ItemMigrationContentMother.create(
+                        encryptedItemContents = EncryptedByteArray(Base64.decodeBase64(item.request.content)),
+                        contentFormatVersion = item.request.contentFormatVersion
+                    )
+                )
+            )
+        )
+
+        val encryptedMigrateItemBody = migrateItem.migrate(destinationShareKey, payload)
+        assertThat(encryptedMigrateItemBody.history.size).isEqualTo(1)
+
+        val encryptedMigrateHistoryItemBody = encryptedMigrateItemBody.history.first()
+        val decryptedContent = decryptEncryptedItemContent(
+            encryptionKey = decryptedDestinationShareKey,
+            encryptedMigrateContent = encryptedMigrateHistoryItemBody.content
+        )
+        assertThat(encryptedMigrateHistoryItemBody.revision).isEqualTo(itemRevision)
+        val expectedItem = ItemV1.Item.parseFrom(decryptedContent)
+        assertThat(expectedItem.metadata.name).isEqualTo(itemContents.title)
+        assertThat(expectedItem.metadata.note).isEqualTo(itemContents.note)
+    }
+
+    private fun decryptEncryptedItemContent(
+        encryptionKey: EncryptionKey,
+        encryptedMigrateContent: EncryptedMigrateContent
+    ) = encryptionContextProvider.withEncryptionContext(encryptionKey) {
+        EncryptionKey(decrypt(EncryptedByteArray(Base64.decodeBase64(encryptedMigrateContent.itemKey))))
+    }.let { decryptedItemKey ->
+        encryptionContextProvider.withEncryptionContext(decryptedItemKey) {
+            decrypt(EncryptedByteArray(Base64.decodeBase64(encryptedMigrateContent.content)))
+        }
+    }
+}
+
+private object ItemMigrationPayloadMother {
+
+    fun create(
+        itemContent: ItemMigrationContent = ItemMigrationContentMother.create(),
+        historyContents: List<ItemMigrationHistoryContent> = emptyList()
+    ): ItemMigrationPayload = ItemMigrationPayload(
+        itemContent = itemContent,
+        historyContents = historyContents
+    )
+
+}
+
+private object ItemMigrationContentMother {
+
+    fun create(
+        encryptedItemContents: EncryptedByteArray = EncryptedByteArray(byteArrayOf()),
+        contentFormatVersion: Int = Random.nextInt()
+    ): ItemMigrationContent = ItemMigrationContent(
+        encryptedItemContents = encryptedItemContents,
+        contentFormatVersion = contentFormatVersion
+    )
+
+}
+
+private object ItemMigrationHistoryContentMother {
+
+    fun create(
+        revision: Long = Random.nextLong(),
+        itemContent: ItemMigrationContent = ItemMigrationContentMother.create()
+    ): ItemMigrationHistoryContent = ItemMigrationHistoryContent(
+        revision = revision,
+        itemContent = itemContent
+    )
+
 }

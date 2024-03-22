5114d1818f677c1a0331bc9f2bc68491da251af1
Carlos Quintana <carlos.quintana@proton.ch>
2024-03-22 15:51:08 +0100

    fix(passkeys): add snackbar message handling to passkey apps

--- pass/feature-passkeys/src/main/kotlin/proton/android/pass/featurepasskeys/select/ui/app/SelectPasskeyApp.kt
+++ pass/feature-passkeys/src/main/kotlin/proton/android/pass/featurepasskeys/select/ui/app/SelectPasskeyApp.kt
@@ -23,6 +23,7 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -32,7 +33,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import proton.android.pass.commonpresentation.api.snackbar.SnackbarViewModel
+import proton.android.pass.commonpresentation.impl.snackbar.SnackBarViewModelImpl
 import proton.android.pass.commonui.api.PassTheme
+import proton.android.pass.composecomponents.impl.messages.PassSnackbarHost
+import proton.android.pass.composecomponents.impl.messages.rememberPassSnackbarHostState
+import proton.android.pass.composecomponents.impl.snackbar.SnackBarLaunchedEffect
 import proton.android.pass.composecomponents.impl.theme.SystemUIEffect
 import proton.android.pass.composecomponents.impl.theme.isDark
 import proton.android.pass.featurepasskeys.select.navigation.SelectPasskeyNavigation
@@ -45,9 +51,20 @@ fun SelectPasskeyApp(
     modifier: Modifier = Modifier,
     appState: SelectPasskeyAppState.Ready,
     onNavigate: (SelectPasskeyNavigation) -> Unit,
-    viewModel: SelectPasskeyAppViewModel = hiltViewModel()
+    viewModel: SelectPasskeyAppViewModel = hiltViewModel(),
+    snackbarViewModel: SnackbarViewModel = hiltViewModel<SnackBarViewModelImpl>()
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
+
+    val scaffoldState = rememberScaffoldState()
+    val passSnackbarHostState = rememberPassSnackbarHostState(scaffoldState.snackbarHostState)
+
+    val snackbarState by snackbarViewModel.state.collectAsStateWithLifecycle()
+    SnackBarLaunchedEffect(
+        snackBarMessage = snackbarState.value(),
+        passSnackBarHostState = passSnackbarHostState,
+        onSnackBarMessageDelivered = { snackbarViewModel.onSnackbarMessageDelivered() }
+    )
 
     LaunchedEffect(Unit) {
         viewModel.setInitialData(
@@ -84,7 +101,8 @@ fun SelectPasskeyApp(
             modifier = modifier
                 .background(PassTheme.colors.backgroundStrong)
                 .systemBarsPadding()
-                .imePadding()
+                .imePadding(),
+            snackbarHost = { PassSnackbarHost(snackbarHostState = passSnackbarHostState) }
         ) { padding ->
             SelectPasskeyAppContent(
                 modifier = Modifier.padding(padding),

4dde331a3eb98bdeacd3e39cde085a981747aee2
Carlos Quintana <carlos.quintana@proton.ch>
2024-03-26 14:30:17 +0100

    chore: setup fdroid version file

---
+++ scripts/generateFdroidVersionFile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]; then
+    echo "Invalid usage. ${0} VERSION"
+    exit 1
+fi
+
+VERSION=$1
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+REPO_ROOT=$(echo "${SCRIPT_DIR}" | sed 's:scripts::g')
+
+VERSION_CODE=$("${REPO_ROOT}/scripts/versionCode.sh" "${VERSION}")
+
+echo -e "versionName=${VERSION}\nversionCode=${VERSION_CODE}" > metadata/fdroid_version.txt
+

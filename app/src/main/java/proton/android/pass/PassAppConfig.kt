6ebb340e7229005da1656968da1701a4eed1d343
Artiom Košelev <artiom.koselev@proton.ch>
2024-03-27 11:06:54 +0200

    feat: Added environment configuration module.

--- app/src/main/java/proton/android/pass/PassAppConfig.kt
+++ app/src/main/java/proton/android/pass/PassAppConfig.kt
@@ -19,20 +19,24 @@
 package proton.android.pass
 
 import android.os.Build
+import me.proton.core.configuration.EnvironmentConfiguration
+import me.proton.core.util.kotlin.takeIfNotBlank
 import proton.android.pass.appconfig.api.AppConfig
 import proton.android.pass.appconfig.api.BuildFlavor
 import javax.inject.Inject
 
-class PassAppConfig @Inject constructor() : AppConfig {
+class PassAppConfig @Inject constructor(
+    environmentConfiguration: EnvironmentConfiguration
+) : AppConfig {
     override val isDebug: Boolean = BuildConfig.DEBUG
     override val applicationId: String = BuildConfig.APPLICATION_ID
     override val flavor: BuildFlavor = BuildFlavor.from(BuildConfig.FLAVOR)
     override val versionCode: Int = BuildConfig.VERSION_CODE
     override val versionName: String = BuildConfig.VERSION_NAME
-    override val host: String = BuildConfig.HOST
-    override val humanVerificationHost: String = BuildConfig.HV_HOST
-    override val proxyToken: String? = BuildConfig.PROXY_TOKEN
-    override val useDefaultPins: Boolean = BuildConfig.USE_DEFAULT_PINS
+    override val host: String = environmentConfiguration.apiHost
+    override val humanVerificationHost: String = environmentConfiguration.hv3Host
+    override val proxyToken: String? = environmentConfiguration.proxyToken.takeIfNotBlank()
+    override val useDefaultPins: Boolean = environmentConfiguration.useDefaultPins
     override val sentryDSN: String? = BuildConfig.SENTRY_DSN.takeIf { !BuildConfig.DEBUG }
     override val accountSentryDSN: String? =
         BuildConfig.ACCOUNT_SENTRY_DSN.takeIf { !BuildConfig.DEBUG }

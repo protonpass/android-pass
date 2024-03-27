9f8229fd84fa3c380600e47395f15dcd1b95a93e
Carlos Quintana <carlos.quintana@proton.ch>
2024-03-27 15:07:20 +0100

    feat(security-center): add missing 2fa section to security center

---
+++ pass/features/security-center/src/main/kotlin/proton/android/pass/features/security/center/missingtfa/presentation/SecurityCenterMissingTFAViewModel.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.features.security.center.missingtfa.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.collections.immutable.toPersistentList
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import proton.android.pass.commonui.api.toUiModel
+import proton.android.pass.crypto.api.context.EncryptionContextProvider
+import proton.android.pass.data.api.usecases.ItemTypeFilter
+import proton.android.pass.data.api.usecases.ObserveItems
+import proton.android.pass.domain.Item
+import proton.android.pass.domain.ItemState
+import proton.android.pass.domain.ShareSelection
+import proton.android.pass.preferences.UserPreferencesRepository
+import proton.android.pass.preferences.value
+import proton.android.pass.securitycenter.api.passwords.MissingTfaChecker
+import javax.inject.Inject
+
+@HiltViewModel
+class SecurityCenterMissingTFAViewModel @Inject constructor(
+    observeItems: ObserveItems,
+    missingTfaChecker: MissingTfaChecker,
+    userPreferencesRepository: UserPreferencesRepository,
+    private val encryptionContextProvider: EncryptionContextProvider
+) : ViewModel() {
+
+    internal val state: StateFlow<SecurityCenterMissingTFAState> = combine(
+        observeItems(ShareSelection.AllShares, ItemState.Active, ItemTypeFilter.Logins),
+        userPreferencesRepository.getUseFaviconsPreference()
+    ) { loginItems, useFavIconsPreference ->
+        val report = missingTfaChecker(loginItems)
+        SecurityCenterMissingTFAState(
+            missingTfaItems = report.items.toUiModels().toPersistentList(),
+            canLoadExternalImages = useFavIconsPreference.value()
+        )
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000L),
+        initialValue = SecurityCenterMissingTFAState.Initial
+    )
+
+    private fun List<Item>.toUiModels() = encryptionContextProvider.withEncryptionContext {
+        map { item ->
+            item.toUiModel(this@withEncryptionContext).copy(isPinned = false)
+        }
+    }
+
+}

54141af548f43121e6f3f175bab0379341f4a644
Alexander Talledo <alexander.talledo@proton.ch>
2024-04-03 12:50:22 +0200

    chore: added proton sentinel row to security center home screen

---
+++ pass/features/security-center/src/main/kotlin/proton/android/pass/features/security/center/shared/ui/rows/SecurityCenterToggleRow.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.features.security.center.shared.ui.rows
+
+import androidx.compose.material.Switch
+import androidx.compose.material.SwitchDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import proton.android.pass.commonui.api.PassTheme
+
+@Composable
+internal fun SecurityCenterToggleRow(
+    modifier: Modifier = Modifier,
+    title: String,
+    subtitle: String,
+    isChecked: Boolean
+) {
+    SecurityCenterRow(
+        modifier = modifier,
+        title = title,
+        subtitle = subtitle,
+        isClickable = false,
+        trailingContent = {
+            Switch(
+                checked = isChecked,
+                colors = SwitchDefaults.colors(
+                    checkedThumbColor = PassTheme.colors.interactionNormMajor1
+                ),
+                onCheckedChange = null
+            )
+        }
+    )
+}

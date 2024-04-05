24efc19c2bf00077ddaf3aaf4dc23c1a9a54ed1a
Carlos Quintana <carlos.quintana@proton.ch>
2024-04-05 16:02:44 +0200

    bump: version 1.20.2 → 1.20.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ println(
 )
 
 val jobId: Int = System.getenv("CI_JOB_ID")?.take(3)?.toInt() ?: 0
-val appVersionName: String = "1.20.2"
+val appVersionName: String = "1.20.3"
 val appVersionCode: Int = versionCode(appVersionName)
 val archivesBaseName = "ProtonPass-$appVersionName"
 

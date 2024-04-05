09b41ca2e64e03acd2f7904d3c516eaa78a6e181
Carlos Quintana <carlos.quintana@proton.ch>
2024-04-05 16:01:59 +0200

    feat(data): breach emails data layer

---
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/remote/RemoteBreachDataSource.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.data.impl.remote
+
+import me.proton.core.domain.entity.UserId
+import me.proton.core.network.data.ApiProvider
+import proton.android.pass.data.impl.api.PasswordManagerApi
+import proton.android.pass.data.impl.requests.BreachAddEmailRequest
+import proton.android.pass.data.impl.requests.BreachVerifyEmailRequest
+import proton.android.pass.data.impl.responses.BreachCustomEmailsResponse
+import proton.android.pass.domain.breach.BreachCustomEmailId
+import javax.inject.Inject
+
+interface RemoteBreachDataSource {
+    suspend fun getCustomEmails(userId: UserId): BreachCustomEmailsResponse
+    suspend fun addCustomEmail(userId: UserId, email: String)
+    suspend fun verifyCustomEmail(
+        userId: UserId,
+        id: BreachCustomEmailId,
+        code: String
+    )
+}
+
+class RemoteBreachDataSourceImpl @Inject constructor(
+    private val apiProvider: ApiProvider
+) : RemoteBreachDataSource {
+    override suspend fun getCustomEmails(userId: UserId): BreachCustomEmailsResponse = apiProvider
+        .get<PasswordManagerApi>(userId)
+        .invoke {
+            getBreachCustomEmails()
+        }
+        .valueOrThrow
+
+
+    override suspend fun addCustomEmail(userId: UserId, email: String) {
+        apiProvider
+            .get<PasswordManagerApi>(userId)
+            .invoke {
+                addBreachEmailToMonitor(BreachAddEmailRequest(email))
+            }
+            .valueOrThrow
+    }
+
+    override suspend fun verifyCustomEmail(
+        userId: UserId,
+        id: BreachCustomEmailId,
+        code: String
+    ) {
+        apiProvider
+            .get<PasswordManagerApi>(userId)
+            .invoke {
+                verifyBreachEmail(emailId = id.id, request = BreachVerifyEmailRequest(code))
+            }
+            .valueOrThrow
+    }
+
+}

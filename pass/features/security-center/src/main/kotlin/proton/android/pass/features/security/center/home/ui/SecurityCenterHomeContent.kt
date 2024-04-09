a9f7e41c14daa9ad68e62d8321e6d00c2d14cb25
Alexander Talledo <alexander.talledo@proton.ch>
2024-04-09 16:24:44 +0200

    chore: solved linter issues

--- pass/features/security-center/src/main/kotlin/proton/android/pass/features/security/center/home/ui/SecurityCenterHomeContent.kt
+++ pass/features/security-center/src/main/kotlin/proton/android/pass/features/security/center/home/ui/SecurityCenterHomeContent.kt
@@ -24,9 +24,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource

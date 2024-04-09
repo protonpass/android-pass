c7901f35c9ae7461a3124f7d9d0388488904666e
Carlos Quintana <carlos.quintana@proton.ch>
2024-04-09 11:57:00 +0200

    feat(security-center): add breached emails ui

---
+++ pass/features/security-center/src/main/kotlin/proton/android/pass/features/security/center/darkweb/presentation/DarkWebUiState.kt
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.features.security.center.darkweb.presentation
+
+import androidx.compose.runtime.Stable
+import kotlinx.collections.immutable.ImmutableList
+import proton.android.pass.common.api.None
+import proton.android.pass.common.api.Option
+import proton.android.pass.domain.breach.BreachCustomEmailId
+
+@Stable
+sealed interface CustomEmailUiStatus {
+    @JvmInline
+    value class NotVerified(val usedInLoginsCount: Int) : CustomEmailUiStatus
+
+    @JvmInline
+    value class Verified(val breachesDetected: Int) : CustomEmailUiStatus
+}
+
+@Stable
+data class CustomEmailUiState(
+    val id: BreachCustomEmailId,
+    val email: String,
+    val status: CustomEmailUiStatus
+)
+
+@Stable
+enum class DarkWebStatus {
+    AllGood,
+    Warning,
+    Loading
+}
+
+@Stable
+enum class DarkWebEmailsError {
+    CannotLoad,
+    Unknown
+}
+
+@Stable
+sealed interface DarkWebEmailsState {
+    data object Loading : DarkWebEmailsState
+
+    @JvmInline
+    value class Error(val reason: DarkWebEmailsError) : DarkWebEmailsState
+
+    @JvmInline
+    value class Success(val emails: ImmutableList<CustomEmailUiState>) : DarkWebEmailsState
+}
+
+@Stable
+data class DarkWebUiState(
+    val customEmails: DarkWebEmailsState,
+    val darkWebStatus: DarkWebStatus,
+    val lastCheckTime: Option<String>
+) {
+    companion object {
+        val Initial = DarkWebUiState(
+            customEmails = DarkWebEmailsState.Loading,
+            darkWebStatus = DarkWebStatus.Loading,
+            lastCheckTime = None
+        )
+    }
+}
---
+++ pass/features/security-center/src/main/kotlin/proton/android/pass/features/security/center/darkweb/ui/DarkWebStatusIndicator.kt
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.features.security.center.darkweb.ui
+
+import androidx.annotation.DrawableRes
+import androidx.compose.material.Icon
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import proton.android.pass.commonui.api.PassTheme
+import proton.android.pass.commonui.api.ThemedBooleanPreviewProvider
+import proton.android.pass.composecomponents.impl.container.Circle
+import proton.android.pass.features.security.center.darkweb.presentation.DarkWebStatus
+import me.proton.core.presentation.R as CoreR
+
+@Composable
+internal fun DarkWebStatusIndicator(modifier: Modifier = Modifier, status: DarkWebStatus) {
+    val content = status.toContent()
+    Circle(
+        modifier = modifier,
+        backgroundColor = content.backgroundColor
+    ) {
+        Icon(
+            painter = painterResource(content.icon),
+            tint = content.iconColor,
+            contentDescription = null
+        )
+    }
+}
+
+@Composable
+private fun DarkWebStatus.toContent() = when (this) {
+    DarkWebStatus.AllGood -> DarkWebStatusIndicatorContent(
+        backgroundColor = PassTheme.colors.cardInteractionNormMinor2,
+        iconColor = PassTheme.colors.cardInteractionNormMajor2,
+        icon = CoreR.drawable.ic_proton_check_circle_full
+
+    )
+    DarkWebStatus.Warning -> DarkWebStatusIndicatorContent(
+        backgroundColor = PassTheme.colors.noteInteractionNormMinor2,
+        iconColor = PassTheme.colors.noteInteractionNormMajor2,
+        icon = CoreR.drawable.ic_proton_exclamation_circle_filled
+    )
+    DarkWebStatus.Loading -> DarkWebStatusIndicatorContent(
+        backgroundColor = PassTheme.colors.interactionNormMinor2,
+        iconColor = PassTheme.colors.interactionNormMajor2,
+        icon = CoreR.drawable.ic_proton_question_circle_filled
+    )
+}
+
+internal data class DarkWebStatusIndicatorContent(
+    val backgroundColor: Color,
+    val iconColor: Color,
+    @DrawableRes val icon: Int
+)
+
+@Preview
+@Composable
+internal fun DarkWebStatusIndicatorPreview(
+    @PreviewParameter(ThemedBooleanPreviewProvider::class) input: Pair<Boolean, Boolean>
+) {
+    val status = if (input.second) DarkWebStatus.AllGood else DarkWebStatus.Warning
+
+    PassTheme(isDark = input.first) {
+        Surface {
+            DarkWebStatusIndicator(status = status)
+        }
+    }
+}

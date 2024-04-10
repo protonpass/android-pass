973018a5fe5b51d6f8cd362a6b62604b6306e352
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-04-10 08:43:44 +0200

    feat: custom email screen

---
+++ pass/features/security-center/src/main/kotlin/proton/android/pass/features/security/center/customemail/presentation/SecurityCenterCustomEmailViewModel.kt
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.features.security.center.customemail.presentation
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.SavedStateHandleSaveableApi
+import androidx.lifecycle.viewmodel.compose.saveable
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import proton.android.pass.common.api.CommonRegex
+import proton.android.pass.commonui.api.SavedStateHandleProvider
+import proton.android.pass.data.api.usecases.breach.AddBreachCustomEmail
+import proton.android.pass.log.api.PassLogger
+import proton.android.pass.notifications.api.SnackbarDispatcher
+import javax.inject.Inject
+
+@HiltViewModel
+class SecurityCenterCustomEmailViewModel @Inject constructor(
+    private val addBreachCustomEmail: AddBreachCustomEmail,
+    private val snackbarDispatcher: SnackbarDispatcher,
+    savedStateHandleProvider: SavedStateHandleProvider
+) : ViewModel() {
+
+    @OptIn(SavedStateHandleSaveableApi::class)
+    var emailAddress: String by savedStateHandleProvider.get()
+        .saveable { mutableStateOf("") }
+
+    private val isLoadingStateFlow = MutableStateFlow(false)
+    private val emailNotValidStateFlow = MutableStateFlow(false)
+    private val eventFlow =
+        MutableStateFlow<SecurityCenterCustomEmailEvent>(SecurityCenterCustomEmailEvent.Idle)
+
+    val state: StateFlow<SecurityCenterCustomEmailState> = combine(
+        eventFlow,
+        isLoadingStateFlow,
+        emailNotValidStateFlow,
+        ::SecurityCenterCustomEmailState
+    ).stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000L),
+        initialValue = SecurityCenterCustomEmailState.Initial
+    )
+
+    fun onEmailChange(text: String) {
+        emailAddress = text
+        emailNotValidStateFlow.value = false
+    }
+
+    fun addCustomEmail() {
+        if (!CommonRegex.EMAIL_VALIDATION_REGEX.matches(emailAddress)) {
+            emailNotValidStateFlow.value = true
+            return
+        }
+        viewModelScope.launch {
+            isLoadingStateFlow.value = true
+            runCatching { addBreachCustomEmail(email = emailAddress) }
+                .onSuccess {
+                    if (it.verified) {
+                        PassLogger.i(TAG, "Email already verified")
+                        snackbarDispatcher(SecurityCenterCustomEmailSnackbarMessage.EmailAlreadyVerified)
+                        return@onSuccess
+                    } else {
+                        eventFlow.value =
+                            SecurityCenterCustomEmailEvent.OnEmailSent(it.id.id, it.email)
+                    }
+                }
+                .onFailure {
+                    PassLogger.i(TAG, "Failed to add custom email")
+                    PassLogger.w(TAG, it)
+                }
+            isLoadingStateFlow.value = false
+        }
+    }
+
+    fun onEventConsumed() {
+        eventFlow.value = SecurityCenterCustomEmailEvent.Idle
+    }
+
+    companion object {
+        private const val TAG = "SecurityCenterCustomEmailViewModel"
+    }
+}

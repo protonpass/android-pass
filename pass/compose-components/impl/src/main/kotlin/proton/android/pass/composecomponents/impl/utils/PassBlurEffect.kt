01c2b6f5a96117410a838a2e2a767fab5a118953
Alexander Talledo <alexander.talledo@proton.ch>
2024-04-16 11:15:23 +0200

    chore: created reusable blur effect composable

---
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/utils/PassBlurEffect.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.composecomponents.impl.utils
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import dev.chrisbanes.haze.HazeState
+import dev.chrisbanes.haze.HazeStyle
+import dev.chrisbanes.haze.haze
+import dev.chrisbanes.haze.hazeChild
+
+@Composable
+fun PassBlurEffect(
+    modifier: Modifier = Modifier,
+    blurRadius: Dp = 20.dp,
+    content: @Composable BoxScope.(Modifier) -> Unit
+) {
+    val hazeState = remember { HazeState() }
+
+    Box(
+        modifier = modifier.haze(state = hazeState)
+    ) {
+        content(
+            Modifier.hazeChild(
+                state = hazeState,
+                style = HazeStyle(
+                    blurRadius = blurRadius
+                )
+            )
+        )
+    }
+}

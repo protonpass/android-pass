79fe4372434b0f0a545919e34199bd3a04b7b65c
Carlos Quintana <carlos.quintana@proton.ch>
2024-04-19 13:08:44 +0200

    fix(security-center): align security center counters

--- pass/features/security-center/src/main/kotlin/proton/android/pass/features/security/center/shared/ui/rows/SecurityCenterRow.kt
+++ pass/features/security-center/src/main/kotlin/proton/android/pass/features/security/center/shared/ui/rows/SecurityCenterRow.kt
@@ -24,8 +24,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
@@ -35,6 +37,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import me.proton.core.compose.theme.ProtonTheme
 import me.proton.core.compose.theme.defaultNorm
 import proton.android.pass.commonui.api.PassTheme
@@ -106,10 +109,13 @@ internal fun SecurityCenterRow(
 
             if (displayChevronWhenClickable && isClickable) {
                 Icon(
+                    modifier = Modifier.size(ICON_SIZE),
                     painter = painterResource(R.drawable.ic_chevron_tiny_right),
                     contentDescription = null,
                     tint = chevronTintColor
                 )
+            } else {
+                Spacer(modifier = Modifier.size(ICON_SIZE))
             }
         }
     }
@@ -127,3 +133,5 @@ fun SecurityCenterRowPreview(@PreviewParameter(ThemePreviewProvider::class) isDa
         }
     }
 }
+
+private val ICON_SIZE = 10.dp

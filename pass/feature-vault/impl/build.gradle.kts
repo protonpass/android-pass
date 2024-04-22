93b54f67fc4f5494d837be3a27cef20de51dcbb4
Alexander Talledo <alexander.talledo@proton.ch>
2024-04-22 18:27:38 +0200

    test: provide missing UI test fake dependencies

--- pass/feature-vault/impl/build.gradle.kts
+++ pass/feature-vault/impl/build.gradle.kts
@@ -98,4 +98,5 @@ dependencies {
     androidTestImplementation(projects.pass.notifications.fakes)
     androidTestImplementation(projects.pass.preferences.fakes)
     androidTestImplementation(projects.pass.telemetry.fakes)
+    androidTestImplementation(projects.pass.securityCenter.fakes)
 }

868ded9acb2a39044112027bde752becf157e419
Alexander Talledo <alexander.talledo@proton.ch>
2024-04-25 17:05:31 +0200

    fix: take user id into account when performing custom emails CRUD operations

--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/local/LocalBreachesDataSource.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/local/LocalBreachesDataSource.kt
@@ -23,6 +23,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
+import me.proton.core.domain.entity.UserId
 import proton.android.pass.domain.breach.BreachCustomEmail
 import proton.android.pass.domain.breach.BreachEmail
 import proton.android.pass.domain.breach.BreachEmailId
@@ -30,71 +31,85 @@ import javax.inject.Inject
 
 interface LocalBreachesDataSource {
 
-    suspend fun getCustomEmail(customEmailId: BreachEmailId.Custom): BreachCustomEmail
+    suspend fun getCustomEmail(
+        userId: UserId,
+        customEmailId: BreachEmailId.Custom
+    ): BreachCustomEmail
 
-    fun observeCustomEmail(customEmailId: BreachEmailId.Custom): Flow<BreachCustomEmail>
+    fun observeCustomEmail(
+        userId: UserId,
+        customEmailId: BreachEmailId.Custom
+    ): Flow<BreachCustomEmail>
 
-    suspend fun upsertCustomEmail(customEmail: BreachCustomEmail)
+    suspend fun upsertCustomEmail(userId: UserId, customEmail: BreachCustomEmail)
 
     fun observeCustomEmails(): Flow<List<BreachCustomEmail>>
 
-    suspend fun upsertCustomEmails(customEmails: List<BreachCustomEmail>)
+    suspend fun upsertCustomEmails(userId: UserId, customEmails: List<BreachCustomEmail>)
 
     fun observeCustomEmailBreaches(): Flow<List<BreachEmail>>
 
-    suspend fun upsertCustomEmailBreaches(customEmailBreaches: List<BreachEmail>)
+    suspend fun upsertCustomEmailBreaches(userId: UserId, customEmailBreaches: List<BreachEmail>)
 
 }
 
 class LocalBreachesDataSourceImpl @Inject constructor() : LocalBreachesDataSource {
 
-    private val customEmailsFlow = MutableSharedFlow<Map<BreachEmailId.Custom, BreachCustomEmail>>(
-        replay = 1,
-        onBufferOverflow = BufferOverflow.DROP_OLDEST
-    )
+    private val customEmailsFlow =
+        MutableSharedFlow<Map<Pair<UserId, BreachEmailId.Custom>, BreachCustomEmail>>(
+            replay = 1,
+            onBufferOverflow = BufferOverflow.DROP_OLDEST
+        )
 
-    private val customEmailBreachesFlow = MutableSharedFlow<Map<BreachEmailId, BreachEmail>>(
-        replay = 1,
-        onBufferOverflow = BufferOverflow.DROP_OLDEST
-    )
+    private val customEmailBreachesFlow =
+        MutableSharedFlow<Map<Pair<UserId, BreachEmailId>, BreachEmail>>(
+            replay = 1,
+            onBufferOverflow = BufferOverflow.DROP_OLDEST
+        )
 
-    private val customEmailsCache = mutableMapOf<BreachEmailId.Custom, BreachCustomEmail>()
+    private val customEmailsCache =
+        mutableMapOf<Pair<UserId, BreachEmailId.Custom>, BreachCustomEmail>()
 
-    private val customEmailBreachesCache = mutableMapOf<BreachEmailId, BreachEmail>()
+    private val customEmailBreachesCache = mutableMapOf<Pair<UserId, BreachEmailId>, BreachEmail>()
 
     override suspend fun getCustomEmail(
+        userId: UserId,
         customEmailId: BreachEmailId.Custom
-    ): BreachCustomEmail = customEmailsCache[customEmailId]
+    ): BreachCustomEmail = customEmailsCache[Pair(userId, customEmailId)]
         ?: throw IllegalArgumentException("There's no custom email with id: ${customEmailId.id.id}")
 
     override fun observeCustomEmail(
+        userId: UserId,
         customEmailId: BreachEmailId.Custom
     ): Flow<BreachCustomEmail> = customEmailsFlow
-        .map { customEmailsMap -> customEmailsMap[customEmailId] }
+        .map { customEmailsMap -> customEmailsMap[Pair(userId, customEmailId)] }
         .filterNotNull()
 
-    override suspend fun upsertCustomEmail(customEmail: BreachCustomEmail) {
+    override suspend fun upsertCustomEmail(userId: UserId, customEmail: BreachCustomEmail) {
         customEmailsCache
-            .put(customEmail.id, customEmail)
+            .put(Pair(userId, customEmail.id), customEmail)
             .also { emitCustomEmailsChanges() }
     }
 
     override fun observeCustomEmails(): Flow<List<BreachCustomEmail>> = customEmailsFlow
         .map { customEmailsMap -> customEmailsMap.values.toList() }
 
-    override suspend fun upsertCustomEmails(customEmails: List<BreachCustomEmail>) {
+    override suspend fun upsertCustomEmails(userId: UserId, customEmails: List<BreachCustomEmail>) {
         customEmails
-            .forEach { customEmail -> customEmailsCache[customEmail.id] = customEmail }
+            .forEach { customEmail -> customEmailsCache[Pair(userId, customEmail.id)] = customEmail }
             .also { emitCustomEmailsChanges() }
     }
 
     override fun observeCustomEmailBreaches(): Flow<List<BreachEmail>> = customEmailBreachesFlow
         .map { customEmailBreachesMap -> customEmailBreachesMap.values.toList() }
 
-    override suspend fun upsertCustomEmailBreaches(customEmailBreaches: List<BreachEmail>) {
+    override suspend fun upsertCustomEmailBreaches(
+        userId: UserId,
+        customEmailBreaches: List<BreachEmail>
+    ) {
         customEmailBreaches
             .forEach { customEmailBreach ->
-                customEmailBreachesCache[customEmailBreach.emailId] = customEmailBreach
+                customEmailBreachesCache[Pair(userId, customEmailBreach.emailId)] = customEmailBreach
             }
             .also { emitCustomEmailBreachesChanges() }
     }

93a031af11a5c6b9393eeec4716e71c37860cd05
Carlos Quintana <carlos.quintana@proton.ch>
2024-04-25 16:54:50 +0200

    fix(security-center): custom email suggestions

--- pass/data/impl/src/test/kotlin/proton/android/pass/data/impl/usecases/breaches/ObserveCustomEmailSuggestionsImplTest.kt
+++ pass/data/impl/src/test/kotlin/proton/android/pass/data/impl/usecases/breaches/ObserveCustomEmailSuggestionsImplTest.kt
@@ -25,6 +25,7 @@ import me.proton.core.domain.entity.UserId
 import org.junit.Before
 import org.junit.Test
 import proton.android.pass.account.fakes.TestAccountManager
+import proton.android.pass.account.fakes.TestUserAddressRepository
 import proton.android.pass.data.api.usecases.breach.CustomEmailSuggestion
 import proton.android.pass.data.fakes.usecases.TestObserveItems
 import proton.android.pass.data.impl.usecases.breach.ObserveCustomEmailSuggestionsImpl
@@ -42,7 +43,14 @@ class ObserveCustomEmailSuggestionsImplTest {
             accountManager = TestAccountManager().apply {
                 sendPrimaryUserId(UserId("test-user-id"))
             },
-            observeItems = observeItems
+            observeItems = observeItems,
+            addressRepository = TestUserAddressRepository().apply {
+                val address = generateAddress(
+                    displayName = "testaddress",
+                    email = USER_PROTON_ADDRESS
+                )
+                setAddresses(listOf(address))
+            }
         )
     }
 
@@ -84,4 +92,46 @@ class ObserveCustomEmailSuggestionsImplTest {
         assertThat(res).isEqualTo(expected)
     }
 
+    @Test
+    fun `sorting is respected`() = runTest {
+        val first = "first@email.test"
+        val second = "second@email.test"
+
+        val logins = listOf(
+            TestObserveItems.createLogin(username = second),
+            TestObserveItems.createLogin(username = first),
+            TestObserveItems.createLogin(username = first),
+            TestObserveItems.createLogin(username = second)
+        )
+        observeItems.emitValue(logins)
+
+        val res = instance().first()
+
+        val expected = listOf(
+            CustomEmailSuggestion(first, 2),
+            CustomEmailSuggestion(second, 2)
+        )
+        assertThat(res).isEqualTo(expected)
+    }
+
+    @Test
+    fun `excludes proton addresses`() = runTest {
+        val email = "first@email.test"
+
+        val logins = listOf(
+            TestObserveItems.createLogin(username = email),
+            TestObserveItems.createLogin(username = email),
+            TestObserveItems.createLogin(username = USER_PROTON_ADDRESS)
+        )
+        observeItems.emitValue(logins)
+
+        val res = instance().first()
+
+        val expected = listOf(CustomEmailSuggestion(email, 2))
+        assertThat(res).isEqualTo(expected)
+    }
+
+    companion object {
+        private const val USER_PROTON_ADDRESS = "some@address.test"
+    }
 }

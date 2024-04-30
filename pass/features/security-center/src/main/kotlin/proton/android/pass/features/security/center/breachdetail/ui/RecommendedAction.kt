b9520a268e67939f0894e4f9d3b646136a080d87
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-04-30 09:20:56 +0200

    refactor: extract each component to different file

---
+++ pass/features/security-center/src/main/kotlin/proton/android/pass/features/security/center/breachdetail/ui/RecommendedAction.kt
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.features.security.center.breachdetail.ui
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import me.proton.core.presentation.R
+import proton.android.pass.commonui.api.PassTheme
+import proton.android.pass.commonui.api.Spacing
+import proton.android.pass.commonui.api.ThemedBooleanPreviewProvider
+import proton.android.pass.commonui.api.applyIf
+import proton.android.pass.composecomponents.impl.container.roundedContainer
+
+@Composable
+internal fun RecommendedAction(
+    modifier: Modifier = Modifier,
+    text: String,
+    @DrawableRes icon: Int,
+    url: String?,
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = modifier
+            .roundedContainer(
+                backgroundColor = Color.Transparent,
+                borderColor = PassTheme.colors.inputBorderNorm
+            )
+            .applyIf(
+                condition = url != null,
+                ifTrue = { clickable(onClick = onClick) }
+            )
+            .fillMaxWidth()
+            .padding(Spacing.medium),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(Spacing.small)
+    ) {
+        Icon(
+            painter = painterResource(icon),
+            contentDescription = null,
+            tint = PassTheme.colors.loginInteractionNormMajor2
+        )
+        Text(
+            modifier = Modifier.weight(1f),
+            text = text
+        )
+        if (url != null) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_proton_arrow_out_square),
+                contentDescription = null
+            )
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun RecommendedActionPreview(@PreviewParameter(ThemedBooleanPreviewProvider::class) input: Pair<Boolean, Boolean>) {
+    val url = if (input.second) "test.url" else null
+    PassTheme(isDark = input.first) {
+        Surface {
+            RecommendedAction(
+                text = "A recommended action",
+                icon = R.drawable.ic_proton_key,
+                url = url,
+                onClick = {}
+            )
+        }
+    }
+}

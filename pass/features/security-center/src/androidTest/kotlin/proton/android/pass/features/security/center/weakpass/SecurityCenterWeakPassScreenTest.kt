3af6446a74cc7d4a5db4bfa7210296c7c8f22be6
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-05-02 09:36:13 +0200

    test: add back navigation tests

---
+++ pass/features/security-center/src/androidTest/kotlin/proton/android/pass/features/security/center/weakpass/SecurityCenterWeakPassScreenTest.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.features.security.center.weakpass
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.performClick
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import proton.android.pass.commonui.api.PassTheme
+import proton.android.pass.composecomponents.impl.R
+import proton.android.pass.features.security.center.weakpass.navigation.SecurityCenterWeakPassDestination
+import proton.android.pass.features.security.center.weakpass.ui.SecurityCenterWeakPassScreen
+import proton.android.pass.test.CallChecker
+import proton.android.pass.test.HiltComponentActivity
+
+@HiltAndroidTest
+class SecurityCenterWeakPassScreenTest {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeTestRule = createAndroidComposeRule<HiltComponentActivity>()
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun onNavigateBackCalled() {
+        val checker = CallChecker<Unit>()
+        composeTestRule.apply {
+            setContent {
+                PassTheme(isDark = true) {
+                    SecurityCenterWeakPassScreen(
+                        onNavigated = {
+                            if (it is SecurityCenterWeakPassDestination.Back) {
+                                checker.call()
+                            }
+                        }
+                    )
+                }
+            }
+
+            val backArrow =
+                composeTestRule.activity.getString(R.string.navigate_back_icon_content_description)
+            onNodeWithContentDescription(backArrow).performClick()
+
+            waitUntil { checker.isCalled }
+        }
+    }
+
+}

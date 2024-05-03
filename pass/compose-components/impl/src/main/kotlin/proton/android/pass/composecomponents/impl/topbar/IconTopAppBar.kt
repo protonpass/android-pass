c9826052abba063053ca212b40195f6d45415578
Carlos Quintana <carlos.quintana@proton.ch>
2024-05-03 12:24:50 +0200

    chore: detect too many failed custom email verification + cross back icon

---
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/topbar/IconTopAppBar.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.composecomponents.impl.topbar
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import me.proton.core.compose.component.appbar.ProtonTopAppBar
+import proton.android.pass.commonui.api.PassTheme
+import proton.android.pass.commonui.api.Spacing
+import proton.android.pass.commonui.api.heroNorm
+
+@Composable
+fun IconTopAppBar(
+    modifier: Modifier = Modifier,
+    title: String? = null,
+    backgroundColor: Color = PassTheme.colors.backgroundStrong,
+    actions: (@Composable RowScope.() -> Unit)? = null,
+    navigationIcon: (@Composable () -> Unit)
+) {
+    ProtonTopAppBar(
+        modifier = modifier,
+        backgroundColor = backgroundColor,
+        title = {
+            title?.let {
+                Text(
+                    text = title,
+                    style = PassTheme.typography.heroNorm()
+                )
+            }
+        },
+        navigationIcon = navigationIcon,
+        actions = {
+            actions?.let {
+                actions()
+                Spacer(modifier = Modifier.width(Spacing.medium - Spacing.extraSmall))
+            }
+        }
+    )
+}
--- pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/topbar/iconbutton/CrossBackCircleIconButton.kt
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/topbar/iconbutton/CrossBackCircleIconButton.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Proton AG
+ * Copyright (c) 2024 Proton AG
  * This file is part of Proton AG and Proton Pass.
  *
  * Proton Pass is free software: you can redistribute it and/or modify
@@ -21,15 +21,29 @@ package proton.android.pass.composecomponents.impl.topbar.iconbutton
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import me.proton.core.presentation.R
+import androidx.compose.ui.res.stringResource
+import proton.android.pass.composecomponents.impl.R
+import me.proton.core.presentation.R as CoreR
+import proton.android.pass.composecomponents.impl.container.Circle
 
 @Composable
-fun CrossBackIconButton(modifier: Modifier = Modifier, onUpClick: () -> Unit) {
-    NavigationIconButton(modifier = modifier, onUpClick = onUpClick) {
+fun CrossBackCircleIconButton(
+    modifier: Modifier = Modifier,
+    color: Color,
+    backgroundColor: Color,
+    onUpClick: () -> Unit
+) {
+    Circle(
+        modifier = modifier,
+        backgroundColor = backgroundColor,
+        onClick = onUpClick
+    ) {
         Icon(
-            painter = painterResource(R.drawable.ic_proton_close),
-            contentDescription = null
+            painter = painterResource(CoreR.drawable.ic_proton_cross),
+            contentDescription = stringResource(R.string.navigate_back_icon_content_description),
+            tint = color
         )
     }
 }

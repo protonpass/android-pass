156685358a372ca3b32d6667a11fb1fd2038282f
Alexander Talledo <alexander.talledo@proton.ch>
2024-05-08 12:58:47 +0200

    chore: hide username split feature behind a feature flag

--- pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/login/UpdateLoginViewModel.kt
+++ pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/login/UpdateLoginViewModel.kt
@@ -123,7 +123,8 @@ class UpdateLoginViewModel @Inject constructor(
         .require<String>(CommonNavArgId.ItemId.key)
         .let(::ItemId)
 
-    private val updateEventFlow: MutableStateFlow<UpdateUiEvent> = MutableStateFlow(UpdateUiEvent.Idle)
+    private val updateEventFlow: MutableStateFlow<UpdateUiEvent> =
+        MutableStateFlow(UpdateUiEvent.Idle)
 
     private val coroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
         PassLogger.w(TAG, throwable)

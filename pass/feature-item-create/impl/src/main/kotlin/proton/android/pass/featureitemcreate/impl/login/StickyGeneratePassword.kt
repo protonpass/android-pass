1f60f2c784dc935775c6de9f361690c873aea372
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-05-14 15:59:12 +0200

    refactor: use launched effect to hide keyboard and launch action

--- pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/login/StickyGeneratePassword.kt
+++ pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/login/StickyGeneratePassword.kt
@@ -26,7 +26,6 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -41,10 +40,8 @@ import proton.android.pass.featureitemcreate.impl.R
 
 @Composable
 fun StickyGeneratePassword(modifier: Modifier = Modifier, onClick: () -> Unit) {
-    val focusManager = LocalFocusManager.current
     StickyImeRow(
         modifier = modifier.clickable {
-            focusManager.clearFocus()
             onClick()
         }
     ) {

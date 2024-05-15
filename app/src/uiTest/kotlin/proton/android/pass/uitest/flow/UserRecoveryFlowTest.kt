aa25fe86ce26f5fa603f6467257492be15e1e596
Mateusz Armatys <mateusz.armatys@proton.ch>
2024-05-15 15:52:40 +0200

    test: Add UI test for user recovery.

---
+++ app/src/uiTest/kotlin/proton/android/pass/uitest/flow/UserRecoveryFlowTest.kt
@@ -0,0 +1,66 @@
+package proton.android.pass.uitest.flow
+
+import android.Manifest
+import android.os.Build
+import androidx.test.rule.GrantPermissionRule
+import dagger.hilt.android.testing.BindValue
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import me.proton.core.auth.test.usecase.WaitForPrimaryAccount
+import me.proton.core.domain.entity.UserId
+import me.proton.core.test.quark.Quark
+import me.proton.core.userrecovery.dagger.CoreDeviceRecoveryFeaturesModule
+import me.proton.core.userrecovery.domain.IsDeviceRecoveryEnabled
+import me.proton.core.userrecovery.domain.repository.DeviceRecoveryRepository
+import me.proton.core.userrecovery.presentation.compose.DeviceRecoveryHandler
+import me.proton.core.userrecovery.presentation.compose.DeviceRecoveryNotificationSetup
+import me.proton.core.userrecovery.test.MinimalUserRecoveryTest
+import org.junit.Rule
+import proton.android.pass.featureonboarding.impl.OnBoardingPageName
+import proton.android.pass.uitest.BaseTest
+import proton.android.pass.uitest.robot.HomeRobot
+import proton.android.pass.uitest.robot.OnBoardingRobot
+import javax.inject.Inject
+
+@HiltAndroidTest
+@UninstallModules(CoreDeviceRecoveryFeaturesModule::class)
+class UserRecoveryFlowTest : BaseTest(), MinimalUserRecoveryTest {
+    @get:Rule
+    val grantPermissionRule: GrantPermissionRule = if (Build.VERSION.SDK_INT >= 33) {
+        GrantPermissionRule.grant(Manifest.permission.POST_NOTIFICATIONS)
+    } else {
+        GrantPermissionRule.grant()
+    }
+
+    override val quark: Quark = BaseTest.quark
+
+    @Inject
+    override lateinit var deviceRecoveryHandler: DeviceRecoveryHandler
+
+    @Inject
+    override lateinit var deviceRecoveryNotificationSetup: DeviceRecoveryNotificationSetup
+
+    @Inject
+    override lateinit var deviceRecoveryRepository: DeviceRecoveryRepository
+
+    @Inject
+    override lateinit var waitForPrimaryAccount: WaitForPrimaryAccount
+
+    @BindValue
+    internal val isDeviceRecoveryEnabled = object : IsDeviceRecoveryEnabled {
+        override fun invoke(userId: UserId?): Boolean = true
+        override fun isLocalEnabled(): Boolean = true
+        override fun isRemoteEnabled(userId: UserId?): Boolean = true
+    }
+
+    override fun signOut() {
+        OnBoardingRobot.onBoardingScreenDisplayed()
+            .clickSkip(OnBoardingPageName.Autofill)
+            .clickMain(OnBoardingPageName.Last)
+        HomeRobot.homeScreenDisplayed()
+            .clickProfile()
+            .clickAccount()
+            .clickSignOut()
+            .confirmSignOut()
+    }
+}

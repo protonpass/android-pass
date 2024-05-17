544f347c30781992d167d80bb832d862f9964a17
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-05-17 11:00:42 +0200

    refactor: use option instead of nullable value

--- pass/data/api/src/main/kotlin/proton/android/pass/data/api/core/datasources/LocalSentinelDataSource.kt
+++ pass/data/api/src/main/kotlin/proton/android/pass/data/api/core/datasources/LocalSentinelDataSource.kt
@@ -19,6 +19,7 @@
 package proton.android.pass.data.api.core.datasources
 
 import kotlinx.coroutines.flow.Flow
+import proton.android.pass.common.api.Option
 
 interface LocalSentinelDataSource {
 
@@ -26,7 +27,7 @@ interface LocalSentinelDataSource {
 
     fun enableSentinel()
 
-    fun observeCanEnableSentinel(): Flow<Boolean?>
+    fun observeCanEnableSentinel(): Flow<Option<Boolean>>
 
     fun updateCanEnableSentinel(value: Boolean)
 

c721107787221778624886acba7a1e0ee1d4c096
Alexander Talledo <alexander.talledo@proton.ch>
2024-05-20 11:42:29 +0200

    chore: removed duplicated when statements

--- pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/login/UpdateLogin.kt
+++ pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/login/UpdateLogin.kt
@@ -201,9 +201,6 @@ internal fun UpdateLogin(
                         actionAfterKeyboardHide =
                             { onNavigate(BaseLoginNavigation.Upgrade) }
 
-                    LoginContentEvent.OnCreatePassword -> onNavigate(BaseLoginNavigation.GeneratePassword)
-                    is LoginContentEvent.OnScanTotp -> onNavigate(BaseLoginNavigation.ScanTotp(it.index))
-                    LoginContentEvent.OnUpgrade -> onNavigate(BaseLoginNavigation.Upgrade)
                 }
             }
         )

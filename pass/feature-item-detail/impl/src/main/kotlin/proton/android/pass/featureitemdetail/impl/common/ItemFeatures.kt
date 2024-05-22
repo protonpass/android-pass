17846fefcea791346e3e28a39d6567f7dccaa086
Alexander Talledo <alexander.talledo@proton.ch>
2024-05-22 17:48:26 +0200

    fix: fixed broken FF due to merge conflicts

--- pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/common/ItemFeatures.kt
+++ pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/common/ItemFeatures.kt
@@ -19,6 +19,7 @@
 package proton.android.pass.featureitemdetail.impl.common
 
 internal data class ItemFeatures(
+    internal val isHistoryEnabled: Boolean,
     internal val isSecurityCenterEnabled: Boolean,
     internal val isUsernameSplitEnabled: Boolean
 )

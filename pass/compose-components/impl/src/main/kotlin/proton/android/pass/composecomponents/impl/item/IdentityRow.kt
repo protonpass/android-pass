389caa646faf6e85bf8da9ccdb376a536033490f
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-05-24 14:45:55 +0200

    feat: search identity and show subtitles

--- pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/item/IdentityRow.kt
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/item/IdentityRow.kt
@@ -24,18 +24,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
-import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toImmutableList
 import proton.android.pass.commonui.api.PassTheme
 import proton.android.pass.commonui.api.ThemePairPreviewProvider
 import proton.android.pass.commonuimodels.api.ItemUiModel
 import proton.android.pass.composecomponents.impl.item.icon.IdentityIcon
 import proton.android.pass.composecomponents.impl.pinning.BoxedPin
 import proton.android.pass.composecomponents.impl.pinning.CircledPin
+import proton.android.pass.domain.AddressDetails
+import proton.android.pass.domain.ContactDetails
 import proton.android.pass.domain.ItemContents
+import proton.android.pass.domain.PersonalDetails
+import proton.android.pass.domain.WorkDetails
 
 private const val MAX_PREVIEW_LENGTH = 128
 
@@ -48,9 +53,24 @@ fun IdentityRow(
     selection: ItemSelectionModeState = ItemSelectionModeState.NotInSelectionMode
 ) {
     val content = item.contents as ItemContents.Identity
-
-    val (title) = remember(content.title, highlight) {
-        getHighlightedFields(content.title)
+    val highlightColor = PassTheme.colors.interactionNorm
+    val fields = remember(
+        content.title,
+        content.personalDetails,
+        content.addressDetails,
+        content.contactDetails,
+        content.workDetails,
+        highlight
+    ) {
+        getHighlightedFields(
+            title = content.title,
+            personalDetails = content.personalDetails,
+            addressDetails = content.addressDetails,
+            contactDetails = content.contactDetails,
+            workDetails = content.workDetails,
+            highlight = highlight,
+            highlightColor = highlightColor
+        )
     }
 
     ItemRow(
@@ -88,22 +108,138 @@ fun IdentityRow(
                 }
             }
         },
-        title = title,
-        subtitles = persistentListOf(),
+        title = fields.title,
+        subtitles = fields.subtitles.toImmutableList(),
         vaultIcon = vaultIcon,
         enabled = selection.isSelectable()
     )
 }
 
-private fun getHighlightedFields(title: String): IdentityHighlightFields {
-    val annotatedTitle = AnnotatedString(title.take(MAX_PREVIEW_LENGTH))
+@Suppress("LongMethod", "LongParameterList")
+private fun getHighlightedFields(
+    title: String,
+    personalDetails: PersonalDetails,
+    addressDetails: AddressDetails,
+    contactDetails: ContactDetails,
+    workDetails: WorkDetails,
+    highlight: String,
+    highlightColor: Color
+): IdentityHighlightFields {
+    var annotatedTitle = AnnotatedString(title.take(MAX_PREVIEW_LENGTH))
+    val nameAndEmail = personalDetails.fullName + " / " + personalDetails.email
+    var annotatedFullNameEmail = AnnotatedString(nameAndEmail.take(MAX_PREVIEW_LENGTH))
 
-    return IdentityHighlightFields(annotatedTitle)
+    val annotatedFields: MutableList<AnnotatedString> = mutableListOf()
+
+    if (highlight.isNotBlank()) {
+        title.highlight(highlight, highlightColor)?.let {
+            annotatedTitle = it
+        }
+        nameAndEmail.highlight(highlight, highlightColor)?.let {
+            annotatedFullNameEmail = it
+        }
+
+        // Highlight fields for PersonalDetails
+        annotatedFields.addAll(
+            highlightFields(
+                listOf(
+                    personalDetails.firstName,
+                    personalDetails.middleName,
+                    personalDetails.lastName,
+                    personalDetails.birthdate,
+                    personalDetails.gender,
+                    personalDetails.phoneNumber
+                ),
+                highlight,
+                highlightColor
+            )
+        )
+
+        // Highlight fields for AddressDetails
+        annotatedFields.addAll(
+            highlightFields(
+                listOf(
+                    addressDetails.organization,
+                    addressDetails.streetAddress,
+                    addressDetails.zipOrPostalCode,
+                    addressDetails.city,
+                    addressDetails.stateOrProvince,
+                    addressDetails.countryOrRegion,
+                    addressDetails.floor,
+                    addressDetails.county
+                ),
+                highlight,
+                highlightColor
+            )
+        )
+
+        // Highlight fields for ContactDetails
+        annotatedFields.addAll(
+            highlightFields(
+                listOf(
+                    contactDetails.socialSecurityNumber,
+                    contactDetails.passportNumber,
+                    contactDetails.licenseNumber,
+                    contactDetails.website,
+                    contactDetails.xHandle,
+                    contactDetails.secondPhoneNumber,
+                    contactDetails.linkedin,
+                    contactDetails.reddit,
+                    contactDetails.facebook,
+                    contactDetails.yahoo,
+                    contactDetails.instagram
+                ),
+                highlight,
+                highlightColor
+            )
+        )
+
+        // Highlight fields for WorkDetails
+        annotatedFields.addAll(
+            highlightFields(
+                listOf(
+                    workDetails.company,
+                    workDetails.jobTitle,
+                    workDetails.personalWebsite,
+                    workDetails.workPhoneNumber,
+                    workDetails.workEmail
+                ),
+                highlight,
+                highlightColor
+            )
+        )
+    }
+
+    return IdentityHighlightFields(
+        title = annotatedTitle,
+        subtitles = listOf(annotatedFullNameEmail) + annotatedFields
+    )
+}
+
+private fun highlightFields(
+    fields: List<String>,
+    highlight: String,
+    highlightColor: Color
+): List<AnnotatedString> {
+    val annotatedFields: MutableList<AnnotatedString> = mutableListOf()
+
+    if (highlight.isNotBlank()) {
+        fields.forEach { fieldValue ->
+            if (fieldValue.contains(highlight, ignoreCase = true)) {
+                fieldValue.highlight(highlight, highlightColor)?.let {
+                    annotatedFields.add(it)
+                }
+            }
+        }
+    }
+
+    return annotatedFields
 }
 
 @Stable
 private data class IdentityHighlightFields(
-    val title: AnnotatedString
+    val title: AnnotatedString,
+    val subtitles: List<AnnotatedString>
 )
 
 class ThemedIdentityItemPreviewProvider :

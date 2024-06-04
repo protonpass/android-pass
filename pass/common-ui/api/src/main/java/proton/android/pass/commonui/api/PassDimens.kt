faded80d360b072ad0c6bafbfd972af2f7c9f204
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-06-04 11:30:51 +0200

    refactor: rename access key for extra password

--- pass/common-ui/api/src/main/java/proton/android/pass/commonui/api/PassDimens.kt
+++ pass/common-ui/api/src/main/java/proton/android/pass/commonui/api/PassDimens.kt
@@ -47,6 +47,7 @@ object Spacing {
     val none: Dp = 0.dp
     val extraSmall: Dp = 4.dp
     val small: Dp = 8.dp
+    val mediumSmall: Dp = 12.dp
     val medium: Dp = 16.dp
     val large: Dp = 32.dp
     val extraLarge: Dp = 64.dp

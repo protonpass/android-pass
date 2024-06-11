8f36d64879ef751cdbe153cfb287e202699fd9ff
Alexander Talledo <alexander.talledo@proton.ch>
2024-06-11 18:03:10 +0200

    refactor: renamed secure links list navigation destination

--- pass/features/secure-links/src/main/kotlin/proton/android/pass/features/secure/links/overview/ui/SecureLinksOverviewScreen.kt
+++ pass/features/secure-links/src/main/kotlin/proton/android/pass/features/secure/links/overview/ui/SecureLinksOverviewScreen.kt
@@ -55,7 +55,7 @@ fun SecureLinksOverviewScreen(
                 }
 
                 SecureLinksOverviewUiEvent.OnViewAllLinksClicked -> {
-                    onNavigated(SecureLinksNavDestination.SecureLinksDetails)
+                    onNavigated(SecureLinksNavDestination.SecureLinksList)
                 }
             }
         }

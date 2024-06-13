c7abb705222f250704ae59ce11cfb73313e5813b
Alexander Talledo <alexander.talledo@proton.ch>
2024-06-13 11:50:20 +0200

    chore: glue secure links list presentation with data layer

---
+++ pass/features/secure-links/src/main/kotlin/proton/android/pass/features/secure/links/list/presentation/SecureLinkModel.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.features.secure.links.list.presentation
+
+import proton.android.pass.domain.ItemType
+import proton.android.pass.domain.items.ItemCategory
+import proton.android.pass.domain.securelinks.SecureLink
+
+internal data class SecureLinkModel(
+    internal val itemTitle: String,
+    private val itemType: ItemType,
+    private val secureLink: SecureLink
+) {
+
+    internal val itemCategory: ItemCategory = itemType.category
+
+    internal val itemWebsite: String? = when (itemType) {
+        is ItemType.Login -> itemType.websites.firstOrNull()
+        is ItemType.Alias,
+        is ItemType.CreditCard,
+        is ItemType.Identity,
+        is ItemType.Note,
+        ItemType.Password,
+        ItemType.Unknown -> null
+    }
+
+    internal val itemPackageName: String? = when (itemType) {
+        is ItemType.Login -> itemType.packageInfoSet.firstOrNull()?.packageName?.value
+        is ItemType.Alias,
+        is ItemType.CreditCard,
+        is ItemType.Identity,
+        is ItemType.Note,
+        ItemType.Password,
+        ItemType.Unknown -> null
+    }
+
+    internal val views: Int = secureLink.readCount
+
+}
--- pass/features/secure-links/src/main/kotlin/proton/android/pass/features/secure/links/list/presentation/SecureLinksListViewModel.kt
+++ pass/features/secure-links/src/main/kotlin/proton/android/pass/features/secure/links/list/presentation/SecureLinksListViewModel.kt
@@ -19,14 +19,59 @@
 package proton.android.pass.features.secure.links.list.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import proton.android.pass.composecomponents.impl.uievents.IsLoadingState
+import proton.android.pass.crypto.api.context.EncryptionContextProvider
+import proton.android.pass.data.api.usecases.GetItemById
+import proton.android.pass.data.api.usecases.securelink.ObserveSecureLinks
+import proton.android.pass.preferences.UserPreferencesRepository
+import proton.android.pass.preferences.value
 import javax.inject.Inject
 
 @HiltViewModel
-class SecureLinksListViewModel @Inject constructor() : ViewModel() {
+class SecureLinksListViewModel @Inject constructor(
+    observeSecureLinks: ObserveSecureLinks,
+    getItemById: GetItemById,
+    encryptionContextProvider: EncryptionContextProvider,
+    userPreferencesRepository: UserPreferencesRepository
+) : ViewModel() {
 
-    internal val state: StateFlow<SecureLinksListState> = MutableStateFlow(SecureLinksListState.Initial)
+    private val secureLinksModelsFlow = observeSecureLinks().map { secureLinks ->
+        secureLinks.map { secureLink ->
+            getItemById(
+                shareId = secureLink.shareId,
+                itemId = secureLink.itemId
+            ).let { item ->
+                encryptionContextProvider.withEncryptionContext {
+                    SecureLinkModel(
+                        itemTitle = decrypt(item.title),
+                        itemType = item.itemType,
+                        secureLink = secureLink
+                    )
+                }
+            }
+        }
+    }
+
+    internal val state: StateFlow<SecureLinksListState> = combine(
+        secureLinksModelsFlow,
+        userPreferencesRepository.getUseFaviconsPreference()
+    ) { secureLinkModels, useFaviconsPreference ->
+        SecureLinksListState(
+            secureLinksModels = secureLinkModels,
+            canLoadExternalImages = useFaviconsPreference.value(),
+            isLoadingState = IsLoadingState.NotLoading
+        )
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000L),
+        initialValue = SecureLinksListState.Initial
+    )
 
 }

07b017c5035a61c6fddb030850c3f10f2621c06a
Carlos Quintana <carlos.quintana@proton.ch>
2024-06-18 08:34:23 +0200

    test: fix tests

--- pass/telemetry/fakes/src/main/kotlin/proton/android/pass/telemetry/fakes/TestTelemetryManager.kt
+++ pass/telemetry/fakes/src/main/kotlin/proton/android/pass/telemetry/fakes/TestTelemetryManager.kt
@@ -18,7 +18,7 @@
 
 package proton.android.pass.telemetry.fakes
 
-import proton.android.pass.telemetry.api.DeferredTelemetryEvent
+import proton.android.pass.telemetry.api.TelemetryEvent
 import proton.android.pass.telemetry.api.TelemetryManager
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -26,11 +26,11 @@ import javax.inject.Singleton
 @Singleton
 class TestTelemetryManager @Inject constructor() : TelemetryManager {
 
-    private val memory: MutableList<DeferredTelemetryEvent> = mutableListOf()
+    private val memory: MutableList<TelemetryEvent> = mutableListOf()
 
-    fun getMemory(): List<DeferredTelemetryEvent> = memory
+    fun getMemory(): List<TelemetryEvent> = memory
 
-    override fun sendEvent(event: DeferredTelemetryEvent) {
+    override fun sendEvent(event: TelemetryEvent) {
         memory.add(event)
     }
 

fb6719d808cb7392cdee42be75e44e4147385253
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-06-19 15:57:55 +0200

    feat: add autofill identity mappings

--- pass/autofill/impl/src/main/kotlin/proton/android/pass/autofill/heuristics/mappers/IdentityMapper.kt
+++ pass/autofill/impl/src/main/kotlin/proton/android/pass/autofill/heuristics/mappers/IdentityMapper.kt
@@ -22,12 +22,54 @@ import proton.android.pass.autofill.entities.AutofillItem
 import proton.android.pass.autofill.entities.AutofillMappings
 import proton.android.pass.autofill.entities.DatasetMapping
 import proton.android.pass.autofill.heuristics.NodeCluster
+import proton.android.pass.log.api.PassLogger
 
 object IdentityMapper {
 
+    private const val TAG = "IdentityMapper"
+
     fun mapIdentityFields(autofillItem: AutofillItem.Identity, cluster: NodeCluster.Identity): AutofillMappings {
         val mappings = mutableListOf<DatasetMapping>()
+
+        mappings.add(
+            DatasetMapping(
+                autofillFieldId = cluster.fullName.id,
+                contents = autofillItem.fullName,
+                displayValue = autofillItem.fullName
+            )
+        )
+        if (cluster.address != null && autofillItem.address != null) {
+            mappings.add(
+                DatasetMapping(
+                    autofillFieldId = cluster.address.id,
+                    contents = autofillItem.address,
+                    displayValue = autofillItem.address
+                )
+            )
+        }
+        if (cluster.postalCode != null && autofillItem.postalCode != null) {
+            mappings.add(
+                DatasetMapping(
+                    autofillFieldId = cluster.postalCode.id,
+                    contents = autofillItem.postalCode,
+                    displayValue = autofillItem.postalCode
+                )
+            )
+        }
+        if (cluster.phoneNumber != null && autofillItem.phoneNumber != null) {
+            mappings.add(
+                DatasetMapping(
+                    autofillFieldId = cluster.phoneNumber.id,
+                    contents = autofillItem.phoneNumber,
+                    displayValue = autofillItem.phoneNumber
+                )
+            )
+        }
+
+        if (mappings.isEmpty()) {
+            PassLogger.w(TAG, "No mappings found for autofill")
+        }
+
         return AutofillMappings(mappings)
     }
-
 }

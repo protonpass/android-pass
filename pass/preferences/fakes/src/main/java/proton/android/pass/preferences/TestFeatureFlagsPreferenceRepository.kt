86e2f8bbe28bb9c90a928487f09e79b4ab23f8a3
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-07-02 12:33:00 +0200

    chore: add missing fake implementations

--- pass/preferences/fakes/src/main/java/proton/android/pass/preferences/TestFeatureFlagsPreferenceRepository.kt
+++ pass/preferences/fakes/src/main/java/proton/android/pass/preferences/TestFeatureFlagsPreferenceRepository.kt
@@ -23,6 +23,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import proton.android.pass.preferences.FeatureFlag.ACCESS_KEY_V1
+import proton.android.pass.preferences.FeatureFlag.ACCOUNT_SWITCH_V1
 import proton.android.pass.preferences.FeatureFlag.AUTOFILL_DEBUG_MODE
 import proton.android.pass.preferences.FeatureFlag.IDENTITY_V1
 import proton.android.pass.preferences.FeatureFlag.SECURE_LINK_V1
@@ -47,6 +48,7 @@ class TestFeatureFlagsPreferenceRepository @Inject constructor() :
             USERNAME_SPLIT -> it.getOrDefault(USERNAME_SPLIT, false) as T
             ACCESS_KEY_V1 -> it.getOrDefault(ACCESS_KEY_V1, false) as T
             SECURE_LINK_V1 -> it.getOrDefault(SECURE_LINK_V1, false) as T
+            ACCOUNT_SWITCH_V1 -> it.getOrDefault(ACCOUNT_SWITCH_V1, false) as T
         }
     }
 

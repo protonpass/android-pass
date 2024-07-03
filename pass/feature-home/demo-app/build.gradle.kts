19eb3fc6ceb667e3cb2cd826e8a80e12ed57d0f9
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-07-03 16:38:04 +0200

    bump: version 1.23.0 → 1.23.1

--- pass/feature-home/demo-app/build.gradle.kts
+++ pass/feature-home/demo-app/build.gradle.kts
@@ -23,7 +23,7 @@ plugins {
     id("org.jetbrains.kotlin.kapt")
 }
 
-val appVersionName: String = "1.23.0"
+val appVersionName: String = "1.23.1"
 val appVersionCode: Int = versionCode(appVersionName)
 val archivesBaseName = "FeatureHomeApp-$appVersionName"
 

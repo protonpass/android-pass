cd60e29bccca2d0128dd04675099bc03ffff0a7e
Alexander Talledo <alexander.talledo@proton.ch>
2024-07-09 18:19:06 +0200

    feat: show item revision diff

---
+++ pass/features/item-history/src/main/java/proton/android/pass/features/item/history/restore/ui/tabs/ItemHistoryRestoreTab.kt
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.features.item.history.restore.ui.tabs
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import proton.android.pass.commonuimodels.api.items.ItemDetailState
+import proton.android.pass.composecomponents.impl.item.details.PassItemDetailsContent
+import proton.android.pass.composecomponents.impl.item.details.PassItemDetailsUiEvent
+import proton.android.pass.composecomponents.impl.utils.PassItemColors
+import proton.android.pass.features.item.history.restore.ItemHistoryRestoreUiEvent
+import proton.android.pass.features.item.history.restore.ui.ItemHistoryRestoreTopBar
+
+@Composable
+internal fun ItemHistoryRestoreTab(
+    modifier: Modifier = Modifier,
+    itemDetailState: ItemDetailState,
+    itemColors: PassItemColors,
+    onEvent: (ItemHistoryRestoreUiEvent) -> Unit
+) {
+    PassItemDetailsContent(
+        modifier = modifier,
+        itemDetailState = itemDetailState,
+        itemColors = itemColors,
+        topBar = {
+            ItemHistoryRestoreTopBar(
+                colors = itemColors,
+                onUpClick = { onEvent(ItemHistoryRestoreUiEvent.OnBackClick) },
+                onRestoreClick = { onEvent(ItemHistoryRestoreUiEvent.OnRestoreClick) }
+            )
+        },
+        shouldDisplayItemHistorySection = false,
+        shouldDisplayItemHistoryButton = false,
+        onEvent = { uiEvent ->
+            when (uiEvent) {
+                is PassItemDetailsUiEvent.OnSectionClick -> ItemHistoryRestoreUiEvent.OnSectionClick(
+                    section = uiEvent.section,
+                    field = uiEvent.field
+                ).also(onEvent)
+
+                is PassItemDetailsUiEvent.OnHiddenSectionClick -> ItemHistoryRestoreUiEvent.OnHiddenSectionClick(
+                    state = uiEvent.state,
+                    field = uiEvent.field
+                ).also(onEvent)
+
+                is PassItemDetailsUiEvent.OnHiddenSectionToggle -> ItemHistoryRestoreUiEvent.OnHiddenSectionToggle(
+                    state = uiEvent.state,
+                    hiddenState = uiEvent.hiddenState,
+                    field = uiEvent.field
+                ).also(onEvent)
+
+                is PassItemDetailsUiEvent.OnLinkClick -> ItemHistoryRestoreUiEvent.OnLinkClick(
+                    linkUrl = uiEvent.link
+                ).also(onEvent)
+
+                is PassItemDetailsUiEvent.OnPasskeyClick -> ItemHistoryRestoreUiEvent.OnPasskeyClick(
+                    passkey = uiEvent.passkey
+                ).also(onEvent)
+
+                PassItemDetailsUiEvent.OnViewItemHistoryClick -> {
+                    // We do nothing since item history widget shouldn't appear on restore screen
+                }
+
+                is PassItemDetailsUiEvent.OnSharedVaultClick -> {
+                    // We do nothing since we don't allow shared vault management from restore screen
+                }
+            }
+        }
+    )
+}
---
+++ pass/features/item-history/src/main/java/proton/android/pass/features/item/history/restore/ui/tabs/ItemHistoryRestoreTabRow.kt
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.features.item.history.restore.ui.tabs
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import kotlinx.collections.immutable.persistentListOf
+import proton.android.pass.commonui.api.PassTheme
+import proton.android.pass.commonui.api.Radius
+import proton.android.pass.commonui.api.ThemePreviewProvider
+import proton.android.pass.commonui.api.applyIf
+import proton.android.pass.commonui.api.body3Norm
+import proton.android.pass.composecomponents.impl.utils.PassItemColors
+import proton.android.pass.composecomponents.impl.utils.passItemColors
+import proton.android.pass.domain.items.ItemCategory
+import proton.android.pass.features.item.history.R
+
+@Composable
+internal fun ItemHistoryRestoreTabRow(
+    modifier: Modifier = Modifier,
+    selectedTabIndex: Int,
+    itemColors: PassItemColors,
+    onSelectTab: (Int) -> Unit
+) {
+    val tabTitles = persistentListOf(
+        R.string.item_history_restore_tab_title_revision,
+        R.string.item_history_restore_tab_title_current
+    )
+
+    TabRow(
+        modifier = modifier
+            .clip(shape = RoundedCornerShape(size = Radius.large))
+            .fillMaxWidth(),
+        selectedTabIndex = selectedTabIndex,
+        indicator = {},
+        divider = {}
+    ) {
+        tabTitles.forEachIndexed { index, titleResId ->
+            val isSelected = selectedTabIndex == index
+
+            Tab(
+                modifier = Modifier
+                    .background(color = PassTheme.colors.backgroundMedium)
+                    .applyIf(
+                        condition = isSelected,
+                        ifTrue = {
+                            clip(shape = RoundedCornerShape(size = Radius.large))
+                                .background(color = itemColors.majorPrimary)
+                        }
+                    ),
+                selected = isSelected,
+                onClick = { onSelectTab(index) },
+                text = {
+                    Text(
+                        text = stringResource(id = titleResId),
+                        style = PassTheme.typography.body3Norm(),
+                        color = if (isSelected) {
+                            PassTheme.colors.textInvert
+                        } else {
+                            PassTheme.colors.textNorm
+                        }
+                    )
+                }
+            )
+        }
+    }
+}
+
+@[Preview Composable]
+internal fun ItemHistoryRestoreTabRowPreview(@PreviewParameter(ThemePreviewProvider::class) isDark: Boolean) {
+    PassTheme(isDark = isDark) {
+        Surface {
+            ItemHistoryRestoreTabRow(
+                selectedTabIndex = 0,
+                itemColors = passItemColors(itemCategory = ItemCategory.Login),
+                onSelectTab = {}
+            )
+        }
+    }
+}

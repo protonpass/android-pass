e4313e7b98322ccf4fd31c8c9e8dc6ddbf5bcaca
Alexander Talledo <alexander.talledo@proton.ch>
2024-07-24 09:42:07 +0200

    chore: added navigation to SL sync options screen

---
+++ pass/features/sl-sync/build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("dagger.hilt.android.plugin")
+    id("com.google.devtools.ksp")
+    id("org.jetbrains.kotlin.kapt")
+}
+
+android {
+    namespace = "proton.android.pass.features.sl.sync"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+
+
+        testInstrumentationRunner = "proton.android.pass.test.HiltRunner"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.androidx.compose.compiler.get()
+    }
+
+    testOptions {
+        managedDevices {
+            devices {
+                maybeCreate<com.android.build.api.dsl.ManagedVirtualDevice>("pixel2api30").apply {
+                    device = "Pixel 2"
+                    apiLevel = 30
+                    systemImageSource = "aosp-atd"
+                }
+            }
+        }
+    }
+}
+
+dependencies {
+    implementation(projects.pass.commonPresentation.api)
+    implementation(projects.pass.commonUi.api)
+    implementation(projects.pass.commonUiModels.api)
+    implementation(projects.pass.composeComponents.impl)
+    implementation(projects.pass.domain)
+    implementation(projects.pass.navigation.api)
+
+    implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.compose.runtime)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.core.presentation)
+    implementation(libs.core.presentation.compose)
+    implementation(libs.dagger.hilt.android)
+    implementation(libs.kotlinx.collections)
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.kotlinx.serialization.json)
+    kapt(libs.dagger.hilt.android.compiler)
+    kapt(libs.androidx.hilt.compiler)
+    debugImplementation(libs.showkase)
+    kspDebug(libs.showkaseProcessor)
+    implementation(libs.androidx.compose.uiToolingPreview)
+    debugImplementation(libs.androidx.compose.uiTooling)
+    debugImplementation(libs.androidx.compose.uiTestManifest)
+}

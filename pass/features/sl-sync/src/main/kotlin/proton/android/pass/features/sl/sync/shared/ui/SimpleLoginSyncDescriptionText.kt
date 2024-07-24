4a4fda4d3a5522a1824607b21ec7795aa9e828ba
Alexander Talledo <alexander.talledo@proton.ch>
2024-07-24 12:45:37 +0200

    chore: added SL sync settings sections

---
+++ pass/features/sl-sync/src/main/kotlin/proton/android/pass/features/sl/sync/shared/ui/SimpleLoginSyncDescriptionText.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.features.sl.sync.shared.ui
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import proton.android.pass.commonui.api.PassTheme
+import proton.android.pass.commonui.api.Spacing
+import proton.android.pass.commonui.api.body3Norm
+
+@Composable
+internal fun SimpleLoginSyncDescriptionText(
+    modifier: Modifier = Modifier,
+    text: String
+) {
+    Text(
+        modifier = modifier.padding(horizontal = Spacing.small),
+        text = text,
+        style = PassTheme.typography.body3Norm(),
+        color = PassTheme.colors.textWeak
+    )
+}

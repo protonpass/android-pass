cf94b63eba23861fff2bf8084e0720a8642cec67
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-07-25 16:14:48 +0200

    feat: add alias icon new inactive ui

--- pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/container/BoxedIcon.kt
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/container/BoxedIcon.kt
@@ -19,6 +19,7 @@
 package proton.android.pass.composecomponents.impl.container
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
@@ -26,7 +27,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
@@ -39,18 +39,22 @@ fun BoxedIcon(
     size: Int = 40,
     shape: Shape = PassTheme.shapes.squircleMediumShape,
     backgroundColor: Color,
+    borderColor: Color? = null,
     onClick: (() -> Unit)? = null,
     content: @Composable BoxScope.() -> Unit
 ) {
     Box(
         modifier = modifier
             .size(size.dp)
-            .clip(shape)
             .applyIf(
                 condition = onClick != null,
                 ifTrue = { clickable { onClick?.invoke() } }
             )
-            .background(backgroundColor),
+            .background(backgroundColor, shape)
+            .applyIf(
+                borderColor != null,
+                ifTrue = { borderColor?.let { border(2.dp, borderColor, shape) } ?: Modifier }
+            ),
         contentAlignment = Alignment.Center
     ) {
         content()

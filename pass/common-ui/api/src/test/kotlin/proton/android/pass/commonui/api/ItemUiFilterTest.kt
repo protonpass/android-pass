51417947618c6da11e21026c3e51a6a67046764c
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-07-26 12:09:29 +0200

    test: fix tests

--- pass/common-ui/api/src/test/kotlin/proton/android/pass/commonui/api/ItemUiFilterTest.kt
+++ pass/common-ui/api/src/test/kotlin/proton/android/pass/commonui/api/ItemUiFilterTest.kt
@@ -254,14 +254,16 @@ internal class ItemUiFilterTest {
             itemContents = ItemContents.Alias(
                 title = "Title",
                 note = "Note",
-                aliasEmail = "alias@example.com"
+                aliasEmail = "alias@example.com",
+                isDisabled = false
             )
         ),
         TestItemUiModel.create(
             itemContents = ItemContents.Alias(
                 title = "Another Title",
                 note = "Another Note",
-                aliasEmail = "test@example.com"
+                aliasEmail = "test@example.com",
+                isDisabled = false
             )
         )
     )

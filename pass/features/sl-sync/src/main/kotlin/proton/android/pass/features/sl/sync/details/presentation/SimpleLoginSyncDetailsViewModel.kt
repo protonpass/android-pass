3626295f8b98b00219acc7028edf5e38ac17f7a6
Alexander Talledo <alexander.talledo@proton.ch>
2024-08-21 14:59:28 +0200

    refactor: extract duplicated userId retrieval logic to a common function

--- pass/features/sl-sync/src/main/kotlin/proton/android/pass/features/sl/sync/details/presentation/SimpleLoginSyncDetailsViewModel.kt
+++ pass/features/sl-sync/src/main/kotlin/proton/android/pass/features/sl/sync/details/presentation/SimpleLoginSyncDetailsViewModel.kt
@@ -62,7 +62,7 @@ class SimpleLoginSyncDetailsViewModel @Inject constructor(
         observeSimpleLoginAliasDomains(),
         observeSimpleLoginAliasMailboxes(),
         observeSimpleLoginAliasSettings(),
-        observeSimpleLoginSyncStatus()
+        observeSimpleLoginSyncStatus(),
     ) { aliasDomains, aliasMailboxes, aliasSettings, syncStatusOption ->
         SimpleLoginSyncDetailsModel(
             aliasDomains = aliasDomains,

86a2833e522b99e8516cda0fac1d97395a358d5b
Alexander Talledo <alexander.talledo@proton.ch>
2024-08-27 17:01:50 +0200

    fix: avoid the possibility to spam alias toggle while changing alias state

--- pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/alias/AliasMailboxesPreviewProvider.kt
+++ pass/feature-item-detail/impl/src/main/kotlin/proton/android/pass/featureitemdetail/impl/alias/AliasMailboxesPreviewProvider.kt
@@ -21,11 +21,12 @@ package proton.android.pass.featureitemdetail.impl.alias
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import proton.android.pass.domain.AliasMailbox
 
-class AliasMailboxesPreviewProvider : PreviewParameterProvider<List<AliasMailbox>> {
-    override val values: Sequence<List<AliasMailbox>>
-        get() = sequenceOf(
-            listOf(AliasMailbox(1, "some@mail.box")),
-            listOf(AliasMailbox(1, "some@mail.box"), AliasMailbox(2, "other@mail.box")),
-            listOf(AliasMailbox(1, "some.very.long.mailbox.that.can.get.even.longer.than.you.expect@mail.box"))
-        )
+internal class AliasMailboxesPreviewProvider : PreviewParameterProvider<List<AliasMailbox>> {
+
+    override val values: Sequence<List<AliasMailbox>> = sequenceOf(
+        listOf(AliasMailbox(1, "some@mail.box")),
+        listOf(AliasMailbox(1, "some@mail.box"), AliasMailbox(2, "other@mail.box")),
+        listOf(AliasMailbox(1, "some.very.long.mailbox.that.can.get.even.longer.than.you.expect@mail.box"))
+    )
+
 }

4826a795d1ebd2723c326e17efd3804485d963e8
Alexander Talledo <alexander.talledo@proton.ch>
2024-08-29 11:22:08 +0200

    bump: version 1.24.4 → 1.24.5

--- pass/autofill/test-app/build.gradle.kts
+++ pass/autofill/test-app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("kotlin-android")
 }
 
-val appVersionName = "1.24.4"
+val appVersionName = "1.24.5"
 val appVersionCode = versionCode(appVersionName)
 val archivesBaseName = "AutofillTestApp-$appVersionName"
 

393e1efac3c79c002b1626937b70e6ffcf986c28
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-09-06 10:20:51 +0200

    revert: applyIf modification

--- pass/common-ui/api/src/main/java/proton/android/pass/commonui/api/ModifierExt.kt
+++ pass/common-ui/api/src/main/java/proton/android/pass/commonui/api/ModifierExt.kt
@@ -18,14 +18,18 @@
 
 package proton.android.pass.commonui.api
 
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 
 fun Modifier.applyIf(
     condition: Boolean,
-    ifTrue: Modifier.() -> Modifier,
-    ifFalse: (Modifier.() -> Modifier)? = null
-): Modifier = when {
-    condition -> then(ifTrue(Modifier))
-    ifFalse != null -> then(ifFalse(Modifier))
-    else -> this
+    ifTrue: @Composable Modifier.() -> Modifier,
+    ifFalse: @Composable (Modifier.() -> Modifier)? = null
+): Modifier = composed {
+    when {
+        condition -> then(ifTrue(Modifier))
+        ifFalse != null -> then(ifFalse(Modifier))
+        else -> this
+    }
 }

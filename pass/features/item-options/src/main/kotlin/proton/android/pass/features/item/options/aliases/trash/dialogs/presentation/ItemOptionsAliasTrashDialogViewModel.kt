50231c37ab2b0931d27198c2a08e0fa273ddc589
Alexander Talledo <alexander.talledo@proton.ch>
2024-09-19 17:57:12 +0200

    refactor: extract remove alias dialog to item options feature

---
+++ pass/features/item-options/src/main/kotlin/proton/android/pass/features/item/options/aliases/trash/dialogs/presentation/ItemOptionsAliasTrashDialogViewModel.kt
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.features.item.options.aliases.trash.dialogs.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import proton.android.pass.commonui.api.SavedStateHandleProvider
+import proton.android.pass.commonui.api.require
+import proton.android.pass.composecomponents.impl.uievents.IsLoadingState
+import proton.android.pass.data.api.usecases.ChangeAliasStatus
+import proton.android.pass.data.api.usecases.TrashItems
+import proton.android.pass.domain.ItemId
+import proton.android.pass.domain.ShareId
+import proton.android.pass.log.api.PassLogger
+import proton.android.pass.navigation.api.CommonNavArgId
+import proton.android.pass.preferences.AliasTrashDialogStatusPreference
+import proton.android.pass.preferences.UserPreferencesRepository
+import javax.inject.Inject
+
+class ItemOptionsAliasTrashDialogViewModel @Inject constructor(
+    savedStateHandleProvider: SavedStateHandleProvider,
+    private val userPreferencesRepository: UserPreferencesRepository,
+    private val changeAliasStatus: ChangeAliasStatus,
+    private val trashItems: TrashItems
+) : ViewModel() {
+
+    private val shareId: ShareId = savedStateHandleProvider.get()
+        .require<String>(CommonNavArgId.ShareId.key)
+        .let(::ShareId)
+
+    private val itemId: ItemId = savedStateHandleProvider.get()
+        .require<String>(CommonNavArgId.ItemId.key)
+        .let(::ItemId)
+
+    private val isLoadingStateFlow = MutableStateFlow<IsLoadingState>(
+        value = IsLoadingState.NotLoading
+    )
+
+    private val eventFlow = MutableStateFlow<ItemOptionsAliasTrashDialogEvent>(
+        value = ItemOptionsAliasTrashDialogEvent.Idle
+    )
+
+    internal val stateFlow = combine(
+        isLoadingStateFlow,
+        eventFlow,
+        userPreferencesRepository.observeAliasTrashDialogStatusPreference(),
+        ::ItemOptionsAliasTrashDialogState
+    ).stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5_000),
+        initialValue = ItemOptionsAliasTrashDialogState.Initial
+    )
+
+    internal fun onConsumeEvent(event: ItemOptionsAliasTrashDialogEvent) {
+        eventFlow.compareAndSet(event, ItemOptionsAliasTrashDialogEvent.Idle)
+    }
+
+    internal fun onDisableAlias() {
+        viewModelScope.launch {
+            isLoadingStateFlow.update { IsLoadingState.Loading }
+
+            runCatching { changeAliasStatus(shareId = shareId, itemId = itemId, enabled = false) }
+                .onFailure { error ->
+                    PassLogger.w(TAG, "There was an error while disabling alias item")
+                    PassLogger.w(TAG, error)
+                    eventFlow.update { ItemOptionsAliasTrashDialogEvent.OnDisableError }
+                }
+                .onSuccess {
+                    eventFlow.update { ItemOptionsAliasTrashDialogEvent.OnDisableSuccess }
+                }
+
+            isLoadingStateFlow.update { IsLoadingState.NotLoading }
+        }
+    }
+
+    internal fun onChangeRemindMe(isRemindMeEnabled: Boolean) {
+        AliasTrashDialogStatusPreference.from(value = isRemindMeEnabled)
+            .also(userPreferencesRepository::setAliasTrashDialogStatusPreference)
+    }
+
+    internal fun onTrashAlias() {
+        viewModelScope.launch {
+            isLoadingStateFlow.update { IsLoadingState.Loading }
+
+            runCatching { trashItems(items = mapOf(shareId to listOf(itemId))) }
+                .onFailure { error ->
+                    PassLogger.w(TAG, "There was an error while trashing alias item")
+                    PassLogger.w(TAG, error)
+                    eventFlow.update { ItemOptionsAliasTrashDialogEvent.OnTrashSuccess }
+                }
+                .onSuccess {
+                    eventFlow.update { ItemOptionsAliasTrashDialogEvent.OnTrashError }
+                }
+
+            isLoadingStateFlow.update { IsLoadingState.NotLoading }
+        }
+    }
+
+    private companion object {
+
+        private const val TAG = "ItemOptionsAliasTrashDialogViewModel"
+
+    }
+
+}
---
+++ pass/features/item-options/src/main/kotlin/proton/android/pass/features/item/options/aliases/trash/dialogs/ui/ItemOptionsAliasTrashDialog.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.features.item.options.aliases.trash.dialogs.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import proton.android.pass.features.item.options.aliases.trash.dialogs.presentation.ItemOptionsAliasTrashDialogEvent
+import proton.android.pass.features.item.options.aliases.trash.dialogs.presentation.ItemOptionsAliasTrashDialogViewModel
+import proton.android.pass.features.item.options.shared.navigation.ItemOptionsNavDestination
+
+@Composable
+fun ItemOptionsAliasTrashDialog(
+    onNavigated: (ItemOptionsNavDestination) -> Unit,
+    viewModel: ItemOptionsAliasTrashDialogViewModel = hiltViewModel()
+) = with(viewModel) {
+    val state by stateFlow.collectAsStateWithLifecycle()
+
+    LaunchedEffect(state.event) {
+        when (state.event) {
+            ItemOptionsAliasTrashDialogEvent.Idle -> {}
+
+            ItemOptionsAliasTrashDialogEvent.OnDisableError,
+            ItemOptionsAliasTrashDialogEvent.OnDisableSuccess,
+            ItemOptionsAliasTrashDialogEvent.OnTrashError,
+            ItemOptionsAliasTrashDialogEvent.OnTrashSuccess -> {
+                onNavigated(ItemOptionsNavDestination.Back)
+            }
+        }
+
+        onConsumeEvent(state.event)
+    }
+
+    ItemOptionsAliasTrashDialogContent(
+        state = state,
+        onUiEvent = { uiEvent ->
+            when (uiEvent) {
+                ItemOptionsAliasTrashDialogUiEvent.OnDismiss -> {
+                    onNavigated(ItemOptionsNavDestination.Back)
+                }
+
+                ItemOptionsAliasTrashDialogUiEvent.OnDisable -> {
+                    onDisableAlias()
+                }
+
+                is ItemOptionsAliasTrashDialogUiEvent.OnRemindMeChange -> {
+                    onChangeRemindMe(isRemindMeEnabled = uiEvent.value)
+                }
+
+                ItemOptionsAliasTrashDialogUiEvent.OnTrash -> {
+                    onTrashAlias()
+                }
+            }
+        }
+    )
+}

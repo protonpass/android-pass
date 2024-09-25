63ed7499714d2f16744a605e5705bf76e5e2b940
Alexander Talledo <alexander.talledo@proton.ch>
2024-09-25 16:57:33 +0200

    feature: revamp alias details screen to alias management screen

---
+++ pass/features/sl-sync/src/main/kotlin/proton/android/pass/features/sl/sync/management/ui/SimpleLoginSyncManagementMailboxSection.kt
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.features.sl.sync.management.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+import proton.android.pass.commonui.api.PassTheme
+import proton.android.pass.commonui.api.Spacing
+import proton.android.pass.commonui.api.ThemePreviewProvider
+import proton.android.pass.commonui.api.asAnnotatedString
+import proton.android.pass.composecomponents.impl.container.roundedContainerNorm
+import proton.android.pass.composecomponents.impl.item.SectionSubtitle
+import proton.android.pass.composecomponents.impl.item.SectionTitle
+import proton.android.pass.composecomponents.impl.item.icon.ThreeDotsMenuButton
+import proton.android.pass.domain.simplelogin.SimpleLoginAliasMailbox
+import proton.android.pass.features.sl.sync.R
+import proton.android.pass.features.sl.sync.shared.ui.SimpleLoginSyncAddButton
+import proton.android.pass.features.sl.sync.shared.ui.SimpleLoginSyncDescriptionText
+import proton.android.pass.features.sl.sync.shared.ui.SimpleLoginSyncLabelText
+
+@Composable
+internal fun SimpleLoginSyncManagementMailboxSection(
+    modifier: Modifier = Modifier,
+    aliasMailboxes: ImmutableList<SimpleLoginAliasMailbox>,
+    onAddClick: () -> Unit,
+    onMenuClick: (SimpleLoginAliasMailbox) -> Unit
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(space = Spacing.small)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            SimpleLoginSyncLabelText(
+                modifier = Modifier.weight(1f, fill = true),
+                text = stringResource(id = R.string.simple_login_sync_management_mailboxes_label)
+            )
+
+            SimpleLoginSyncAddButton(
+                onClick = onAddClick
+            )
+        }
+
+        SimpleLoginSyncManagementMailboxes(
+            aliasMailboxes = aliasMailboxes,
+            onMenuClick = onMenuClick
+        )
+
+        SimpleLoginSyncDescriptionText(
+            text = stringResource(id = R.string.simple_login_sync_management_mailboxes_description)
+        )
+    }
+}
+
+@Composable
+private fun SimpleLoginSyncManagementMailboxes(
+    modifier: Modifier = Modifier,
+    aliasMailboxes: ImmutableList<SimpleLoginAliasMailbox>,
+    onMenuClick: (SimpleLoginAliasMailbox) -> Unit
+) {
+    Column(
+        modifier = modifier
+            .roundedContainerNorm()
+            .padding(vertical = Spacing.medium),
+        verticalArrangement = Arrangement.spacedBy(space = Spacing.medium)
+    ) {
+        aliasMailboxes.forEach { aliasMailbox ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(
+                    modifier = Modifier
+                        .weight(weight = 1f, fill = true)
+                        .padding(start = Spacing.medium),
+                    verticalArrangement = Arrangement.spacedBy(space = Spacing.extraSmall)
+                ) {
+                    SectionSubtitle(
+                        text = aliasMailbox.email.asAnnotatedString()
+                    )
+
+                    SectionTitle(
+                        text = "3 aliases"
+                    )
+                }
+
+                ThreeDotsMenuButton(
+                    onClick = {
+                        onMenuClick(aliasMailbox)
+                    }
+                )
+            }
+        }
+    }
+}
+
+@[Preview Composable]
+internal fun SimpleLoginSyncMailboxSectionPreview(@PreviewParameter(ThemePreviewProvider::class) isDark: Boolean) {
+    PassTheme(isDark = isDark) {
+        Surface {
+            SimpleLoginSyncManagementMailboxSection(
+                aliasMailboxes = persistentListOf(
+                    SimpleLoginAliasMailbox(
+                        id = "1",
+                        email = "user@email.com",
+                        isDefault = true,
+                        isVerified = true
+                    )
+                ),
+                onAddClick = {},
+                onMenuClick = {}
+            )
+        }
+    }
+}

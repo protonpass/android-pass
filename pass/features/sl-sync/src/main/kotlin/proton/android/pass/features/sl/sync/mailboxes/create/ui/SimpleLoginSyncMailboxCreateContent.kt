de45c390386f68d05062e0918fac22ad5f56aa82
Alexander Talledo <alexander.talledo@proton.ch>
2024-09-27 10:54:14 +0200

    chore: created add new mailbox screen

---
+++ pass/features/sl-sync/src/main/kotlin/proton/android/pass/features/sl/sync/mailboxes/create/ui/SimpleLoginSyncMailboxCreateContent.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.features.sl.sync.mailboxes.create.ui
+
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import proton.android.pass.commonui.api.PassTopBarBackButtonType
+import proton.android.pass.composecomponents.impl.topbar.PassExtendedTopBar
+import proton.android.pass.features.sl.sync.R
+import proton.android.pass.features.sl.sync.mailboxes.create.presentation.SimpleLoginSyncMailboxCreateState
+
+@Composable
+internal fun SimpleLoginSyncMailboxCreateContent(
+    modifier: Modifier = Modifier,
+    state: SimpleLoginSyncMailboxCreateState,
+    onUiEvent: (SimpleLoginSyncMailboxCreateUiEvent) -> Unit
+) = with(state) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            PassExtendedTopBar(
+                modifier = modifier,
+                backButton = PassTopBarBackButtonType.BackArrow,
+                title = stringResource(id = R.string.simple_login_sync_mailbox_create_title),
+                onUpClick = { onUiEvent(SimpleLoginSyncMailboxCreateUiEvent.OnBackClicked) }
+            )
+        }
+    ) { innerPaddingValue ->
+
+    }
+}
---
+++ pass/features/sl-sync/src/main/kotlin/proton/android/pass/features/sl/sync/mailboxes/create/ui/SimpleLoginSyncMailboxCreateScreen.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.features.sl.sync.mailboxes.create.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import proton.android.pass.features.sl.sync.mailboxes.create.presentation.SimpleLoginSyncMailboxCreateViewModel
+import proton.android.pass.features.sl.sync.shared.navigation.SimpleLoginSyncNavDestination
+
+@Composable
+fun SimpleLoginSyncMailboxCreateScreen(
+    onNavigated: (SimpleLoginSyncNavDestination) -> Unit,
+    viewModel: SimpleLoginSyncMailboxCreateViewModel = hiltViewModel()
+) = with(viewModel) {
+    val state by stateFlow.collectAsStateWithLifecycle()
+
+    SimpleLoginSyncMailboxCreateContent(
+        state = state,
+        onUiEvent = { uiEvent ->
+            when (uiEvent) {
+                SimpleLoginSyncMailboxCreateUiEvent.OnBackClicked -> {
+                    SimpleLoginSyncNavDestination.Back()
+                        .also(onNavigated)
+                }
+
+                SimpleLoginSyncMailboxCreateUiEvent.OnCreateClicked -> {
+                    onCreateMailbox()
+                }
+
+                is SimpleLoginSyncMailboxCreateUiEvent.OnEmailChanged -> {
+                    onMailboxEmailChanged(uiEvent.email)
+                }
+            }
+        }
+    )
+
+}

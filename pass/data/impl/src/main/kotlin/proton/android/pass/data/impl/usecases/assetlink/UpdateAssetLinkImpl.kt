41acd018c40e92ebb7170f63ee133e8e92bb3cfa
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-10-01 14:12:54 +0200

    feat: add single item worker

---
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/assetlink/UpdateAssetLinkImpl.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.data.impl.usecases.assetlink
+
+import proton.android.pass.data.api.repositories.AssetLinkRepository
+import proton.android.pass.data.impl.util.runConcurrently
+import proton.android.pass.domain.assetlink.AssetLink
+import proton.android.pass.log.api.PassLogger
+import javax.inject.Inject
+
+class UpdateAssetLinkImpl @Inject constructor(
+    private val assetLinkRepository: AssetLinkRepository
+) : UpdateAssetLink {
+    override suspend fun invoke(websites: Set<String>) {
+        val results: List<Result<AssetLink>> = runConcurrently(
+            items = websites,
+            block = assetLinkRepository::fetch
+        )
+        val (successes, failures) = results.partition { it.isSuccess }
+        if (failures.isNotEmpty()) {
+            PassLogger.w(
+                TAG,
+                "${failures.size} from ${results.size} websites failed to get asset links"
+            )
+        }
+        val assetLinks = successes.mapNotNull { it.getOrNull() }
+        if (assetLinks.isNotEmpty()) {
+            assetLinkRepository.insert(assetLinks)
+        }
+    }
+
+    companion object {
+        private const val TAG = "UpdateAssetLinkWorkerImpl"
+    }
+}
---
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/work/SingleItemAssetLinkWorker.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.data.impl.work
+
+import android.content.Context
+import androidx.hilt.work.HiltWorker
+import androidx.work.Constraints
+import androidx.work.CoroutineWorker
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequest
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkerParameters
+import androidx.work.workDataOf
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import proton.android.pass.commonrust.api.DomainManager
+import proton.android.pass.data.impl.usecases.assetlink.UpdateAssetLink
+import proton.android.pass.log.api.PassLogger
+
+@HiltWorker
+class SingleItemAssetLinkWorker @AssistedInject constructor(
+    @Assisted appContext: Context,
+    @Assisted private val workerParameters: WorkerParameters,
+    private val updateAssetLink: UpdateAssetLink,
+    private val domainManager: DomainManager
+) : CoroutineWorker(appContext, workerParameters) {
+
+    override suspend fun doWork(): Result = runCatching {
+        PassLogger.i(TAG, "Starting $TAG attempt $runAttemptCount")
+        val inputWebsites = workerParameters.inputData.getStringArray(WEBSITES_KEY)
+            ?: return Result.failure()
+        val processedWebsites = inputWebsites.map(domainManager::getRoot).toSet()
+        updateAssetLink(processedWebsites)
+    }
+        .onFailure {
+            PassLogger.w(TAG, "Failed to refresh asset links")
+            PassLogger.w(TAG, it)
+        }
+        .toWorkerResult()
+
+    companion object {
+        private const val WEBSITES_KEY = "WEBSITES"
+        private const val TAG = "SingleItemAssetLinkWorker"
+
+        fun getRequestFor(websites: Set<String>): OneTimeWorkRequest {
+            val inputData = workDataOf(WEBSITES_KEY to websites.toTypedArray())
+            return OneTimeWorkRequestBuilder<SingleItemAssetLinkWorker>()
+                .setConstraints(
+                    Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
+                )
+                .setInputData(inputData)
+                .build()
+        }
+    }
+}

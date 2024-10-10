eb526fd906bc8bed0493efb6c37a677112ccbe75
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-10-10 10:27:15 +0200

    refactor: move account module

---
+++ pass/features/account/build.gradle.kts
@@ -0,0 +1,103 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("dagger.hilt.android.plugin")
+    id("com.google.devtools.ksp")
+    id("org.jetbrains.kotlin.kapt")
+    id("kotlin-parcelize")
+}
+
+android {
+    namespace = "proton.android.pass.features.account"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+    
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+        testInstrumentationRunner = "proton.android.pass.test.HiltRunner"
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.androidx.compose.compiler.get()
+    }
+
+    testOptions {
+        managedDevices {
+            devices {
+                maybeCreate<com.android.build.api.dsl.ManagedVirtualDevice>("pixel2api30").apply {
+                    device = "Pixel 2"
+                    apiLevel = 30
+                    systemImageSource = "aosp-atd"
+                }
+            }
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.runtime)
+    implementation(libs.core.presentation)
+    implementation(libs.core.presentation.compose)
+    implementation(libs.core.user.domain)
+    implementation(libs.core.userSettings.domain)
+    implementation(libs.core.payment.domain)
+    implementation(libs.kotlinx.collections)
+
+    debugImplementation(libs.androidx.compose.uiTooling)
+    implementation(libs.androidx.compose.uiToolingPreview)
+    debugImplementation(libs.androidx.compose.uiTestManifest)
+
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.dagger.hilt.android)
+    kapt(libs.dagger.hilt.android.compiler)
+    kapt(libs.androidx.hilt.compiler)
+
+    debugImplementation(libs.showkase)
+    kspDebug(libs.showkaseProcessor)
+
+    implementation(projects.pass.domain)
+    implementation(projects.pass.commonUi.api)
+    implementation(projects.pass.composeComponents.impl)
+    implementation(projects.pass.data.api)
+    implementation(projects.pass.navigation.api)
+    implementation(projects.pass.preferences.api)
+    implementation(projects.pass.notifications.api)
+    implementation(projects.pass.log.api)
+
+    androidTestImplementation(libs.androidx.test.espresso.intents)
+
+    kaptAndroidTest(libs.dagger.hilt.android.compiler)
+    androidTestImplementation(projects.pass.commonTest)
+    androidTestImplementation(projects.pass.commonUi.fakes)
+    androidTestImplementation(projects.pass.data.fakes)
+    androidTestImplementation(projects.pass.preferences.fakes)
+    androidTestImplementation(projects.pass.notifications.fakes)
+    androidTestImplementation(projects.pass.securityCenter.fakes)
+
+    testImplementation(projects.pass.account.fakes)
+    testImplementation(projects.pass.commonTest)
+    testImplementation(projects.pass.data.fakes)
+    testImplementation(projects.pass.notifications.fakes)
+    testImplementation(projects.pass.preferences.fakes)
+    testImplementation(libs.core.test.kotlin)
+    testImplementation(libs.truth)
+    testImplementation(libs.turbine)
+    testImplementation(libs.coroutines.test)
+    testImplementation(libs.junit)
+}

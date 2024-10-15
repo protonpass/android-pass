f5cf6864305aae21c1ac16e4772867273a1b1a25
Alexander Talledo <alexander.talledo@proton.ch>
2024-10-15 14:19:26 +0200

    fix: fixed transfer mailbox request id type

--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/requests/SimpleLoginDeleteAliasMailboxRequest.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/requests/SimpleLoginDeleteAliasMailboxRequest.kt
@@ -24,5 +24,5 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class SimpleLoginDeleteAliasMailboxRequest(
     @SerialName("TransferMailboxID")
-    val transferMailboxId: String?
+    val transferMailboxId: Long?
 )

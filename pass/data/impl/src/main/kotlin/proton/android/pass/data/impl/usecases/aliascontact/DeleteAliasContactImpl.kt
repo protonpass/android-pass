ddd552c7a6e9b2feab038962fab23ffc86de2f6d
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-10-15 12:44:25 +0200

    feat: add alias contacts usecases

---
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/aliascontact/DeleteAliasContactImpl.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.data.impl.usecases.aliascontact
+
+import kotlinx.coroutines.flow.first
+import me.proton.core.accountmanager.domain.AccountManager
+import proton.android.pass.data.api.errors.UserIdNotAvailableError
+import proton.android.pass.data.api.repositories.AliasContactsRepository
+import proton.android.pass.data.api.usecases.aliascontact.DeleteAliasContact
+import proton.android.pass.domain.ItemId
+import proton.android.pass.domain.ShareId
+import proton.android.pass.domain.aliascontacts.ContactId
+import javax.inject.Inject
+
+class DeleteAliasContactImpl @Inject constructor(
+    private val accountManager: AccountManager,
+    private val aliasContactsRepository: AliasContactsRepository
+) : DeleteAliasContact {
+
+    override suspend fun invoke(
+        shareId: ShareId,
+        itemId: ItemId,
+        contactId: ContactId
+    ) {
+        val userId = accountManager.getPrimaryUserId().first() ?: throw UserIdNotAvailableError()
+        aliasContactsRepository.deleteAliasContact(userId, shareId, itemId, contactId)
+    }
+}
+

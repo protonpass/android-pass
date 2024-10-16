30d2c940488118ecce186c1580175f444546028d
Alexander Talledo <alexander.talledo@proton.ch>
2024-10-16 09:42:16 +0200

    chore: bring back alias details screen

---
+++ pass/features/sl-sync/src/main/kotlin/proton/android/pass/features/sl/sync/details/presentation/SimpleLoginSyncDetailsState.kt
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.features.sl.sync.details.presentation
+
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.mutableStateListOf
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toPersistentList
+import proton.android.pass.common.api.None
+import proton.android.pass.common.api.Option
+import proton.android.pass.common.api.Some
+import proton.android.pass.domain.Vault
+import proton.android.pass.domain.simplelogin.SimpleLoginAliasDomain
+import proton.android.pass.domain.simplelogin.SimpleLoginAliasMailbox
+
+@Stable
+internal data class SimpleLoginSyncDetailsState(
+    internal val isUpdating: Boolean,
+    internal val event: SimpleLoginSyncDetailsEvent,
+    private val modelOption: Option<SimpleLoginSyncDetailsModel>,
+    private val selectedDomainOption: Option<String?>,
+    private val selectedMailboxOption: Option<SimpleLoginAliasMailbox>
+) {
+
+    internal val defaultDomain: String? = when (modelOption) {
+        None -> null
+        is Some -> modelOption.value.defaultDomain
+    }
+
+    private val defaultMailbox: SimpleLoginAliasMailbox? = when (modelOption) {
+        None -> null
+        is Some ->
+            modelOption.value
+                .aliasMailboxes
+                .firstOrNull { aliasMailbox -> aliasMailbox.id == modelOption.value.defaultMailboxId }
+    }
+
+    internal val defaultMailboxEmail: String = defaultMailbox?.email.orEmpty()
+
+    internal val defaultVault: Vault? = when (modelOption) {
+        None -> null
+        is Some -> modelOption.value.defaultVault
+    }
+
+    private val aliasDomains: List<SimpleLoginAliasDomain> = when (modelOption) {
+        None -> emptyList()
+        is Some -> modelOption.value.aliasDomains
+    }
+
+    internal val aliasDomainOptions: ImmutableList<String?> = mutableStateListOf<String?>().apply {
+        add(null) // This is to support the "Not selected" option
+        addAll(aliasDomains.map { it.domain })
+    }.toPersistentList()
+
+    internal val canSelectDomain: Boolean = aliasDomains.size > 1
+
+    private val aliasMailboxes: List<SimpleLoginAliasMailbox> = when (modelOption) {
+        None -> emptyList()
+        is Some -> modelOption.value.aliasMailboxes
+    }
+
+    internal val aliasMailboxOptions: ImmutableList<String> = aliasMailboxes
+        .map { it.email }
+        .toPersistentList()
+
+    internal val canSelectMailbox: Boolean = aliasMailboxes.size > 1
+
+    internal val selectedAliasDomain: String? = when (selectedDomainOption) {
+        None -> defaultDomain
+        is Some -> selectedDomainOption.value
+    }
+
+    internal val selectedAliasMailboxId: Option<Long> = selectedMailboxOption.map { it.id }
+
+    internal val selectedAliasMailboxEmail: String = when (selectedMailboxOption) {
+        None -> defaultMailbox?.email.orEmpty()
+        is Some -> selectedMailboxOption.value.email
+    }
+
+    internal val isSyncEnabled: Boolean = when (modelOption) {
+        None -> false
+        is Some -> modelOption.value.isSyncEnabled
+    }
+
+    internal val pendingAliasesCount: Int = when (modelOption) {
+        None -> 0
+        is Some -> modelOption.value.pendingAliasesCount
+    }
+
+    internal val hasPendingAliases: Boolean = pendingAliasesCount > 0
+
+    internal val isLoading: Boolean = when (modelOption) {
+        None -> true
+        is Some -> false
+    }
+
+    internal val canUpdateDomain: Boolean = defaultDomain != selectedAliasDomain
+
+    internal val canUpdateMailbox: Boolean = defaultMailboxEmail != selectedAliasMailboxEmail
+
+    // The -1 is required since we added a null item to the beginning of the list to support the "Not selected" option
+    internal fun getAliasDomain(position: Int): SimpleLoginAliasDomain? = aliasDomains
+        .getOrNull(position.minus(1))
+
+    internal fun getAliasMailbox(position: Int): SimpleLoginAliasMailbox = aliasMailboxes[position]
+
+    internal companion object {
+
+        internal val Initial: SimpleLoginSyncDetailsState = SimpleLoginSyncDetailsState(
+            isUpdating = false,
+            event = SimpleLoginSyncDetailsEvent.Idle,
+            modelOption = None,
+            selectedDomainOption = None,
+            selectedMailboxOption = None
+        )
+
+    }
+
+}
---
+++ pass/features/sl-sync/src/main/kotlin/proton/android/pass/features/sl/sync/details/ui/SimpleLoginSyncDetailsSections.kt
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.features.sl.sync.details.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.pluralStringResource
+import androidx.compose.ui.res.stringResource
+import proton.android.pass.commonui.api.Spacing
+import proton.android.pass.composecomponents.impl.extension.toColor
+import proton.android.pass.composecomponents.impl.extension.toResource
+import proton.android.pass.composecomponents.impl.icon.VaultIcon
+import proton.android.pass.composecomponents.impl.widgets.PassSingleActionWidget
+import proton.android.pass.domain.Vault
+import proton.android.pass.features.sl.sync.R
+import proton.android.pass.features.sl.sync.shared.ui.SimpleLoginSyncLabelText
+import proton.android.pass.features.sl.sync.shared.ui.SimpleLoginSyncSectionRow
+import proton.android.pass.composecomponents.impl.R as CompR
+
+@Composable
+internal fun SimpleLoginSyncDetailsSections(
+    modifier: Modifier = Modifier,
+    defaultDomain: String?,
+    defaultMailboxEmail: String,
+    defaultVault: Vault?,
+    isSyncEnabled: Boolean,
+    hasPendingAliases: Boolean,
+    pendingAliasesCount: Int,
+    canSelectDomain: Boolean,
+    canSelectMailbox: Boolean,
+    onUiEvent: (SimpleLoginSyncDetailsUiEvent) -> Unit
+) {
+    Column(
+        modifier = modifier
+            .padding(all = Spacing.medium)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(space = Spacing.medium)
+    ) {
+        SimpleLoginSyncSectionRow(
+            label = stringResource(id = R.string.simple_login_sync_management_domain_label),
+            title = stringResource(id = R.string.simple_login_sync_management_domain_title),
+            subtitle = defaultDomain ?: stringResource(
+                id = R.string.simple_login_sync_management_domain_option_blank
+            ),
+            onClick = { onUiEvent(SimpleLoginSyncDetailsUiEvent.OnDomainClicked) },
+            isClickable = canSelectDomain
+        )
+
+        SimpleLoginSyncSectionRow(
+            label = stringResource(id = R.string.simple_login_sync_management_mailboxes_label),
+            title = stringResource(id = R.string.simple_login_sync_management_mailboxes_title),
+            subtitle = defaultMailboxEmail,
+            onClick = { onUiEvent(SimpleLoginSyncDetailsUiEvent.OnMailboxClicked) },
+            isClickable = canSelectMailbox
+        )
+
+        if (isSyncEnabled && defaultVault != null) {
+            with(defaultVault) {
+                SimpleLoginSyncSectionRow(
+                    leadingIcon = {
+                        VaultIcon(
+                            backgroundColor = color.toColor(isBackground = true),
+                            icon = icon.toResource(),
+                            iconColor = color.toColor()
+                        )
+                    },
+                    label = stringResource(id = R.string.simple_login_sync_management_vault_title),
+                    title = stringResource(id = R.string.simple_login_sync_shared_default_vault_title),
+                    subtitle = name,
+                    description = stringResource(
+                        id = R.string.simple_login_sync_shared_default_vault_description
+                    ),
+                    onClick = {
+                        SimpleLoginSyncDetailsUiEvent.OnDefaultVaultClicked(
+                            shareId = shareId
+                        ).also(onUiEvent)
+                    }
+                )
+            }
+        } else if (hasPendingAliases) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(space = Spacing.small)
+            ) {
+                SimpleLoginSyncLabelText(
+                    text = stringResource(id = R.string.simple_login_sync_management_vault_title)
+                )
+
+                PassSingleActionWidget(
+                    title = stringResource(id = CompR.string.simple_login_widget_title),
+                    message = pluralStringResource(
+                        id = CompR.plurals.simple_login_widget_pending_aliases_message,
+                        count = pendingAliasesCount,
+                        pendingAliasesCount
+                    ),
+                    actionText = stringResource(id = CompR.string.simple_login_widget_action),
+                    onActionClick = {
+                        SimpleLoginSyncDetailsUiEvent.OnSyncSettingsClicked(
+                            shareId = defaultVault?.shareId
+                        ).also(onUiEvent)
+                    }
+                )
+            }
+        }
+    }
+}

67125c037b9f12e671dd0d55a903c4bc228ec5d0
Alexander Talledo <alexander.talledo@proton.ch>
2024-10-18 12:52:37 +0200

    refactor: extract email verification by code screen to a common package

--- pass/features/sl-sync/src/main/kotlin/proton/android/pass/features/sl/sync/mailboxes/verify/ui/SimpleLoginSyncMailboxVerifyContent.kt
+++ pass/features/sl-sync/src/main/kotlin/proton/android/pass/features/sl/sync/mailboxes/verify/ui/SimpleLoginSyncMailboxVerifyContent.kt
@@ -18,26 +18,12 @@
 
 package proton.android.pass.features.sl.sync.mailboxes.verify.ui
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
-import me.proton.core.compose.theme.ProtonTheme
-import me.proton.core.compose.theme.defaultSmallNorm
-import proton.android.pass.commonui.api.PassTheme
-import proton.android.pass.commonui.api.PassTopBarBackButtonType
-import proton.android.pass.commonui.api.Spacing
-import proton.android.pass.composecomponents.impl.buttons.LoadingCircleButton
-import proton.android.pass.composecomponents.impl.topbar.PassExtendedTopBar
+import proton.android.pass.composecomponents.impl.screens.PassCodeVerificationScreen
 import proton.android.pass.features.sl.sync.R
 import proton.android.pass.features.sl.sync.mailboxes.verify.presentation.SimpleLoginSyncMailboxVerifyState
-import proton.android.pass.composecomponents.impl.R as CompR
 
 @Composable
 internal fun SimpleLoginSyncMailboxVerifyContent(
@@ -46,75 +32,99 @@ internal fun SimpleLoginSyncMailboxVerifyContent(
     state: SimpleLoginSyncMailboxVerifyState,
     onUiEvent: (SimpleLoginSyncMailboxVerifyUiEvent) -> Unit
 ) = with(state) {
-    Scaffold(
+    PassCodeVerificationScreen(
         modifier = modifier,
-        topBar = {
-            PassExtendedTopBar(
-                backButton = PassTopBarBackButtonType.Cross,
-                title = stringResource(id = R.string.simple_login_sync_mailbox_verify_title),
-                subtitle = stringResource(
-                    id = R.string.simple_login_sync_mailbox_verify_subtitle,
-                    mailboxEmail.ifEmpty {
-                        stringResource(id = R.string.simple_login_sync_mailbox_verify_email_fallback)
-                    }
-                ),
-                onUpClick = { onUiEvent(SimpleLoginSyncMailboxVerifyUiEvent.OnCloseClicked) },
-                actions = {
-                    LoadingCircleButton(
-                        modifier = Modifier.padding(
-                            horizontal = Spacing.mediumSmall,
-                            vertical = Spacing.small
-                        ),
-                        isLoading = isLoading,
-                        buttonEnabled = canVerifyMailbox(verificationCode),
-                        color = if (canVerifyMailbox(verificationCode)) {
-                            PassTheme.colors.interactionNormMajor1
-                        } else {
-                            PassTheme.colors.interactionNormMajor1.copy(alpha = 0.6f)
-                        },
-                        text = {
-                            Text(
-                                text = stringResource(id = CompR.string.action_continue),
-                                fontWeight = FontWeight.W400,
-                                fontSize = 14.sp,
-                                color = PassTheme.colors.textInvert,
-                                style = ProtonTheme.typography.defaultSmallNorm,
-                                maxLines = 1
-                            )
-                        },
-                        onClick = {
-                            onUiEvent(SimpleLoginSyncMailboxVerifyUiEvent.OnVerifyClicked)
-                        }
-                    )
-                }
-            )
+        topBarTitle = stringResource(id = R.string.simple_login_sync_mailbox_verify_title),
+        topBarSubtitle = stringResource(
+            id = R.string.simple_login_sync_mailbox_verify_subtitle,
+            mailboxEmail.ifEmpty {
+                stringResource(id = R.string.simple_login_sync_mailbox_verify_email_fallback)
+            }
+        ),
+        onUpClick = { onUiEvent(SimpleLoginSyncMailboxVerifyUiEvent.OnCloseClicked) },
+        isActionLoading = isLoading,
+        onActionClick = {
+            onUiEvent(SimpleLoginSyncMailboxVerifyUiEvent.OnVerifyClicked)
+        },
+        verificationCode = verificationCode,
+        verificationCodeLength = verificationCodeLength,
+        canEnterVerificationCode = canEnterVerificationCode,
+        onVerificationCodeChange = { newVerificationCode ->
+            SimpleLoginSyncMailboxVerifyUiEvent.OnVerificationCodeChanged(
+                newVerificationCode = newVerificationCode
+            ).also(onUiEvent)
+        },
+        onResendVerificationCodeClick = {
+            onUiEvent(SimpleLoginSyncMailboxVerifyUiEvent.OnResendVerificationCodeClicked)
         }
-    ) { innerPaddingValues ->
-        Column(
-            modifier = Modifier
-                .padding(paddingValues = innerPaddingValues)
-                .padding(
-                    horizontal = Spacing.medium,
-                    vertical = Spacing.large
-                ),
-            verticalArrangement = Arrangement.spacedBy(space = Spacing.medium)
-        ) {
-            SimpleLoginSyncMailboxVerifyCodeSection(
-                verificationCode = verificationCode,
-                verificationCodeLength = verificationCodeLength,
-                verificationCodeTimerSeconds = verificationCodeTimerSeconds,
-                showResendVerificationCodeTimer = showResendVerificationCodeTimer,
-                canRequestVerificationCode = canRequestVerificationCode,
-                canEnterVerificationCode = canEnterVerificationCode,
-                onVerificationCodeChange = { newVerificationCode ->
-                    SimpleLoginSyncMailboxVerifyUiEvent.OnVerificationCodeChanged(
-                        newVerificationCode = newVerificationCode
-                    ).also(onUiEvent)
-                },
-                onResendVerificationCodeClick = {
-                    onUiEvent(SimpleLoginSyncMailboxVerifyUiEvent.OnResendVerificationCodeClicked)
-                }
-            )
-        }
-    }
+    )
+
+//    Scaffold(
+//        modifier = modifier,
+//        topBar = {
+//            PassExtendedTopBar(
+//                backButton = PassTopBarBackButtonType.Cross,
+//                title = stringResource(id = R.string.simple_login_sync_mailbox_verify_title),
+//                subtitle = stringResource(
+//                    id = R.string.simple_login_sync_mailbox_verify_subtitle,
+//                    mailboxEmail.ifEmpty {
+//                        stringResource(id = R.string.simple_login_sync_mailbox_verify_email_fallback)
+//                    }
+//                ),
+//                onUpClick = { onUiEvent(SimpleLoginSyncMailboxVerifyUiEvent.OnCloseClicked) },
+//                actions = {
+//                    LoadingCircleButton(
+//                        modifier = Modifier.padding(vertical = Spacing.small),
+//                        isLoading = isLoading,
+//                        buttonEnabled = canVerifyMailbox(verificationCode),
+//                        color = if (canVerifyMailbox(verificationCode)) {
+//                            PassTheme.colors.interactionNormMajor1
+//                        } else {
+//                            PassTheme.colors.interactionNormMajor1.copy(alpha = 0.6f)
+//                        },
+//                        text = {
+//                            Text(
+//                                text = stringResource(id = CompR.string.action_continue),
+//                                fontWeight = FontWeight.W400,
+//                                fontSize = 14.sp,
+//                                color = PassTheme.colors.textInvert,
+//                                style = ProtonTheme.typography.defaultSmallNorm,
+//                                maxLines = 1
+//                            )
+//                        },
+//                        onClick = {
+//                            onUiEvent(SimpleLoginSyncMailboxVerifyUiEvent.OnVerifyClicked)
+//                        }
+//                    )
+//                }
+//            )
+//        }
+//    ) { innerPaddingValues ->
+//        Column(
+//            modifier = Modifier
+//                .padding(paddingValues = innerPaddingValues)
+//                .padding(
+//                    horizontal = Spacing.medium,
+//                    vertical = Spacing.large
+//                ),
+//            verticalArrangement = Arrangement.spacedBy(space = Spacing.medium)
+//        ) {
+//            SimpleLoginSyncMailboxVerifyCodeSection(
+//                verificationCode = verificationCode,
+//                verificationCodeLength = verificationCodeLength,
+//                verificationCodeTimerSeconds = verificationCodeTimerSeconds,
+//                showResendVerificationCodeTimer = showResendVerificationCodeTimer,
+//                canRequestVerificationCode = canRequestVerificationCode,
+//                canEnterVerificationCode = canEnterVerificationCode,
+//                onVerificationCodeChange = { newVerificationCode ->
+//                    SimpleLoginSyncMailboxVerifyUiEvent.OnVerificationCodeChanged(
+//                        newVerificationCode = newVerificationCode
+//                    ).also(onUiEvent)
+//                },
+//                onResendVerificationCodeClick = {
+//                    onUiEvent(SimpleLoginSyncMailboxVerifyUiEvent.OnResendVerificationCodeClicked)
+//                }
+//            )
+//        }
+//    }
 }

46d87ea28e6f2c56715ff34ea8c416946744748d
Carlos Quintana <carlos.quintana@proton.ch>
2024-10-22 08:47:49 +0200

    feat(data): add canAutofill field

--- pass/data/impl/src/test/kotlin/proton/android/pass/data/impl/usecases/ObserveUsableVaultsImplTest.kt
+++ pass/data/impl/src/test/kotlin/proton/android/pass/data/impl/usecases/ObserveUsableVaultsImplTest.kt
@@ -21,235 +21,85 @@ package proton.android.pass.data.impl.usecases
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
-import kotlinx.datetime.Clock
 import me.proton.core.domain.entity.UserId
 import org.junit.Before
 import org.junit.Test
-import proton.android.pass.data.fakes.usecases.TestGetUserPlan
 import proton.android.pass.data.fakes.usecases.TestObserveVaults
-import proton.android.pass.domain.Plan
-import proton.android.pass.domain.PlanLimit
-import proton.android.pass.domain.PlanType
 import proton.android.pass.domain.ShareId
-import proton.android.pass.domain.ShareRole
 import proton.android.pass.domain.Vault
 import proton.android.pass.test.domain.TestVault
-import java.util.Calendar
-import java.util.Date
 
 class ObserveUsableVaultsImplTest {
 
     private lateinit var instance: ObserveUsableVaultsImpl
 
-    private lateinit var getUserPlan: TestGetUserPlan
     private lateinit var observeVaults: TestObserveVaults
 
     @Before
     fun setup() {
-        getUserPlan = TestGetUserPlan()
         observeVaults = TestObserveVaults()
 
         instance = ObserveUsableVaultsImpl(
-            getUserPlan = getUserPlan,
             observeVaults = observeVaults
         )
     }
 
     @Test
-    fun `only writeable vaults if plan is free`() = runTest {
+    fun `can handle empty list`() = runTest {
+        setVaults(emptyMap())
+
+        val res = instance().first()
+        assertThat(res).isEmpty()
+    }
+
+    @Test
+    fun `only vaults with canAutofill`() = runTest {
         val vault1 = ShareId("1")
         val vault2 = ShareId("2")
         val vault3 = ShareId("3")
 
-        setPlan(PlanType.Free("", ""))
         setVaults(
             mapOf(
-                vault1 to ShareRole.Write,
-                vault2 to ShareRole.Write,
-                vault3 to ShareRole.Read
+                vault1 to true,
+                vault2 to true,
+                vault3 to false
             )
         )
 
         val res = instance().first()
         assertThat(res).hasSize(2)
         assertThat(res.map { it.shareId }).containsExactly(vault1, vault2)
-        assertThat(res.map { it.role }).containsExactly(ShareRole.Write, ShareRole.Write)
     }
 
     @Test
-    fun `owned vaults and first N shared vaults if plan is free`() = runTest {
-        // Setup plan with 2 owned vault limit
-        val plan = Plan(
-            planType = PlanType.Free("", ""),
-            hideUpgrade = false,
-            vaultLimit = PlanLimit.Limited(2),
-            aliasLimit = PlanLimit.Limited(1),
-            totpLimit = PlanLimit.Limited(1),
-            updatedAt = Clock.System.now().epochSeconds
-        )
-        getUserPlan.setResult(userId = DEFAULT_USER_ID, value = Result.success(plan))
-
-        // 3 vault setup, one owned, two not owned
-        // shared1 is Read to show that the order does not care about the role
-        val now = Date()
-        val otherUserId = UserId("OtherUserID")
-        val ownedVault1 = TestVault.create(
-            shareId = ShareId("owned1"),
-            userId = DEFAULT_USER_ID,
-            role = ShareRole.Write,
-            name = "unused",
-            createTime = now
-        )
-        val sharedVault1 = TestVault.create(
-            shareId = ShareId("shared1"),
-            userId = otherUserId,
-            isOwned = false,
-            role = ShareRole.Read,
-            name = "unused",
-            createTime = now.plusHours(1)
-        )
-        val sharedVault2 = TestVault.create(
-            shareId = ShareId("shared2"),
-            userId = otherUserId,
-            isOwned = false,
-            role = ShareRole.Write,
-            name = "unused",
-            createTime = now.plusHours(2)
-        )
-        observeVaults.sendResult(Result.success(listOf(ownedVault1, sharedVault1, sharedVault2)))
-
-        val res = instance().first()
-        assertThat(res).hasSize(2)
-        assertThat(res).isEqualTo(listOf(ownedVault1, sharedVault1))
-    }
-
-    @Test
-    fun `only writeable vaults if plan is unknown`() = runTest {
+    fun `can handle all vaults with canAutofill false`() = runTest {
         val vault1 = ShareId("1")
         val vault2 = ShareId("2")
         val vault3 = ShareId("3")
 
-        setPlan(PlanType.Unknown("", ""))
         setVaults(
             mapOf(
-                vault1 to ShareRole.Write,
-                vault2 to ShareRole.Write,
-                vault3 to ShareRole.Read
+                vault1 to false,
+                vault2 to false,
+                vault3 to false
             )
         )
 
         val res = instance().first()
-        assertThat(res).hasSize(2)
-        assertThat(res.map { it.shareId }).containsExactly(vault1, vault2)
-        assertThat(res.map { it.role }).containsExactly(ShareRole.Write, ShareRole.Write)
-
+        assertThat(res).isEmpty()
     }
 
-    @Test
-    fun `all vaults if plan is plus`() = runTest {
-        val vault1 = ShareId("1")
-        val vault2 = ShareId("2")
-        val vault3 = ShareId("3")
-
-        setPlan(PlanType.Paid.Plus("", ""))
-        setVaults(
-            mapOf(
-                vault1 to ShareRole.Write,
-                vault2 to ShareRole.Write,
-                vault3 to ShareRole.Read
-            )
-        )
-
-        val res = instance().first()
-        assertThat(res).hasSize(3)
-        assertThat(res.map { it.shareId }).containsExactly(vault1, vault2, vault3)
-        assertThat(res.map { it.role }).containsExactly(
-            ShareRole.Write,
-            ShareRole.Write,
-            ShareRole.Read
-        )
-    }
-
-    @Test
-    fun `all vaults if plan is business`() = runTest {
-        val vault1 = ShareId("1")
-        val vault2 = ShareId("2")
-        val vault3 = ShareId("3")
-
-        setPlan(PlanType.Paid.Business("", ""))
-        setVaults(
-            mapOf(
-                vault1 to ShareRole.Write,
-                vault2 to ShareRole.Write,
-                vault3 to ShareRole.Read
-            )
-        )
-
-        val res = instance().first()
-        assertThat(res).hasSize(3)
-        assertThat(res.map { it.shareId }).containsExactly(vault1, vault2, vault3)
-        assertThat(res.map { it.role }).containsExactly(
-            ShareRole.Write,
-            ShareRole.Write,
-            ShareRole.Read
-        )
-    }
-
-    @Test
-    fun `all vaults if plan is trial`() = runTest {
-        val vault1 = ShareId("1")
-        val vault2 = ShareId("2")
-        val vault3 = ShareId("3")
-
-        setPlan(PlanType.Trial("", "", remainingDays = 1))
-        setVaults(
-            mapOf(
-                vault1 to ShareRole.Write,
-                vault2 to ShareRole.Write,
-                vault3 to ShareRole.Read
-            )
-        )
-
-        val res = instance().first()
-        assertThat(res).hasSize(3)
-        assertThat(res.map { it.shareId }).containsExactly(vault1, vault2, vault3)
-        assertThat(res.map { it.role }).containsExactly(
-            ShareRole.Write,
-            ShareRole.Write,
-            ShareRole.Read
-        )
-    }
-
-    private fun setVaults(vaults: Map<ShareId, ShareRole>) {
-        val vaultInstances: List<Vault> = vaults.map { (shareId, role) ->
+    private fun setVaults(vaults: Map<ShareId, Boolean>) {
+        val vaultInstances: List<Vault> = vaults.map { (shareId, canAutofill) ->
             TestVault.create(
                 shareId = shareId,
                 userId = DEFAULT_USER_ID,
-                role = role,
-                name = "unused"
+                name = "unused",
+                canAutofill = canAutofill
             )
         }
 
         observeVaults.sendResult(Result.success(vaultInstances))
-    }
-
-    private fun setPlan(planType: PlanType) {
-        val plan = Plan(
-            planType = planType,
-            hideUpgrade = false,
-            vaultLimit = PlanLimit.Limited(1),
-            aliasLimit = PlanLimit.Limited(1),
-            totpLimit = PlanLimit.Limited(1),
-            updatedAt = Clock.System.now().epochSeconds
-        )
-        getUserPlan.setResult(userId = DEFAULT_USER_ID, value = Result.success(plan))
-    }
-
-    private fun Date.plusHours(hours: Int): Date {
-        val calendar = Calendar.getInstance()
-        calendar.time = this
-        calendar.add(Calendar.HOUR, hours)
-        return calendar.time
     }
 
     companion object {

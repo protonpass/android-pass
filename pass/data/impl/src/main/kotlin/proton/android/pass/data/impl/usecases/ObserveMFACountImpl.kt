3b514b181916f738a8ecf3ee30f7ca626a85ed78
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-10-25 09:43:15 +0200

    fix: totp count in filters

--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/ObserveMFACountImpl.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/ObserveMFACountImpl.kt
@@ -21,7 +21,6 @@ package proton.android.pass.data.impl.usecases
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.map
 import me.proton.core.accountmanager.domain.AccountManager
 import proton.android.pass.data.api.usecases.ObserveMFACount
 import proton.android.pass.data.impl.local.LocalItemDataSource
@@ -31,10 +30,8 @@ class ObserveMFACountImpl @Inject constructor(
     private val accountManager: AccountManager,
     private val localItemDataSource: LocalItemDataSource
 ) : ObserveMFACount {
+
     override fun invoke(): Flow<Int> = accountManager.getPrimaryUserId()
         .filterNotNull()
-        .flatMapLatest {
-            localItemDataSource.observeAllItemsWithTotp(it)
-        }
-        .map { it.size }
+        .flatMapLatest(localItemDataSource::countAllItemsWithTotp)
 }

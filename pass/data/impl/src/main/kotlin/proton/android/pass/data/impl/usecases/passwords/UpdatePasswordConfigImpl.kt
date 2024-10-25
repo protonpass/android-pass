61dd8ea5017ac2ad90550f47f749eff45c21025a
Alexander Talledo <alexander.talledo@proton.ch>
2024-10-25 09:50:18 +0200

    refactor: migrate random password length configuration change

---
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/passwords/UpdatePasswordConfigImpl.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.data.impl.usecases.passwords
+
+import kotlinx.coroutines.flow.firstOrNull
+import proton.android.pass.commonrust.api.passwords.PasswordConfig
+import proton.android.pass.data.api.usecases.passwords.UpdatePasswordConfig
+import proton.android.pass.preferences.UserPreferencesRepository
+import javax.inject.Inject
+
+class UpdatePasswordConfigImpl @Inject constructor(
+    private val userPreferencesRepository: UserPreferencesRepository
+) : UpdatePasswordConfig {
+
+    override suspend fun invoke(config: PasswordConfig) {
+        userPreferencesRepository.getPasswordGenerationPreference()
+            .firstOrNull()
+            ?.let { passwordGenerationPreference ->
+                when (config) {
+                    is PasswordConfig.Memorable -> passwordGenerationPreference.copy(
+                        wordsCount = config.wordsCount,
+//                        wordsSeparator = config.wordSeparator,
+                        wordsCapitalise = config.capitalizeWords,
+                        wordsIncludeNumbers = config.includeNumbers
+                    )
+
+                    is PasswordConfig.Random -> passwordGenerationPreference.copy(
+                        randomPasswordLength = config.length,
+                        randomHasSpecialCharacters = config.includeSymbols,
+                        randomHasCapitalLetters = config.includeUppercase,
+                        randomIncludeNumbers = config.includeNumbers
+                    )
+                }.also(userPreferencesRepository::setPasswordGenerationPreference)
+            }
+    }
+
+}

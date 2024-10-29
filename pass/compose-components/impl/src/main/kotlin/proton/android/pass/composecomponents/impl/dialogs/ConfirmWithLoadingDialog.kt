6005519ef4e380fdfbd1c34c0439712bc527def3
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-10-29 11:58:40 +0100

    feat: add ui for new dialogs and refactor a bit dialogs

--- pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/dialogs/ConfirmWithLoadingDialog.kt
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/dialogs/ConfirmWithLoadingDialog.kt
@@ -19,20 +19,29 @@
 package proton.android.pass.composecomponents.impl.dialogs
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material.AlertDialog
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.Placeable
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import me.proton.core.compose.component.ProtonAlertDialog
+import androidx.compose.ui.util.fastForEach
+import androidx.compose.ui.util.fastForEachIndexed
+import androidx.compose.ui.window.DialogProperties
 import me.proton.core.compose.component.ProtonAlertDialogText
-import me.proton.core.compose.component.ProtonTextButton
 import me.proton.core.compose.theme.ProtonTheme
 import me.proton.core.compose.theme.headlineNorm
+import proton.android.pass.commonui.api.PassTheme
+import proton.android.pass.commonui.api.Spacing
+import kotlin.math.max
 
 @Composable
 fun ConfirmWithLoadingDialog(
@@ -45,6 +54,7 @@ fun ConfirmWithLoadingDialog(
     message: String,
     confirmText: String,
     cancelText: String,
+    extraButtons: (@Composable () -> Unit)? = null,
     onDismiss: () -> Unit,
     onConfirm: () -> Unit,
     onCancel: () -> Unit
@@ -61,6 +71,7 @@ fun ConfirmWithLoadingDialog(
                 text = message
             )
         },
+        extraButtons = extraButtons,
         confirmText = confirmText,
         cancelText = cancelText,
         onDismiss = onDismiss,
@@ -78,6 +89,7 @@ fun ConfirmWithLoadingDialog(
     isConfirmEnabled: Boolean = true,
     title: String,
     content: @Composable () -> Unit,
+    extraButtons: (@Composable () -> Unit)? = null,
     confirmText: String,
     cancelText: String,
     onDismiss: () -> Unit,
@@ -87,19 +99,55 @@ fun ConfirmWithLoadingDialog(
     if (!show) return
     BackHandler { onDismiss() }
 
-    val confirmColor = if (isConfirmEnabled) {
-        if (isConfirmActionDestructive) {
-            ProtonTheme.colors.notificationError
-        } else {
-            ProtonTheme.colors.brandNorm
-        }
-    } else {
-        ProtonTheme.colors.textDisabled
-    }
-
-    ProtonAlertDialog(
+    LoadingDialog(
         modifier = modifier,
-        onDismissRequest = onDismiss,
+        title = title,
+        content = content,
+        onDismiss = onDismiss,
+        buttons = {
+            DialogButton(text = cancelText, isEnabled = !isLoading, onClick = onCancel)
+
+            extraButtons?.invoke()
+
+            DialogButton(
+                text = confirmText,
+                textColor = dialogConfirmColor(isConfirmEnabled, isConfirmActionDestructive),
+                isEnabled = isConfirmEnabled,
+                isLoading = isLoading,
+                onClick = onConfirm
+            )
+        }
+    )
+}
+
+@Composable
+fun dialogConfirmColor(isConfirmEnabled: Boolean = true, isConfirmActionDestructive: Boolean) = if (isConfirmEnabled) {
+    if (isConfirmActionDestructive) {
+        PassTheme.colors.signalDanger
+    } else {
+        PassTheme.colors.interactionNormMajor2
+    }
+} else {
+    PassTheme.colors.textDisabled
+}
+
+@Composable
+fun LoadingDialog(
+    modifier: Modifier = Modifier,
+    title: String,
+    content: @Composable () -> Unit,
+    buttons: @Composable () -> Unit,
+    onDismiss: () -> Unit
+) {
+    AlertDialog(
+        modifier = modifier
+            .fillMaxWidth(fraction = ALERT_DIALOG_WIDTH_FRACTION)
+            .widthIn(max = MAX_ALERT_DIALOG_WIDTH),
+        shape = ProtonTheme.shapes.medium,
+        backgroundColor = ProtonTheme.colors.backgroundNorm,
+        properties = DialogProperties(
+            usePlatformDefaultWidth = false
+        ),
         title = {
             Text(
                 text = title,
@@ -108,42 +156,126 @@ fun ConfirmWithLoadingDialog(
             )
         },
         text = content,
-        confirmButton = {
-            if (isLoading) {
-                CircularProgressIndicator(
-                    modifier = Modifier.height(48.dp)
-                )
-            } else {
-                ProtonTextButton(
-                    onClick = onConfirm,
-                    enabled = isConfirmEnabled,
-                    colors = ButtonDefaults.buttonColors(
-                        backgroundColor = Color.Transparent,
-                        disabledBackgroundColor = Color.Transparent,
-                        contentColor = confirmColor
+        onDismissRequest = onDismiss,
+        buttons = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        horizontal = Spacing.small,
+                        vertical = Spacing.extraSmall / 2
                     )
+            ) {
+                AlertDialogFlowRow(
+                    mainAxisSpacing = Spacing.small,
+                    crossAxisSpacing = Spacing.mediumSmall
                 ) {
-                    Text(
-                        text = confirmText,
-                        color = confirmColor,
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis
-                    )
-                }
-            }
-        },
-        dismissButton = {
-            if (!isLoading) {
-                ProtonTextButton(onClick = onCancel) {
-                    Text(
-                        text = cancelText,
-                        color = ProtonTheme.colors.brandNorm,
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis
-                    )
+                    buttons()
                 }
             }
         }
     )
 }
 
+// Copied from AndroidAlertDialog.Kt
+@Composable
+private fun AlertDialogFlowRow(
+    mainAxisSpacing: Dp,
+    crossAxisSpacing: Dp,
+    content: @Composable () -> Unit
+) {
+    Layout(content) { measurables, constraints ->
+        val sequences = mutableListOf<List<Placeable>>()
+        val crossAxisSizes = mutableListOf<Int>()
+        val crossAxisPositions = mutableListOf<Int>()
+
+        var mainAxisSpace = 0
+        var crossAxisSpace = 0
+
+        val currentSequence = mutableListOf<Placeable>()
+        var currentMainAxisSize = 0
+        var currentCrossAxisSize = 0
+
+        val childConstraints = Constraints(maxWidth = constraints.maxWidth)
+
+        // Return whether the placeable can be added to the current sequence.
+        fun canAddToCurrentSequence(placeable: Placeable) =
+            currentSequence.isEmpty() || currentMainAxisSize + mainAxisSpacing.roundToPx() +
+                placeable.width <= constraints.maxWidth
+
+        // Store current sequence information and start a new sequence.
+        fun startNewSequence() {
+            if (sequences.isNotEmpty()) {
+                crossAxisSpace += crossAxisSpacing.roundToPx()
+            }
+            // Ensures that confirming actions appear above dismissive actions.
+            @Suppress("ListIterator")
+            sequences.add(0, currentSequence.toList())
+            crossAxisSizes += currentCrossAxisSize
+            crossAxisPositions += crossAxisSpace
+
+            crossAxisSpace += currentCrossAxisSize
+            mainAxisSpace = max(mainAxisSpace, currentMainAxisSize)
+
+            currentSequence.clear()
+            currentMainAxisSize = 0
+            currentCrossAxisSize = 0
+        }
+
+        measurables.fastForEach { measurable ->
+            // Ask the child for its preferred size.
+            val placeable = measurable.measure(childConstraints)
+
+            // Start a new sequence if there is not enough space.
+            if (!canAddToCurrentSequence(placeable)) startNewSequence()
+
+            // Add the child to the current sequence.
+            if (currentSequence.isNotEmpty()) {
+                currentMainAxisSize += mainAxisSpacing.roundToPx()
+            }
+            currentSequence.add(placeable)
+            currentMainAxisSize += placeable.width
+            currentCrossAxisSize = max(currentCrossAxisSize, placeable.height)
+        }
+
+        if (currentSequence.isNotEmpty()) startNewSequence()
+
+        val mainAxisLayoutSize = if (constraints.maxWidth != Constraints.Infinity) {
+            constraints.maxWidth
+        } else {
+            max(mainAxisSpace, constraints.minWidth)
+        }
+        val crossAxisLayoutSize = max(crossAxisSpace, constraints.minHeight)
+
+        val layoutWidth = mainAxisLayoutSize
+
+        val layoutHeight = crossAxisLayoutSize
+
+        layout(layoutWidth, layoutHeight) {
+            sequences.fastForEachIndexed { i, placeables ->
+                val childrenMainAxisSizes = IntArray(placeables.size) { j ->
+                    placeables[j].width +
+                        if (j < placeables.lastIndex) mainAxisSpacing.roundToPx() else 0
+                }
+                val arrangement = Arrangement.Bottom
+                // Handle vertical direction
+                val mainAxisPositions = IntArray(childrenMainAxisSizes.size) { 0 }
+                with(arrangement) {
+                    arrange(mainAxisLayoutSize, childrenMainAxisSizes, mainAxisPositions)
+                }
+                placeables.fastForEachIndexed { j, placeable ->
+                    placeable.place(
+                        x = mainAxisPositions[j],
+                        y = crossAxisPositions[i]
+                    )
+                }
+            }
+        }
+    }
+}
+
+private const val ALERT_DIALOG_WIDTH_FRACTION = 0.9f
+
+// Mobile alert on desktop is 560dp wide
+// https://material.io/components/dialogs#specs
+private val MAX_ALERT_DIALOG_WIDTH = 560.dp

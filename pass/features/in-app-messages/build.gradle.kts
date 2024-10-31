fc228c2fcbfc83164b7491acd8bc087aa9326b85
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-10-31 10:33:17 +0100

    feat: add feature in app module

---
+++ pass/features/in-app-messages/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("dagger.hilt.android.plugin")
+    id("com.google.devtools.ksp")
+    id("org.jetbrains.kotlin.kapt")
+}
+
+android {
+    namespace = "proton.android.pass.features.inappmessages"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+    
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+        testInstrumentationRunner = "proton.android.pass.test.HiltRunner"
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.androidx.compose.compiler.get()
+    }
+}
+
+androidComponents.beforeVariants { variant ->
+    variant.enableAndroidTest = false
+}
+
+dependencies {
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.runtime)
+    implementation(libs.core.utilKotlin)
+    implementation(libs.kotlinx.collections)
+
+    debugImplementation(libs.androidx.compose.uiTooling)
+    implementation(libs.androidx.compose.uiToolingPreview)
+    debugImplementation(libs.androidx.compose.uiTestManifest)
+
+    implementation(libs.dagger.hilt.android)
+    kapt(libs.dagger.hilt.android.compiler)
+    kapt(libs.androidx.hilt.compiler)
+
+    debugImplementation(libs.showkase)
+    kspDebug(libs.showkaseProcessor)
+}

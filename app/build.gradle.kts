b730fd9568da04b733928bdcb0d211f654027e62
Alexander Talledo <alexander.talledo@proton.ch>
2024-11-06 16:01:28 +0100

    bump: version 1.26.2 → 1.26.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ println(
 )
 
 val jobId: Int = System.getenv("CI_JOB_ID")?.take(3)?.toInt() ?: 0
-val appVersionName: String = "1.26.2"
+val appVersionName: String = "1.26.3"
 val appVersionCode: Int = versionCode(appVersionName)
 val archivesBaseName = "ProtonPass-$appVersionName"
 

7cea457fc49bfc939c5db867aaec0f333205dd7b
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-11-08 12:04:38 +0100

    bump: version 1.26.3 → 1.27.0

--- pass/feature-home/demo-app/build.gradle.kts
+++ pass/feature-home/demo-app/build.gradle.kts
@@ -24,7 +24,7 @@ plugins {
     id("androidx.baselineprofile")
 }
 
-val appVersionName: String = "1.26.3"
+val appVersionName: String = "1.27.0"
 val appVersionCode: Int = versionCode(appVersionName)
 val archivesBaseName = "FeatureHomeApp-$appVersionName"
 

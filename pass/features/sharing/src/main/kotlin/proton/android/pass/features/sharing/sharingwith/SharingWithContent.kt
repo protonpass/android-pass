f97ae18c6f80a9003311c18e3211b56b47751e6a
Alexander Talledo <alexander.talledo@proton.ch>
2024-11-13 15:52:41 +0100

    style: adjust UI to design

--- pass/features/sharing/src/main/kotlin/proton/android/pass/features/sharing/sharingwith/SharingWithContent.kt
+++ pass/features/sharing/src/main/kotlin/proton/android/pass/features/sharing/sharingwith/SharingWithContent.kt
@@ -18,6 +18,7 @@
 
 package proton.android.pass.features.sharing.sharingwith
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -28,13 +29,14 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -43,9 +45,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
@@ -57,23 +59,24 @@ import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import me.proton.core.compose.theme.ProtonTheme
-import me.proton.core.compose.theme.defaultNorm
 import me.proton.core.compose.theme.subheadlineNorm
 import proton.android.pass.commonui.api.PassTheme
+import proton.android.pass.commonui.api.PassTopBarBackButtonType
+import proton.android.pass.commonui.api.Radius
 import proton.android.pass.commonui.api.RequestFocusLaunchedEffect
 import proton.android.pass.commonui.api.Spacing
 import proton.android.pass.commonui.api.ThemedBooleanPreviewProvider
-import proton.android.pass.commonui.api.body3Norm
-import proton.android.pass.commonui.api.heroNorm
+import proton.android.pass.commonui.api.applyIf
 import proton.android.pass.composecomponents.impl.buttons.LoadingCircleButton
-import proton.android.pass.composecomponents.impl.container.roundedContainer
 import proton.android.pass.composecomponents.impl.form.PassDivider
 import proton.android.pass.composecomponents.impl.form.ProtonTextField
 import proton.android.pass.composecomponents.impl.form.ProtonTextFieldPlaceHolder
-import proton.android.pass.composecomponents.impl.topbar.BackArrowTopAppBar
+import proton.android.pass.composecomponents.impl.text.Text
+import proton.android.pass.composecomponents.impl.topbar.PassExtendedTopBar
 import proton.android.pass.features.sharing.R
 import proton.android.pass.features.sharing.SharingNavigation
 import me.proton.core.presentation.R as CoreR
+import proton.android.pass.composecomponents.impl.R as CompR
 
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
@@ -84,45 +87,76 @@ internal fun SharingWithContent(
     onNavigateEvent: (SharingNavigation) -> Unit,
     onEvent: (SharingWithUiEvent) -> Unit
 ) {
+    val scrollState = rememberScrollState()
+
+    val focusRequester = remember { FocusRequester() }
+
+    var parentHeight: Dp by remember { mutableStateOf(Dp.Unspecified) }
+
+    val density = LocalDensity.current
+
+    val errorMessageRes = remember(state.errorMessage) {
+        when (state.errorMessage) {
+            ErrorMessage.NoAddressesCanBeInvited -> {
+                R.string.sharing_with_no_addresses_can_be_invited
+            }
+
+            ErrorMessage.SomeAddressesCannotBeInvited -> {
+                R.string.sharing_with_some_addresses_cannot_be_invited
+            }
+
+            ErrorMessage.EmailNotValid -> {
+                R.string.share_with_email_error
+            }
+
+            ErrorMessage.CannotInviteOutsideOrg -> {
+                R.string.share_with_email_cannot_invite_outside_org
+            }
+
+            ErrorMessage.EmailAlreadyAdded -> {
+                R.string.share_with_email_email_already_added
+            }
+
+            ErrorMessage.None -> null
+        }
+    }
+
+    LaunchedEffect(state.scrollToBottom) {
+        if (state.scrollToBottom) {
+            scrollState.animateScrollTo(scrollState.maxValue)
+            onEvent(SharingWithUiEvent.OnScrolledToBottom)
+        }
+    }
+
     Scaffold(
         modifier = modifier,
         topBar = {
-            BackArrowTopAppBar(
-                title = "",
+            PassExtendedTopBar(
+                backButton = PassTopBarBackButtonType.BackArrow,
+                title = stringResource(R.string.share_with_title),
                 onUpClick = { onNavigateEvent(SharingNavigation.BackToHome) },
                 actions = {
                     LoadingCircleButton(
+                        modifier = Modifier.padding(vertical = Spacing.small),
+                        isLoading = state.isLoading,
                         buttonEnabled = state.isContinueEnabled,
                         color = if (state.isContinueEnabled) {
                             PassTheme.colors.interactionNormMajor1
                         } else {
                             PassTheme.colors.interactionNormMinor1
                         },
-                        onClick = { onEvent(SharingWithUiEvent.ContinueClick) },
-                        isLoading = state.isLoading,
                         text = {
-                            Text(
-                                text = stringResource(R.string.share_continue),
-                                style = PassTheme.typography.body3Norm(),
+                            Text.Body2Regular(
+                                text = stringResource(id = CompR.string.action_continue),
                                 color = PassTheme.colors.textInvert
                             )
-                        }
+                        },
+                        onClick = { onEvent(SharingWithUiEvent.ContinueClick) }
                     )
                 }
             )
         }
     ) { padding ->
-
-        val scrollState = rememberScrollState()
-        LaunchedEffect(state.scrollToBottom) {
-            if (state.scrollToBottom) {
-                scrollState.animateScrollTo(scrollState.maxValue)
-                onEvent(SharingWithUiEvent.OnScrolledToBottom)
-            }
-        }
-
-        var parentHeight: Dp by remember { mutableStateOf(Dp.Unspecified) }
-        val density = LocalDensity.current
         Column(
             modifier = Modifier
                 .padding(padding)
@@ -130,11 +164,6 @@ internal fun SharingWithContent(
                 .onSizeChanged { parentHeight = with(density) { it.height.toDp() } },
             verticalArrangement = Arrangement.spacedBy(Spacing.medium)
         ) {
-            Text(
-                text = stringResource(R.string.share_with_title),
-                style = PassTheme.typography.heroNorm()
-            )
-
             if (state.showEditVault && state.vault != null) {
                 CustomizeVault(
                     vault = state.vault,
@@ -151,7 +180,8 @@ internal fun SharingWithContent(
             ) {
                 FlowRow(
                     modifier = Modifier.fillMaxWidth(),
-                    verticalArrangement = Arrangement.Center
+                    verticalArrangement = Arrangement.spacedBy(space = Spacing.small),
+                    horizontalArrangement = Arrangement.spacedBy(space = Spacing.small)
                 ) {
                     state.enteredEmails.forEachIndexed { idx, emailState ->
                         SharingWithChip(
@@ -165,27 +195,6 @@ internal fun SharingWithContent(
                 }
             }
 
-            val focusRequester = remember { FocusRequester() }
-
-            val (isError, errorMessage) = when (state.errorMessage) {
-                ErrorMessage.NoAddressesCanBeInvited -> {
-                    true to stringResource(R.string.sharing_with_no_addresses_can_be_invited)
-                }
-                ErrorMessage.SomeAddressesCannotBeInvited -> {
-                    true to stringResource(R.string.sharing_with_some_addresses_cannot_be_invited)
-                }
-                ErrorMessage.EmailNotValid -> {
-                    true to stringResource(R.string.share_with_email_error)
-                }
-                ErrorMessage.CannotInviteOutsideOrg -> {
-                    true to stringResource(R.string.share_with_email_cannot_invite_outside_org)
-                }
-                ErrorMessage.EmailAlreadyAdded -> {
-                    true to stringResource(R.string.share_with_email_email_already_added)
-                }
-                else -> false to ""
-            }
-
             ProtonTextField(
                 modifier = Modifier.focusRequester(focusRequester),
                 value = editingEmail,
@@ -197,8 +206,8 @@ internal fun SharingWithContent(
                         )
                     )
                 },
-                isError = isError,
-                errorMessage = errorMessage,
+                isError = errorMessageRes != null,
+                errorMessage = errorMessageRes?.let { stringResource(it) }.orEmpty(),
                 textStyle = ProtonTheme.typography.subheadlineNorm,
                 keyboardOptions = KeyboardOptions.Default.copy(
                     capitalization = KeyboardCapitalization.None,
@@ -216,6 +225,7 @@ internal fun SharingWithContent(
             RequestFocusLaunchedEffect(focusRequester)
 
             PassDivider()
+
             InviteSuggestions(
                 state = state.suggestionsUIState,
                 onItemClicked = { email, state ->
@@ -233,51 +243,59 @@ private fun SharingWithChip(
     isSelected: Boolean,
     onClick: () -> Unit
 ) {
+    val contentColor = if (emailState.isError) {
+        PassTheme.colors.passwordInteractionNormMajor2
+    } else {
+        PassTheme.colors.textNorm
+    }
+
     Row(
         modifier = modifier
-            .padding(
-                top = Spacing.small,
-                bottom = Spacing.small,
-                end = Spacing.small
-            )
-            .roundedContainer(
-                backgroundColor = PassTheme.colors.interactionNormMinor1,
-                borderColor = if (emailState.isError) {
-                    PassTheme.colors.signalDanger
-                } else Color.Transparent
+            .clip(shape = RoundedCornerShape(size = Radius.small))
+            .applyIf(
+                condition = emailState.isError,
+                ifTrue = { background(color = PassTheme.colors.passwordInteractionNormMinor1) },
+                ifFalse = { background(color = PassTheme.colors.interactionNormMinor1) }
             )
             .clickable { onClick() }
-            .padding(Spacing.small),
-        verticalAlignment = Alignment.CenterVertically
+            .padding(all = Spacing.small),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(space = Spacing.small)
     ) {
-        Text(
+        Text.Body2Regular(
             text = emailState.email,
-            style = ProtonTheme.typography.defaultNorm
+            color = contentColor
         )
 
         if (isSelected) {
             Icon(
-                modifier = Modifier.padding(start = Spacing.small),
-                painter = painterResource(id = CoreR.drawable.ic_proton_cross_circle),
-                tint = PassTheme.colors.textNorm,
-                contentDescription = stringResource(R.string.share_with_remove_email_content_description)
+                modifier = Modifier.size(size = Spacing.mediumSmall),
+                painter = painterResource(id = CoreR.drawable.ic_proton_cross),
+                contentDescription = stringResource(R.string.share_with_remove_email_content_description),
+                tint = contentColor
             )
         }
     }
 }
 
-@Preview
-@Composable
-fun SharingWithChipPreview(@PreviewParameter(ThemedBooleanPreviewProvider::class) input: Pair<Boolean, Boolean>) {
-    PassTheme(isDark = input.first) {
+@[Preview Composable]
+internal fun SharingWithChipPreview(
+    @PreviewParameter(ThemedBooleanPreviewProvider::class) input: Pair<Boolean, Boolean>
+) {
+    val (isDark, isSelected) = input
+
+    PassTheme(isDark = isDark) {
         Surface {
             SharingWithChip(
-                emailState = EnteredEmailState("some@email.test", isError = input.second),
-                isSelected = input.second,
+                emailState = EnteredEmailState(
+                    email = "some@email.test",
+                    isError = isSelected
+                ),
+                isSelected = isSelected,
                 onClick = {}
             )
         }
     }
 }
 
-private const val RATIO_HEIGHT_EMAIL_LIST = 0.2f
+private const val RATIO_HEIGHT_EMAIL_LIST = 0.3f

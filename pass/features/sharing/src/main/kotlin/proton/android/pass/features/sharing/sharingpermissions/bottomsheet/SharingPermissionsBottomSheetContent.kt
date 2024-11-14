ea5ca1ad052b4178e7ddd6e298bed6a4926a59e0
Alexander Talledo <alexander.talledo@proton.ch>
2024-11-14 12:22:01 +0100

    style: tweak access level selector UI to adjust to design

--- pass/features/sharing/src/main/kotlin/proton/android/pass/features/sharing/sharingpermissions/bottomsheet/SharingPermissionsBottomSheetContent.kt
+++ pass/features/sharing/src/main/kotlin/proton/android/pass/features/sharing/sharingpermissions/bottomsheet/SharingPermissionsBottomSheetContent.kt
@@ -19,16 +19,12 @@
 package proton.android.pass.features.sharing.sharingpermissions.bottomsheet
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import kotlinx.collections.immutable.toPersistentList
-import me.proton.core.compose.theme.ProtonTheme
-import me.proton.core.compose.theme.headlineNorm
 import proton.android.pass.commonui.api.PassTheme
 import proton.android.pass.commonui.api.bottomSheet
 import proton.android.pass.composecomponents.impl.bottomsheet.BottomSheetItem
@@ -37,6 +33,7 @@ import proton.android.pass.composecomponents.impl.bottomsheet.BottomSheetItemLis
 import proton.android.pass.composecomponents.impl.bottomsheet.BottomSheetItemSubtitle
 import proton.android.pass.composecomponents.impl.bottomsheet.BottomSheetItemTitle
 import proton.android.pass.composecomponents.impl.bottomsheet.bottomSheetDivider
+import proton.android.pass.composecomponents.impl.text.Text
 import proton.android.pass.features.sharing.R
 import proton.android.pass.features.sharing.sharingpermissions.SharingType
 import me.proton.core.presentation.R as CoreR
@@ -48,7 +45,7 @@ internal fun SharingPermissionsBottomSheetContent(
     onPermissionClick: (SharingType) -> Unit,
     onDeleteUserClick: () -> Unit
 ) {
-    val (titleText, checkedPermission) = when (state.mode) {
+    val (titleText, selectedSharingType) = when (state.mode) {
         is SharingPermissionsEditMode.All ->
             stringResource(R.string.sharing_edit_permissions_set_access_level_all_members) to null
 
@@ -58,74 +55,80 @@ internal fun SharingPermissionsBottomSheetContent(
         ) to state.mode.sharingType
     }
 
-    Column(modifier = modifier.bottomSheet()) {
-        val items = mutableListOf(
-            titleText(titleText),
-            setAdminPermission(
-                checked = checkedPermission == SharingType.Admin,
-                onClick = { onPermissionClick(SharingType.Admin) }
-            ),
-            setWritePermission(
-                checked = checkedPermission == SharingType.Write,
-                onClick = { onPermissionClick(SharingType.Write) }
-            ),
-            setReadPermission(
-                checked = checkedPermission == SharingType.Read,
-                onClick = { onPermissionClick(SharingType.Read) }
-            )
-        )
+    buildList {
+        titleText(titleText).also(::add)
+
+        setAdminPermission(
+            sharingType = selectedSharingType,
+            onClick = onPermissionClick
+        ).also(::add)
+
+        setWritePermission(
+            sharingType = selectedSharingType,
+            onClick = onPermissionClick
+        ).also(::add)
+
+        setReadPermission(
+            sharingType = selectedSharingType,
+            onClick = onPermissionClick
+        ).also(::add)
 
         if (state.displayRemove) {
-            items.add(bottomSheetDivider())
-            items.add(removeUserRow { onDeleteUserClick() })
-        }
+            bottomSheetDivider().also(::add)
 
+            removeUserRow(onClick = onDeleteUserClick).also(::add)
+        }
+    }.also { items ->
         BottomSheetItemList(
+            modifier = modifier.bottomSheet(),
             items = items.toPersistentList()
         )
     }
 }
 
 private fun titleText(text: String): BottomSheetItem = object : BottomSheetItem {
-    override val title: @Composable () -> Unit
-        get() = {
-            Text(
-                modifier = Modifier.fillMaxWidth(),
-                text = text,
-                style = ProtonTheme.typography.headlineNorm.copy(textAlign = TextAlign.Center)
-            )
-        }
-    override val subtitle: (@Composable () -> Unit)?
-        get() = null
-    override val leftIcon: (@Composable () -> Unit)?
-        get() = null
-    override val endIcon: (@Composable () -> Unit)?
-        get() = null
-    override val onClick: (() -> Unit)?
-        get() = null
+    override val title: @Composable () -> Unit = {
+        Text.Body2Regular(
+            modifier = Modifier.fillMaxWidth(),
+            text = text,
+            textAlign = TextAlign.Center
+        )
+    }
+
+    override val subtitle: (@Composable () -> Unit)? = null
+
+    override val leftIcon: (@Composable () -> Unit)? = null
+
+    override val endIcon: (@Composable () -> Unit)? = null
+
+    override val onClick: (() -> Unit)? = null
+
     override val isDivider = false
 }
 
-private fun setAdminPermission(checked: Boolean, onClick: () -> Unit): BottomSheetItem = permissionRow(
-    title = R.string.sharing_can_manage,
-    subtitle = R.string.sharing_can_manage_description,
-    checked = checked,
-    onClick = onClick
-)
+private fun setAdminPermission(sharingType: SharingType?, onClick: (SharingType) -> Unit): BottomSheetItem =
+    permissionRow(
+        title = R.string.sharing_can_manage,
+        subtitle = R.string.sharing_can_manage_description,
+        checked = sharingType == SharingType.Admin,
+        onClick = { onClick(SharingType.Admin) }
+    )
 
-private fun setWritePermission(checked: Boolean, onClick: () -> Unit): BottomSheetItem = permissionRow(
-    title = R.string.sharing_can_edit,
-    subtitle = R.string.sharing_can_edit_description,
-    checked = checked,
-    onClick = onClick
-)
+private fun setWritePermission(sharingType: SharingType?, onClick: (SharingType) -> Unit): BottomSheetItem =
+    permissionRow(
+        title = R.string.sharing_can_edit,
+        subtitle = R.string.sharing_can_edit_description,
+        checked = sharingType == SharingType.Write,
+        onClick = { onClick(SharingType.Write) }
+    )
 
-private fun setReadPermission(checked: Boolean, onClick: () -> Unit): BottomSheetItem = permissionRow(
-    title = R.string.sharing_can_view,
-    subtitle = R.string.sharing_can_view_description,
-    checked = checked,
-    onClick = onClick
-)
+private fun setReadPermission(sharingType: SharingType?, onClick: (SharingType) -> Unit): BottomSheetItem =
+    permissionRow(
+        title = R.string.sharing_can_view,
+        subtitle = R.string.sharing_can_view_description,
+        checked = sharingType == SharingType.Read,
+        onClick = { onClick(SharingType.Read) }
+    )
 
 private fun permissionRow(
     @StringRes title: Int,
@@ -133,53 +136,57 @@ private fun permissionRow(
     checked: Boolean,
     onClick: () -> Unit
 ): BottomSheetItem = object : BottomSheetItem {
-    override val title: @Composable () -> Unit
-        get() = {
-            BottomSheetItemTitle(
-                text = stringResource(id = title),
-                color = PassTheme.colors.textNorm
+    override val title: @Composable () -> Unit = {
+        BottomSheetItemTitle(
+            text = stringResource(id = title),
+            color = PassTheme.colors.textNorm
+        )
+    }
+
+    override val subtitle: (@Composable () -> Unit) = {
+        BottomSheetItemSubtitle(
+            text = stringResource(id = subtitle),
+            maxLines = 2,
+            color = PassTheme.colors.textWeak
+        )
+    }
+
+    override val leftIcon: (@Composable () -> Unit)? = null
+
+    override val endIcon: (@Composable () -> Unit) = {
+        if (checked) {
+            BottomSheetItemIcon(
+                iconId = CoreR.drawable.ic_proton_checkmark,
+                tint = PassTheme.colors.interactionNormMajor2
             )
         }
-    override val subtitle: (@Composable () -> Unit)
-        get() = {
-            BottomSheetItemSubtitle(
-                text = stringResource(id = subtitle),
-                maxLines = 2
-            )
-        }
-    override val leftIcon: (@Composable () -> Unit)?
-        get() = null
-    override val endIcon: (@Composable () -> Unit)?
-        get() = if (checked) {
-            { BottomSheetItemIcon(iconId = CoreR.drawable.ic_proton_checkmark) }
-        } else {
-            null
-        }
-    override val onClick: (() -> Unit)
-        get() = onClick
+    }
+
+    override val onClick: (() -> Unit) = onClick
+
     override val isDivider = false
 }
 
 private fun removeUserRow(onClick: () -> Unit): BottomSheetItem = object : BottomSheetItem {
-    override val title: @Composable () -> Unit
-        get() = {
-            BottomSheetItemTitle(
-                text = stringResource(id = R.string.sharing_edit_permissions_remove_user),
-                color = PassTheme.colors.signalDanger
-            )
-        }
-    override val subtitle: (@Composable () -> Unit)?
-        get() = null
-    override val leftIcon: (@Composable () -> Unit)
-        get() = {
-            BottomSheetItemIcon(
-                iconId = CoreR.drawable.ic_proton_trash_cross,
-                tint = PassTheme.colors.signalDanger
-            )
-        }
-    override val endIcon: (@Composable () -> Unit)?
-        get() = null
-    override val onClick: (() -> Unit)
-        get() = onClick
+    override val title: @Composable () -> Unit = {
+        BottomSheetItemTitle(
+            text = stringResource(id = R.string.sharing_edit_permissions_remove_user),
+            color = PassTheme.colors.signalDanger
+        )
+    }
+
+    override val subtitle: (@Composable () -> Unit)? = null
+
+    override val leftIcon: (@Composable () -> Unit) = {
+        BottomSheetItemIcon(
+            iconId = CoreR.drawable.ic_proton_trash_cross,
+            tint = PassTheme.colors.signalDanger
+        )
+    }
+
+    override val endIcon: (@Composable () -> Unit)? = null
+
+    override val onClick: (() -> Unit) = onClick
+
     override val isDivider = false
 }

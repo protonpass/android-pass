abc5a55609416119f0162ec576c5dddc80c4c3f5
Alexander Talledo <alexander.talledo@proton.ch>
2024-11-21 11:11:39 +0100

    chore: removed duplicated feature

--- app/src/main/java/proton/android/pass/ui/navigation/AppGraph.kt
+++ app/src/main/java/proton/android/pass/ui/navigation/AppGraph.kt
@@ -375,9 +375,14 @@ fun NavGraphBuilder.appGraph(
                     force = true
                 )
 
+<<<<<<< HEAD
                 is HomeNavigation.ConfirmedInvite -> appNavigator.navigate(
                     destination = AcceptInvite,
                     route = AcceptInvite.createRoute(it.inviteToken),
+=======
+                HomeNavigation.ConfirmedInvite -> appNavigator.navigate(
+                    destination = AcceptInvite,
+>>>>>>> be3785bf5 (chore: removed duplicated feature)
                     force = true
                 )
 

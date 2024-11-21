505e53107cded974d9704f6936387400067b5abf
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-11-21 14:37:10 +0100

    feature: add file attachment bottomsheets ui

---
+++ pass/feature-item-create/impl/src/main/kotlin/proton/android/pass/featureitemcreate/impl/attachments/attachmentoptions/ui/AttachmentOptionsBottomsheet.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.featureitemcreate.impl.attachments.attachmentoptions.ui
+
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import proton.android.pass.commonui.api.PassTheme
+import proton.android.pass.commonui.api.ThemePreviewProvider
+import proton.android.pass.commonui.api.bottomSheet
+import proton.android.pass.featureitemcreate.impl.attachments.attachmentoptions.navigation.AttachmentOptionsNavigation
+
+@Composable
+fun AttachmentOptionsBottomsheet(modifier: Modifier = Modifier, onNavigate: (AttachmentOptionsNavigation) -> Unit) {
+    AttachmentOptionsContent(
+        modifier = modifier.bottomSheet(),
+        onEvent = {
+            when (it) {
+                AttachmentOptionsEvent.Delete -> Unit
+                AttachmentOptionsEvent.Open -> Unit
+                AttachmentOptionsEvent.Rename -> Unit
+                AttachmentOptionsEvent.Save -> Unit
+            }
+        }
+    )
+}
+
+
+@Preview
+@Composable
+fun AttachmentOptionsBottomsheetPreview(@PreviewParameter(ThemePreviewProvider::class) isDark: Boolean) {
+    PassTheme(isDark = isDark) {
+        Surface {
+            AttachmentOptionsBottomsheet(
+                onNavigate = {}
+            )
+        }
+    }
+}

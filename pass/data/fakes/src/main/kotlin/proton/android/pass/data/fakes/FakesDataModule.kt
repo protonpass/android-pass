121fe39b6d953639451d11485fe91d6f507a1891
Alexander Talledo <alexander.talledo@proton.ch>
2024-11-25 16:00:15 +0100

    test: add missing fakes

--- pass/data/fakes/src/main/kotlin/proton/android/pass/data/fakes/FakesDataModule.kt
+++ pass/data/fakes/src/main/kotlin/proton/android/pass/data/fakes/FakesDataModule.kt
@@ -174,6 +174,7 @@ import proton.android.pass.data.api.usecases.securelink.ObserveInactiveSecureLin
 import proton.android.pass.data.api.usecases.securelink.ObserveSecureLink
 import proton.android.pass.data.api.usecases.securelink.ObserveSecureLinks
 import proton.android.pass.data.api.usecases.securelink.ObserveSecureLinksCount
+import proton.android.pass.data.api.usecases.shares.ObserveShare
 import proton.android.pass.data.api.usecases.simplelogin.CreateSimpleLoginAliasMailbox
 import proton.android.pass.data.api.usecases.simplelogin.DeleteSimpleLoginAliasMailbox
 import proton.android.pass.data.api.usecases.simplelogin.DisableSimpleLoginSyncPreference
@@ -344,6 +345,7 @@ import proton.android.pass.data.fakes.usecases.securelink.FakeObserveInactiveSec
 import proton.android.pass.data.fakes.usecases.securelink.FakeObserveSecureLink
 import proton.android.pass.data.fakes.usecases.securelink.FakeObserveSecureLinks
 import proton.android.pass.data.fakes.usecases.securelink.FakeObserveSecureLinksCount
+import proton.android.pass.data.fakes.usecases.shares.FakeObserveShare
 import proton.android.pass.data.fakes.usecases.simplelogin.FakeCreateSimpleLoginAliasMailbox
 import proton.android.pass.data.fakes.usecases.simplelogin.FakeDeleteSimpleLoginAliasMailbox
 import proton.android.pass.data.fakes.usecases.simplelogin.FakeDisableSimpleLoginSyncPreference
@@ -921,5 +923,8 @@ abstract class FakesDataModule {
 
     @Binds
     abstract fun bindObserveInvite(impl: FakeObserveInvite): ObserveInvite
+
+    @Binds
+    abstract fun bindObserveShare(impl: FakeObserveShare): ObserveShare
 
 }
---
+++ pass/data/fakes/src/main/kotlin/proton/android/pass/data/fakes/usecases/shares/FakeObserveShare.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.data.fakes.usecases.shares
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import proton.android.pass.common.api.FlowUtils.testFlow
+import proton.android.pass.common.api.Option
+import proton.android.pass.data.api.usecases.shares.ObserveShare
+import proton.android.pass.domain.Share
+import proton.android.pass.domain.ShareId
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class FakeObserveShare @Inject constructor() : ObserveShare {
+
+    private val shareOptionFlow: MutableSharedFlow<Option<Share>> = testFlow()
+
+    fun emitValue(value: Option<Share>) {
+        shareOptionFlow.tryEmit(value)
+    }
+
+    override fun invoke(shareId: ShareId): Flow<Option<Share>> = shareOptionFlow
+
+}

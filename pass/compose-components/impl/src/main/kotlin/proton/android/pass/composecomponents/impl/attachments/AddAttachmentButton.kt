658c96f5eabbbebd707a71c90f1119281367c513
Victor Hidalgo <victor.hidalgo@proton.ch>
2024-11-28 12:17:19 +0100

    feat: add colors based on item type

--- pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/attachments/AddAttachmentButton.kt
+++ pass/compose-components/impl/src/main/kotlin/proton/android/pass/composecomponents/impl/attachments/AddAttachmentButton.kt
@@ -20,38 +20,59 @@ package proton.android.pass.composecomponents.impl.attachments
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import proton.android.pass.commonui.api.PassTheme
 import proton.android.pass.commonui.api.Spacing
 import proton.android.pass.composecomponents.impl.R
 import proton.android.pass.composecomponents.impl.buttons.Button
 import proton.android.pass.composecomponents.impl.text.Text
+import proton.android.pass.composecomponents.impl.utils.PassItemColors
+import proton.android.pass.composecomponents.impl.utils.passItemColors
+import proton.android.pass.domain.items.ItemCategory
 
 @Composable
 fun AddAttachmentButton(
     modifier: Modifier = Modifier,
+    colors: PassItemColors,
     isEnabled: Boolean,
     onClick: () -> Unit
 ) {
     Button.Circular(
         modifier = modifier.fillMaxWidth(),
-        color = PassTheme.colors.loginInteractionNormMinor1,
+        color = colors.minorPrimary,
         contentPadding = PaddingValues(Spacing.mediumSmall),
         enabled = isEnabled,
         onClick = onClick
     ) {
         val adjustedTextColor = if (isEnabled) {
-            PassTheme.colors.loginInteractionNormMajor2
+            colors.majorSecondary
         } else {
-            PassTheme.colors.loginInteractionNormMajor2.copy(
-                alpha = 0.3f
-            )
+            colors.majorSecondary.copy(alpha = 0.3f)
         }
         Text.Body1Regular(
             text = stringResource(R.string.attachment_add_file),
             color = adjustedTextColor
         )
+    }
+}
+
+@Preview
+@Composable
+fun AddAttachmentButtonPreview(
+    @PreviewParameter(ThemeCreateVaultTopBarPreviewProvider::class) input: Pair<Boolean, ItemCategory>
+) {
+    PassTheme(isDark = input.first) {
+        Surface {
+            AddAttachmentButton(
+                colors = passItemColors(input.second),
+                isEnabled = true,
+                onClick = {}
+            )
+        }
     }
 }

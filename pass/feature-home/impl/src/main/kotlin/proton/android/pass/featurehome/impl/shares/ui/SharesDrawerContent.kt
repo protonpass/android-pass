bacbfeca156bca3ed4ae54e15acd399469fd5b7a
Alexander Talledo <alexander.talledo@proton.ch>
2024-12-17 15:09:26 +0100

    chore: added new home drawer

---
+++ pass/feature-home/impl/src/main/kotlin/proton/android/pass/featurehome/impl/shares/ui/SharesDrawerContent.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.featurehome.impl.shares.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import proton.android.pass.commonui.api.PassTheme
+import proton.android.pass.commonui.api.Spacing
+import proton.android.pass.composecomponents.impl.buttons.PassCircleButton
+import proton.android.pass.featurehome.impl.R
+import proton.android.pass.featurehome.impl.shares.presentation.SharesDrawerState
+
+@Composable
+internal fun SharesDrawerContent(modifier: Modifier = Modifier, state: SharesDrawerState) = with(state) {
+    Column(
+        modifier = modifier.background(color = PassTheme.colors.backgroundWeak)
+    ) {
+        SharesDrawerShareList(
+            modifier = modifier
+                .fillMaxHeight()
+                .weight(weight = 1f, fill = true)
+        )
+
+        if (canCreateVaults) {
+            PassCircleButton(
+                modifier = Modifier.padding(all = Spacing.medium),
+                text = stringResource(R.string.vault_drawer_create_vault),
+                textColor = PassTheme.colors.interactionNormMajor2,
+                backgroundColor = PassTheme.colors.interactionNormMinor1,
+                onClick = {}
+            )
+        }
+    }
+}

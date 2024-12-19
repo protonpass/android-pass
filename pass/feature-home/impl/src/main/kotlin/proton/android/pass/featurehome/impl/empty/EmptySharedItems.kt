8537ba085e00ebd3132b84fb23c44ed18a3ae72e
Alexander Talledo <alexander.talledo@proton.ch>
2024-12-19 09:30:32 +0100

    style: center texts aligment

--- pass/feature-home/impl/src/main/kotlin/proton/android/pass/featurehome/impl/empty/EmptySharedItems.kt
+++ pass/feature-home/impl/src/main/kotlin/proton/android/pass/featurehome/impl/empty/EmptySharedItems.kt
@@ -22,12 +22,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import me.proton.core.compose.theme.ProtonTheme
+import androidx.compose.ui.text.style.TextAlign
+import proton.android.pass.commonui.api.PassTheme
 import proton.android.pass.commonui.api.Spacing
 import proton.android.pass.composecomponents.impl.text.Text
 import proton.android.pass.domain.items.ItemSharedType
@@ -48,15 +51,22 @@ internal fun EmptySharedItems(modifier: Modifier = Modifier, itemSharedType: Ite
         contentAlignment = Alignment.Center
     ) {
         Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = Spacing.medium),
             verticalArrangement = Arrangement.spacedBy(space = Spacing.small)
         ) {
             Text.Headline(
-                text = stringResource(id = R.string.home_empty_shared_items_title)
+                modifier = Modifier.fillMaxWidth(),
+                text = stringResource(id = R.string.home_empty_shared_items_title),
+                textAlign = TextAlign.Center
             )
 
             Text.Body3Regular(
+                modifier = Modifier.fillMaxWidth(),
                 text = stringResource(id = subtitleResId),
-                color = ProtonTheme.colors.textWeak
+                color = PassTheme.colors.textWeak,
+                textAlign = TextAlign.Center
             )
         }
     }

60e968369536fb68f0834e58e78e0d9aeeea3b2e
Alexander Talledo <alexander.talledo@proton.ch>
2024-12-20 15:24:03 +0100

    feat: support leaving shared item

--- pass/features/item-details/src/main/kotlin/proton/android/pass/features/item/details/detailleave/presentation/ItemDetailsLeaveViewModel.kt
+++ pass/features/item-details/src/main/kotlin/proton/android/pass/features/item/details/detailleave/presentation/ItemDetailsLeaveViewModel.kt
@@ -24,22 +24,74 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import proton.android.pass.commonui.api.SavedStateHandleProvider
+import proton.android.pass.commonui.api.require
 import proton.android.pass.composecomponents.impl.uievents.IsLoadingState
+import proton.android.pass.data.api.usecases.LeaveShare
+import proton.android.pass.domain.ShareId
+import proton.android.pass.log.api.PassLogger
+import proton.android.pass.navigation.api.CommonNavArgId
+import proton.android.pass.notifications.api.SnackbarDispatcher
 import javax.inject.Inject
 
 @HiltViewModel
-class ItemDetailsLeaveViewModel @Inject constructor() : ViewModel() {
+class ItemDetailsLeaveViewModel @Inject constructor(
+    savedStateHandleProvider: SavedStateHandleProvider,
+    private val leaveShare: LeaveShare,
+    private val snackbarDispatcher: SnackbarDispatcher
+) : ViewModel() {
+
+    private val shareId: ShareId = savedStateHandleProvider.get()
+        .require<String>(CommonNavArgId.ShareId.key)
+        .let(::ShareId)
+
+    private val eventFlow = MutableStateFlow<ItemDetailsLeaveEvent>(ItemDetailsLeaveEvent.Idle)
 
     private val isLoadingStateFlow = MutableStateFlow<IsLoadingState>(IsLoadingState.NotLoading)
 
-    internal val stateFlow: StateFlow<ItemDetailsLeaveState> = isLoadingStateFlow
-        .mapLatest(::ItemDetailsLeaveState)
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5_000),
-            initialValue = ItemDetailsLeaveState.Initial
-        )
+    internal val stateFlow: StateFlow<ItemDetailsLeaveState> = combine(
+        eventFlow,
+        isLoadingStateFlow,
+        ::ItemDetailsLeaveState
+    ).stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5_000),
+        initialValue = ItemDetailsLeaveState.Initial
+    )
+
+    internal fun onConsumeEvent(event: ItemDetailsLeaveEvent) {
+        eventFlow.compareAndSet(event, ItemDetailsLeaveEvent.Idle)
+    }
+
+    internal fun onLeaveShare() {
+        viewModelScope.launch {
+            isLoadingStateFlow.update { IsLoadingState.Loading }
+
+            runCatching { leaveShare(shareId) }
+                .onFailure { error ->
+                    PassLogger.w(TAG, "There was an error leaving the share")
+                    PassLogger.w(TAG, error)
+
+                    eventFlow.update { ItemDetailsLeaveEvent.OnLeaveShareError }
+                    snackbarDispatcher(ItemDetailsLeaveMessage.LeaveItemError)
+                }
+                .onSuccess {
+                    eventFlow.update { ItemDetailsLeaveEvent.OnLeaveShareError }
+                    snackbarDispatcher(ItemDetailsLeaveMessage.LeaveItemSuccess)
+                }
+
+            isLoadingStateFlow.update { IsLoadingState.NotLoading }
+        }
+    }
+
+    private companion object {
+
+        private const val TAG = "ItemDetailsLeaveViewModel"
+
+    }
 
 }

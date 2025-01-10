4d88aa38d55fb297fd978ad169d193da23d4b7fa
Victor Hidalgo <victor.hidalgo@proton.ch>
2025-01-10 15:34:01 +0100

    refactor: use proper LocalLifecycleOwner

--- pass/features/item-create/src/main/kotlin/proton/android/pass/features/itemcreate/totp/camera/CameraPreviewContent.kt
+++ pass/features/item-create/src/main/kotlin/proton/android/pass/features/itemcreate/totp/camera/CameraPreviewContent.kt
@@ -30,8 +30,8 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.asExecutor
 import proton.android.pass.log.api.PassLogger

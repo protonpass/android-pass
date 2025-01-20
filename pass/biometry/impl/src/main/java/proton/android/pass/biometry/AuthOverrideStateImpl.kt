a795597aa4b2b912dd73d183d978d8beed3220af
Victor Hidalgo <victor.hidalgo@proton.ch>
2025-01-20 12:32:16 +0100

    fix: auth during attachments camera

---
+++ pass/biometry/impl/src/main/java/proton/android/pass/biometry/AuthOverrideStateImpl.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2025 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.biometry
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class AuthOverrideStateImpl @Inject constructor() : AuthOverrideState {
+    private val _isAuthOverrideEnabled = MutableStateFlow(false)
+    override val isAuthOverrideEnabled: StateFlow<Boolean> by ::_isAuthOverrideEnabled
+
+    override fun setAuthOverride(enabled: Boolean) {
+        _isAuthOverrideEnabled.value = enabled
+    }
+}

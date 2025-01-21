d9ab6feee87c06444aa738299ef0b4e4f035f166
Victor Hidalgo <victor.hidalgo@proton.ch>
2025-01-21 14:49:44 +0100

    feat: use only owned shares on reports

--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/breach/SendUserMonitorCredentialsReportImpl.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/usecases/breach/SendUserMonitorCredentialsReportImpl.kt
@@ -20,20 +20,22 @@ package proton.android.pass.data.impl.usecases.breach
 
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
-import me.proton.core.account.domain.entity.Account
 import me.proton.core.account.domain.entity.AccountState
 import me.proton.core.accountmanager.domain.AccountManager
 import me.proton.core.accountmanager.domain.getAccounts
 import proton.android.pass.common.api.AppDispatchers
 import proton.android.pass.data.api.usecases.GetUserPlan
 import proton.android.pass.data.api.usecases.ItemTypeFilter
+import proton.android.pass.data.api.usecases.ObserveAllShares
 import proton.android.pass.data.api.usecases.ObserveItems
 import proton.android.pass.data.api.usecases.breach.SendUserMonitorCredentialsReport
 import proton.android.pass.data.impl.remote.RemoteOrganizationReportDataSource
 import proton.android.pass.data.impl.util.runConcurrently
 import proton.android.pass.domain.ItemFlag
 import proton.android.pass.domain.ItemState
+import proton.android.pass.domain.Share
 import proton.android.pass.domain.ShareSelection
 import proton.android.pass.securitycenter.api.passwords.InsecurePasswordChecker
 import proton.android.pass.securitycenter.api.passwords.MissingTfaChecker
@@ -48,26 +50,31 @@ class SendUserMonitorCredentialsReportImpl @Inject constructor(
     private val insecurePasswordChecker: InsecurePasswordChecker,
     private val missing2faChecker: MissingTfaChecker,
     private val observeItems: ObserveItems,
+    private val observeAllShares: ObserveAllShares,
     private val dispatchers: AppDispatchers
 ) : SendUserMonitorCredentialsReport {
     override suspend fun invoke() {
         val accounts = accountManager.getAccounts(AccountState.Ready).firstOrNull().orEmpty()
         val reports = accounts
-            .map { account: Account ->
+            .map { account ->
                 account.userId to getUserPlan(account.userId).first()
             }
             .filter { (_, plan) -> plan.isBusinessPlan }
             .associate { (userId, _) ->
+                val shares = observeAllShares(userId)
+                    .map { it.filter(Share::isOwner).map(Share::id) }
+                    .firstOrNull()
+                    .orEmpty()
                 val monitoredItems = observeItems(
                     userId = userId,
-                    selection = ShareSelection.AllShares,
+                    selection = ShareSelection.Shares(shares),
                     filter = ItemTypeFilter.Logins,
                     itemState = ItemState.Active,
                     itemFlags = mapOf(ItemFlag.SkipHealthCheck to false)
                 ).first()
                 val excludedItems = observeItems(
                     userId = userId,
-                    selection = ShareSelection.AllShares,
+                    selection = ShareSelection.Shares(shares),
                     filter = ItemTypeFilter.Logins,
                     itemState = ItemState.Active,
                     itemFlags = mapOf(ItemFlag.SkipHealthCheck to true)

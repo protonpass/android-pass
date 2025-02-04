f95cfe5ce86ae897a61cc56fdfa60286f1fa5eb1
Victor Hidalgo <victor.hidalgo@proton.ch>
2025-02-04 16:02:13 +0100

    test: add telemetry fakes

---
+++ pass/account/fakes/src/main/kotlin/proton/android/pass/account/fakes/FakeCoroutineScopeProvider.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2025 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.account.fakes
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import me.proton.core.util.kotlin.CoroutineScopeProvider
+import me.proton.core.util.kotlin.DispatcherProvider
+import javax.inject.Inject
+
+class FakeCoroutineScopeProvider @Inject constructor(dispatcherProvider: DispatcherProvider) :
+    CoroutineScopeProvider {
+    override val GlobalDefaultSupervisedScope =
+        CoroutineScope(dispatcherProvider.Comp + SupervisorJob())
+    override val GlobalIOSupervisedScope = CoroutineScope(dispatcherProvider.Io + SupervisorJob())
+}

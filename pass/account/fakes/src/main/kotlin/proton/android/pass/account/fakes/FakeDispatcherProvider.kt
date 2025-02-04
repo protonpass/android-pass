24c74f9a8f8c9f8522f09c326c85ba6991a61097
Victor Hidalgo <victor.hidalgo@proton.ch>
2025-02-04 16:02:13 +0100

    test: add telemetry fakes

---
+++ pass/account/fakes/src/main/kotlin/proton/android/pass/account/fakes/FakeDispatcherProvider.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2025 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.account.fakes
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import me.proton.core.util.kotlin.DispatcherProvider
+import javax.inject.Inject
+
+class FakeDispatcherProvider @Inject constructor() : DispatcherProvider {
+    override val Io: CoroutineDispatcher = Dispatchers.IO
+    override val Comp: CoroutineDispatcher = Dispatchers.Default
+    override val Main: CoroutineDispatcher = Dispatchers.Main
+}

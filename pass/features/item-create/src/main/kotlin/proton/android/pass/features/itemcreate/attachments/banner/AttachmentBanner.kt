37bbf28054c9c48dbb77f429e1184fdc51a5eb91
Victor Hidalgo <victor.hidalgo@proton.ch>
2025-02-12 14:23:06 +0100

    fix: round corners of image in attachments banner

--- pass/features/item-create/src/main/kotlin/proton/android/pass/features/itemcreate/attachments/banner/AttachmentBanner.kt
+++ pass/features/item-create/src/main/kotlin/proton/android/pass/features/itemcreate/attachments/banner/AttachmentBanner.kt
@@ -24,6 +24,7 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -51,7 +52,10 @@ fun AttachmentBanner(modifier: Modifier = Modifier, onClose: () -> Unit) {
                     title = stringResource(R.string.banner_title_attachments),
                     body = stringResource(R.string.banner_body_attachments)
                 )
-                Image.Default(R.drawable.attachments_file)
+                Image.Default(
+                    modifier = Modifier.clip(PassTheme.shapes.containerInputShape),
+                    id = R.drawable.attachments_file
+                )
             }
         },
         closeIcon = {

cc3b54858317b3e21f120d7efdcea0c59d3224ef
Victor Hidalgo <victor.hidalgo@proton.ch>
2025-03-12 15:32:19 +0100

    feat: add date field to detail

--- pass/common-ui/api/src/main/java/proton/android/pass/commonui/api/DateFormatUtils.kt
+++ pass/common-ui/api/src/main/java/proton/android/pass/commonui/api/DateFormatUtils.kt
@@ -23,9 +23,22 @@ import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.minus
+import kotlinx.datetime.toJavaLocalDateTime
 import kotlinx.datetime.toLocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 object DateFormatUtils {
+
+    fun formatDateFromMillis(pattern: String, epochMillis: Long): String = runCatching {
+        DateTimeFormatter.ofPattern(pattern)
+            .withLocale(Locale.getDefault())
+            .format(
+                Instant.fromEpochMilliseconds(epochMillis)
+                    .toLocalDateTime(TimeZone.currentSystemDefault())
+                    .toJavaLocalDateTime()
+            )
+    }.getOrDefault("")
 
     fun getFormat(
         now: Instant,

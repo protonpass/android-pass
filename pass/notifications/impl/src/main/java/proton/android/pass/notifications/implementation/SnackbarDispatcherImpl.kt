5ff2879402e129f96ec51ebbde8251514313edc4
Victor Hidalgo <victor.hidalgo@proton.ch>
2025-03-28 08:02:13 +0100

    Revert "fix: queue snackbar messages"
    This reverts commit 03d49cd06aa3b50c3c995fb538e755e537413c63.

--- pass/notifications/impl/src/main/java/proton/android/pass/notifications/implementation/SnackbarDispatcherImpl.kt
+++ pass/notifications/impl/src/main/java/proton/android/pass/notifications/implementation/SnackbarDispatcherImpl.kt
@@ -20,13 +20,13 @@ package proton.android.pass.notifications.implementation
 
 import android.os.Build
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import proton.android.pass.common.api.None
 import proton.android.pass.common.api.Option
 import proton.android.pass.common.api.some
-import proton.android.pass.common.api.toOption
 import proton.android.pass.notifications.api.SnackbarDispatcher
 import proton.android.pass.notifications.api.SnackbarMessage
 import javax.inject.Inject
@@ -34,40 +34,32 @@ import javax.inject.Singleton
 
 @Singleton
 class SnackbarDispatcherImpl @Inject constructor() : SnackbarDispatcher {
-
     private val mutex = Mutex()
 
-    private val pendingMessages = LinkedHashSet<SnackbarMessage>()
-    private val messageSharedFlow: MutableSharedFlow<Option<SnackbarMessage>> = MutableSharedFlow()
+    private val snackbarState = MutableStateFlow<Option<SnackbarMessage>>(None)
 
-    override val snackbarMessage: Flow<Option<SnackbarMessage>> = messageSharedFlow
+    override val snackbarMessage: Flow<Option<SnackbarMessage>> = snackbarState
 
     override suspend fun invoke(snackbarMessage: SnackbarMessage) {
         if (!shouldDisplay(snackbarMessage)) return
-
         mutex.withLock {
-            val wasAdded = pendingMessages.add(snackbarMessage)
-            val isFirst = pendingMessages.size == 1
-            if (wasAdded && isFirst) {
-                messageSharedFlow.emit(snackbarMessage.some())
-            }
+            snackbarState.update { snackbarMessage.some() }
         }
     }
 
     override suspend fun snackbarMessageDelivered() {
         mutex.withLock {
-            pendingMessages.firstOrNull()?.let { pendingMessages.remove(it) }
-            messageSharedFlow.emit(pendingMessages.firstOrNull().toOption())
+            snackbarState.update { None }
         }
     }
 
     override fun reset() {
-        pendingMessages.clear()
-        messageSharedFlow.tryEmit(None)
+        snackbarState.update { None }
     }
 
     private fun shouldDisplay(snackbarMessage: SnackbarMessage): Boolean {
         if (snackbarMessage !is SnackbarMessage.StructuredMessage) return true
         return !(snackbarMessage.isClipboard && Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
     }
+
 }

45c8b2cda6025bd20cf5103a0cdb3bb4dfb6649c
Victor Hidalgo <victor.hidalgo@proton.ch>
2025-05-21 09:27:06 +0200

    fix: add no actions option to remaining item bottomsheets

--- pass/features/home/src/main/kotlin/proton/android/pass/features/home/bottomsheet/AliasOptionsBottomSheetContents.kt
+++ pass/features/home/src/main/kotlin/proton/android/pass/features/home/bottomsheet/AliasOptionsBottomSheetContents.kt
@@ -40,6 +40,7 @@ import proton.android.pass.composecomponents.impl.bottomsheet.BottomSheetItemLis
 import proton.android.pass.composecomponents.impl.bottomsheet.BottomSheetItemRow
 import proton.android.pass.composecomponents.impl.bottomsheet.BottomSheetItemSubtitle
 import proton.android.pass.composecomponents.impl.bottomsheet.BottomSheetItemTitle
+import proton.android.pass.composecomponents.impl.bottomsheet.noOptions
 import proton.android.pass.composecomponents.impl.bottomsheet.pin
 import proton.android.pass.composecomponents.impl.bottomsheet.trash
 import proton.android.pass.composecomponents.impl.bottomsheet.unpin
@@ -107,6 +108,10 @@ internal fun AliasOptionsBottomSheetContents(
 
             if (isRecentSearch) {
                 add(removeFromRecentSearch(itemUiModel, onRemoveFromRecentSearch))
+            }
+
+            if (isEmpty()) {
+                add(noOptions())
             }
         }.also { bottomSheetItems ->
             BottomSheetItemList(

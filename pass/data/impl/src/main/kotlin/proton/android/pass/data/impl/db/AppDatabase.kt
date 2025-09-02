2e6a25765dd255b413224a00a4fb691833f32a0d
Yoann Goular <yoann.goular@proton.ch>
2025-09-02 09:03:58 +0200

    chore: update database schema and add migration

--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/db/AppDatabase.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/db/AppDatabase.kt
@@ -256,7 +256,7 @@ abstract class AppDatabase :
     AuthDatabase {
 
     companion object {
-        const val VERSION = 75
+        const val VERSION = 76
 
         const val DB_NAME = "db-passkey"
 
@@ -285,7 +285,8 @@ abstract class AppDatabase :
             AppDatabaseMigrations.MIGRATION_54_55,
             AppDatabaseMigrations.MIGRATION_56_57,
             AppDatabaseMigrations.MIGRATION_58_59,
-            AppDatabaseMigrations.MIGRATION_74_75
+            AppDatabaseMigrations.MIGRATION_74_75,
+            AppDatabaseMigrations.MIGRATION_75_76
         )
 
         fun buildDatabase(context: Context): AppDatabase = databaseBuilder<AppDatabase>(context, DB_NAME)

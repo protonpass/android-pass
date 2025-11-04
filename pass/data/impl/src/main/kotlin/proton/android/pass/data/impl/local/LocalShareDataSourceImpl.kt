c417c68eee0f4f4e7f3453b972ea8205b6519aed
Victor Hidalgo <victor.hidalgo@proton.ch>
2025-11-04 17:05:10 +0100

    refactor: refactor share local usage to be simpler

--- pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/local/LocalShareDataSourceImpl.kt
+++ pass/data/impl/src/main/kotlin/proton/android/pass/data/impl/local/LocalShareDataSourceImpl.kt
@@ -21,13 +21,11 @@ package proton.android.pass.data.impl.local
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.firstOrNull
 import me.proton.core.domain.entity.UserId
-import me.proton.core.user.domain.entity.AddressId
 import proton.android.pass.data.impl.db.PassDatabase
 import proton.android.pass.data.impl.db.entities.ShareEntity
 import proton.android.pass.domain.ShareId
 import proton.android.pass.domain.ShareRole
 import proton.android.pass.domain.ShareType
-import proton.android.pass.log.api.PassLogger
 import javax.inject.Inject
 
 class LocalShareDataSourceImpl @Inject constructor(
@@ -46,25 +44,17 @@ class LocalShareDataSourceImpl @Inject constructor(
     override fun observeById(userId: UserId, shareId: ShareId): Flow<ShareEntity?> =
         database.sharesDao().observeById(userId.id, shareId.id)
 
-    override fun getAllSharesForUser(userId: UserId): Flow<List<ShareEntity>> =
-        database.sharesDao().observeAllForUser(userId.id)
+    override fun observeAllSharesForUser(userId: UserId): Flow<List<ShareEntity>> =
+        database.sharesDao().observe(userId = userId.id)
 
     override fun observeAllActiveSharesForUser(userId: UserId): Flow<List<ShareEntity>> =
-        database.sharesDao().observeAllActiveForUser(userId.id)
+        database.sharesDao().observe(userId = userId.id, isActive = true)
 
-    override fun getAllSharesForAddress(addressId: AddressId): Flow<List<ShareEntity>> =
-        database.sharesDao().observeAllForAddress(addressId.id)
-
-    override suspend fun deleteShares(shareIds: Set<ShareId>): Boolean {
-        PassLogger.i(TAG, "Deleting shares: ${shareIds.map { it.id }}")
-        return database.sharesDao().delete(shareIds.map { it.id }.toTypedArray()) > 0
-    }
-
-    override suspend fun hasShares(userId: UserId): Boolean = database.sharesDao().countShares(userId.id) > 0
+    override suspend fun deleteShares(userId: UserId, shareIds: Set<ShareId>): Boolean =
+        database.sharesDao().deleteShares(userId.id, shareIds.map { it.id }) > 0
 
     override suspend fun deleteSharesForUser(userId: UserId) {
-        PassLogger.i(TAG, "Deleting all shares for user")
-        database.sharesDao().deleteSharesForUser(userId.id)
+        database.sharesDao().deleteShares(userId.id)
     }
 
     override fun observeActiveVaultCount(userId: UserId): Flow<Int> =
@@ -85,7 +75,7 @@ class LocalShareDataSourceImpl @Inject constructor(
         shareType: ShareType,
         isActive: Boolean?
     ): Flow<List<ShareEntity>> = database.sharesDao()
-        .observeByType(
+        .observe(
             userId = userId.id,
             shareType = shareType.value,
             isActive = isActive
@@ -95,20 +85,15 @@ class LocalShareDataSourceImpl @Inject constructor(
         .observeSharedIds(
             userId = userId.id,
             shareType = ShareType.Item.value,
-            shareRole = null
+            shareRole = null,
+            isActive = true
         )
 
     override fun observeSharedByMeIds(userId: UserId): Flow<List<String>> = database.sharesDao()
         .observeSharedIds(
             userId = userId.id,
             shareType = null,
-            shareRole = ShareRole.SHARE_ROLE_ADMIN
+            shareRole = ShareRole.SHARE_ROLE_ADMIN,
+            isActive = true
         )
-
-    private companion object {
-
-        private const val TAG = "LocalShareDataSourceImpl"
-
-    }
-
 }

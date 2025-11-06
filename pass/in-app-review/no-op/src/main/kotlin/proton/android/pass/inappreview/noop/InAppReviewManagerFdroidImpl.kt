abea4f0e4af24dc1ff40bba30328db6d5078aab2
Victor Hidalgo <victor.hidalgo@proton.ch>
2025-11-06 15:42:04 +0100

    chore: add quest dep guard

---
+++ pass/in-app-review/no-op/src/main/kotlin/proton/android/pass/inappreview/noop/InAppReviewManagerFdroidImpl.kt
@@ -0,0 +1,16 @@
+package proton.android.pass.inappreview.noop
+
+import android.app.Activity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import proton.android.pass.commonui.api.ClassHolder
+import proton.android.pass.inappreview.api.InAppReviewManager
+import javax.inject.Inject
+
+class InAppReviewManagerFdroidImpl @Inject constructor() : InAppReviewManager {
+    override fun shouldRequestReview(): Flow<Boolean> = flowOf(false)
+
+    override fun requestReview(activityHolder: ClassHolder<Activity>) {
+
+    }
+}

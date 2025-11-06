a197e795c0ccb1f29536465633a2566f554d376f
Victor Hidalgo <victor.hidalgo@proton.ch>
2025-11-06 15:42:04 +0100

    chore: add quest dep guard

---
+++ pass/in-app-review/no-op/src/main/kotlin/proton/android/pass/inappreview/noop/InAppReviewFdroidModule.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2023-2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.inappreview.noop
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import proton.android.pass.inappreview.api.InAppReviewManager
+import proton.android.pass.inappreview.api.InAppReviewTriggerMetrics
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class InAppReviewFdroidModule {
+
+    @Binds
+    abstract fun bindInAppReviewManager(impl: InAppReviewManagerFdroidImpl): InAppReviewManager
+
+    @Binds
+    abstract fun bindInAppReviewTriggerMetrics(impl: InAppReviewTriggerMetricsFdroidImpl): InAppReviewTriggerMetrics
+}
---
+++ pass/in-app-updates/no-op/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("dagger.hilt.android.plugin")
+    id("org.jetbrains.kotlin.kapt")
+}
+
+android {
+    namespace = "proton.android.pass.inappupdates.noop"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+androidComponents.beforeVariants { variant ->
+    variant.enableAndroidTest = false
+}
+
+dependencies {
+    api(projects.pass.inAppUpdates.api)
+
+    implementation(libs.kotlinx.coroutines.core)
+
+    implementation(libs.dagger.hilt.android)
+    kapt(libs.dagger.hilt.android.compiler)
+    kapt(libs.androidx.hilt.compiler)
+}

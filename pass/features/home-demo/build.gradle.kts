35b2ed1eb55d365199883504b38f04e74e7c3e2f
Victor Hidalgo <victor.hidalgo@proton.ch>
2025-11-12 11:10:17 +0100

    bump: version 1.34.0 → 1.34.1

--- pass/features/home-demo/build.gradle.kts
+++ pass/features/home-demo/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     alias(libs.plugins.gradlePlugin.compose.compiler)
 }
 
-val appVersionName: String = "1.34.0"
+val appVersionName: String = "1.34.1"
 val appVersionCode: Int = versionCode(appVersionName)
 val archivesBaseName = "FeatureHomeApp-$appVersionName"
 

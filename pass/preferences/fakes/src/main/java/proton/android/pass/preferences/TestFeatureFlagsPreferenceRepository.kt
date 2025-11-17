237aedfbbe839caf1897cdce6926e5b2bb7c296d
Victor Hidalgo <victor.hidalgo@proton.ch>
2025-11-17 08:42:19 +0100

    chore: add user events ff

--- pass/preferences/fakes/src/main/java/proton/android/pass/preferences/TestFeatureFlagsPreferenceRepository.kt
+++ pass/preferences/fakes/src/main/java/proton/android/pass/preferences/TestFeatureFlagsPreferenceRepository.kt
@@ -28,6 +28,7 @@ import proton.android.pass.preferences.FeatureFlag.EXTRA_LOGGING
 import proton.android.pass.preferences.FeatureFlag.PASS_HIDE_SHOW_VAULT
 import proton.android.pass.preferences.FeatureFlag.RENAME_ADMIN_TO_MANAGER
 import proton.android.pass.preferences.FeatureFlag.PASS_ALLOW_NO_VAULT
+import proton.android.pass.preferences.FeatureFlag.PASS_USER_EVENTS_V1
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -46,6 +47,7 @@ class TestFeatureFlagsPreferenceRepository @Inject constructor() :
             RENAME_ADMIN_TO_MANAGER -> it.getOrDefault(RENAME_ADMIN_TO_MANAGER, false) as T
             PASS_HIDE_SHOW_VAULT -> it.getOrDefault(PASS_HIDE_SHOW_VAULT, false) as T
             PASS_ALLOW_NO_VAULT -> it.getOrDefault(PASS_ALLOW_NO_VAULT, false) as T
+            PASS_USER_EVENTS_V1 -> it.getOrDefault(PASS_USER_EVENTS_V1, false) as T
         }
     }
 

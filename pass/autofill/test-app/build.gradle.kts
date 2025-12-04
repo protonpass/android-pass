2ba577e69ab88535518256a845470f6c5058fefb
Victor Hidalgo <victor.hidalgo@proton.ch>
2025-12-04 08:25:33 +0100

    bump: version 1.36.1 → 1.36.2

--- pass/autofill/test-app/build.gradle.kts
+++ pass/autofill/test-app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.gradlePlugin.compose.compiler)
 }
 
-val appVersionName = "1.36.1"
+val appVersionName = "1.36.2"
 val appVersionCode = versionCode(appVersionName)
 val archivesBaseName = "AutofillTestApp-$appVersionName"
 

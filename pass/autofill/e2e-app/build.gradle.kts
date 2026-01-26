c1f6a0c2cd703b72640bf6876edf092599929e6a
Victor Hidalgo <victor.hidalgo@proton.ch>
2026-01-26 09:37:33 +0100

    bump: version 1.36.5 → 1.37.0

--- pass/autofill/e2e-app/build.gradle.kts
+++ pass/autofill/e2e-app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     alias(libs.plugins.gradlePlugin.compose.compiler)
 }
 
-val appVersionName: String = "1.36.5"
+val appVersionName: String = "1.37.0"
 val appVersionCode: Int = versionCode(appVersionName)
 val archivesBaseName = "AutofillE2EApp-$appVersionName"
 

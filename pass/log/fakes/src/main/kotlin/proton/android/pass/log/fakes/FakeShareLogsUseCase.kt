466b62f3e144dadff86274a5c9454607b4998dde
Victor <victor.hidalgo@proton.ch>
2026-02-04 15:09:00 +0100

    refactor: delete file and fix fake

---
+++ pass/log/fakes/src/main/kotlin/proton/android/pass/log/fakes/FakeShareLogsUseCase.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ * This file is part of Proton AG and Proton Pass.
+ *
+ * Proton Pass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Pass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Pass.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package proton.android.pass.log.fakes
+
+import android.content.Context
+import proton.android.pass.log.api.ShareLogsUseCase
+import java.io.File
+import javax.inject.Inject
+
+class FakeShareLogsUseCase @Inject constructor() : ShareLogsUseCase {
+
+    private var result: Result<File> = Result.failure(Exception("Not implemented"))
+
+    fun setResult(result: Result<File>) {
+        this.result = result
+    }
+
+    override suspend fun invoke(context: Context): Result<File> = result
+}

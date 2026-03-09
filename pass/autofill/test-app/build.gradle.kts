d342931bbc3808b3b1372599786e0d6ba5134715
Victor <victor.hidalgo@proton.ch>
2026-03-09 14:58:17 +0100

    chore(release): version 1.37.2 → 1.37.3

--- pass/autofill/test-app/build.gradle.kts
+++ pass/autofill/test-app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.gradlePlugin.compose.compiler)
 }
 
-val appVersionName = "1.37.2"
+val appVersionName = "1.37.3"
 val appVersionCode = versionCode(appVersionName)
 val archivesBaseName = "AutofillTestApp-$appVersionName"
 

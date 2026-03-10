6f3f04976c42a2776b3cb8715ff7e5f931b6504c
Antoine Marchaud <antoine.marchaud@external.proton.ch>
2026-03-10 09:49:11 +0100

    chore: use same detekt for hook and gradle

--- scripts/detekt.sh
+++ scripts/detekt.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+DETEKT_JAR="$SCRIPT_DIR/detekt-cli-1.23.5-all.jar"
+
 echo "Running detekt check..."
 fileArray=($@)
 detektInput=$(IFS=,;printf  "%s" "${fileArray[*]}")
 echo "Input files: $detektInput"
 
-OUTPUT=$(detekt --input "$detektInput" --config "./config/detekt/config.yml" 2>&1)
+OUTPUT=$(java -jar "$DETEKT_JAR" --input "$detektInput" --config "./config/detekt/config.yml" 2>&1)
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
   echo "${OUTPUT}"
